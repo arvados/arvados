9cd04569c6ed31931749494acab7f17ddddc2a84
Tom Clegg <tom@clinicalfuture.com>
2013-01-11 14:21:53 -0800

    fix param name in node#ping

--- app/controllers/orvos/v1/nodes_controller.rb
+++ app/controllers/orvos/v1/nodes_controller.rb
@@ -13,7 +13,7 @@ class Orvos::V1::NodesController < ApplicationController
   def ping
     @object.ping({ ip: params[:local_ipv4] || request.env['REMOTE_ADDR'],
                    ping_secret: params[:ping_secret],
-                   ec2_instance_id: params[:ec2_instance_id] })
+                   ec2_instance_id: params[:instance_id] })
     show
   end
 end
