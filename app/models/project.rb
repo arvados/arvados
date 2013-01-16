4a69a168976c9ef04f0881c9f5c0e3a743b426d3
Tom Clegg <tom@clinicalfuture.com>
2013-01-16 14:43:01 -0800

    add boilerplate for Project resource

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,10 @@
 class Project < ActiveRecord::Base
+  include AssignUuid
+  include KindAndEtag
+  include CommonApiTemplate
+
+  api_accessible :superuser, :extend => :common do |t|
+    t.add :name
+    t.add :description
+  end
 end
