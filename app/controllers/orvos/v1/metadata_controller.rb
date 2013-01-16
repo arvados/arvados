094cb7f35827d93ac9d845586fcf58ddc07cda01
Tom Clegg <tom@clinicalfuture.com>
2013-01-16 14:43:25 -0800

    remove extraneous Metadata#create

--- app/controllers/orvos/v1/metadata_controller.rb
+++ app/controllers/orvos/v1/metadata_controller.rb
@@ -6,22 +6,4 @@ class Orvos::V1::MetadataController < ApplicationController
     end
     super
   end
-
-  def create
-    @m = params[:metadatum]
-    if params[:metadatum].class == String
-      @m = uncamelcase_hash_keys(JSON.parse params[:metadatum])
-    end
-    @m = Metadatum.new @m
-    respond_to do |format|
-      if @m.save
-        format.html { redirect_to @m, notice: 'Metadatum was successfully created.' }
-        format.json { render json: @m, status: :created, location: @m }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @m.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
 end
