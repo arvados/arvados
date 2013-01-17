cf0cd1a7ceae4f37eb74b7515f7b8de7973b4282
Tom Clegg <tom@clinicalfuture.com>
2013-01-17 01:17:42 -0800

    beginnings of upload->pipeline status page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :find_object_by_uuid, :except => :index
+
+  def index
+    @objects ||= model_class.all
+  end
+
+  def show
+    if !@object
+      render_not_found("object not found")
+    end
+  end
+
+  protected
+    
+  def model_class
+    controller_name.classify.constantize
+  end
 end
--- app/models/metadatum.rb
+++ app/models/metadatum.rb
@@ -1,2 +1,5 @@
 class Metadatum < OrvosBase
+  def self.by_target(t, opts={})
+    where(opts.merge :target_kind => t.kind, :target_uuid => t.uuid)
+  end
 end
