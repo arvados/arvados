75956dc6d37f653eaf4f740a611eccb300d91432
Tom Clegg <tom@clinicalfuture.com>
2013-01-18 01:27:47 -0800

    easier access to metadata, and objects connected by metadata

--- app/models/orvos_base.rb
+++ app/models/orvos_base.rb
@@ -22,8 +22,7 @@ class OrvosBase < ActiveRecord::Base
     ActiveRecord::ConnectionAdapters::Column.new(name.to_s, default, sql_type.to_s, null)
   end
   def self.all
-    thelist = api('')
-    thelist[:items].collect { |x| new(x) }
+    unpack_api_response(api(''))
   end
   def self.find(uuid)
     new(api('/' + uuid))
@@ -63,6 +62,17 @@ class OrvosBase < ActiveRecord::Base
     @kind = h.delete :kind
     super
   end
+  def metadata(*args)
+    o = {}
+    o.merge!(args.pop) if args[-1].is_a? Hash
+    o[:metadata_class] ||= args.shift
+    o[:name] ||= args.shift
+    o[:head_kind] ||= args.shift
+    o[:tail_kind] = self.kind
+    o[:tail] = self.uuid
+    @metadata = self.class.api '', { _method: 'GET', where: o, eager: true }, { resource_path: 'metadata' }
+    @metadata = self.class.unpack_api_response(@metadata)
+  end
 
   protected
   def self.api(action, data=nil, o={})
@@ -70,8 +80,10 @@ class OrvosBase < ActiveRecord::Base
     if !data.nil?
       data.each do |k,v|
         dataargs << '-d'
-        if v.is_a? String
+        if v.is_a? String or v.nil?
           dataargs << "#{k}=#{v}"
+        elsif v == true or v == false
+          dataargs << "#{k}=#{v ? 1 : 0}"
         else
           dataargs << "#{k}=#{JSON.generate v}"
         end
@@ -95,5 +107,30 @@ class OrvosBase < ActiveRecord::Base
 
   def self.orvos_schema
     $orvos_schema ||= api '', nil, {resource_path: 'schema'}
+  end
+
+  def self.kind_class(kind)
+    kind.match(/^orvos\#(.+?)(_list|List)?$/)[1].pluralize.classify.constantize rescue nil
+  end
+
+  def self.unpack_api_response(j, kind=nil)
+    if j.is_a? Hash and j[:items].is_a? Array and j[:kind].match(/(_list|List)$/)
+      j[:items].collect { |x| unpack_api_response x, j[:kind] }
+    elsif j.is_a? Hash and (kind || j[:kind])
+      oclass = self.kind_class(kind || j[:kind])
+      if oclass
+        j.keys.each do |k|
+          childkind = j["#{k.to_s}_kind".to_sym]
+          if childkind
+            j[k] = self.unpack_api_response(j[k], childkind)
+          end
+        end
+        oclass.new(j)
+      else
+        j
+      end
+    else
+      j
+    end
   end
 end
