e72be76fa965ae6eafe09e6d88d520a16f51bb84
Ward Vandewege <ward@clinicalfuture.com>
2013-01-21 10:23:00 -0500

    Very basic tests for /collections, /nodes, /pipeline_invocations

--- test/functional/nodes_controller_test.rb
+++ test/functional/nodes_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class NodesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:objects)
+  end
+
 end
--- test/functional/pipeline_invocations_controller_test.rb
+++ test/functional/pipeline_invocations_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class PipelineInvocationsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:objects)
+  end
+
 end
