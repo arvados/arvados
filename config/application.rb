8e9d07f0bd8d4b00a922f514c6c86f3dfc5bb58a
Tom Clegg <tom@clinicalfuture.com>
2013-01-24 14:47:47 -0800

    move html into app/assets

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module Vcffarm
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.paths << "#{Rails.root}/app/assets/html"
   end
 end
