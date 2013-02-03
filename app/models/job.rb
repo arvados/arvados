a3ff0b5978de5820ce35c237f583b9671068e38f
Tom Clegg <tom@clinicalfuture.com>
2013-02-03 03:30:33 -0800

    add Job and JobStep resources

---
+++ app/models/job.rb
@@ -0,0 +1,36 @@
+class Job < OrvosModel
+  include AssignUuid
+  include KindAndEtag
+  include CommonApiTemplate
+  serialize :command_parameters, Hash
+  before_create :ensure_unique_submit_id
+
+  class SubmitIdReused < StandardError
+  end
+
+  api_accessible :superuser, :extend => :common do |t|
+    t.add :submit_id
+    t.add :priority
+    t.add :command
+    t.add :command_parameters
+    t.add :command_version
+    t.add :cancelled_at
+    t.add :cancelled_by_client
+    t.add :cancelled_by_user
+    t.add :started_at
+    t.add :finished_at
+    t.add :success
+    t.add :running
+  end
+
+  protected
+
+  def ensure_unique_submit_id
+    if !submit_id.nil?
+      if Job.where('submit_id=?',self.submit_id).first
+        raise SubmitIdReused.new
+      end
+    end
+    true
+  end
+end
---
+++ db/migrate/20130203104824_create_job_steps.rb
@@ -0,0 +1,23 @@
+class CreateJobSteps < ActiveRecord::Migration
+  def change
+    create_table :job_steps do |t|
+      t.string :uuid
+      t.string :owner
+      t.string :modified_by_client
+      t.string :modified_by_user
+      t.datetime :modified_at
+      t.string :job_uuid
+      t.integer :sequence
+      t.text :parameters
+      t.text :output
+      t.float :progress
+      t.boolean :success
+
+      t.timestamps
+    end
+    add_index :job_steps, :uuid, :unique => true
+    add_index :job_steps, :job_uuid
+    add_index :job_steps, :sequence
+    add_index :job_steps, :success
+  end
+end
