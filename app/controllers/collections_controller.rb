fdb05f9f5a1613b80f371a4dc0561ff05f678586
Tom Clegg <tom@clinicalfuture.com>
2013-02-05 10:20:22 -0500

    limit 100 instead of 1000

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -7,10 +7,10 @@ class CollectionsController < ApplicationController
   end
 
   def index
-    @links = Link.eager.limit(1000).where(head_kind: 'orvos#collection') |
-      Link.eager.limit(1000).where(tail_kind: 'orvos#collection')
+    @links = Link.eager.limit(100).where(head_kind: 'orvos#collection') |
+      Link.eager.limit(100).where(tail_kind: 'orvos#collection')
     @collections = {}
-    Collection.limit(1000).where({}).each do |c|
+    Collection.limit(100).where({}).each do |c|
       @collections[c.uuid] = {uuid: c.uuid}
     end
     @links.each do |l|
