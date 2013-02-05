b53aa56101dfdbbf2b2bced8fc921e189934af0a
Tom Clegg <tom@clinicalfuture.com>
2013-02-05 08:47:58 -0500

    human readable data size

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,19 @@ module ApplicationHelper
   def current_user
     controller.current_user
   end
+  def human_readable_bytes_html(n)
+    return h(n) unless n.is_a? Fixnum
+    raw = n.to_s
+    cooked = ''
+    while raw.length > 3
+      cooked = ',' + raw[-3..-1] + cooked
+      raw = raw[0..-4]
+    end
+    cooked = raw + cooked
+    if cooked.length >= 9
+      '<b>' + cooked[0..-9] + '</b>' + cooked[-8..-1]
+    else
+      cooked
+    end
+  end
 end
