7958273916a9da637c50cf2f4729f65b85c11c52
Tom Clegg <tom@clinicalfuture.com>
2013-02-22 15:17:12 -0800

    show "sinfo" results on /nodes page

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,5 +1,30 @@
 class NodesController < ApplicationController
   def index
     @objects = model_class.order("created_at desc")
+
+    @slurm_state = {}
+    IO.popen('sinfo --noheader --Node || echo "compute[1-3] foo bar DOWN"').readlines.each do |line|
+      tokens = line.strip.split
+      nodestate = tokens.last
+      nodenames = []
+      if (re = tokens.first.match /^([^\[]*)\[([-\d,]+)\]$/)
+        nodeprefix = re[1]
+        re[2].split(',').each do |number_range|
+          if number_range.index('-')
+            range = number_range.split('-').collect(&:to_i)
+            (range[0]..range[1]).each do |n|
+              nodenames << "#{nodeprefix}#{n}"
+            end
+          else
+            nodenames << "#{nodeprefix}#{number_range}"
+          end
+        end
+      else
+        nodenames << tokens.first
+      end
+      nodenames.each do |nodename|
+        @slurm_state[nodename] = nodestate.downcase
+      end
+    end
   end
 end
