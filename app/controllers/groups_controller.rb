54b9e96c3a1adf75761b8d2a96860f2d30e0ca5e
Tom Clegg <tom@clinicalfuture.com>
2013-03-18 23:24:35 -0700

    add group#index and group#show pages. refs #1415

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,11 @@
 class GroupsController < ApplicationController
   before_filter :ensure_current_user_is_admin
+
+  def index
+    @groups = Group.limit(10000).all
+    @group_uuids = @groups.collect &:uuid
+    @owned_users = User.where owner: @group_uuids
+    @links_from = Link.where link_class: 'permission', tail_uuid: @group_uuids
+    @links_to = Link.where link_class: 'permission', head_uuid: @group_uuids
+  end
 end
