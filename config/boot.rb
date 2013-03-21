3385a0865bd12fadb82743d09901ec18328dadb3
Tom Clegg <tom@clinicalfuture.com>
2013-03-21 10:13:20 -0700

    use ORVOS_API_TOKEN environment variable in rails console

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,13 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# Use ORVOS_API_TOKEN environment variable (if set) in console
+require 'rails'
+module OrvosApiClientConsoleMode
+  class Railtie < Rails::Railtie
+    console do
+      Thread.current[:orvos_api_token] ||= ENV['ORVOS_API_TOKEN']
+    end
+  end
+end
