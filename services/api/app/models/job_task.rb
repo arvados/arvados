ec56eaa573a3b46d5cb1c1c2f3e4374c3b30f866
Tom Clegg <tom@clinicalfuture.com>
2013-05-10 19:57:03 -0700

    add attributes to job_task api response

--- services/api/app/models/job_task.rb
+++ services/api/app/models/job_task.rb
@@ -4,5 +4,11 @@ class JobTask < ArvadosModel
   include CommonApiTemplate
 
   api_accessible :superuser, :extend => :common do |t|
+    t.add :job_uuid
+    t.add :sequence
+    t.add :parameters
+    t.add :output
+    t.add :progress
+    t.add :success
   end
 end
