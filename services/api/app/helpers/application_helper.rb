1b18f10835f5dd4a32cfeb79697f1058d9b1e383
Tom Clegg <tom@clinicalfuture.com>
2013-05-21 13:33:24 -0700

    add script/update_node_attributes.rb

--- services/api/app/helpers/application_helper.rb
+++ services/api/app/helpers/application_helper.rb
@@ -2,4 +2,24 @@ module ApplicationHelper
   def current_user
     controller.current_user
   end
+
+  def act_as_system_user
+    if not $system_user
+      Thread.current[:user] = User.new(is_admin: true)
+      sysuser_id = [Server::Application.config.uuid_prefix,
+                    User.uuid_prefix,
+                    '000000000000000'].join('-')
+      $system_user = User.where('uuid=?', sysuser_id).first
+      if !$system_user
+        $system_user = User.new(uuid: sysuser_id,
+                                is_admin: true,
+                                email: 'root',
+                                first_name: 'root',
+                                last_name: '')
+        $system_user.save!
+        $system_user.reload
+      end
+    end
+    Thread.current[:user] = $system_user
+  end
 end
