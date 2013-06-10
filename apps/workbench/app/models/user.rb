821055e78b774a01e00ba1f53335b1aeff35ac54
Tom Clegg <tom@clinicalfuture.com>
2013-06-09 20:31:20 -0400

    add users#index, allow non-admin users#show

--- apps/workbench/app/models/user.rb
+++ apps/workbench/app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ArvadosBase
+  def initialize(*args)
+    super(*args)
+    @attribute_sortkey['first_name'] = '050'
+    @attribute_sortkey['last_name'] = '051'
+  end
+
   def self.current
     res = $arvados_api_client.api self, '/current'
     $arvados_api_client.unpack_api_response(res)
