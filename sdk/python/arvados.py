bbc6c481f587eaf5bc6b330b9a0766ed26b1ae62
Tom Clegg <tom@clinicalfuture.com>
2013-06-19 23:33:27 -0400

    add CollectionWriter and Keep classes to Python SDK

--- sdk/python/arvados.py
+++ sdk/python/arvados.py
@@ -9,6 +9,7 @@ import subprocess
 import json
 import UserDict
 import re
+import hashlib
 
 from apiclient import errors
 from apiclient.discovery import build
@@ -131,4 +132,112 @@ class DataReader:
             self.p.stderr.close()
         self.p.wait()
         if self.p.returncode != 0:
-            raise Exception("subprocess exited %d" % self.p.returncode)
+            raise Exception("whget subprocess exited %d" % self.p.returncode)
+
+class CollectionWriter:
+    KEEP_BLOCK_SIZE = 2**26
+    def __init__(self):
+        self.data_buffer = ''
+        self.current_stream_files = []
+        self.current_stream_length = 0
+        self.current_stream_locators = []
+        self.current_stream_name = '.'
+        self.current_file_name = None
+        self.current_file_pos = 0
+        self.finished_streams = []
+    def __enter__(self):
+        pass
+    def __exit__(self):
+        self.commit()
+    def write(self, newdata):
+        self.data_buffer += newdata
+        self.current_stream_length += len(newdata)
+        while len(self.data_buffer) >= self.KEEP_BLOCK_SIZE:
+            self.flush_data()
+    def flush_data(self):
+        if self.data_buffer != '':
+            self.current_stream_locators += [Keep.put(self.data_buffer[0:self.KEEP_BLOCK_SIZE])]
+            self.data_buffer = self.data_buffer[self.KEEP_BLOCK_SIZE:]
+    def start_new_file(self, newfilename=None):
+        self.finish_current_file()
+        self.current_file_name = newfilename
+    def set_current_file_name(self, newfilename):
+        self.current_file_name = newfilename
+    def finish_current_file(self):
+        if self.current_file_name == None:
+            if self.current_file_pos == self.current_stream_length:
+                return
+            raise Exception("cannot finish an unnamed file (%d bytes at offset %d in '%s' stream)" % (self.current_stream_length - self.current_file_pos, self.current_file_pos, self.current_stream_name))
+        self.current_stream_files += [[self.current_file_pos,
+                                       self.current_stream_length - self.current_file_pos,
+                                       self.current_file_name]]
+        self.current_file_pos = self.current_stream_length
+    def start_new_stream(self, newstreamname=None):
+        self.finish_current_stream()
+        self.current_stream_name = newstreamname
+    def set_current_stream_name(self, newstreamname):
+        self.current_stream_name = newstreamname
+    def finish_current_stream(self):
+        self.finish_current_file()
+        self.flush_data()
+        if len(self.current_stream_files) == 0:
+            pass
+        elif self.current_stream_name == None:
+            raise Exception("cannot finish an unnamed stream (%d bytes in %d files)" % (self.current_stream_length, len(self.current_stream_files)))
+        else:
+            self.finished_streams += [[self.current_stream_name,
+                                       self.current_stream_locators,
+                                       self.current_stream_files]]
+        self.current_stream_files = []
+        self.current_stream_length = 0
+        self.current_stream_locators = []
+        self.current_stream_name = None
+        self.current_file_pos = 0
+        self.current_file_name = None
+    def finish(self):
+        return Keep.put(self.manifest_text())
+    def manifest_text(self):
+        self.finish_current_stream()
+        manifest = ''
+        for stream in self.finished_streams:
+            manifest += stream[0]
+            if len(stream[1]) == 0:
+                manifest += " d41d8cd98f00b204e9800998ecf8427e+0"
+            else:
+                for locator in stream[1]:
+                    manifest += " %s" % locator
+            for sfile in stream[2]:
+                manifest += " %d:%d:%s" % (sfile[0], sfile[1], sfile[2])
+            manifest += "\n"
+        return manifest
+
+class Keep:
+    @staticmethod
+    def put(data):
+        p = subprocess.Popen(["whput", "-"],
+                             stdout=subprocess.PIPE,
+                             stdin=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             shell=False, close_fds=True)
+        stdoutdata, stderrdata = p.communicate(data)
+        if p.returncode != 0:
+            raise Exception("whput subprocess exited %d - stderr:\n%s" % (p.returncode, stderrdata))
+        return stdoutdata.rstrip()
+    @staticmethod
+    def get(locator):
+        p = subprocess.Popen(["whget", locator, "-"],
+                             stdout=subprocess.PIPE,
+                             stdin=None,
+                             stderr=subprocess.PIPE,
+                             shell=False, close_fds=True)
+        stdoutdata, stderrdata = p.communicate(None)
+        if p.returncode != 0:
+            raise Exception("whget subprocess exited %d - stderr:\n%s" % (p.returncode, stderrdata))
+        m = hashlib.new('md5')
+        m.update(stdoutdata)
+        try:
+            if locator.index(m.hexdigest()) == 0:
+                return stdoutdata
+        except ValueError:
+            pass
+        raise Exception("md5 checksum mismatch: md5(get(%s)) == %s" % (locator, m.hexdigest()))
