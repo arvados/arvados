b10a80d5726f25e7284bf4d5f039f1e3fef83b34
Tom Clegg <tom@clinicalfuture.com>
2013-07-10 10:03:40 -0400

    pipeline_templates and pipeline_instances are not admin-only

--- apps/workbench/app/controllers/pipeline_instances_controller.rb
+++ apps/workbench/app/controllers/pipeline_instances_controller.rb
@@ -1,6 +1,2 @@
 class PipelineInstancesController < ApplicationController
-  before_filter :ensure_current_user_is_admin
-  def index
-    @objects = model_class.all
-  end
 end
--- apps/workbench/app/controllers/pipeline_templates_controller.rb
+++ apps/workbench/app/controllers/pipeline_templates_controller.rb
@@ -1,6 +1,2 @@
 class PipelineTemplatesController < ApplicationController
-  before_filter :ensure_current_user_is_admin
-  def index
-    @objects = model_class.all
-  end
 end
