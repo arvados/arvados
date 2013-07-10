9d2b0e4e2939c16c193c9cd2fae3499f0e6bb205
Tom Clegg <tom@clinicalfuture.com>
2013-07-09 23:58:23 -0400

    remove special group/show stuff

--- apps/workbench/app/controllers/groups_controller.rb
+++ apps/workbench/app/controllers/groups_controller.rb
@@ -6,26 +6,4 @@ class GroupsController < ApplicationController
     @links_from = Link.where link_class: 'permission', tail_uuid: @group_uuids
     @links_to = Link.where link_class: 'permission', head_uuid: @group_uuids
   end
-
-  def show
-    @collections = Collection.where(owner_uuid: @object.uuid)
-    @names = {}
-    @keep_flag = {}
-    @pgp_hu_id = {}
-    Link.
-      limit(10000).
-      where(head_uuid: @collections.collect(&:uuid)).
-      each do |link|
-      if link.properties[:name]
-        @names[link.head_uuid] ||= []
-        @names[link.head_uuid] << link.properties[:name]
-      end
-      if link.link_class == 'resources' and link.name == 'wants'
-        @keep_flag[link.head_uuid] = true
-      end
-      @pgp_hu_id[link.head_uuid] ||= link.properties[:pgp_hu_id]
-    end
-    @collections_total_bytes = @collections.collect(&:total_bytes).inject(0,&:+)
-    super
-  end
 end
