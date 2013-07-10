43dc711a2c5246ed036cdd16ca8184224b9372f8
Tom Clegg <tom@clinicalfuture.com>
2013-07-10 05:01:51 -0400

    hide expired and job-specific auth tokens from workbench list

--- apps/workbench/app/controllers/api_client_authorizations_controller.rb
+++ apps/workbench/app/controllers/api_client_authorizations_controller.rb
@@ -1,2 +1,8 @@
 class ApiClientAuthorizationsController < ApplicationController
+  def index
+    @objects = model_class.all.to_ary.reject do |x|
+      x.api_client_id == 0 or (x.expires_at and x.expires_at < Time.now) rescue false
+    end
+    super
+  end
 end
