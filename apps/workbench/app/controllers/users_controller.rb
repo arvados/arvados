169b8e0907cd03859c6b32366d278ca9f79b8ef3
Tom Clegg <tom@clinicalfuture.com>
2013-07-17 04:52:44 -0400

    new dashboard content

--- apps/workbench/app/controllers/users_controller.rb
+++ apps/workbench/app/controllers/users_controller.rb
@@ -13,11 +13,10 @@ class UsersController < ApplicationController
     @my_ssh_keys = AuthorizedKey.where(authorized_user_uuid: current_user.uuid)
     @my_vm_perms = Link.where(tail_uuid: current_user.uuid, head_kind: 'arvados#virtual_machine', link_class: 'permission', name: 'can_login')
     @my_repo_perms = Link.where(tail_uuid: current_user.uuid, head_kind: 'arvados#repository', link_class: 'permission', name: 'can_write')
-    @my_last_job = Job.
-      limit(1).
-      order(:created_at).
-      where(created_by: current_user.uuid).
-      last
+    @my_jobs = Job.
+      limit(10).
+      order('created_at desc').
+      where(created_by: current_user.uuid)
 
     # A Tutorial is a Link which has link_class "resources" and name
     # "wants", and is owned by the Tutorials Group (i.e., named
