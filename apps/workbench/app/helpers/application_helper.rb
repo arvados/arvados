5737e8daa925356ea0cc342a833a1576330e0e97
Tom Clegg <tom@clinicalfuture.com>
2013-07-31 11:19:13 -0400

    add links beside editable object uuids

--- apps/workbench/app/helpers/application_helper.rb
+++ apps/workbench/app/helpers/application_helper.rb
@@ -22,8 +22,12 @@ module ApplicationHelper
     cooked = raw + cooked
   end
 
+  def resource_class_for_uuid(attrvalue, opts={})
+    ArvadosBase::resource_class_for_uuid(attrvalue, opts)
+  end
+
   def link_to_if_arvados_object(attrvalue, opts={}, style_opts={})
-    if (resource_class = ArvadosBase::resource_class_for_uuid(attrvalue, opts))
+    if (resource_class = resource_class_for_uuid(attrvalue, opts))
       link_uuid = attrvalue.is_a?(ArvadosBase) ? attrvalue.uuid : attrvalue
       link_name = opts[:link_text]
       if !link_name
@@ -31,7 +35,6 @@ module ApplicationHelper
         if opts[:with_class_name]
           link_name = "#{resource_class.to_s} #{link_name}"
         end
-        style_opts = style_opts.merge(style: 'font-family: monospace')
       end
       link_to link_name, { controller: resource_class.to_s.underscore.pluralize, action: 'show', id: link_uuid }, style_opts
     else
