92640a35aadb0353e9f20dc395284117cb398927
Tim Pierce <twp@clinicalfuture.com>
2013-11-06 18:11:51 -0500

    Merging the postgres container back into the api container (simpler and more consistent with best practices).
    'docker build -t arvados/api api' now builds a container with postgres
    and apache2, and installs the API Rails database.
    
    To have this container start multiple services at run time
    (i.e. postgres + apache2) the popular solution is to make supervisord
    the entrypoint for the container.

--- docker/api/config_rake.sh
+++ docker/api/config_rake.sh
@@ -1,8 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
-# set up the RVM environment
-source /usr/local/rvm/scripts/rvm
-
-/usr/bin/service postgresql start
+/etc/init.d/postgresql start
 rake -f /usr/src/arvados/services/api/Rakefile db:setup
-
--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
-# create a arvados/debian base image
+# build the base wheezy image
 ./mkimage-debootstrap.sh arvados/debian wheezy http://debian.lcs.mit.edu/debian/
 
-# build the Docker base image
+# build the Docker images
 docker build -t arvados/base base
+docker build -t arvados/api api
