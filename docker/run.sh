078a4f74fa9c078b9423be6f4d35e0b6932e191c
Ward Vandewege <ward@clinicalfuture.com>
2013-11-13 12:43:20 -0500

    Add workbench server to build.sh and run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 
-echo "Starting documentation server on port 9898"
-
+echo "Starting documentation server: http://localhost:9898"
 docker run -d -i -t -p 9898:80 arvados/docserver
+
+echo "Starting workbench server:     http://localhost:9899"
+docker run -d -i -t -p 9899:80 arvados/workbench
