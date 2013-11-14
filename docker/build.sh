3ea91de2e665634140f1dca970136ce39a9320e1
Tim Pierce <twp@clinicalfuture.com>
2013-11-14 02:10:05 -0500

    Deploy containers with working copy of sources.

--- docker/build.sh
+++ docker/build.sh
@@ -6,6 +6,15 @@
 
 # build the Docker images
 docker build -t arvados/base base
+
+mkdir -p api/generated
+tar -c -z -f api/generated/api.tar.gz -C ../services api
 docker build -t arvados/api api
+
+mkdir -p docserver/generated
+tar -c -z -f docserver/generated/doc.tar.gz -C .. doc
 docker build -t arvados/docserver docserver
+
+mkdir -p workbench/generated
+tar -c -z -f workbench/generated/workbench.tar.gz -C ../apps workbench
 docker build -t arvados/workbench workbench
