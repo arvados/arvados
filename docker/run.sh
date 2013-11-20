63216071476c0c29274a31c06e8c31d6227566f1
Ward Vandewege <ward@clinicalfuture.com>
2013-11-20 10:15:07 -0500

    Make workbench work with the API server.

--- docker/run.sh
+++ docker/run.sh
@@ -8,9 +8,9 @@ echo "Starting sso server:     https://localhost:9901"
 docker run -d -i -t -p 9901:443 -name sso_server arvados/sso
 
 echo "Starting api server:     https://localhost:9900"
-docker run -d -i -t -p 9900:443 -link sso_server:sso arvados/api
+docker run -d -i -t -p 9900:443 -name api_server -link sso_server:sso arvados/api
 
 echo "Starting workbench server:     http://localhost:9899"
-docker run -d -i -t -p 9899:80 arvados/workbench
+docker run -d -i -t -p 9899:80 -link api_server:api arvados/workbench
 
 
