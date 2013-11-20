9a34798694440329fde1980e4ca55f2768e68228
Tim Pierce <twp@clinicalfuture.com>
2013-11-20 13:32:31 -0500

    Improve Makefile rules. Start Keep container from run.sh.

--- docker/run.sh
+++ docker/run.sh
@@ -13,4 +13,24 @@ docker run -d -i -t -p 9900:443 -name api_server -link sso_server:sso arvados/ap
 echo "Starting workbench server:     http://localhost:9899"
 docker run -d -i -t -p 9899:80 -link api_server:api arvados/workbench
 
+echo "Starting keep server:        http://localhost:25107"
 
+# Mount a keep volume if we don't already have one
+keepvolume=""
+for mountpoint in $(cut -d ' ' -f 2 /proc/mounts)
+do
+    if [ -d "$mountpoint/keep" ]
+    then
+	keepvolume=$mountpoint
+    fi
+done
+
+if [ ! "$keepvolume" ]
+then
+    keepvolume=$(mktemp -d)
+    echo "mounting 512M tmpfs keep volume in $keepvolume"
+    sudo mount -t tmpfs -o size=512M tmpfs $keepvolume
+    mkdir $keepvolume/keep
+fi
+
+docker run -d -i -t -p 25107:25107 -v $keepvolume:/dev/keep-0 arvados/warehouse
