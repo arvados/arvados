43a1e4bb3e4f97a81359bddcb98a6ac50c6a0f34
Ward Vandewege <ward@clinicalfuture.com>
2013-11-19 23:31:13 -0500

    * Mention SSO server to documentation in README
    * Fix up SSO and API server configuration to allow API and SSO server to communicate

--- docker/run.sh
+++ docker/run.sh
@@ -4,11 +4,13 @@
 echo "Starting documentation server: http://localhost:9898"
 docker run -d -i -t -p 9898:80 arvados/docserver
 
+echo "Starting sso server:     https://localhost:9901"
+docker run -d -i -t -p 9901:443 -name sso_server arvados/sso
+
 echo "Starting api server:     https://localhost:9900"
-docker run -d -i -t -p 9900:443 arvados/api
+docker run -d -i -t -p 9900:443 -link sso_server:sso arvados/api
 
 echo "Starting workbench server:     http://localhost:9899"
 docker run -d -i -t -p 9899:80 arvados/workbench
 
-echo "Starting sso server:     https://localhost:9901"
-docker run -d -i -t -p 9901:443 arvados/sso
+
