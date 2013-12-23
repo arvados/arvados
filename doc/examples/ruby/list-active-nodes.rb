b6f6708ada5dd029f90022f7b1b82fccc4868dcc
Tom Clegg <tom@clinicalfuture.com>
2013-12-23 08:26:05 -0800

    Specify api_version when instantiating Arvados client. refs #1773

--- doc/examples/ruby/list-active-nodes.rb
+++ doc/examples/ruby/list-active-nodes.rb
@@ -4,7 +4,7 @@ abort 'Error: Ruby >= 1.9.3 required.' if RUBY_VERSION < '1.9.3'
 
 require 'arvados'
 
-arv = Arvados.new
+arv = Arvados.new(api_version: 'v1')
 arv.node.list[:items].each do |node|
   if node[:crunch_worker_state] != 'down'
     ping_age = (Time.now - Time.parse(node[:last_ping_at])).to_i rescue -1
