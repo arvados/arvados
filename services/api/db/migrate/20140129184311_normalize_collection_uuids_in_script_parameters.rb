2728f59746f7a57ed0283b899f9636cfef77d001
Tom Clegg <tom@curoverse.com>
2014-01-29 11:04:12 -0800

    Remove +K@foo parts from collection uuids in jobs' script_parameters and other job fields cached in PipelineInstances.
    refs #1881

---
+++ services/api/db/migrate/20140129184311_normalize_collection_uuids_in_script_parameters.rb
@@ -0,0 +1,45 @@
+class NormalizeCollectionUuidsInScriptParameters < ActiveRecord::Migration
+  include CurrentApiClient
+  def up
+    act_as_system_user do
+      PipelineInstance.all.each do |pi|
+        pi.save! if fix_values_recursively(pi.components)
+      end
+      Job.all.each do |j|
+        changed = false
+        j.script_parameters.each do |p, v|
+          if v.is_a? String and v.match /\+K/
+            v.gsub! /\+K\@\w+/, ''
+            changed = true
+          end
+        end
+        j.save! if changed
+      end
+    end
+  end
+
+  def down
+  end
+
+  protected
+  def fix_values_recursively fixme
+    changed = false
+    if fixme.is_a? String
+      if fixme.match /\+K/
+        fixme.gsub! /\+K\@\w+/, ''
+        return true
+      else
+        return false
+      end
+    elsif fixme.is_a? Array
+      fixme.each do |v|
+        changed = fix_values_recursively(v) || changed
+      end
+    elsif fixme.is_a? Hash
+      fixme.each do |p, v|
+        changed = fix_values_recursively(v) || changed
+      end
+    end
+    changed
+  end
+end
