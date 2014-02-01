bf5d503e0ccc0a8b08c1c0a5e0c53677357c476d
Peter Amstutz <tetron@cosmos>
2014-01-31 20:38:47 -0500

    Concept layout puts right hand sidebar on all pages.

--- apps/workbench/app/controllers/application_controller.rb
+++ apps/workbench/app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   before_filter :find_object_by_uuid, :except => [:index, :render_exception, :render_not_found]
   before_filter :check_user_agreements, :except => [:render_exception, :render_not_found]
   theme :select_theme
+  before_filter :populate_side_panel
 
   begin
     rescue_from Exception,
@@ -261,5 +262,51 @@ class ApplicationController < ActionController::Base
 
   def select_theme
     return Rails.configuration.arvados_theme
+  end
+
+  def populate_side_panel
+    @showallalerts = false
+    @my_ssh_keys = AuthorizedKey.where(authorized_user_uuid: current_user.uuid)
+    # @my_vm_perms = Link.where(tail_uuid: current_user.uuid, head_kind: 'arvados#virtual_machine', link_class: 'permission', name: 'can_login')
+    # @my_repo_perms = Link.where(tail_uuid: current_user.uuid, head_kind: 'arvados#repository', link_class: 'permission', name: 'can_write')
+
+    @my_tag_links = {}
+
+    @my_jobs = Job.
+      limit(10).
+      order('created_at desc').
+      where(created_by: current_user.uuid)
+
+    @my_collections = Collection.
+      limit(10).
+      order('created_at desc').
+      where(created_by: current_user.uuid)
+
+    Link.limit(1000).where(head_uuid: @my_collections.collect(&:uuid),
+                           link_class: 'tag').each do |link|
+      (@my_tag_links[link.head_uuid] ||= []) << link
+    end
+
+    @my_pipelines = PipelineInstance.
+      limit(10).
+      order('created_at desc').
+      where(created_by: current_user.uuid)
+
+
+    # A Tutorial is a Link which has link_class "resources" and name
+    # "wants", and is owned by the Tutorials Group (i.e., named
+    # "Arvados Tutorials" and owned by the system user).
+    @tutorial_group = Group.where(owner_uuid: User.system.uuid,
+                                  name: 'Arvados Tutorials').first
+    if @tutorial_group
+      @tutorial_links = Link.where(tail_uuid: @tutorial_group.uuid,
+                                   link_class: 'resources',
+                                   name: 'wants')
+    else
+      @tutorial_links = []
+    end
+    @tutorial_complete = {
+      'Run a job' => @my_last_job
+    }
   end
 end
