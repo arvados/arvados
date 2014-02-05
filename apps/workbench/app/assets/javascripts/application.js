928aa8a0496f0c6f37579cac821e1048ce6cca96
Peter Amstutz <peter.amstutz@curoverse.com>
2014-02-05 11:54:18 -0500

    Reworked top menu.  Started upgrading to bootstrap 3.

--- apps/workbench/app/assets/javascripts/application.js
+++ apps/workbench/app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
+//= require bootstrap
+//= require bootstrap/dropdown
 //= require bootstrap-editable
 //= require bootstrap-editable-rails
 //= require_tree .
