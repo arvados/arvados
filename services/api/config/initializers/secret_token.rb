8b7308cd7e9034efecf703b1cb88c382a4ef84fc
Peter Amstutz <peter.amstutz@curoverse.com>
2014-03-03 22:05:45 -0500

    Making the default configuration easier for setting up dev environment

--- services/api/config/initializers/secret_token.rb
+++ services/api/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Server::Application.config.secret_token = 'a107d661bc696fd1263e92c76e7e88d8fa44b6a9793e8f56ccfb23f17cfc95ea8894e28ed7dd132a3a6069673961fb1bf32edd7f8a94c8e88d8a7047bfacdde2'
+Server::Application.config.secret_token = rand(2**512).to_s(36)
