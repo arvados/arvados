74aa6539fccff9b135f3edec58801976b9a51538
Tom Clegg <tom@curoverse.com>
2014-03-05 04:35:45 -0500

    Read configuration from config.defaults.yml and config.yml.
    Refuse to start if any configs are set to nil in config.default.yml
    and not overridden in config/environments/*.rb or config.yml.
    
    refs #2076

---
+++ services/api/config/initializers/load_config.rb
@@ -0,0 +1,44 @@
+conf = {}
+%w(config.defaults config).each do |cfgfile|
+  path = "#{::Rails.root.to_s}/config/#{cfgfile}.yml"
+  if File.exists? path
+    yaml = ERB.new(IO.read path).result(binding)
+    confs = YAML.load(yaml)
+    conf.merge!(confs['common'] || {})
+    conf.merge!(confs[::Rails.env.to_s] || {})
+  end
+end
+
+Server::Application.configure do
+  nils = []
+  conf.each do |k, v|
+    # "foo.bar: baz" --> { config.foo.bar = baz }
+    cfg = config
+    ks = k.split '.'
+    k = ks.pop
+    ks.each do |kk|
+      cfg = cfg.send(kk)
+    end
+    if cfg.respond_to?(k.to_sym) and !cfg.send(k).nil?
+      # Config must have been set already in environments/*.rb.
+      #
+      # After config files have been migrated, this mechanism should
+      # be deprecated, then removed.
+    elsif v.nil?
+      # Config variables are not allowed to be nil. Make a "naughty"
+      # list, and present it below.
+      nils << k
+    else
+      cfg.send "#{k}=", v
+    end
+  end
+  if !nils.empty?
+    raise <<EOS
+Refusing to start in #{::Rails.env.to_s} mode with missing configuration.
+
+The following configuration settings must be specified in config/config.yml:
+* #{nils.join "\n* "}
+
+EOS
+  end
+end
