c7ee682e45f191903e5f82b0b5104aa236cfe2e0
Tim Pierce <twp@curoverse.com>
2014-03-11 10:12:55 -0400

    Force ruby1.9.3 install. (refs #2221)

--- docker/build.sh
+++ docker/build.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-# make sure Ruby is installed before proceeding
-if ! ruby -v > /dev/null 2>&1
+# make sure Ruby 1.9.3 is installed before proceeding
+if ! ruby -v 2>/dev/null | grep '1\.9\.3' > /dev/null
 then
     echo "Installing Ruby. You may be required to enter your password."
     sudo apt-get update
-    sudo apt-get install ruby
+    sudo apt-get -y install ruby1.9.3
 fi
 
 ./build.rb $*
