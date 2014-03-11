394c762a07ac25c9db586a614ef44045291edd72
Ward Vandewege <ward@curoverse.com>
2014-03-11 18:22:12 -0400

    Fix links to docs in workbench

--- apps/workbench/app/controllers/application_controller.rb
+++ apps/workbench/app/controllers/application_controller.rb
@@ -320,14 +320,14 @@ class ApplicationController < ActionController::Base
     }
   }
 
-  @@notification_tests.push lambda { |controller, current_user|
-    Job.limit(1).where(created_by: current_user.uuid).each do
-      return nil
-    end
-    return lambda { |view|
-      view.render partial: 'notifications/jobs_notification'
-    }
-  }
+  #@@notification_tests.push lambda { |controller, current_user|
+  #  Job.limit(1).where(created_by: current_user.uuid).each do
+  #    return nil
+  #  end
+  #  return lambda { |view|
+  #    view.render partial: 'notifications/jobs_notification'
+  #  }
+  #}
 
   @@notification_tests.push lambda { |controller, current_user|
     Collection.limit(1).where(created_by: current_user.uuid).each do
