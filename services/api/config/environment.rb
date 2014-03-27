0136e134c210fe60e6a1e0eee5e2f77d8bc939dc
Tom Clegg <tom@curoverse.com>
2014-03-27 17:59:34 -0400

    Ignore "cache directory does not exist" error when clearing cache at server startup.

--- services/api/config/environment.rb
+++ services/api/config/environment.rb
@@ -4,4 +4,9 @@ require 'josh_id'
 
 # Initialize the rails application
 Server::Application.initialize!
-Rails.cache.clear
+begin
+  Rails.cache.clear
+rescue Errno::ENOENT => e
+  # Cache directory does not exist? Then cache is clear, proceed.
+  Rails.logger.warn "In Rails.cache.clear, ignoring #{e.inspect}"
+end
