8905037aede017ccdb66dd2850bfbd2284904e4e
Tom Clegg <tom@curoverse.com>
2014-04-23 16:36:41 -0400

    Add explanation comments to assertions.

--- apps/workbench/test/unit/user_test.rb
+++ apps/workbench/test/unit/user_test.rb
@@ -4,8 +4,10 @@ class UserTest < ActiveSupport::TestCase
   test "get owned_items" do
     use_token :active
     oi = User.find(api_fixture('users')['active']['uuid']).owned_items
-    assert_operator 0, :<, oi.count
-    assert_operator 0, :<, oi.items_available
+    assert_operator(0, :<, oi.count,
+                    "Expected to find some items belonging to :active user")
+    assert_operator(0, :<, oi.items_available
+                    "Expected owned_items response to have items_available > 0")
     oi_uuids = oi.collect { |i| i['uuid'] }
     expect = api_fixture('specimens')['owned_by_active_user']['uuid']
     assert_includes(oi_uuids, expect,
