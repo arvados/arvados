5c2758ca38d01a905253f86e63be3a5fe03a3871
Brett Smith <brett@curoverse.com>
2014-04-28 13:01:18 -0400

    api: Support filters in API client auths index.
    Per comments on Refs #1904.  filters is generally the preferred way to
    do searching now.  I maintained existing limits on what can be
    searched with this method.

--- services/api/app/controllers/application_controller.rb
+++ services/api/app/controllers/application_controller.rb
@@ -207,6 +207,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def default_orders
+    ["#{table_name}.modified_at desc"]
+  end
+
   def load_limit_offset_order_params
     if params[:limit]
       unless params[:limit].to_s.match(/^\d+$/)
@@ -239,7 +243,7 @@ class ApplicationController < ActionController::Base
       end
     end
     if @orders.empty?
-      @orders << "#{table_name}.modified_at desc"
+      @orders = default_orders
     end
   end
 
