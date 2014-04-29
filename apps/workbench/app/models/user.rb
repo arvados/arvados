9cfbcdf804974812b1a4ec514f5dc7902fce6137
Tom Clegg <tom@curoverse.com>
2014-04-29 17:00:20 -0400

    Rename owned_items to contents, remove user method, update docs.

--- apps/workbench/app/models/user.rb
+++ apps/workbench/app/models/user.rb
@@ -17,15 +17,6 @@ class User < ArvadosBase
                              end
   end
 
-  def owned_items params={}
-    res = $arvados_api_client.api self.class, "/#{self.uuid}/owned_items", {
-      _method: 'GET'
-    }.merge(params)
-    ret = ArvadosResourceList.new
-    ret.results = $arvados_api_client.unpack_api_response(res)
-    ret
-  end
-
   def full_name
     (self.first_name || "") + " " + (self.last_name || "")
   end
