9820178bf518d9ddace8c27ea383e5c37d86c8fe
Tom Clegg <tom@curoverse.com>
2014-04-30 16:57:16 -0400

    Add integration test for folders.

---
+++ apps/workbench/test/integration/folders_test.rb
@@ -0,0 +1,24 @@
+require 'integration_helper'
+require 'selenium-webdriver'
+require 'headless'
+
+class FoldersTest < ActionDispatch::IntegrationTest
+
+  test 'Find a folder and edit its description' do
+    Capybara.current_driver = Capybara.javascript_driver
+    visit page_with_token 'active', '/'
+    find('nav a', text: 'Folders').click
+    find('tr', text: 'A Folder').
+      find('a', text: 'zzzzz-').
+      click
+    within('.panel', text: api_fixture('groups')['afolder']['name']) do
+      find('span', text: api_fixture('groups')['afolder']['name']).click
+      find('.glyphicon-ok').click
+      find('.btn', text: 'Edit description').click
+      find('textarea').set('I just edited this.')
+      find('.glyphicon-ok').click
+    end
+    find('.panel', text: 'I just edited this.')
+  end
+
+end
