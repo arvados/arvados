33b49fa0be7d8e0774c80ec315985dcdf8535962
Peter Amstutz <peter.amstutz@curoverse.com>
2014-05-01 10:06:19 -0400

    Changed Log.all.each to Log.find_each because Log.all loads all records (!) and Log.find_each loads records in batches.  Hopefully this will enable migration to run without being killed.

--- services/api/db/migrate/20140423132913_add_object_owner_to_logs.rb
+++ services/api/db/migrate/20140423132913_add_object_owner_to_logs.rb
@@ -4,7 +4,7 @@ class AddObjectOwnerToLogs < ActiveRecord::Migration
   def up
     add_column :logs, :object_owner_uuid, :string
     act_as_system_user do
-      Log.all.each do |log|
+      Log.find_each do |log|
         if log.properties[:new_attributes]
           log.object_owner_uuid = log.properties[:new_attributes][:owner_uuid]
         elsif log.properties[:old_attributes]
