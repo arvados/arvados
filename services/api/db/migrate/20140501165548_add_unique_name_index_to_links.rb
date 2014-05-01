70fddd1f7cacaa0db92b3b76a0baeeddf6f0d9b6
Tom Clegg <tom@curoverse.com>
2014-05-01 14:38:46 -0400

    Use partial unique index for name links.

--- services/api/db/migrate/20140501165548_add_unique_name_index_to_links.rb
+++ services/api/db/migrate/20140501165548_add_unique_name_index_to_links.rb
@@ -1,6 +1,13 @@
 class AddUniqueNameIndexToLinks < ActiveRecord::Migration
   def change
-    # todo: add "check (link_class is not 'name' or name is not null)"
-    add_index :links, [:tail_uuid, :name], where: "link_class='name'", unique: true
+    # Make sure PgPower is here. Otherwise the "where" will be ignored
+    # and we'll end up with a far too restrictive unique
+    # constraint. (Rails4 should work without PgPower, but that isn't
+    # tested.)
+    if not PgPower then raise "No partial column support" end
+
+    add_index(:links, [:tail_uuid, :name], unique: true,
+              where: "link_class='name'",
+              name: 'links_tail_name_unique_if_link_class_name')
   end
 end
