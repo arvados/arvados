647c8b8c47ef521046bea7675af84b621c235e0f
Tom Clegg <tom@curoverse.com>
2014-05-04 16:22:35 -0400

    Add client proxy hack so existing themes continue to work.

--- apps/workbench/app/helpers/arvados_api_client_helper.rb
+++ apps/workbench/app/helpers/arvados_api_client_helper.rb
@@ -3,3 +3,11 @@ module ArvadosApiClientHelper
     ArvadosApiClient.new_or_current
   end
 end
+
+# For the benefit of themes that still expect $arvados_api_client to work:
+class ArvadosClientProxyHack
+  def method_missing *args
+    ArvadosApiClient.new_or_current.send *args
+  end
+end
+$arvados_api_client = ArvadosClientProxyHack.new
