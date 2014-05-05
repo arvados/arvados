c6ad75e4241247ac18f5a5a8c397537dc6be1e3c
Tom Clegg <tom@curoverse.com>
2014-05-05 04:01:48 -0400

    Implement simple client-side folder contents search.

--- apps/workbench/app/assets/javascripts/folders.js
+++ apps/workbench/app/assets/javascripts/folders.js
@@ -4,4 +4,9 @@ $(document).
             e.stopPropagation();
             $($(this).attr('data-toggle-selector')).editable('toggle');
         });
+    }).on('paste keyup change', 'input.search-folder-contents', function() {
+        var q = new RegExp($(this).val(), 'i');
+        $(this).closest('div.panel').find('tbody tr').each(function() {
+            $(this).toggle(!!$(this).text().match(q));
+        });
     });
