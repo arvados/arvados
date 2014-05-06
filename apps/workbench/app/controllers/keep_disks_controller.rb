638d136057f4d9ed19f132075079da396217d6fd
Misha Zatsman <misha-arvados@zatsman.com>
2014-05-06 23:30:26 +0000

    Added printing of histogram json.

--- apps/workbench/app/controllers/keep_disks_controller.rb
+++ apps/workbench/app/controllers/keep_disks_controller.rb
@@ -5,7 +5,27 @@ class KeepDisksController < ApplicationController
     super
   end
   def index
-    # TODO(misha):Retrieve the histogram info
+
+    # Retrieve cache age histogram info
+    @cache_age_histogram = []
+    @histogram_date = 0
+    histogram_log = Log.
+      filter([[:event_type, '=', 'block-age-free-space-histogram']]).
+      order(:created_at => :desc).
+      limit(1)
+    histogram_log.each do |log_entry|
+      # We expect this block to only execute at most once since we
+      # specified limit(1)
+      @cache_age_histogram = log_entry['properties'][:histogram]
+      # Javascript wants dates in milliseconds.
+      @histogram_date = log_entry['event_at'].to_i * 1000
+
+      total_free_cache = @cache_age_histogram[-1][1]
+      persisted_storage = 1 - total_free_cache
+      @cache_age_histogram.map! { |x| {:age => @histogram_date - x[0]*1000,
+          :cache => total_free_cache - x[1],
+          :persisted => persisted_storage} }
+    end
     super
   end
 end
