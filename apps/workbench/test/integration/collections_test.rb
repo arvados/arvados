01d5f03da039d8ad11a3323906e2416be120cb89
Brett Smith <brett@curoverse.com>
2014-05-06 14:21:34 -0400

    workbench: File downloads include reader tokens.
    This makes it possible to download an entire collection by pointing
    `wget -r` at a collection page with a reader_tokens parameter.
    
    wget respects robots.txt, so this required a finer-grained approach to
    shooing away robots.  We use a meta tag to mark everything as NOINDEX,
    and implement NOFOLLOW on everything except Collections pages.

--- apps/workbench/test/integration/collections_test.rb
+++ apps/workbench/test/integration/collections_test.rb
@@ -3,7 +3,6 @@ require 'selenium-webdriver'
 require 'headless'
 
 class CollectionsTest < ActionDispatch::IntegrationTest
-
   def change_persist oldstate, newstate
     find "div[data-persistent-state='#{oldstate}']"
     page.assert_no_selector "div[data-persistent-state='#{newstate}']"
@@ -39,4 +38,25 @@ class CollectionsTest < ActionDispatch::IntegrationTest
     change_persist 'persistent', 'cache'
   end
 
+  test "can download an entire collection with a reader token" do
+    uuid = api_fixture('collections')['foo_file']['uuid']
+    token = api_fixture('api_client_authorizations')['active']['api_token']
+    q_string = URI.encode_www_form('reader_tokens[]' => token)
+    visit "/collections/#{uuid}?#{q_string}"
+    # It seems that Capybara can't inspect tags outside the body, so this is
+    # a very blunt approach.
+    assert_no_match(/\bnofollow\b/i, page.html,
+                    "wget prohibited from recursing the collection page")
+    # TODO: When we can test against a Keep server, actually click the link
+    # and check the contents, rather than testing the href directly
+    # (this is too closely tied to implementation details).
+    link = nil
+    assert_nothing_raised("failed to list foo files with reader token") do
+      link = find_link('Download')
+    end
+    assert_match(%r{^/collections/#{Regexp.escape uuid}/foo}, link[:href],
+                 "download link doesn't point to foo file")
+    assert_match(/\b#{Regexp.escape q_string}\b/, link[:href],
+                 "collection file download link did not inherit reader tokens")
+  end
 end
