fdbea9e3c225a3184f8f7c2fac52abde643603c6
radhika <radhika@curoverse.com>
2014-05-06 21:33:26 -0400

    2525: Address review feedback.

--- sdk/java/ArvadosSDKJavaExample.java
+++ sdk/java/ArvadosSDKJavaExample.java
@@ -14,7 +14,14 @@ import java.util.Map;
 
 public class ArvadosSDKJavaExample {
   /** Make sure the following environment variables are set before using Arvados:
-   *      ARVADOS_API_TOKEN, ARVADOS_API_HOST, ARVADOS_API_HOST_INSECURE
+   *      ARVADOS_API_TOKEN, ARVADOS_API_HOST and ARVADOS_API_HOST_INSECURE 
+   *      Set ARVADOS_API_HOST_INSECURE to true if you are using self-singed
+   *      certificates in development and want to bypass certificate validations.
+   *
+   *  If you are not using env variables, you can pass them to Arvados constructor.
+   *
+   *  Please refer to http://doc.arvados.org/api/index.html for a complete list
+   *      of the available API methods.
    */
   public static void main(String[] args) throws Exception {
     String apiName = "arvados";
@@ -22,7 +29,8 @@ public class ArvadosSDKJavaExample {
 
     Arvados arv = new Arvados(apiName, apiVersion);
 
-    // Make a users.list call
+    // Make a users list call. Here list on users is the method being invoked.
+    // Expect a Map containing the list of users as the response.
     System.out.println("Making an arvados users.list api call");
 
     Map<String, Object> params = new HashMap<String, Object>();
@@ -36,15 +44,15 @@ public class ArvadosSDKJavaExample {
     Map firstUser = (Map)items.get(0);
     String userUuid = (String)firstUser.get("uuid");
     
-    // Make a users.get call on the uuid obtained above
-    System.out.println("Making a users.get call for " + userUuid);
+    // Make a users get call on the uuid obtained above
+    System.out.println("\n\n\nMaking a users.get call for " + userUuid);
     params = new HashMap<String, Object>();
     params.put("uuid", userUuid);
     response = arv.call("users", "get", params);
     System.out.println("Arvados users.get:\n" + response);
 
-    // Make a pipeline_templates.list call
-    System.out.println("Making a pipeline_templates.list call.");
+    // Make a pipeline_templates list call
+    System.out.println("\n\n\nMaking a pipeline_templates.list call.");
 
     params = new HashMap<String, Object>();
     response = arv.call("pipeline_templates", "list", params);
