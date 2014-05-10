6022ddd1b07217b9b4f31e73493fc259bd19b696
Peter Amstutz <peter.amstutz@curoverse.com>
2014-05-10 17:18:21 -0400

    2798: Started adding tests that ShuffledServiceRoots behavior is consistent with Python reference implementation.

--- sdk/go/src/arvados.org/keepclient/keepclient_test.go
+++ sdk/go/src/arvados.org/keepclient/keepclient_test.go
@@ -19,4 +19,13 @@ func (s *MySuite) TestGetKeepDisks(c *C) {
 	c.Assert(sr[0], Equals, "http://localhost:25107")
 	c.Assert(sr[1], Equals, "http://localhost:25108")
 
+	service_roots := []string{"http://localhost:25107", "http://localhost:25108", "http://localhost:25109", "http://localhost:25110", "http://localhost:25111", "http://localhost:25112", "http://localhost:25113", "http://localhost:25114", "http://localhost:25115", "http://localhost:25116", "http://localhost:25117", "http://localhost:25118", "http://localhost:25119", "http://localhost:25120", "http://localhost:25121", "http://localhost:25122", "http://localhost:25123"}
+
+	// "foo" acbd18db4cc2f85cedef654fccc4a4d8
+	//foo_shuffle := []string{"http://localhost:25116", "http://localhost:25120", "http://localhost:25119", "http://localhost:25122", "http://localhost:25108", "http://localhost:25114", "http://localhost:25112", "http://localhost:25107", "http://localhost:25118", "http://localhost:25111", "http://localhost:25113", "http://localhost:25121", "http://localhost:25110", "http://localhost:25117", "http://localhost:25109", "http://localhost:25115", "http://localhost:25123"}
+	ShuffledServiceRoots(service_roots, "acbd18db4cc2f85cedef654fccc4a4d8")
+
+	// "bar" 37b51d194a7513e45b56f6524f2d51f2
+	//bar_shuffle := []string{"http://localhost:25108", "http://localhost:25112", "http://localhost:25119", "http://localhost:25107", "http://localhost:25110", "http://localhost:25116", "http://localhost:25122", "http://localhost:25120", "http://localhost:25121", "http://localhost:25117", "http://localhost:25111", "http://localhost:25123", "http://localhost:25118", "http://localhost:25113", "http://localhost:25114", "http://localhost:25115", "http://localhost:25109"}
+
 }
