e2eca205db4e40de582117790b1e48dbf3e5bf3a
radhika <radhika@curoverse.com>
2014-05-15 15:52:15 -0400

    2756: store websocket in window to facilitate reuse within page. also, remove elementId from map when leaving page.

--- apps/workbench/app/assets/javascripts/event_log.js
+++ apps/workbench/app/assets/javascripts/event_log.js
@@ -14,11 +14,21 @@ function subscribeToEventLog (url, uuid, elementId) {
     return;  
   }
 
-  // create the event log dispatcher
-  event_log_disp = new WebSocket(url);
+  // grab websocket connection from window, if one exists
+  event_log_disp = $(window).data("arv-websocket");
+  if (event_log_disp == null) {
+    // create the event log dispatcher
+    event_log_disp = new WebSocket(url);
 
-  event_log_disp.onopen = function(event) { onEventLogDispatcherOpen(event) };
-  event_log_disp.onmessage = function(event) { onEventLogDispatcherMessage(event) };
+    event_log_disp.onopen = function(event) { onEventLogDispatcherOpen(event) };
+    event_log_disp.onmessage = function(event) { onEventLogDispatcherMessage(event) };
+
+    // store websocket in window to allow reuse when multiple divs subscribe for events
+    $(window).data("arv-websocket", event_log_disp);
+
+    // detach elementId when user leaves this window
+    window.onbeforeunload = function (event) {detachElement(event, elementId);}
+  }
 
   // Add the elementId to listener map
   event_log_listener_map = JSON.parse(sessionStorage.getItem("event_log_listener_map"));
@@ -57,5 +67,21 @@ function onEventLogDispatcherMessage(event) {
       // append to the registered element
       $('#'+key).append(eventData.summary + " " + new_properties + "&#13;&#10;");
     }
+  }
+}
+
+/**
+ * When leaving this window, remove elementId from map
+ */
+function detachElement(event, elementId) {
+  event_log_listener_map = JSON.parse(sessionStorage.getItem("event_log_listener_map"));
+  if (event_log_listener_map !== null) {
+    delete event_log_listener_map[elementId];
+  }
+
+  // According to documenation, browser will close the connection when page is left.
+  // However, there are suggestions of inconsistency; so to be safe, close explicitly.
+  if (event_log_disp !== null) {
+    event_log_disp.close();
   }
 }
