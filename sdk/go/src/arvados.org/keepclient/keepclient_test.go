d3b11ddc2506de37b8e6538be69237d6d2a60a4a
Peter Amstutz <peter.amstutz@curoverse.com>
2014-05-15 16:47:14 -0400

    2798: Completed move of Transfer() related code out to 'buffer' package.

--- sdk/go/src/arvados.org/keepclient/keepclient_test.go
+++ sdk/go/src/arvados.org/keepclient/keepclient_test.go
@@ -1,6 +1,7 @@
 package keepclient
 
 import (
+	"arvados.org/buffer"
 	"crypto/md5"
 	"flag"
 	"fmt"
@@ -15,7 +16,6 @@ import (
 	"sort"
 	"strings"
 	"testing"
-	"time"
 )
 
 // Gocheck boilerplate
@@ -87,354 +87,6 @@ func (s *StandaloneSuite) TestShuffleServiceRoots(c *C) {
 	// "bar" 37b51d194a7513e45b56f6524f2d51f2
 	bar_shuffle := []string{"http://localhost:25108", "http://localhost:25112", "http://localhost:25119", "http://localhost:25107", "http://localhost:25110", "http://localhost:25116", "http://localhost:25122", "http://localhost:25120", "http://localhost:25121", "http://localhost:25117", "http://localhost:25111", "http://localhost:25123", "http://localhost:25118", "http://localhost:25113", "http://localhost:25114", "http://localhost:25115", "http://localhost:25109"}
 	c.Check(kc.ShuffledServiceRoots("37b51d194a7513e45b56f6524f2d51f2"), DeepEquals, bar_shuffle)
-}
-
-func ReadIntoBufferHelper(c *C, bufsize int) {
-	buffer := make([]byte, bufsize)
-
-	reader, writer := io.Pipe()
-	slices := make(chan ReaderSlice)
-
-	go ReadIntoBuffer(buffer, reader, slices)
-
-	{
-		out := make([]byte, 128)
-		for i := 0; i < 128; i += 1 {
-			out[i] = byte(i)
-		}
-		writer.Write(out)
-		s1 := <-slices
-		c.Check(len(s1.slice), Equals, 128)
-		c.Check(s1.reader_error, Equals, nil)
-		for i := 0; i < 128; i += 1 {
-			c.Check(s1.slice[i], Equals, byte(i))
-		}
-		for i := 0; i < len(buffer); i += 1 {
-			if i < 128 {
-				c.Check(buffer[i], Equals, byte(i))
-			} else {
-				c.Check(buffer[i], Equals, byte(0))
-			}
-		}
-	}
-	{
-		out := make([]byte, 96)
-		for i := 0; i < 96; i += 1 {
-			out[i] = byte(i / 2)
-		}
-		writer.Write(out)
-		s1 := <-slices
-		c.Check(len(s1.slice), Equals, 96)
-		c.Check(s1.reader_error, Equals, nil)
-		for i := 0; i < 96; i += 1 {
-			c.Check(s1.slice[i], Equals, byte(i/2))
-		}
-		for i := 0; i < len(buffer); i += 1 {
-			if i < 128 {
-				c.Check(buffer[i], Equals, byte(i))
-			} else if i < (128 + 96) {
-				c.Check(buffer[i], Equals, byte((i-128)/2))
-			} else {
-				c.Check(buffer[i], Equals, byte(0))
-			}
-		}
-	}
-	{
-		writer.Close()
-		s1 := <-slices
-		c.Check(len(s1.slice), Equals, 0)
-		c.Check(s1.reader_error, Equals, io.EOF)
-	}
-}
-
-func (s *StandaloneSuite) TestReadIntoBuffer(c *C) {
-	ReadIntoBufferHelper(c, 512)
-	ReadIntoBufferHelper(c, 225)
-	ReadIntoBufferHelper(c, 224)
-}
-
-func (s *StandaloneSuite) TestReadIntoShortBuffer(c *C) {
-	buffer := make([]byte, 223)
-	reader, writer := io.Pipe()
-	slices := make(chan ReaderSlice)
-
-	go ReadIntoBuffer(buffer, reader, slices)
-
-	{
-		out := make([]byte, 128)
-		for i := 0; i < 128; i += 1 {
-			out[i] = byte(i)
-		}
-		writer.Write(out)
-		s1 := <-slices
-		c.Check(len(s1.slice), Equals, 128)
-		c.Check(s1.reader_error, Equals, nil)
-		for i := 0; i < 128; i += 1 {
-			c.Check(s1.slice[i], Equals, byte(i))
-		}
-		for i := 0; i < len(buffer); i += 1 {
-			if i < 128 {
-				c.Check(buffer[i], Equals, byte(i))
-			} else {
-				c.Check(buffer[i], Equals, byte(0))
-			}
-		}
-	}
-	{
-		out := make([]byte, 96)
-		for i := 0; i < 96; i += 1 {
-			out[i] = byte(i / 2)
-		}
-
-		// Write will deadlock because it can't write all the data, so
-		// spin it off to a goroutine
-		go writer.Write(out)
-		s1 := <-slices
-
-		c.Check(len(s1.slice), Equals, 95)
-		c.Check(s1.reader_error, Equals, nil)
-		for i := 0; i < 95; i += 1 {
-			c.Check(s1.slice[i], Equals, byte(i/2))
-		}
-		for i := 0; i < len(buffer); i += 1 {
-			if i < 128 {
-				c.Check(buffer[i], Equals, byte(i))
-			} else if i < (128 + 95) {
-				c.Check(buffer[i], Equals, byte((i-128)/2))
-			} else {
-				c.Check(buffer[i], Equals, byte(0))
-			}
-		}
-	}
-	{
-		writer.Close()
-		s1 := <-slices
-		c.Check(len(s1.slice), Equals, 0)
-		c.Check(s1.reader_error, Equals, io.ErrShortBuffer)
-	}
-
-}
-
-func (s *StandaloneSuite) TestTransfer(c *C) {
-	reader, writer := io.Pipe()
-
-	// Buffer for reads from 'r'
-	buffer := make([]byte, 512)
-
-	// Read requests on Transfer() buffer
-	requests := make(chan ReadRequest)
-	defer close(requests)
-
-	// Reporting reader error states
-	reader_status := make(chan error)
-
-	go Transfer(buffer, reader, requests, reader_status)
-
-	br1 := MakeBufferReader(requests)
-	out := make([]byte, 128)
-
-	{
-		// Write some data, and read into a buffer shorter than
-		// available data
-		for i := 0; i < 128; i += 1 {
-			out[i] = byte(i)
-		}
-
-		writer.Write(out[:100])
-
-		in := make([]byte, 64)
-		n, err := br1.Read(in)
-
-		c.Check(n, Equals, 64)
-		c.Check(err, Equals, nil)
-
-		for i := 0; i < 64; i += 1 {
-			c.Check(in[i], Equals, out[i])
-		}
-	}
-
-	{
-		// Write some more data, and read into buffer longer than
-		// available data
-		in := make([]byte, 64)
-		n, err := br1.Read(in)
-		c.Check(n, Equals, 36)
-		c.Check(err, Equals, nil)
-
-		for i := 0; i < 36; i += 1 {
-			c.Check(in[i], Equals, out[64+i])
-		}
-
-	}
-
-	{
-		// Test read before write
-		type Rd struct {
-			n   int
-			err error
-		}
-		rd := make(chan Rd)
-		in := make([]byte, 64)
-
-		go func() {
-			n, err := br1.Read(in)
-			rd <- Rd{n, err}
-		}()
-
-		time.Sleep(100 * time.Millisecond)
-		writer.Write(out[100:])
-
-		got := <-rd
-
-		c.Check(got.n, Equals, 28)
-		c.Check(got.err, Equals, nil)
-
-		for i := 0; i < 28; i += 1 {
-			c.Check(in[i], Equals, out[100+i])
-		}
-	}
-
-	br2 := MakeBufferReader(requests)
-	{
-		// Test 'catch up' reader
-		in := make([]byte, 256)
-		n, err := br2.Read(in)
-
-		c.Check(n, Equals, 128)
-		c.Check(err, Equals, nil)
-
-		for i := 0; i < 128; i += 1 {
-			c.Check(in[i], Equals, out[i])
-		}
-	}
-
-	{
-		// Test closing the reader
-		writer.Close()
-		status := <-reader_status
-		c.Check(status, Equals, io.EOF)
-
-		in := make([]byte, 256)
-		n1, err1 := br1.Read(in)
-		n2, err2 := br2.Read(in)
-		c.Check(n1, Equals, 0)
-		c.Check(err1, Equals, io.EOF)
-		c.Check(n2, Equals, 0)
-		c.Check(err2, Equals, io.EOF)
-	}
-
-	{
-		// Test 'catch up' reader after closing
-		br3 := MakeBufferReader(requests)
-		in := make([]byte, 256)
-		n, err := br3.Read(in)
-
-		c.Check(n, Equals, 128)
-		c.Check(err, Equals, nil)
-
-		for i := 0; i < 128; i += 1 {
-			c.Check(in[i], Equals, out[i])
-		}
-
-		n, err = br3.Read(in)
-
-		c.Check(n, Equals, 0)
-		c.Check(err, Equals, io.EOF)
-	}
-}
-
-func (s *StandaloneSuite) TestTransferShortBuffer(c *C) {
-	reader, writer := io.Pipe()
-
-	// Buffer for reads from 'r'
-	buffer := make([]byte, 100)
-
-	// Read requests on Transfer() buffer
-	requests := make(chan ReadRequest)
-	defer close(requests)
-
-	// Reporting reader error states
-	reader_status := make(chan error)
-
-	go Transfer(buffer, reader, requests, reader_status)
-
-	out := make([]byte, 101)
-	go writer.Write(out)
-
-	status := <-reader_status
-	c.Check(status, Equals, io.ErrShortBuffer)
-}
-
-func (s *StandaloneSuite) TestTransferFromBuffer(c *C) {
-	// Buffer for reads from 'r'
-	buffer := make([]byte, 100)
-	for i := 0; i < 100; i += 1 {
-		buffer[i] = byte(i)
-	}
-
-	// Read requests on Transfer() buffer
-	requests := make(chan ReadRequest)
-	defer close(requests)
-
-	go Transfer(buffer, nil, requests, nil)
-
-	br1 := MakeBufferReader(requests)
-
-	in := make([]byte, 64)
-	{
-		n, err := br1.Read(in)
-
-		c.Check(n, Equals, 64)
-		c.Check(err, Equals, nil)
-
-		for i := 0; i < 64; i += 1 {
-			c.Check(in[i], Equals, buffer[i])
-		}
-	}
-	{
-		n, err := br1.Read(in)
-
-		c.Check(n, Equals, 36)
-		c.Check(err, Equals, nil)
-
-		for i := 0; i < 36; i += 1 {
-			c.Check(in[i], Equals, buffer[64+i])
-		}
-	}
-	{
-		n, err := br1.Read(in)
-
-		c.Check(n, Equals, 0)
-		c.Check(err, Equals, io.EOF)
-	}
-}
-
-func (s *StandaloneSuite) TestTransferIoCopy(c *C) {
-	// Buffer for reads from 'r'
-	buffer := make([]byte, 100)
-	for i := 0; i < 100; i += 1 {
-		buffer[i] = byte(i)
-	}
-
-	// Read requests on Transfer() buffer
-	requests := make(chan ReadRequest)
-	defer close(requests)
-
-	go Transfer(buffer, nil, requests, nil)
-
-	br1 := MakeBufferReader(requests)
-
-	reader, writer := io.Pipe()
-
-	go func() {
-		p := make([]byte, 100)
-		n, err := reader.Read(p)
-		c.Check(n, Equals, 100)
-		c.Check(err, Equals, nil)
-		c.Check(p, DeepEquals, buffer)
-	}()
-
-	io.Copy(writer, br1)
 }
 
 type StubPutHandler struct {
@@ -521,18 +173,18 @@ func (s *StandaloneSuite) TestUploadToStubKeepServerBufferReader(c *C) {
 			writer io.WriteCloser, upload_status chan UploadStatus) {
 
 			// Buffer for reads from 'r'
-			buffer := make([]byte, 512)
+			buf := make([]byte, 512)
 
 			// Read requests on Transfer() buffer
-			requests := make(chan ReadRequest)
+			requests := make(chan buffer.ReadRequest)
 			defer close(requests)
 
 			// Reporting reader error states
 			reader_status := make(chan error)
 
-			go Transfer(buffer, reader, requests, reader_status)
+			go buffer.Transfer(buf, reader, requests, reader_status)
 
-			br1 := MakeBufferReader(requests)
+			br1 := buffer.MakeBufferReader(requests)
 
 			go kc.uploadToKeepServer(url, st.expectPath, br1, upload_status, 3)
 
