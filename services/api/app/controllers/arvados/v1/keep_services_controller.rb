040f1c94dfa1a53356d24e77afdbc0f2e5cfe91c
Peter Amstutz <peter.amstutz@curoverse.com>
2014-05-20 14:11:15 -0400

    2776: Added test for keep_services/accessable route.

--- services/api/app/controllers/arvados/v1/keep_services_controller.rb
+++ services/api/app/controllers/arvados/v1/keep_services_controller.rb
@@ -1,5 +1,8 @@
 class Arvados::V1::KeepServicesController < ApplicationController
 
+  skip_before_filter :find_object_by_uuid, only: :accessable
+  skip_before_filter :render_404_if_no_object, only: :accessable
+
   def find_objects_for_index
     # all users can list all keep services
     @objects = model_class.where('1=1')
@@ -7,11 +10,14 @@ class Arvados::V1::KeepServicesController < ApplicationController
   end
 
   def accessable
+    puts "Hello world"
     if request.headers['X-Keep-Proxy-Required']
       @objects = model_class.where('service_type=?', 'proxy')
     else
       @objects = model_class.where('service_type=?', 'disk')
     end
+
+    puts "Rendering list now"
 
     render_list
   end
