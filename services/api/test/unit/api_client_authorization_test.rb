057f835f3c2f28fcaad3baebe9c279a9d6548731
Tom Clegg <tom@curoverse.com>
2014-05-29 16:48:30 -0400

    2762: Do not try to auto-destroy referring links when destroying a model with no uuid. refs #2762

--- services/api/test/unit/api_client_authorization_test.rb
+++ services/api/test/unit/api_client_authorization_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ApiClientAuthorizationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include CurrentApiClient
+
+  [:admin_trustedclient, :active_trustedclient].each do |token|
+    test "ApiClientAuthorization can be created then deleted by #{token}" do
+      set_user_from_auth token
+      x = ApiClientAuthorization.create!(user_id: current_user.id,
+                                         api_client_id: 0,
+                                         scopes: [])
+      newtoken = x.api_token
+      assert x.destroy, "Failed to destroy new ApiClientAuth"
+      assert_empty ApiClientAuthorization.where(api_token: newtoken), "Destroyed ApiClientAuth is still in database"
+    end
+  end
 end
