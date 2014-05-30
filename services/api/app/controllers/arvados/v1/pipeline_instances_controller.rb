95709eedf2452d3ceddeeb02d24b823a552300cf
radhika <radhika@curoverse.com>
2014-05-30 17:42:22 -0400

    2903: merge master

--- services/api/app/controllers/arvados/v1/pipeline_instances_controller.rb
+++ services/api/app/controllers/arvados/v1/pipeline_instances_controller.rb
@@ -1,5 +1,4 @@
 class Arvados::V1::PipelineInstancesController < ApplicationController
-  accept_attribute_as_json :components_summary, Hash
   accept_attribute_as_json :components, Hash
   accept_attribute_as_json :properties, Hash
   accept_attribute_as_json :components_summary, Hash
