4f6e2f4b6b289ca7b2016543b425b6b5c2da10bd
radhika <radhika@curoverse.com>
2014-06-06 15:53:12 -0400

    2872: added graph and run button checks back. the test does not pass right now due to bug in graph tab display in pipeline instance display.

--- apps/workbench/test/integration/pipeline_instances_test.rb
+++ apps/workbench/test/integration/pipeline_instances_test.rb
@@ -67,12 +67,17 @@ class PipelineInstancesTest < ActionDispatch::IntegrationTest
 
     first('a,button', text: 'Run').click
 
+    # Go over to the graph tab
+    click_link 'Provenance'
+    assert page.has_css? 'div#provenance_graph'
+
     # Pipeline is running. We have a "Stop" button instead now.
     page.assert_selector 'a,button', text: 'Stop'
     find('a,button', text: 'Stop').click
 
-    # Pipeline is stopped. It should now be in paused state.
+    # Pipeline is stopped. It should now be in paused state and Runnable again.
     assert page.has_text? 'Paused'
+    page.assert_no_selector 'a.disabled,button.disabled', text: 'Run'
     page.assert_selector 'a,button', text: 'Clone and edit'
   end
 end
