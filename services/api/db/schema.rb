4880e56df98982397fbdb2d9ee98e929cf6f5818
radhika <radhika@curoverse.com>
2014-06-12 11:02:59 -0400

    2659: db migration to add anonymous group and user

--- services/api/db/schema.rb
+++ services/api/db/schema.rb
@@ -11,8 +11,9 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
+ActiveRecord::Schema.define(:version => 20140610210837) do
 
-ActiveRecord::Schema.define(:version => 20140602143352) do
+
 
   create_table "api_client_authorizations", :force => true do |t|
     t.string   "api_token",                                           :null => false
