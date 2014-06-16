189ce25a32a5916194597665435b269192a9baec
Peter Amstutz <peter.amstutz@curoverse.com>
2014-06-16 19:11:08 +0000

    2883: Added overview, node column and sort by node, style tweaks.

--- apps/workbench/app/assets/javascripts/log_viewer.js
+++ apps/workbench/app/assets/javascripts/log_viewer.js
@@ -15,7 +15,12 @@ function addToLogViewer(logViewer, lines, taskState) {
 
             var message = v[12];
             var type = "";
+            var node = "";
             if (v11 !== "") {
+                if (!taskState.hasOwnProperty(v11)) {
+                    taskState[v11] = {};
+                }
+
                 if (/^stderr /.test(message)) {
                     message = message.substr(7);
                     if (/^crunchstat: /.test(message)) {
@@ -28,13 +33,31 @@ function addToLogViewer(logViewer, lines, taskState) {
                     }
                 } else {
                     if (/^success /.test(message)) {
-                        taskState[v11] = "success";
+                        taskState[v11].outcome = "success";
+                        taskState.success_count += 1;
                     }
                     else if (/^failure /.test(message)) {
-                        taskState[v11] = "failure";
+                        taskState[v11].outcome = "failure";
+                        taskState.failure_count += 1;
+                    }
+                    else {
+                        var child = message.match(/^child \d+ started on (.*)/);
+                        if (child != null) {
+                            taskState[v11].node = child[1];
+                            for (var i in logViewer.items) {
+                                if (i > 0) {
+                                    var val = logViewer.items[i].values();
+                                    if (val.taskid === v11) {
+                                        val.node = child[1];
+                                        logViewer.items[i].values(val);
+                                    }
+                                }
+                            }
+                        }
                     }
                     type = "task-dispatch";
                 }
+                node = taskState[v11].node;
             } else {
                 if (/^status: /.test(message)) {
                     type = "job-status";
@@ -46,8 +69,10 @@ function addToLogViewer(logViewer, lines, taskState) {
 
             logViewer.add({
                 id: logViewer.items.length,
+                ts: ts,
                 timestamp: ts.toLocaleDateString() + " " + ts.toLocaleTimeString(),
                 taskid: v11,
+                node: node,
                 message: message,
                 type: type
             });
@@ -59,25 +84,9 @@ function addToLogViewer(logViewer, lines, taskState) {
     logViewer.update();
 }
 
-function sortByTaskThenId(a, b, opt) {
+function sortById(a, b, opt) {
     a = a.values();
     b = b.values();
-
-    if (a["taskid"] === "" && b["taskid"] !== "") {
-        return -1;
-    }
-    if (a["taskid"] !== "" && b["taskid"] === "") {
-        return 1;
-    }
-
-    if (a["taskid"] !== "" && b["taskid"] !== "") {
-        if (a["taskid"] > b["taskid"]) {
-            return 1;
-        }
-        if (a["taskid"] < b["taskid"]) {
-            return -1;
-        }
-    }
 
     if (a["id"] > b["id"]) {
         return 1;
@@ -86,4 +95,110 @@ function sortByTaskThenId(a, b, opt) {
         return -1;
     }
     return 0;
+}
+
+function sortByTask(a, b, opt) {
+    var aa = a.values();
+    var bb = b.values();
+
+    if (aa["taskid"] === "" && bb["taskid"] !== "") {
+        return -1;
+    }
+    if (aa["taskid"] !== "" && bb["taskid"] === "") {
+        return 1;
+    }
+
+    if (aa["taskid"] !== "" && bb["taskid"] !== "") {
+        if (aa["taskid"] > bb["taskid"]) {
+            return 1;
+        }
+        if (aa["taskid"] < bb["taskid"]) {
+            return -1;
+        }
+    }
+
+    return sortById(a, b, opt);
+}
+
+function sortByNode(a, b, opt) {
+    var aa = a.values();
+    var bb = b.values();
+
+    if (aa["node"] === "" && bb["node"] !== "") {
+        return -1;
+    }
+    if (aa["node"] !== "" && bb["node"] === "") {
+        return 1;
+    }
+
+    if (aa["node"] !== "" && bb["node"] !== "") {
+        if (aa["node"] > bb["node"]) {
+            return 1;
+        }
+        if (aa["node"] < bb["node"]) {
+            return -1;
+        }
+    }
+
+    return sortById(a, b, opt);
+}
+
+
+function dumbPluralize(n, s, p) {
+    if (typeof p === 'undefined') {
+        p = "s";
+    }
+    if (n == 0 || n > 1) {
+        return (s + p);
+    } else {
+        return s;
+    }
+}
+
+function generateJobOverview(id, logViewer, taskState) {
+    var html = "";
+
+    var first = logViewer.items[1];
+    var last = logViewer.items[logViewer.items.length-1];
+
+    {
+        html += "<div>";
+        html += "Started at " + first.values().timestamp;
+
+        var duration = (last.values().ts.getTime() - first.values().ts.getTime()) / 1000;
+
+        var hours = 0;
+        var minutes = 0;
+        var seconds;
+
+        if (duration >= 3600) {
+            hours = Math.floor(duration / 3600);
+            duration -= (hours * 3600);
+        }
+        if (duration >= 60) {
+            minutes = Math.floor(duration / 60);
+            duration -= (minutes * 60);
+        }
+        seconds = duration;
+
+        var tcount = taskState.success_count + taskState.failure_count;
+
+        html += ".  " + tcount + dumbPluralize(tcount, " task") + " completed in ";
+        if (hours > 0) {
+            html += hours + dumbPluralize(hours, " hour");
+        }
+        if (minutes > 0) {
+            html += " " + minutes + dumbPluralize(minutes, " minute");
+        }
+        if (seconds > 0) {
+            html += " " + seconds + dumbPluralize(seconds, " second");
+        }
+        html += ".  " + taskState.success_count + dumbPluralize(taskState.success_count, " success", "es");
+        html += ", " + taskState.failure_count + dumbPluralize(taskState.failure_count, " failure");
+
+        html += ".  Completed at " + last.values().timestamp;
+        html += "</div>";
+    }
+
+    $(id).html(html);
 }
