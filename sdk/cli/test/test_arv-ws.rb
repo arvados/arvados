91cea2d82e477833d322b7267988c490ed284b62
radhika <radhika@curoverse.com>
2014-06-16 15:58:40 -0400

    2896: Add test file. Currently, the test only checks that arv-ws command is available and the options.

---
+++ sdk/cli/test/test_arv-ws.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+
+class TestArvWs < Minitest::Test
+  def setup
+  end
+
+  def test_arv_ws_get_help
+    out, err = capture_subprocess_io do
+      system ('arv-ws -h')
+    end
+    assert_equal '', err
+  end
+
+  def test_arv_ws_such_option
+    out, err = capture_subprocess_io do
+      system ('arv-ws --junk')
+    end
+    refute_equal '', err
+  end
+
+end
