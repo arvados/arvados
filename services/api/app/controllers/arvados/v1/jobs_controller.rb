b832c418442822700c684a31e04d5c095735ce3b
Brett Smith <brett@curoverse.com>
2014-06-19 13:24:47 -0400

    2879: Job Docker filters respect reader tokens.

--- services/api/app/controllers/arvados/v1/jobs_controller.rb
+++ services/api/app/controllers/arvados/v1/jobs_controller.rb
@@ -190,7 +190,9 @@ class Arvados::V1::JobsController < ApplicationController
         false
       when ["docker_image_locator", "in docker"], ["docker_image_locator", "not in docker"]
         filter[1].sub!(/ docker$/, '')
-        filter[2] = Collection.uuids_for_docker_image(*filter[2].split(':', 2))
+        image_search, image_tag = filter[2].split(':', 2)
+        filter[2] = Collection.
+          uuids_for_docker_image(image_search, image_tag, @read_users)
         true
       else
         true
