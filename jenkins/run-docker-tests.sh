2e7d59676bf6098d0c3f277e450ffd38a0bf938d
Ward Vandewege <ward@curoverse.com>
2014-07-01 11:41:17 -0400

    Add docker test script.
    no issue #

---
+++ jenkins/run-docker-tests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+EXITCODE=0
+
+COLUMNS=80
+
+title () {
+  printf "\n%*s\n\n" $(((${#title}+$COLUMNS)/2)) "********** $1 **********"
+}
+
+echo $WORKSPACE
+
+# clean up existing docker containers and images
+docker.io stop $(docker.io ps -a -q)
+docker.io rm $(docker.io ps -a -q)
+docker.io rmi $(docker.io images -q)
+
+# clean up build files so we can re-build
+rm -f $WORKSPACE/docker/*-image
+
+rm -f docker/config.yml
+
+# Get test config.yml file
+cp $HOME/docker/config.yml docker/
+
+# DOCS
+title "Starting docker build"
+cd "$WORKSPACE"
+cd docker
+./build.sh
+
+ECODE=$?
+
+if [[ "$ECODE" != "0" ]]; then
+  title "!!!!!! docker BUILD FAILED !!!!!!"
+  EXITCODE=$(($EXITCODE + $ECODE))
+fi
+
+title "docker build complete"
+
