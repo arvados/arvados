25e33b1caae27d60d8a915b708b431766d9eb7cd
Brett Smith <brett@curoverse.com>
2014-07-08 16:00:08 -0400

    Start API pipeline integration test.
    No issue #.  I wrote this while debugging the issue in the previous
    commit.

---
+++ services/api/test/integration/pipeline_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class PipelineTest < ActionDispatch::IntegrationTest
+  # These tests simulate the workflow of arv-run-pipeline-instance
+  # and other pipeline-running code.
+
+  def check_component_match(comp_key, comp_hash)
+    assert_response :success
+    built_json = json_response
+    built_component = built_json["components"][comp_key]
+    comp_hash.each_pair do |key, expected|
+      assert_equal(expected, built_component[key.to_s],
+                   "component's #{key} field changed")
+    end
+  end
+
+  test "creating a pipeline instance preserves required component parameters" do
+    comp_name = "test_component"
+    component = {
+      repository: "test_repo",
+      script: "test_script",
+      script_version: "test_refspec",
+      script_parameters: {},
+    }
+
+    post("/arvados/v1/pipeline_instances",
+         {pipeline_instance: {components: {comp_name => component}}.to_json},
+         auth(:active))
+    check_component_match(comp_name, component)
+    pi_uuid = json_response["uuid"]
+
+    @response = nil
+    get("/arvados/v1/pipeline_instances/#{pi_uuid}", {}, auth(:active))
+    check_component_match(comp_name, component)
+  end
+end
