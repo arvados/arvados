3270dae2ff541e5fd8fba9a89b9cfe07127f2800
Brett Smith <brett@curoverse.com>
2014-07-17 11:39:24 -0400

    2044: Workbench can query the API /permissions method.

--- apps/workbench/app/models/link.rb
+++ apps/workbench/app/models/link.rb
@@ -8,4 +8,14 @@ class Link < ArvadosBase
   def default_name
     self.class.resource_class_for_uuid(head_uuid).default_name rescue super
   end
+
+  def self.permissions_for(thing)
+    if thing.respond_to? :uuid
+      uuid = thing.uuid
+    else
+      uuid = thing
+    end
+    result = arvados_api_client.api("permissions", "/#{uuid}")
+    arvados_api_client.unpack_api_response(result)
+  end
 end
