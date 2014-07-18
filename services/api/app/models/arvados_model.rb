2f835cb83e5b495217831bdfc44bc6e4b01ec780
Brett Smith <brett@curoverse.com>
2014-07-18 11:09:41 -0400

    2044: Revert ff9323418d6a13da701be80aee9e3134e8aa6bab.
    This commit was not the correct approach.
    Non-managers should not be allowed to see sharing information for
    projects they can read.

--- services/api/app/models/arvados_model.rb
+++ services/api/app/models/arvados_model.rb
@@ -130,11 +130,11 @@ class ArvadosModel < ActiveRecord::Base
       sql_params += [uuid_list, user_uuids]
 
       if sql_table == "links" and users_list.any?
-        # This row is a 'permission' or 'resources' link class that
-        # references a member of users_list or a group readable by
-        # those users.
+        # This row is a 'permission' or 'resources' link class
+        # The uuid for a member of users_list is referenced in either the head
+        # or tail of the link
         sql_conds += ["(#{sql_table}.link_class in (#{sanitize 'permission'}, #{sanitize 'resources'}) AND (#{sql_table}.head_uuid IN (?) OR #{sql_table}.tail_uuid IN (?)))"]
-        sql_params += [uuid_list, uuid_list]
+        sql_params += [user_uuids, user_uuids]
       end
 
       if sql_table == "logs" and users_list.any?
