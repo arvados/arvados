6a2406f04875497a7bfad72fc8bcd73b792fefae
Tom Clegg <tom@curoverse.com>
2014-07-18 02:43:17 -0400

    3170: On pipeline instance page, refresh the tab panes rather than the entire page.

--- apps/workbench/app/assets/javascripts/tab_panes.js
+++ apps/workbench/app/assets/javascripts/tab_panes.js
@@ -10,7 +10,20 @@ $(document).on('shown.bs.tab', '[data-toggle="tab"]', function(e) {
         });
 });
 
-$(document).on('arv-log-event', function() {
-    $('[data-pane-content-url]').removeClass('loaded');
-    $('.tab-pane.active').trigger('shown.bs.tab');
+$(document).on('arv:pane:reload', '.tab-pane', function() {
+    // Unload a single pane. Reload it if it's active.
+    $(this).removeClass('loaded');
+    if ($(this).hasClass('active')) {
+        $('[href=#' + $(this).attr('id') + ']').trigger('shown.bs.tab');
+    } else {
+        // When the user selects this tab, show a spinner instead of
+        // old content while loading.
+        $('> div > div', this).
+            html('<div class="spinner spinner-32px spinner-h-center"></div>');
+    }
+});
+
+$(document).on('arv-log-event arv:pane:reload:all', function() {
+    // Reload all panes (except ones that haven't even loaded yet).
+    $('.tab-pane.loaded').trigger('arv:pane:reload');
 });
