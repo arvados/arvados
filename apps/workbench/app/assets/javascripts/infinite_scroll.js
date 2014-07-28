4ad1947da490a998d68380b1520c4f95b1784676
Tom Clegg <tom@curoverse.com>
2014-07-28 17:52:51 -0400

    3235: Fix infinite scroll broken by merge.

--- apps/workbench/app/assets/javascripts/infinite_scroll.js
+++ apps/workbench/app/assets/javascripts/infinite_scroll.js
@@ -1,25 +1,23 @@
-function maybe_load_more_content() {
+function maybe_load_more_content(event) {
     var scroller = this;        // element with scroll bars
-    var container;              // element that receives new content
+    var $container;             // element that receives new content
     var src;                    // url for retrieving content
     var scrollHeight;
     var spinner, colspan;
     var serial = Date.now();
     scrollHeight = scroller.scrollHeight || $('body')[0].scrollHeight;
-    var num_scrollers = $(window).data("arv-num-scrollers");
     if ($(scroller).scrollTop() + $(scroller).height()
         >
         scrollHeight - 50)
     {
-      for (var i = 0; i < num_scrollers; i++) {
-        $container = $($(this).data('infinite-container'+i));
-        if (!$(container).attr('data-infinite-content-href0')) {
+        $container = $(event.data.container);
+        if (!$container.attr('data-infinite-content-href0')) {
             // Remember the first page source url, so we can refresh
             // from page 1 later.
-            $(container).attr('data-infinite-content-href0',
-                              $(container).attr('data-infinite-content-href'));
+            $container.attr('data-infinite-content-href0',
+                            $container.attr('data-infinite-content-href'));
         }
-        src = $(container).attr('data-infinite-content-href');
+        src = $container.attr('data-infinite-content-href');
         if (!src || !$container.is(':visible'))
             // Finished
             return;
@@ -27,7 +25,7 @@ function maybe_load_more_content() {
         // Don't start another request until this one finishes
         $container.attr('data-infinite-content-href', null);
         spinner = '<div class="spinner spinner-32px spinner-h-center"></div>';
-        if ($(container).is('table,tbody,thead,tfoot')) {
+        if ($container.is('table,tbody,thead,tfoot')) {
             // Hack to determine how many columns a new tr should have
             // in order to reach full width.
             colspan = $container.closest('table').
@@ -38,17 +36,18 @@ function maybe_load_more_content() {
                        spinner +
                        '</td></tr>');
         }
-        $(container).find(".spinner").detach();
-        $(container).append(spinner);
-        $(container).attr('data-infinite-serial', serial);
+        $container.find(".spinner").detach();
+        $container.append(spinner);
+        $container.attr('data-infinite-serial', serial);
         $.ajax(src,
                {dataType: 'json',
                 type: 'GET',
-                data: ($(container).data('infinite-content-params') || {}),
-                context: {container: container, src: src, serial: serial}}).
+                data: ($container.data('infinite-content-params') || {}),
+                context: {container: $container, src: src, serial: serial}}).
             fail(function(jqxhr, status, error) {
                 var $faildiv;
-                if ($(this.container).attr('data-infinite-serial') != this.serial) {
+                var $container = this.container;
+                if ($container.attr('data-infinite-serial') != this.serial) {
                     // A newer request is already in progress.
                     return;
                 }
@@ -65,20 +64,18 @@ function maybe_load_more_content() {
                     attr('data-infinite-content-href', this.src).
                     addClass('infinite-retry').
                     append('<span class="fa fa-warning" /> Oops, request failed. <button class="btn btn-xs btn-primary">Retry</button>');
-                $(this.container).find('div.spinner').replaceWith($faildiv);
+                $container.find('div.spinner').replaceWith($faildiv);
             }).
             done(function(data, status, jqxhr) {
-                if ($(this.container).attr('data-infinite-serial') != this.serial) {
+                if ($container.attr('data-infinite-serial') != this.serial) {
                     // A newer request is already in progress.
                     return;
                 }
-                $(this.container).find(".spinner").detach();
-                $(this.container).append(data.content);
-                $(this.container).attr('data-infinite-content-href', data.next_page_href);
+                $container.find(".spinner").detach();
+                $container.append(data.content);
+                $container.attr('data-infinite-content-href', data.next_page_href);
             });
-        break;
      }
-   }
 }
 
 $(document).
@@ -103,17 +100,17 @@ $(document).
             trigger('scroll');
     }).
     on('ready ajax:complete', function() {
-        var num_scrollers = 0;
         $('[data-infinite-scroller]').each(function() {
+            if ($(this).hasClass('infinite-scroller-ready'))
+                return;
+            $(this).addClass('infinite-scroller-ready');
+
             var $scroller = $($(this).attr('data-infinite-scroller'));
             if (!$scroller.hasClass('smart-scroll') &&
                 'scroll' != $scroller.css('overflow-y'))
                 $scroller = $(window);
             $scroller.
                 addClass('infinite-scroller').
-                data('infinite-container'+num_scrollers, this).
-                on('scroll', maybe_load_more_content);
-            num_scrollers++;
+                on('scroll', { container: this }, maybe_load_more_content);
         });
-        $(window).data("arv-num-scrollers", num_scrollers);
     });
