6f231c2537c196644f15eb7cac6f6861ea24e429
Peter Amstutz <peter.amstutz@curoverse.com>
2014-08-08 15:44:51 -0400

    3373: decompress-all: cleaned up code a bit.  Fail properly with error message on bad input.

--- crunch_scripts/decompress-all.py
+++ crunch_scripts/decompress-all.py
@@ -17,6 +17,7 @@ import arvados
 import re
 import subprocess
 import os
+import sys
 
 arvados.job_setup.one_task_per_input_file(if_sequence=0, and_end_task=True,
                                           input_as_path=True)
@@ -25,27 +26,31 @@ task = arvados.current_task()
 
 input_file = task['parameters']['input']
 
-result = re.match(r"(^[a-f0-9]{32}\+\d+)(\+\S+)*(/.*)(/[^/]+)$", input_file)
+infile_parts = re.match(r"(^[a-f0-9]{32}\+\d+)(\+\S+)*(/.*)?(/[^/]+)$", input_file)
 
 outdir = os.path.join(task.tmpdir, "output")
 os.makedirs(outdir)
 os.chdir(outdir)
 
-if result != None:
-    cr = arvados.CollectionReader(result.group(1))
-    streamname = result.group(3)[1:]
-    filename = result.group(4)[1:]
+if infile_parts == None:
+    print >>sys.stderr, "Failed to parse input filename '%s' as a Keep file\n" % input_file
+    sys.exit(1)
 
+cr = arvados.CollectionReader(infile_parts.group(1))
+streamname = infile_parts.group(3)[1:]
+filename = infile_parts.group(4)[1:]
+
+if streamname != None:
     subprocess.call(["mkdir", "-p", streamname])
     os.chdir(streamname)
-    streamreader = filter(lambda s: s.name() == streamname, cr.all_streams())[0]
-    filereader = streamreader.files()[filename]
-    rc = subprocess.call(["dtrx", "-r", "-n", "-q", arvados.get_task_param_mount('input')])
-    if rc == 0:
-        out = arvados.CollectionWriter()
-        out.write_directory_tree(outdir, max_manifest_depth=0)
-        task.set_output(out.finish())
-    else:
-        task.set_output(streamname + filereader.as_manifest()[1:])
 else:
-    sys.exit(1)
+    streamname = '.'
+streamreader = filter(lambda s: s.name() == streamname, cr.all_streams())[0]
+filereader = streamreader.files()[filename]
+rc = subprocess.call(["dtrx", "-r", "-n", "-q", arvados.get_task_param_mount('input')])
+if rc == 0:
+    out = arvados.CollectionWriter()
+    out.write_directory_tree(outdir, max_manifest_depth=0)
+    task.set_output(out.finish())
+else:
+    task.set_output(streamname + filereader.as_manifest()[1:])
