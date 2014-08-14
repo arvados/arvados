f8b650c589de9aa6a92c93089050fffc6aca7fab
radhika <radhika@curoverse.com>
2014-08-14 15:12:17 -0400

    3112: log when an issue is reported

--- apps/workbench/app/controllers/actions_controller.rb
+++ apps/workbench/app/controllers/actions_controller.rb
@@ -186,6 +186,9 @@ class ActionsController < ApplicationController
   end
 
   def report_issue
+    puts "Issue being reported with these parameters: #{params.inspect}"
+    logger.warn "Issue being reported with these parameters: #{params.inspect}"
+
     respond_to do |format|
       IssueReporter.send_report(current_user, params).deliver
       format.js
