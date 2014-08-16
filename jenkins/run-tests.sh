06f6b31925a51c5100017c4a05bba430d29252a4
Tom Clegg <tom@curoverse.com>
2014-08-16 02:00:23 -0400

    Cleanup, add some error checks, add --skip and --only args.

--- jenkins/run-tests.sh
+++ jenkins/run-tests.sh
@@ -1,227 +1,276 @@
 #!/bin/bash
 
-EXITCODE=0
+# Install and test Arvados components.
+#
+# Exit non-zero if any tests fail.
+#
+# Arguments:
+# --skip FOO   Do not test the FOO component.
+# --only FOO   Do not test anything except the FOO component.
+#
+# Regardless of which components are tested, install all components in
+# the usual sequence. (Many test suites depend on other components
+# being installed.)
 
 COLUMNS=80
 
 ARVADOS_API_HOST=qr1hi.arvadosapi.com
 
-title () {
-  printf "\n%*s\n\n" $(((${#title}+$COLUMNS)/2)) "********** $1 **********"
-}
+export GOPATH=$(mktemp -d)
+VENVDIR=$(mktemp -d)
 
 source /etc/profile.d/rvm.sh
-echo $WORKSPACE
 
-export GOPATH=$(mktemp -d)
+fatal() {
+    clear_temp
+    echo >&2 "Fatal: $* in ${FUNCNAME[1]} at ${BASH_SOURCE[1]} line ${BASH_LINENO[0]}"
+    exit 1
+}
+
+# Sanity check
+echo "WORKSPACE=$WORKSPACE"
+[[ -n "$WORKSPACE" ]] || fatal "WORKSPACE not set"
+
+# Set up temporary install dirs
 mkdir -p "$GOPATH/src/git.curoverse.com"
-ln -sfn "$WORKSPACE" "$GOPATH/src/git.curoverse.com/arvados.git"
+ln -sfn "$WORKSPACE" "$GOPATH/src/git.curoverse.com/arvados.git" \
+    || fatal "symlink failed"
 
-VENVDIR=$(mktemp -d)
-virtualenv --setuptools "$VENVDIR"
+virtualenv --setuptools "$VENVDIR" || fatal "virtualenv $VENVDIR failed"
 PATH="$VENVDIR/bin:$PATH"
 
-# DOCS
-title "Starting DOC build"
-cd "$WORKSPACE"
-cd doc
-bundle install --deployment
-rm -rf .site
-# Make sure python-epydoc is installed or the next line won't do much good!
-PYTHONPATH=$WORKSPACE/sdk/python/ bundle exec rake generate baseurl=file://$WORKSPACE/doc/.site/ arvados_workbench_host=workbench.$ARVADOS_API_HOST arvados_api_host=$ARVADOS_API_HOST
+declare -a failures
+declare -A skip
+
+# Always skip CLI tests. They don't know how to use run_test_server.py.
+skip[cli]=1
+
+while [[ -n "$1" ]]
+do
+    arg="$1"; shift
+    case "$arg" in
+        --skip)
+            skipwhat="$1"; shift
+            skip[$skipwhat]=1
+            ;;
+        --only)
+            only="$1"; shift
+            ;;
+        *)
+            echo >&2 "$0: Unrecognized option: '$arg'"
+            exit 1
+            ;;
+    esac
+done
 
 checkexit() {
-    ECODE=$?
-
-    if [[ "$ECODE" != "0" ]]; then
+    if [[ "$?" != "0" ]]; then
         title "!!!!!! $1 FAILED !!!!!!"
-        EXITCODE=$(($EXITCODE + $ECODE))
+        failures+=("$1")
+    else
+        successes+=("$1")
     fi
 }
 
-goget() {
-    go get -t "git.curoverse.com/arvados.git/$1"
+do_test() {
+    if [[ -z "${skip[$1]}" ]] && ( [[ -z "$only" ]] || [[ "$only" == "$1" ]] )
+    then
+        title "Running $1 tests"
+        if [[ "$2" == "go" ]]
+        then
+            go test "git.curoverse.com/arvados.git/$1"
+        else
+            "test_$1"
+        fi
+        checkexit "$1 tests"
+        title "End of $1 tests"
+    else
+        title "Skipping $1 tests"
+    fi
+}
+
+do_install() {
+    title "Running $1 install"
+    if [[ "$2" == "go" ]]
+    then
+        go get -t "git.curoverse.com/arvados.git/$1"
+    else
+        "install_$1"
+    fi
     checkexit "$1 install"
+    title "End of $1 install"
 }
 
-gotest() {
-    title "Starting $1 tests"
-    cd "$WORKSPACE"
-
-    go test "git.curoverse.com/arvados.git/$1"
-
-    checkexit "$1 tests"
-    title "$1 tests complete"
+title () {
+    txt="********** $1 **********"
+    printf "\n%*s%s\n\n" $((($COLUMNS-${#txt})/2)) "" "$txt"
 }
 
-checkexit "Doc build"
-title "DOC build complete"
+clear_temp() {
+    for t in "$VENVDIR" "$GOPATH"
+    do
+        if [[ -n "$t" ]]
+        then
+            rm -rf "$t"
+        fi
+    done
+}
 
-# DOC linkchecker
-title "Starting DOC linkchecker"
-cd "$WORKSPACE"
-cd doc
-bundle exec rake linkchecker baseurl=file://$WORKSPACE/doc/.site/
+install_docs() {
+    cd "$WORKSPACE/doc"
+    bundle install --deployment
+    rm -rf .site
+    # Make sure python-epydoc is installed or the next line won't do much good!
+    PYTHONPATH=$WORKSPACE/sdk/python/ bundle exec rake generate baseurl=file://$WORKSPACE/doc/.site/ arvados_workbench_host=workbench.$ARVADOS_API_HOST arvados_api_host=$ARVADOS_API_HOST
+}
+do_install docs
 
-checkexit "Doc linkchecker"
-title "DOC linkchecker complete"
+test_doclinkchecker() {
+    cd "$WORKSPACE/doc"
+    bundle exec rake linkchecker baseurl=file://$WORKSPACE/doc/.site/
+}
+do_test doclinkchecker
 
-# API SERVER
-title "Starting API server tests"
-cd "$WORKSPACE"
-cd services/api
-bundle install --deployment
+install_apiserver() {
+    cd "$WORKSPACE/services/api"
+    bundle install --deployment
 
-rm -f config/database.yml
-rm -f config/environments/test.rb
-cp config/environments/test.rb.example config/environments/test.rb
+    rm -f config/environments/test.rb
+    cp config/environments/test.rb.example config/environments/test.rb
 
-# Get test database config
-cp $HOME/arvados-api-server/database.yml config/
-# Get test application.yml file
-cp $HOME/arvados-api-server/application.yml config/
+    cp $HOME/arvados-api-server/database.yml config/ || fatal "database.yml"
+    cp $HOME/arvados-api-server/application.yml config/ || fatal "application.yml"
 
-# Fill in a random secret_token and blob_signing_key for testing
-SECRET_TOKEN=`echo 'puts rand(2**512).to_s(36)' |ruby`
-BLOB_SIGNING_KEY=`echo 'puts rand(2**512).to_s(36)' |ruby`
+    # Fill in a random secret_token and blob_signing_key for testing
+    SECRET_TOKEN=`echo 'puts rand(2**512).to_s(36)' |ruby`
+    BLOB_SIGNING_KEY=`echo 'puts rand(2**512).to_s(36)' |ruby`
 
-sed -i'' -e "s:SECRET_TOKEN:$SECRET_TOKEN:" config/application.yml
-sed -i'' -e "s:BLOB_SIGNING_KEY:$BLOB_SIGNING_KEY:" config/application.yml
+    sed -i'' -e "s:SECRET_TOKEN:$SECRET_TOKEN:" config/application.yml
+    sed -i'' -e "s:BLOB_SIGNING_KEY:$BLOB_SIGNING_KEY:" config/application.yml
 
-export RAILS_ENV=test
+    export RAILS_ENV=test
 
-# Set up empty git repo (for git tests)
-GITDIR=$WORKSPACE/tmpgit
-rm -rf $GITDIR
-mkdir $GITDIR
-sed -i'' -e "s:/var/cache/git:$GITDIR:" config/application.default.yml
+    # Set up empty git repo (for git tests)
+    GITDIR="$WORKSPACE/tmpgit"
+    sed -i'' -e "s:/var/cache/git:$GITDIR:" config/application.default.yml
 
-rm -rf $GITDIR
-mkdir -p $GITDIR/test
-cd $GITDIR/test
-/usr/bin/git init
-/usr/bin/git config user.email "jenkins@ci.curoverse.com"
-/usr/bin/git config user.name "Jenkins, CI"
-touch tmp
-/usr/bin/git add tmp
-/usr/bin/git commit -m 'initial commit'
+    rm -rf $GITDIR
+    mkdir -p $GITDIR/test
+    cd $GITDIR/test \
+        && git init \
+        && git config user.email "jenkins@ci.curoverse.com" \
+        && git config user.name "Jenkins, CI" \
+        && touch tmp \
+        && git add tmp \
+        && git commit -m 'initial commit'
 
-cd "$WORKSPACE"
-cd services/api
+    cd "$WORKSPACE/services/api" \
+        && bundle exec rake db:drop \
+        && bundle exec rake db:create \
+        && bundle exec rake db:setup
+}
+do_install apiserver
 
-bundle exec rake db:drop
-bundle exec rake db:create
-bundle exec rake db:setup
-bundle exec rake test
+test_apiserver() {
+    cd "$WORKSPACE/services/api"
+    bundle exec rake test
+}
+do_test apiserver
 
-checkexit "API server tests"
-title "API server tests complete"
+install_cli() {
+    cd "$WORKSPACE/sdk/cli"
+    bundle install --deployment
+}
+do_install cli
 
-# Install CLI gem's dependencies.
-
-cd "$WORKSPACE/sdk/cli"
-bundle install --deployment
-
-cd "$WORKSPACE"
-gostuff="services/keepstore services/keepproxy sdk/go/arvadosclient sdk/go/keepclient sdk/go/streamer"
-for dir in $gostuff
+declare -a gostuff
+gostuff=(
+    services/keepstore
+    services/keepproxy
+    sdk/go/arvadosclient
+    sdk/go/keepclient
+    sdk/go/streamer
+    )
+for g in "${gostuff[@]}"
 do
-  goget "$dir"
+    do_install "$g" go
 done
 
-# Install the Python SDK early. Various other test suites (like
-# keepproxy) bring up run_test_server.py, which imports the arvados
-# module. We can't actually *test* the Python SDK yet though, because
-# its own test suite brings up some of those other programs (like
-# keepproxy).
+install_python_sdk() {
+    # Install the Python SDK early. Various other test suites (like
+    # keepproxy) bring up run_test_server.py, which imports the arvados
+    # module. We can't actually *test* the Python SDK yet though, because
+    # its own test suite brings up some of those other programs (like
+    # keepproxy).
 
-cd "$WORKSPACE/sdk/python"
-python setup.py egg_info -b ".$(git log --format=format:%ct.%h -n1 .)" sdist rotate --keep=1 --match .tar.gz
-pip install dist/arvados-python-client-0.1.*.tar.gz
+    cd "$WORKSPACE/sdk/python" \
+        && python setup.py egg_info -b ".$(git log --format=format:%ct.%h -n1 .)" sdist rotate --keep=1 --match .tar.gz \
+        && pip install dist/arvados-python-client-0.1.*.tar.gz
+}
+do_install python_sdk
 
-checkexit "Python SDK install"
+test_fuse() {
+    cd "$WORKSPACE/services/fuse" \
+        && python setup.py egg_info -b ".$(git log --format=format:%ct.%h -n1 .)" sdist rotate --keep=1 --match .tar.gz \
+        && pip install dist/arvados_fuse-0.1.*.tar.gz
+}
+do_test fuse
 
-cd "$WORKSPACE/services/fuse"
-python setup.py egg_info -b ".$(git log --format=format:%ct.%h -n1 .)" sdist rotate --keep=1 --match .tar.gz
-pip install dist/arvados_fuse-0.1.*.tar.gz
+test_python_sdk() {
+    # Python SDK. We test this before testing keepproxy: keepproxy runs
+    # run_test_server.py, which depends on the yaml package, which is in
+    # tests_require but not install_requires, and therefore does not get
+    # installed by setuptools until we run "setup.py test" *and* install
+    # the .egg files that setup.py downloads.
 
-checkexit "FUSE install"
+    cd "$WORKSPACE/sdk/python" \
+        && python setup.py test \
+        && easy_install *.egg
+}
+do_test python_sdk
 
-# Python SDK. We test this before testing keepproxy: keepproxy runs
-# run_test_server.py, which depends on the yaml package, which is in
-# tests_require but not install_requires, and therefore does not get
-# installed by setuptools until we run "setup.py test" *and* install
-# the .egg files that setup.py downloads.
+install_fuse() {
+    # Install test dependencies here too, in case run_test_server needs them.
+    cd "$WORKSPACE/services/fuse" \
+        && python setup.py test \
+        && easy_install *.egg
+}
 
-title "Starting Python SDK tests"
-cd "$WORKSPACE/sdk/python"
-
-python setup.py test
-checkexit "Python SDK tests"
-easy_install *.egg
-
-title "Python tests complete"
-
-# FUSE driver
-
-title "Starting FUSE tests"
-cd "$WORKSPACE/services/fuse"
-
-python setup.py test
-checkexit "FUSE tests"
-easy_install *.egg
-
-title "FUSE tests complete"
-
-
-# Go SDK packages
-
-for dir in $gostuff
+for g in "${gostuff[@]}"
 do
-  gotest "$dir"
+    do_test "$g" go
 done
 
-# Workbench
-title "Starting workbench tests"
-cd "$WORKSPACE"
-cd apps/workbench
-bundle install --deployment
+install_workbench() {
+    cd "$WORKSPACE/apps/workbench" \
+        && bundle install --deployment \
+        && bundle exec rake test
+}
 
-echo $PATH
+test_cli() {
+    title "Starting SDK CLI tests"
+    cd "$WORKSPACE/sdk/cli" \
+        && bundle install --deployment \
+        && mkdir -p /tmp/keep \
+        && KEEP_LOCAL_STORE=/tmp/keep bundle exec rake test
+}
+do_test cli
 
-bundle exec rake test
+clear_temp
 
-checkexit "Workbench tests"
-title "Workbench tests complete"
+for x in "${successes[@]}"
+do
+    echo "Pass: $x"
+done
 
-# Clean up temporary virtualenv and GOPATH
-rm -rf "$VENVDIR"
-rm -rf "$GOPATH"
-
-# The CLI SDK tests require a working API server, so let's skip those for now.
-exit $EXITCODE
-
-########################################################################
-
-# CLI SDK
-title "Starting SDK CLI tests"
-cd "$WORKSPACE"
-cd sdk/cli
-bundle install --deployment
-
-# Set up Python SDK and dependencies
-cd ../python
-rm -rf $HOME/lib/python
-mkdir $HOME/lib/python
-PYTHONPATH="$HOME/lib/python" easy_install --install-dir=$HOME/lib/python --upgrade google-api-python-client
-PYTHONPATH="$HOME/lib/python" python setup.py install --home=$HOME
-
-cd ../cli
-mkdir -p /tmp/keep
-export KEEP_LOCAL_STORE=/tmp/keep
-PYTHONPATH="$HOME/lib/python" bundle exec rake test
-
-checkexit "SDK CLI tests"
-title "SDK CLI tests complete"
-
-exit $EXITCODE
+if [[ ${#failures[@]} == 0 ]]
+then
+    echo "All test suites passed."
+else
+    echo "Failures (${#failures[@]}):"
+    for x in "${failures[@]}"
+    do
+        echo "Fail: $x"
+    done
+fi
+exit ${#failures}
