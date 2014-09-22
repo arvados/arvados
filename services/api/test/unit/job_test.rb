96e711607984bd74a682dd3f3819dbc453a6591b
radhika <radhika@curoverse.com>
2014-09-22 15:56:07 -0400

    3898: Update tests to match the updated job status handling.

--- services/api/test/unit/job_test.rb
+++ services/api/test/unit/job_test.rb
@@ -158,26 +158,24 @@ class JobTest < ActiveSupport::TestCase
   [
     # Each test case is of the following format
     # Array of parameters where each parameter is of the format:
-    #     attr name to be changed, attr value, and array of expectations (where each expectation is an array) OR the string "error"
+    #  attr name to be changed, attr value, and array of expectations (where each expectation is an array)
     [['running', false, [['state', 'Queued']]]],
-    [['state', 'Running', 'error']],  # is_locked_by_uuid is not set
+    [['state', 'Running', [['started_at', 'not_nil']]]],
     [['is_locked_by_uuid', 'use_current_user_uuid', [['state', 'Queued']]], ['state', 'Running', [['running', true], ['started_at', 'not_nil'], ['success', 'nil']]]],
     [['running', false, [['state', 'Queued']]], ['state', 'Complete', [['success', true]]]],
-    [['running', true, [['state', 'Running']]], ['cancelled_at', Time.now, [['state', 'Cancelled'],['running', false],['started_at', 'not_nil']]]],
-    [['running', true, [['state', 'Running']]], ['state', 'Cancelled', [['running', false],['cancelled_at', 'not_nil'],['started_at', 'not_nil']]]],
-    [['running', true, [['state', 'Running']]], ['success', true, [['state', 'Complete'],['running', false],['finished_at', 'not_nil']]]],
-    [['running', true, [['state', 'Running']]], ['success', false, [['state', 'Failed'],['running', false],['finished_at', 'not_nil']]]],
-    [['running', true, [['state', 'Running']]], ['state', 'Complete', [['success', true],['running', false],['finished_at', 'not_nil']]]],
-    [['running', true, [['state', 'Running']]], ['state', 'Failed', [['success', false],['running', false],['finished_at', 'not_nil']]]],
-    [['running', true, [['state', 'Running'], ['started_at', 'not_nil']]], ['running', false, [['state', 'Queued']]]],
-    [['cancelled_at', Time.now, [['state', 'Cancelled'],['running', false]]], ['success', false, [['state', 'Cancelled'],['running', false],['finished_at', 'nil'], ['cancelled_at', 'not_nil']]]],
+    [['running', true, [['state', 'Running']]], ['cancelled_at', Time.now, [['state', 'Cancelled']]]],
+    [['running', true, [['state', 'Running']]], ['state', 'Cancelled', [['cancelled_at', 'not_nil']]]],
+    [['running', true, [['state', 'Running']]], ['success', true, [['state', 'Complete']]]],
+    [['running', true, [['state', 'Running']]], ['success', false, [['state', 'Failed']]]],
+    [['running', true, [['state', 'Running']]], ['state', 'Complete', [['success', true],['finished_at', 'not_nil']]]],
+    [['running', true, [['state', 'Running']]], ['state', 'Failed', [['success', false],['finished_at', 'not_nil']]]],
+    [['running', true, [['state', 'Running']]], ['running', false, [['state', 'Queued']]]],
+    [['cancelled_at', Time.now, [['state', 'Cancelled']]], ['success', false, [['state', 'Cancelled'],['finished_at', 'nil'], ['cancelled_at', 'not_nil']]]],
     [['cancelled_at', Time.now, [['state', 'Cancelled'],['running', false]]], ['success', true, [['state', 'Cancelled'],['running', false],['finished_at', 'nil'],['cancelled_at', 'not_nil']]]],
     # potential migration cases
     [['state', nil, [['state', 'Queued']]]],
     [['state', nil, [['state', 'Queued']]], ['cancelled_at', Time.now, [['state', 'Cancelled']]]],
-    [['running', true, [['state', 'Running'], ['started_at', 'not_nil']]], ['state', nil, [['state', 'Running']]]],
-    # bogus initial status (started_at but not running), to produce error while setting state
-    [['started_at', Time.now, [['state', 'Queued']]], ['state', nil, 'error']],
+    [['running', true, [['state', 'Running']]], ['state', nil, [['state', 'Running']]]],
   ].each do |parameters|
     test "verify job status #{parameters}" do
       job = Job.create! job_attrs
@@ -202,16 +200,8 @@ class JobTest < ActiveSupport::TestCase
               assert_equal expectation[1], job[expectation[0]], expectation[0]
             end
           end
-        else # String expectation, looking for error
-          if expectations == 'error'
-            assert_raise ActiveRecord::RecordInvalid do
-              job[parameter[0]] = parameter[1]
-              assert job.valid?, job.errors.full_messages.to_s
-              job.save!
-            end
-          else
-            raise 'I do not know how to handle this expectation'
-          end
+        else
+          raise 'I do not know how to handle this expectation'
         end
       end
     end
