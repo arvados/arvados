34e461fe9d33283d8e2135802ee9accb30a5e34f
Tim Pierce <twp@curoverse.com>
2014-10-01 13:50:53 -0400

    3782: bugfix and added integration test
    * Fixed bug where log viewer would always request partial log and receive
      206, even when total log size is shorter than log_viewer_max_bytes.
    * Added integration tests:
    ** set up KEEP_LOCAL_STORE with dummy log
    ** test retrieving full job log
    ** test retrieving partial job log

--- apps/workbench/test/integration/jobs_test.rb
+++ apps/workbench/test/integration/jobs_test.rb
@@ -1,6 +1,33 @@
+require 'fileutils'
+require 'tmpdir'
+
 require 'integration_helper'
 
 class JobsTest < ActionDispatch::IntegrationTest
+
+  def setup
+    # Set up KEEP_LOCAL_STORE with a file that satisfies
+    # the log collection for job 'job_with_real_log'
+    # TODO: figure out a better way to store this test data
+    # (e.g. in a dummy test fixture)
+    #
+    ENV['KEEP_LOCAL_STORE'] ||= Dir.mktmpdir
+    keepdir = ENV['KEEP_LOCAL_STORE']
+    open(File.join(keepdir, 'b9f7e25b33844d816d8531686f8bf360'), 'w') do |f|
+      f.write("2014-01-01_12:00:01 zzzzz-8i9sb-abcdefghijklmno 0  log message 1\n")
+      f.write("2014-01-01_12:00:02 zzzzz-8i9sb-abcdefghijklmno 0  log message 2\n")
+      f.write("2014-01-01_12:00:03 zzzzz-8i9sb-abcdefghijklmno 0  log message 3\n")
+    end
+
+    @log_viewer_max_bytes = Rails.configuration.log_viewer_max_bytes
+  end
+
+  def teardown
+    keepdir = ENV.delete 'KEEP_LOCAL_STORE'
+    FileUtils.rm_rf(keepdir) if keepdir
+    Rails.configuration.log_viewer_max_bytes = @log_viewer_max_bytes
+  end
+
   test "add job description" do
     Capybara.current_driver = Capybara.javascript_driver
     visit page_with_token("active", "/jobs")
@@ -24,5 +51,34 @@ class JobsTest < ActionDispatch::IntegrationTest
     assert page.has_link? 'Go to dashboard'
     click_link 'Go to dashboard'
     assert page.has_text? 'Active pipelines'
+  end
+
+  test "view job log" do
+    Capybara.current_driver = Capybara.javascript_driver
+    job = api_fixture('jobs')['job_with_real_log']
+
+    visit page_with_token("active", "/jobs/#{job['uuid']}")
+    assert page.has_text? job['script_version']
+
+    click_link 'Log'
+    wait_for_ajax
+    assert page.has_text? 'Started at'
+    assert page.has_text? 'Finished at'
+    assert page.has_text? 'log message 1'
+    assert page.has_text? 'log message 2'
+    assert page.has_text? 'log message 3'
+  end
+
+  test 'view partial job log' do
+    Capybara.current_driver = Capybara.javascript_driver
+    Rails.configuration.log_viewer_max_bytes = 100
+    job = api_fixture('jobs')['job_with_real_log']
+
+    visit page_with_token("active", "/jobs/#{job['uuid']}")
+    assert page.has_text? job['script_version']
+
+    click_link 'Log'
+    wait_for_ajax
+    assert page.has_text? 'Showing only 100 bytes of this log'
   end
 end
