7509b9e08acebd9e28ab2cea7d8b2e383c46859c
Peter Amstutz <peter.amstutz@curoverse.com>
2014-10-15 16:20:04 -0400

    3609: Fix schema so 'order' parameter of 'list' is an array instead of a string.

--- services/api/app/controllers/arvados/v1/schema_controller.rb
+++ services/api/app/controllers/arvados/v1/schema_controller.rb
@@ -230,8 +230,8 @@ class Arvados::V1::SchemaController < ApplicationController
                   location: "query"
                 },
                 order: {
-                  type: "string",
-                  description: "Order in which to return matching #{k.to_s.underscore.pluralize}.",
+                  type: "array",
+                  description: "Fields to use to determine order for returning #{k.to_s.underscore.pluralize} object matches.",
                   location: "query"
                 },
                 select: {
