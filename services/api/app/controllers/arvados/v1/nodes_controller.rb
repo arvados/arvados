91738b3c2331a102e4aefd4be580e0e979bc514f
Brett Smith <brett@curoverse.com>
2014-10-21 10:58:59 -0400

    4139: Remove cloud node setup code from API server.
    This code has never been used in production, and its functionality has
    officially been assumed by Node Manager.

--- services/api/app/controllers/arvados/v1/nodes_controller.rb
+++ services/api/app/controllers/arvados/v1/nodes_controller.rb
@@ -4,10 +4,8 @@ class Arvados::V1::NodesController < ApplicationController
   skip_before_filter :render_404_if_no_object, :only => :ping
 
   def create
-    @object = Node.new
-    @object.save!
-    @object.start!(lambda { |h| ping_arvados_v1_node_url(h) })
-    show
+    @attrs = {}
+    super
   end
 
   def update
