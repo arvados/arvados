b629c24e4dc15257ae8d70f63d037c12668a491a
Tim Pierce <twp@curoverse.com>
2014-12-04 10:50:44 -0500

    4465: added api_version_text helper.
    Per code review: the source_version returned in the discovery document
    may include the string "-modified" if the API server is running from a
    locally modified repository.  The api_version_link that we generate for
    this version must take that into account.

--- apps/workbench/app/helpers/version_helper.rb
+++ apps/workbench/app/helpers/version_helper.rb
@@ -1,9 +1,21 @@
 module VersionHelper
   include ArvadosApiClientHelper
 
+  # api_version returns the git commit hash for the API server's current version.
+  # It is extracted from api_version_text, which returns the source_version provided
+  # by the discovery document and may have the word "-modified" appended to it (if
+  # the API server is running from a locally modified repository).
+
   def api_version()
+    api_version_text().sub(/[^[:xdigit:]].*/, '')
+  end
+
+  def api_version_text()
     arvados_api_client.discovery[:source_version]
   end
+
+  # wb_version and wb_version_text provide the same strings for the
+  # code version that this Workbench is currently running.
 
   def wb_version()
     Rails.configuration.source_version
