44365f7532b85d73322b1a702bf458b489ee0e27
Tom Clegg <tom@curoverse.com>
2014-12-09 03:37:48 -0500

    3781: Fix trigger() usage: second argument is an array of handler args.

--- apps/workbench/app/assets/javascripts/angular_shim.js
+++ apps/workbench/app/assets/javascripts/angular_shim.js
@@ -2,11 +2,11 @@
 // Currently this only works for tabs because they emit an
 // arv:pane:loaded event after updating the DOM.
 
-$(document).on('arv:pane:loaded', function(event, updatedElement) {
-    if (updatedElement) {
-        angular.element(updatedElement).injector().invoke(function($compile) {
-            var scope = angular.element(updatedElement).scope();
-            $compile(updatedElement)(scope);
+$(document).on('arv:pane:loaded', function(event, $updatedElement) {
+    if ($updatedElement) {
+        angular.element($updatedElement).injector().invoke(function($compile) {
+            var scope = angular.element($updatedElement).scope();
+            $compile($updatedElement)(scope);
         });
     }
 });
