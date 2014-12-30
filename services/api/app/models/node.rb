5aa470ef5aa86f7946be8d379e409deacfabba67
Ward Vandewege <ward@curoverse.com>
2014-12-30 13:28:57 -0500

    Cleanups:
    * Remove old commented out code
    * Remove superfluous test for presence of file on disk
    
    refs #4887

--- services/api/app/models/node.rb
+++ services/api/app/models/node.rb
@@ -166,10 +166,6 @@ class Node < ArvadosModel
       STDERR.puts "Unable to write #{hostfile}: #{e.message}"
       return
     end
-    #  f.puts "address=/#{hostname}/#{ip_address}"
-    #  f.puts "address=/#{hostname}.#{@@domain}/#{ip_address}" if @@domain
-    #  f.puts "ptr-record=#{ptr_domain},#{hostname}"
-    #end
     File.open(File.join(@@dns_server_conf_dir, 'restart.txt'), 'w') do |f|
       # this will trigger a dns server restart
       f.puts @@dns_server_reload_command
@@ -182,8 +178,7 @@ class Node < ArvadosModel
 
   # At startup, make sure all DNS entries exist.  Otherwise, slurmctld
   # will refuse to start.
-  if @@dns_server_conf_dir and @@dns_server_conf_template and
-      !File.exists? (File.join(@@dns_server_conf_dir, "#{hostname_for_slot(MAX_SLOTS-1)}.conf"))
+  if @@dns_server_conf_dir and @@dns_server_conf_template
     (0..MAX_SLOTS-1).each do |slot_number|
       hostname = hostname_for_slot(slot_number)
       hostfile = File.join @@dns_server_conf_dir, "#{hostname}.conf"
