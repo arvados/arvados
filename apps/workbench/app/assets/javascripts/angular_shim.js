90cfbd0a9326dcbb9b58c85cbabe4d32596cf665
Tom Clegg <tom@curoverse.com>
2015-01-03 01:52:34 -0500

    3021: Skip angular init if angular is not loaded.

--- apps/workbench/app/assets/javascripts/angular_shim.js
+++ apps/workbench/app/assets/javascripts/angular_shim.js
@@ -1,9 +1,9 @@
 // Compile any new HTML content that was loaded via jQuery.ajax().
-// Currently this only works for tabs because they emit an
+// Currently this only works for tabs, and only because they emit an
 // arv:pane:loaded event after updating the DOM.
 
 $(document).on('arv:pane:loaded', function(event, $updatedElement) {
-    if ($updatedElement) {
+    if (angular && $updatedElement) {
         angular.element($updatedElement).injector().invoke(function($compile) {
             var scope = angular.element($updatedElement).scope();
             $compile($updatedElement)(scope);
