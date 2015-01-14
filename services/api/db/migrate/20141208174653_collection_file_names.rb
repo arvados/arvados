3fa179311344ed08b9bb8df113fb7901eac13ee7
Radhika Chippada <radhika@curoverse.com>
2015-01-14 11:24:20 -0500

    4523: update migration script to use execute instead of update_sql so that output is not printed to console.

--- services/api/db/migrate/20141208174653_collection_file_names.rb
+++ services/api/db/migrate/20141208174653_collection_file_names.rb
@@ -7,7 +7,7 @@ class CollectionFileNames < ActiveRecord::Migration
     act_as_system_user do
       Collection.find_each(batch_size: 20) do |c|
         file_names = c.manifest_files
-        update_sql "UPDATE collections
+        ActiveRecord::Base.connection.execute "UPDATE collections
                     SET file_names = #{ActiveRecord::Base.connection.quote(file_names)}
                     WHERE uuid = '#{c.uuid}'"
       end
