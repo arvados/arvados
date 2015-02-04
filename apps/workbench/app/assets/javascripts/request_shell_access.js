9fc99919d72ee495e66ce98584189c651cf994c1
Radhika Chippada <radhika@curoverse.com>
2015-02-04 18:54:18 -0500

    4951: add ajax handling for request shell access button.

---
+++ apps/workbench/app/assets/javascripts/request_shell_access.js
@@ -0,0 +1,18 @@
+$(document).
+  on('click', "#request_shell_submit", function(e){
+    $(this).html('Sending request ...');
+    $(this).prop('disabled', true);
+    $('div').remove('.request_shell_access_failed');
+
+    $.ajax('/').
+      success(function(data, status, jqxhr) {
+        $('div').remove('.no_shell_access_msg');
+        $('.no_shell_access').append('<div class="alert alert-success"><p class="contain-align-left">Request sent for shell access.</p></div>');
+      }).
+      fail(function(jqxhr, status, error) {
+        var $sendButton = $('#request_shell_submit');
+        $sendButton.html('Request shell access');
+        $sendButton.prop('disabled', false);
+        $('.no_shell_access').append('<div class="request_shell_access_failed alert alert-danger"><p class="contain-align-left">Something went wrong. Please try again.</p></div>');
+      });
+  });
