b93018f033f37a7a7b719fd7dc6d5b3ad4a0f797
Tom Clegg <tom@curoverse.com>
2015-03-16 17:17:07 -0400

    5416: Allow users with repo write permission to update modified_at.

--- services/api/app/models/repository.rb
+++ services/api/app/models/repository.rb
@@ -22,7 +22,15 @@ class Repository < ArvadosModel
   def permission_to_create
     current_user and current_user.is_admin
   end
+
   def permission_to_update
-    current_user and current_user.is_admin
+    return false if not current_user
+    return true if current_user.is_admin
+    # For normal objects, this is a way to check whether you have
+    # write permission. Repositories should be brought closer to the
+    # normal permission model during #4253. Meanwhile, we'll
+    # special-case this so arv-git-httpd can detect write permission:
+    return super if changed_attributes.keys - ['modified_at', 'updated_at'] == []
+    false
   end
 end
--- services/api/test/unit/repository_test.rb
+++ services/api/test/unit/repository_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class RepositoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'write permission allows changing modified_at' do
+    act_as_user users(:active) do
+      r = repositories(:foo)
+      modtime_was = r.modified_at
+      r.modified_at = Time.now
+      assert r.save
+      assert_operator modtime_was, :<, r.modified_at
+    end
+  end
 end
