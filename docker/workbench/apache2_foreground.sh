78691191dfa23c930a00c6d5cb6dcd4ce30b1a5e
Peter Amstutz <peter.amstutz@curoverse.com>
2015-03-23 17:12:55 -0400

    5539: Arvados-in-Docker improvements * arvdock start now restarts existing containers instead of deleting them.  Use arvdock reset to delete the containers. * Uses docker data container for keep. * Prints a note about adding the nameserver to /etc/resolv.conf

--- docker/workbench/apache2_foreground.sh
+++ docker/workbench/apache2_foreground.sh
@@ -5,7 +5,8 @@ trap "kill -TERM -$pgrp; exit" EXIT TERM KILL SIGKILL SIGTERM SIGQUIT
 
 # Override the default API server address if necessary.
 if [[ "$API_PORT_443_TCP_ADDR" != "" ]]; then
-  sed -i "s/localhost:9900/$API_PORT_443_TCP_ADDR/" /usr/src/arvados/apps/workbench/config/application.yml
+    sed -i "s/arvados_login_base: '.*'/arvados_login_base: 'https:\/\/$API_PORT_443_TCP_ADDR\/login'/" /usr/src/arvados/apps/workbench/config/application.yml
+    sed -i "s/arvados_v1_base: '.*'/arvados_v1_base: 'https:\/\/$API_PORT_443_TCP_ADDR\/arvados\/v1'/" /usr/src/arvados/apps/workbench/config/application.yml
 fi
 
 source /etc/apache2/envvars
