e97d70a1f6c687ce110f40d0e44a9fa385873ad6
Peter Amstutz <peter.amstutz@curoverse.com>
2015-03-24 10:32:58 -0400

    5539: Require that the user update resolv.conf, and use FQDN (api.dev.arvados, workbench.dev.arvados) to refer to services more consistently.

--- docker/workbench/apache2_foreground.sh
+++ docker/workbench/apache2_foreground.sh
@@ -3,11 +3,5 @@
 read pid cmd state ppid pgrp session tty_nr tpgid rest < /proc/self/stat
 trap "kill -TERM -$pgrp; exit" EXIT TERM KILL SIGKILL SIGTERM SIGQUIT
 
-# Override the default API server address if necessary.
-if [[ "$API_PORT_443_TCP_ADDR" != "" ]]; then
-    sed -i "s/arvados_login_base: '.*'/arvados_login_base: 'https:\/\/$API_PORT_443_TCP_ADDR\/login'/" /usr/src/arvados/apps/workbench/config/application.yml
-    sed -i "s/arvados_v1_base: '.*'/arvados_v1_base: 'https:\/\/$API_PORT_443_TCP_ADDR\/arvados\/v1'/" /usr/src/arvados/apps/workbench/config/application.yml
-fi
-
 source /etc/apache2/envvars
 /usr/sbin/apache2 -D FOREGROUND
