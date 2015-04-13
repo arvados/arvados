3f016eae9d2431abfb8e8cc1cabdb1a494e49bf2
Tom Clegg <tom@curoverse.com>
2015-04-13 13:48:40 -0400

    5414: Rename zz_load* to load*; alphabetical names are not needed any more.

--- services/api/config/initializers/zz_preload_all_models.rb
+++ services/api/config/initializers/zz_preload_all_models.rb
@@ -3,10 +3,10 @@
 # Config must be done before we load model class files; otherwise they
 # won't be able to use Rails.configuration.* to initialize their
 # classes.
-require_relative 'zz_load_config.rb'
+require_relative 'load_config.rb'
 
 if Rails.env == 'development'
   Dir.foreach("#{Rails.root}/app/models") do |model_file|
     require_dependency model_file if model_file.match /\.rb$/
-  end 
+  end
 end
