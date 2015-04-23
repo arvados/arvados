559fdf14486527af646dfab51f2c8b87af8e2eae
Radhika Chippada <radhika@curoverse.com>
2015-04-23 13:01:15 -0400

    5796: add down migration

--- services/api/db/migrate/20150423145759_no_filenames_in_collection_search_index.rb
+++ services/api/db/migrate/20150423145759_no_filenames_in_collection_search_index.rb
@@ -5,5 +5,7 @@ class NoFilenamesInCollectionSearchIndex < ActiveRecord::Migration
   end
 
   def down
+    remove_index :collections, :name => 'collections_search_index'
+    add_index :collections, ["owner_uuid", "modified_by_client_uuid", "modified_by_user_uuid", "portable_data_hash", "uuid", "name", "file_names"], name: 'collections_search_index'
   end
 end
