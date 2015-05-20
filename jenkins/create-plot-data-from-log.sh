0f0793754990012c75393cd517c85c261579cddd
Nico Cesar <nico@nicocesar.com>
2015-05-20 11:12:18 -0400

    adding 3 more test for the performance suite.
    I'm guessing the names, lets see if they are good or not ...
    
    refs #6061

--- jenkins/create-plot-data-from-log.sh
+++ jenkins/create-plot-data-from-log.sh
@@ -41,7 +41,10 @@ for test in \
  test_combine_selected_collection_files_into_new_collection_active_foo_collection_in_aproject_true \
  test_combine_selected_collection_files_into_new_collection_active_foo_file_false \
  test_combine_selected_collection_files_into_new_collection_project_viewer_foo_collection_in_aproject_false \
- test_combine_selected_collection_files_into_new_collection_project_viewer_foo_file_false 
+ test_combine_selected_collection_files_into_new_collection_project_viewer_foo_file_false \
+ test_Create_and_show_large_collection_with_manifest_text_of_20000000 \
+ test_Create__show__and_update_description_for_large_collection_with_manifest_text_of_100000 \
+ test_Create_one_large_collection_of_20000000_and_one_small_collection_of_10000_and_combine_them
 do
  zgrep -A$MAXLINES "^CollectionsTest: $test" $file | tail --lines=+3|grep -B$MAXLINES -E "^-*$" -m1 > $outputdir/$test-$build.txt
  echo processing  $outputdir/$test-$build.txt creating  $outputdir/$test.csv
