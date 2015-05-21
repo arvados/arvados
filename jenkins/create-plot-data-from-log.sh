31e86126eba36e27c824b769ddea304e52ceeb59
Nico Cesar <nico@nicocesar.com>
2015-05-21 10:27:14 -0400

    6061: comment cleanup and final version tested. it works
    closes #6061

--- jenkins/create-plot-data-from-log.sh
+++ jenkins/create-plot-data-from-log.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 build=$1
 file=$2
@@ -19,7 +19,7 @@ fi
 if [ ! -e $file ]
 then
     usage
-    echo "$file doesn't exists! exiting"
+    echo "$file doesn't exist! exiting"
     exit 2
 fi
 if [ ! -w $outputdir ]
@@ -30,7 +30,9 @@ then
 fi
 
 #------------------------------
-## max lines that a test will output
+## MAXLINE is the amount of lines that will read after the pattern
+## is match (the logfile could be hundred thousands lines long).
+## 1000 should be safe enough to capture all the output of the individual test
 MAXLINES=1000 
 
 ## TODO: check $build and $file make sense
@@ -40,14 +42,15 @@ for test in \
  test_Create,_show,_and_update_description_for_large_collection_with_manifest_text_of_100000 \
  test_Create_one_large_collection_of_20000000_and_one_small_collection_of_10000_and_combine_them
 do
-(zgrep -i -E -A$MAXLINES "^[A-Za-z0-9]+Test: $test" $file && echo "----") | tail --lines=+3|grep -B$MAXLINES -E "^-*$" -m1 > $outputdir/$test-$build.txt
+ cleaned_test=$(echo $test | tr -d ",.:;/")
+ (zgrep -i -E -A$MAXLINES "^[A-Za-z0-9]+Test: $test" $file && echo "----") | tail -n +1 | tail --lines=+3|grep -B$MAXLINES -E "^-*$" -m1 > $outputdir/$cleaned_test-$build.txt
  result=$?
  if [ $result -eq 0 ]
  then 
-   echo processing  $outputdir/$test-$build.txt creating  $outputdir/$test.csv
-   echo $(grep ^Completed $outputdir/$test-$build.txt | perl -n -e '/^Completed (.*) in [0-9]+ms.*$/;print "".++$line."-$1,";' | perl -p -e 's/,$//g'|tr " " "_" ) >  $outputdir/$test.csv
-   echo $(grep ^Completed $outputdir/$test-$build.txt | perl -n -e '/^Completed.*in ([0-9]+)ms.*$/;print "$1,";' | perl -p -e 's/,$//g' ) >>  $outputdir/$test.csv
-   #echo URL=https://ci.curoverse.com/view/job/arvados-api-server/ws/apps/workbench/log/$test-$build.txt/*view*/ >>  $outputdir/$test.properties
+   echo processing  $outputdir/$cleaned_test-$build.txt creating  $outputdir/$cleaned_test.csv
+   echo $(grep ^Completed $outputdir/$cleaned_test-$build.txt | perl -n -e '/^Completed (.*) in [0-9]+ms.*$/;print "".++$line."-$1,";' | perl -p -e 's/,$//g'|tr " " "_" ) >  $outputdir/$cleaned_test.csv
+   echo $(grep ^Completed $outputdir/$cleaned_test-$build.txt | perl -n -e '/^Completed.*in ([0-9]+)ms.*$/;print "$1,";' | perl -p -e 's/,$//g' ) >>  $outputdir/$cleaned_test.csv
+   #echo URL=https://ci.curoverse.com/view/job/arvados-api-server/ws/apps/workbench/log/$cleaned_test-$build.txt/*view*/ >>  $outputdir/$test.properties
  else  
    echo "$test was't found on $file"
  fi
