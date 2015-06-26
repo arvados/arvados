02a3aa9016f4d4fc46eb11a56e121eb4ea0e3453
Tom Clegg <tom@curoverse.com>
2015-06-26 02:54:31 -0400

    5677: Edit structure.sql after db:structure:dump: avoid warnings when loading, and editwars when committing.

--- services/api/db/structure.sql
+++ services/api/db/structure.sql
@@ -19,7 +19,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
 --
 
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+-- COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
 SET search_path = public, pg_catalog;
