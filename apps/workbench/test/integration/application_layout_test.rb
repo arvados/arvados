61d432f332dd9314f8c4cd57c2393e80e8ecef59
Manoj <jonam33@gmail.com>
2015-07-20 14:09:40 -0400

    6652: Moved system menu test into application_layout_test.

--- apps/workbench/test/integration/application_layout_test.rb
+++ apps/workbench/test/integration/application_layout_test.rb
@@ -201,4 +201,35 @@ class ApplicationLayoutTest < ActionDispatch::IntegrationTest
       assert page.has_link?('Report a problem ...'), 'No link - Report a problem'
     end
   end
+
+   [
+    ['Repositories','repository','Attributes'],
+    ['Virtual machines','virtual machine','current_user_logins'],
+    ['SSH keys','authorized key','public_key'],
+    ['Links','link','link_class'],
+    ['Groups','group','group_class'],
+    ['Compute nodes','node','info[ping_secret'],
+    ['Keep services','keep service','service_ssl_flag'],
+    ['Keep disks', 'keep disk','bytes_free'],
+  ].each do |page_name, add_button_text, look_for|
+    test "test system menu #{page_name} link" do
+      skip 'Skip repositories test until #6652 is fixed.' if page_name == 'Repositories'
+
+      visit page_with_token('admin')
+      within('.navbar-fixed-top') do
+        page.find("#system-menu").click
+        within('.dropdown-menu') do
+          assert_selector 'a', text: page_name
+          find('a', text: page_name).click
+        end
+      end
+
+      # click the add button
+      assert_selector 'button', text: "Add a new #{add_button_text}"
+      find('button', text: "Add a new #{add_button_text}").click
+
+      # look for unique property in the created object page
+      assert page.has_text? look_for
+    end
+  end
 end
