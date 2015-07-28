7019bc98593c0603a2589d01cd028a242bf7f33f
Tom Clegg <tom@curoverse.com>
2015-07-28 18:11:59 -0400

    6663: Fix repository permissions.
    Users with is_active==false (except anonymous_user) are not listed by
    get_all_permissions.
    
    A repository that is shared with a group is only shared with users who
    have permissions on that group, not all users who have permissions on
    *any* group.
    
    {User --can_write--> group --can_read--> repo} is correctly listed as
    {User --can_read--> repo}, not can_write.
    
    Tests check that the permissions reported by get_all_permissions do
    not exceed the API server's interpretation of the current permission
    set.

--- services/api/app/controllers/arvados/v1/repositories_controller.rb
+++ services/api/app/controllers/arvados/v1/repositories_controller.rb
@@ -3,14 +3,23 @@ class Arvados::V1::RepositoriesController < ApplicationController
   skip_before_filter :render_404_if_no_object, :only => :get_all_permissions
   before_filter :admin_required, :only => :get_all_permissions
   def get_all_permissions
-    @users = {}
-    User.includes(:authorized_keys).find_each do |u|
-      @users[u.uuid] = u
+    users = {}
+    user_aks = {}
+    admins = []
+    User.eager_load(:authorized_keys).find_each do |u|
+      next unless u.is_active or u.uuid == anonymous_user_uuid
+      users[u.uuid] = u
+      user_aks[u.uuid] ||= u.authorized_keys.
+        collect do |ak|
+        {
+          public_key: ak.public_key,
+          authorized_key_uuid: ak.uuid
+        }
+      end
+      admins << u.uuid if u.is_admin
     end
-    admins = @users.select { |k,v| v.is_admin }
-    @user_aks = {}
     @repo_info = {}
-    Repository.includes(:permissions).find_each do |repo|
+    Repository.eager_load(:permissions).find_each do |repo|
       @repo_info[repo.uuid] = {
         uuid: repo.uuid,
         name: repo.name,
@@ -22,38 +31,33 @@ class Arvados::V1::RepositoriesController < ApplicationController
       perms = []
       repo.permissions.each do |perm|
         if ArvadosModel::resource_class_for_uuid(perm.tail_uuid) == Group
-          @users.each do |user_uuid, user|
-            user.group_permissions.each do |group_uuid, perm_mask|
-              if perm_mask[:manage]
-                perms << {name: 'can_manage', user_uuid: user_uuid}
-              elsif perm_mask[:write]
-                perms << {name: 'can_write', user_uuid: user_uuid}
-              elsif perm_mask[:read]
-                perms << {name: 'can_read', user_uuid: user_uuid}
-              end
+          users.each do |user_uuid, user|
+            perm_mask = user.group_permissions[perm.tail_uuid]
+            if not perm_mask
+              next
+            elsif perm_mask[:manage] and perm.name == 'can_manage'
+              perms << {name: 'can_manage', user_uuid: user_uuid}
+            elsif perm_mask[:write] and ['can_manage', 'can_write'].index perm.name
+              perms << {name: 'can_write', user_uuid: user_uuid}
+            elsif perm_mask[:read]
+              perms << {name: 'can_read', user_uuid: user_uuid}
             end
           end
-        else
+        elsif users[perm.tail_uuid]
+          # user exists and is (active or the anonymous user)
           perms << {name: perm.name, user_uuid: perm.tail_uuid}
         end
       end
       # Owner of the repository, and all admins, can RW
-      ([repo.owner_uuid] + admins.keys).each do |user_uuid|
+      ([repo.owner_uuid] | admins).each do |user_uuid|
+        # no permissions for inactive user, even when owner of repo
+        next unless users[user_uuid]
         perms << {name: 'can_write', user_uuid: user_uuid}
       end
       perms.each do |perm|
         user_uuid = perm[:user_uuid]
-        @user_aks[user_uuid] = @users[user_uuid].andand.authorized_keys.andand.
-          collect do |ak|
-          {
-            public_key: ak.public_key,
-            authorized_key_uuid: ak.uuid
-          }
-        end || []
-        if @user_aks[user_uuid].any?
-          ri = (@repo_info[repo.uuid][:user_permissions][user_uuid] ||= {})
-          ri[perm[:name]] = true
-        end
+        ri = (@repo_info[repo.uuid][:user_permissions][user_uuid] ||= {})
+        ri[perm[:name]] = true
       end
     end
     @repo_info.values.each do |repo_users|
@@ -72,6 +76,6 @@ class Arvados::V1::RepositoriesController < ApplicationController
     end
     send_json(kind: 'arvados#RepositoryPermissionSnapshot',
               repositories: @repo_info.values,
-              user_keys: @user_aks)
+              user_keys: user_aks)
   end
 end
