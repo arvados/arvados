fbe23d045022aac8ff3ae691052af25968680944
Tom Clegg <tom@curoverse.com>
2015-08-03 00:36:46 -0400

    5824: Use fmt "%+q" instead of custom escaping.

--- sdk/go/httpserver/log.go
+++ sdk/go/httpserver/log.go
@@ -1,20 +1,21 @@
 package httpserver
 
 import (
+	"fmt"
 	"log"
-	"strings"
 )
-
-var escaper = strings.NewReplacer("\"", "\\\"", "\\", "\\\\", "\n", "\\n")
 
 // Log calls log.Println but first transforms strings so they are
 // safer to write in logs (e.g., 'foo"bar' becomes
-// '"foo\"bar"'). Non-string args are left alone.
+// '"foo\"bar"'). Arguments that aren't strings and don't have a
+// (String() string) method are left alone.
 func Log(args ...interface{}) {
 	newargs := make([]interface{}, len(args))
 	for i, arg := range args {
 		if s, ok := arg.(string); ok {
-			newargs[i] = "\"" + escaper.Replace(s) + "\""
+			newargs[i] = fmt.Sprintf("%+q", s)
+		} else if s, ok := arg.(fmt.Stringer); ok {
+			newargs[i] = fmt.Sprintf("%+q", s.String())
 		} else {
 			newargs[i] = arg
 		}
