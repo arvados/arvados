c52b7c3f28031e6dec9601ffd8583f689149a380
Tom Clegg <tom@curoverse.com>
2015-08-11 11:13:34 -0400

    6260: Add explanatory comment about testing /status.json.

--- services/keepstore/status_test.go
+++ services/keepstore/status_test.go
@@ -4,6 +4,12 @@ import (
 	"encoding/json"
 )
 
+// We don't have isolated unit tests for /status.json yet, but we do
+// check (e.g., in pull_worker_test.go) that /status.json reports
+// specific statistics correctly at the appropriate times.
+
+// getStatusItem("foo","bar","baz") retrieves /status.json, decodes
+// the response body into resp, and returns resp["foo"]["bar"]["baz"].
 func getStatusItem(keys ...string) interface{} {
 	resp := IssueRequest(&RequestTester{"/status.json", "", "GET", nil})
 	var s interface{}
