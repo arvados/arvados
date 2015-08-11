8e671a545fd8abbf74afa109c0150c1d5772a207
Tom Clegg <tom@curoverse.com>
2015-08-10 20:39:39 -0400

    5824: Document ClientPool.

--- sdk/go/arvadosclient/pool.go
+++ sdk/go/arvadosclient/pool.go
@@ -4,11 +4,17 @@ import (
 	"sync"
 )
 
+// A ClientPool is a pool of ArvadosClients. This is useful for
+// applications that make API calls using a dynamic set of tokens,
+// like web services that pass through their own clients'
+// credentials. See arvados-git-httpd for an example, and sync.Pool
+// for more information about garbage collection.
 type ClientPool struct {
 	sync.Pool
 	lastErr error
 }
 
+// MakeClientPool returns a new empty ClientPool.
 func MakeClientPool() *ClientPool {
 	p := &ClientPool{}
 	p.Pool = sync.Pool{New: func() interface{} {
@@ -22,10 +28,16 @@ func MakeClientPool() *ClientPool {
 	return p
 }
 
+// Err returns the error that was encountered last time Get returned
+// nil.
 func (p *ClientPool) Err() error {
 	return p.lastErr
 }
 
+// Get returns an ArvadosClient taken from the pool, or a new one if
+// the pool is empty. If an existing client is returned, its state
+// (including its ApiToken) will be just as it was when it was Put
+// back in the pool.
 func (p *ClientPool) Get() *ArvadosClient {
 	c, ok := p.Pool.Get().(*ArvadosClient)
 	if !ok {
@@ -34,6 +46,7 @@ func (p *ClientPool) Get() *ArvadosClient {
 	return c
 }
 
+// Put puts an ArvadosClient back in the pool.
 func (p *ClientPool) Put(c *ArvadosClient) {
 	p.Pool.Put(c)
 }
