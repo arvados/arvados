08a10afd0681a04e5bae019870d636d49c3dc223
radhika <radhika@curoverse.com>
2015-08-13 09:29:17 -0400

    6859: fix issue with tempfile not actually being written and also update message logging.

--- services/api/script/salvage_collection.rb
+++ services/api/script/salvage_collection.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
 # Take two input parameters: a collection uuid and reason
-# Get "src" collection with the given uuid
-# Create a new collection "dest" with:
-#   src.manifest_text as "invalid manifest_text.txt"
-#   Locators from src.manifest_text as "salvaged_data"
-# Update src collection:
-#   Set src.manifest_text to: ""
-#   Append to src.name: " (reason; salvaged data at dest.uuid)"
+# Get "src_collection" with the given uuid
+# Create a new collection with:
+#   src_collection.manifest_text as "invalid_manifest_text.txt"
+#   Locators from src_collection.manifest_text as "salvaged_data"
+# Update src_collection:
+#   Set src_collection.manifest_text to: ""
+#   Append to src_collection.name: " (reason; salvaged data at new_collection.uuid)"
 #   Set portable_data_hash to "d41d8cd98f00b204e9800998ecf8427e+0"
 
 require 'trollop'
@@ -38,18 +38,18 @@ require 'tempfile'
 
 def salvage_collection uuid, reason
   act_as_system_user do
-    src = Collection.find_by_uuid uuid
-    if !src
-      puts "No collection found for #{uuid}"
+    src_collection = Collection.find_by_uuid uuid
+    if !src_collection
+      $stderr.puts "No collection found for #{uuid}. Returning."
       return
     end
 
     begin
-      src_manifest_text = src.manifest_text || ''
+      src_manifest = src_collection.manifest_text || ''
 
       # Get all the locators from the original manifest
       locators = []
-      src_manifest_text.each_line do |line|
+      src_manifest.each_line do |line|
         line.split(' ').each do |word|
           if match = Keep::Locator::LOCATOR_REGEXP.match(word)
             word = word.split('+')[0..1].join('+')  # get rid of any hints
@@ -61,11 +61,11 @@ def salvage_collection uuid, reason
 
       # create new collection using 'arv-put' with original manifest_text as the data
       temp_file = Tempfile.new('temp')
-      temp_file.write(src.manifest_text)
+      temp_file.write(src_manifest)
+      temp_file.close
 
       created = %x(arv-put --use-filename invalid_manifest_text.txt #{temp_file.path})
 
-      temp_file.close
       temp_file.unlink
 
       created.rstrip!
@@ -83,27 +83,28 @@ def salvage_collection uuid, reason
       end
       new_manifest += (". #{locators.join(' ')} 0:#{total_size}:salvaged_data\n")
 
-      new_collection.name = "salvaged from #{src.uuid}, #{src.portable_data_hash}"
+      new_collection.name = "salvaged from #{src_collection.uuid}, #{src_collection.portable_data_hash}"
       new_collection.manifest_text = new_manifest
       new_collection.portable_data_hash = Digest::MD5.hexdigest(new_manifest)
 
       new_collection.save!
 
-      puts "Created collection for salvaged #{uuid} with uuid: #{created}  #{match}"
+      $stderr.puts "Salvaged manifest and data for #{uuid} are in #{new_collection.uuid}."
+      puts "Created new collection #{created}"
     rescue => error
-      puts "Error creating collection for #{uuid}: #{error}"
+      $stderr.puts "Error creating collection for #{uuid}: #{error}"
       return
     end
 
     begin
-      # update src collection name, pdh, and manifest_text
-      src.name = (src.name || '') + ' (' + (reason || '') + '; salvaged data at ' + created + ')'
-      src.manifest_text = ''
-      src.portable_data_hash = 'd41d8cd98f00b204e9800998ecf8427e+0'
-      src.save!
-      puts "Salvaged collection #{uuid}"
+      # update src_collection collection name, pdh, and manifest_text
+      src_collection.name = (src_collection.name || '') + ' (' + (reason || '') + '; salvaged data at ' + created + ')'
+      src_collection.manifest_text = ''
+      src_collection.portable_data_hash = 'd41d8cd98f00b204e9800998ecf8427e+0'
+      src_collection.save!
+      $stderr.puts "Collection #{uuid} emptied and renamed to #{src_collection.name.inspect}."
     rescue => error
-      puts "Error salvaging collection #{uuid}: #{error}"
+      $stderr.puts "Error salvaging collection #{uuid}: #{error}"
     end
   end
 end
