965c5b34d9a535a9b45553f07f360d5fea2f6492
Tom Clegg <tom@curoverse.com>
2015-10-07 11:11:14 -0400

    6967: Move source_version detection code from config yaml to lib/app_version.rb.

--- apps/workbench/config/application.rb
+++ apps/workbench/config/application.rb
@@ -12,6 +12,7 @@ module ArvadosWorkbench
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
