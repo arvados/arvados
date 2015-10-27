9290961beb18476f71aa0ae12478fe50493e8ad9
radhika <radhika@curoverse.com>
2015-10-27 11:35:28 -0400

    7453: Disable submit button in add group dialog until a name is entered. Observed that the "Add a new user" button is offered in /users page even to non-admin users. Corrected this bug. Deleted one test in errors_test that was trying to access the "Add new group" button in /groups page. Also, deleted the one test in virtual_machines_test that were trying to click the "Add new" button. Added a new test in users_test that clicks the "Add new group" button and verifies the group is added.

--- apps/workbench/app/assets/javascripts/add_group.js
+++ apps/workbench/app/assets/javascripts/add_group.js
@@ -1,11 +1,19 @@
 $(document).on('shown.bs.modal', '#add-group-modal', function(event) {
+    // Disable the submit button on modal loading
+    $submit = $('#add-group-submit');
+    $submit.prop('disabled', true);
+
     $('input[type=text]', event.target).val('');
     $('#add-group-error', event.target).hide();
+}).on('input propertychange', '#group_name_input', function(event) {
+    group_name = $(event.target).val();
+    $submit = $('#add-group-submit');
+    $submit.prop('disabled', (group_name === null || group_name === ""));
 }).on('submit', '#add-group-form', function(event) {
     var $form = $(event.target),
     $submit = $(':submit', $form),
     $error = $('#add-group-error', $form),
-    group_name = $('input[name="group_name"]', $form).val();
+    group_name = $('input[name="group_name_input"]', $form).val();
 
     $submit.prop('disabled', true);
 
