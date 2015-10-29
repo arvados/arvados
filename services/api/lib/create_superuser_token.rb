a5e86befc352e31600399e9c4a476479d2155413
radhika <radhika@curoverse.com>
2015-10-29 10:30:14 -0400

    7534: refactor the code from create_superuser_token.rb script into lib and verify manually that existing behavior is preserved.

---
+++ services/api/lib/create_superuser_token.rb
@@ -0,0 +1,33 @@
+module CreateSuperUserToken
+  # Install the supplied string (or a randomly generated token, if none
+  # is given) as an API token that authenticates to the system user
+  # account.
+
+  require File.dirname(__FILE__) + '/../config/boot'
+  require File.dirname(__FILE__) + '/../config/environment'
+
+  include ApplicationHelper
+  act_as_system_user
+
+  def create_superuser_token supplied_token
+    if supplied_token
+      api_client_auth = ApiClientAuthorization.
+        where(api_token: supplied_token).
+        first
+      if api_client_auth && !api_client_auth.user.uuid.match(/-000000000000000$/)
+        raise ActiveRecord::RecordNotUnique("Token already exists but is not a superuser token.")
+      end
+    end
+
+    if !api_client_auth
+      api_client_auth = ApiClientAuthorization.
+        new(user: system_user,
+            api_client_id: 0,
+            created_by_ip_address: '::1',
+            api_token: supplied_token)
+      api_client_auth.save!
+    end
+
+    puts api_client_auth.api_token
+  end
+end
