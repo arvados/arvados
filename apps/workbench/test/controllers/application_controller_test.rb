702ff97546e9050155658135694c55a0568be1d5
Brett Smith <brett@curoverse.com>
2015-11-12 15:12:28 -0500

    Fixup last commit per review.

--- apps/workbench/test/controllers/application_controller_test.rb
+++ apps/workbench/test/controllers/application_controller_test.rb
@@ -346,8 +346,11 @@ class ApplicationControllerTest < ActionController::TestCase
       get(:show, {id: test_uuid})
       login_link = css_select(css_selector).first
       assert_not_nil(login_link, "failed to select login link")
+      login_href = URI.unescape(login_link.attributes["href"])
+      # The parameter needs to include the full URL to work.
+      assert_includes(login_href, "://")
       assert_match(/[\?&]return_to=[^&]*\/projects\/#{test_uuid}(&|$)/,
-                   URI.unescape(login_link.attributes["href"]))
+                   login_href)
     end
   end
 
