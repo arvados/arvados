5f7c3a3a4a6926edb67ecb46d8aa3649a47a2643
Tom Clegg <tom@curoverse.com>
2015-11-30 15:28:19 -0500

    7710: Add keep_services#accessible test.

--- services/api/test/functional/arvados/v1/keep_services_controller_test.rb
+++ services/api/test/functional/arvados/v1/keep_services_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class Arvados::V1::KeepServicesControllerTest < ActionController::TestCase
 
-  test "search keep_services by service_port with < query" do
+  test "search by service_port with < query" do
     authorize_with :active
     get :index, {
       filters: [['service_port', '<', 25107]]
@@ -11,13 +11,25 @@ class Arvados::V1::KeepServicesControllerTest < ActionController::TestCase
     assert_equal false, assigns(:objects).any?
   end
 
-  test "search keep_disks by service_port with >= query" do
+  test "search by service_port with >= query" do
     authorize_with :active
     get :index, {
       filters: [['service_port', '>=', 25107]]
     }
     assert_response :success
     assert_equal true, assigns(:objects).any?
+  end
+
+  [:admin, :active, :inactive, :anonymous].each do |u|
+    test "accessible to #{u} user" do
+      authorize_with u
+      get :accessible
+      assert_response :success
+      assert_not_empty json_response['items']
+      json_response['items'].each do |ks|
+        assert_not_equal ks['service_type'], 'proxy'
+      end
+    end
   end
 
 end
