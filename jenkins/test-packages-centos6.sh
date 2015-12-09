e7a2d57f559f84d9a2181526e3a4d6ca6082f3c4
Brett Smith <brett@curoverse.com>
2015-12-09 12:43:58 -0500

    7953: Update test procedure for CentOS 6 packages.
    Don't install any Software Collections in the Dockerfile, just the
    Software Collections tools.  Test that our packages correctly declare
    their dependency on the relevant Software Collection, and that it gets
    pulled in.  We want to tell users to install this way, so this makes
    sure it's working.
    
    Refs #7953, #7957.

--- jenkins/test-packages-centos6.sh
+++ jenkins/test-packages-centos6.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
-if ! python --version |& grep "Python 2.7" >/dev/null ; then
-    # Re-execute in the software collections Python 2.7 environment.
-    exec scl enable python27 $0
-else
-    yum -q clean all
-    touch /var/lib/rpm/*
-    if ! yum -q install --assumeyes \
-         python27-python-arvados-python-client python27-python-arvados-fuse arvados-node-manager
-    then
-        exit 1
-    fi
-
-    mkdir -p /tmp/opts
-    cd /tmp/opts
-
-    for r in /arvados/packages/centos6/python27-python-*x86_64.rpm ; do
-        rpm2cpio $r | cpio -idm
-    done
-
-    for so in $(find . -name "*.so") ; do
-        echo
-        echo "== Packages dependencies for $so =="
-        ldd $so | awk '($3 ~ /^\//){print $3}' | sort -u | xargs rpm -qf | sort -u
-    done
-
-    exec /jenkins/common-test-packages.sh
+yum -q clean all
+touch /var/lib/rpm/*
+if ! yum -q install --assumeyes \
+     python27-python-arvados-python-client python27-python-arvados-fuse arvados-node-manager
+then
+    exit 1
 fi
+
+mkdir -p /tmp/opts
+cd /tmp/opts
+
+for r in /arvados/packages/centos6/python27-python-*x86_64.rpm ; do
+    rpm2cpio $r | cpio -idm
+done
+
+for so in $(find -name "*.so") ; do
+    echo
+    echo "== Packages dependencies for $so =="
+    scl enable python27 "ldd $so" \
+        | awk '($3 ~ /^\//){print $3}' | sort -u | xargs rpm -qf | sort -u
+done
+
+exec scl enable python27 /jenkins/common-test-packages.sh
