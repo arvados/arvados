12b1501e8ee2edc932ceb908ab69189712a761b4
Tom Clegg <tom@curoverse.com>
2016-01-23 01:28:38 -0500

    8123: Include chartjs.js in package.

--- tools/crunchstat-summary/setup.py
+++ tools/crunchstat-summary/setup.py
@@ -23,6 +23,7 @@ setup(name='crunchstat_summary',
       download_url="https://github.com/curoverse/arvados.git",
       license='GNU Affero General Public License, version 3.0',
       packages=['crunchstat_summary'],
+      include_package_data=True,
       scripts=[
           'bin/crunchstat-summary'
       ],
