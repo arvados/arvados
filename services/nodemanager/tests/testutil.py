7b4f98f54598aefc54f88a491dba64793cf749ae
Peter Amstutz <peter.amstutz@curoverse.com>
2016-02-02 10:55:39 -0500

    8206: Mock time.sleep() to avoid unnecessary delay in test.

--- services/nodemanager/tests/testutil.py
+++ services/nodemanager/tests/testutil.py
@@ -131,12 +131,13 @@ class DriverTestMixin(object):
         return getattr(self.driver_mock(), method_name).call_args
 
     def test_driver_create_retry(self):
-        driver_mock2 = mock.MagicMock(name='driver_mock2')
-        self.driver_mock.side_effect = (Exception("oops"), driver_mock2)
-        kwargs = {'user_id': 'foo'}
-        driver = self.new_driver(auth_kwargs=kwargs)
-        self.assertTrue(self.driver_mock.called)
-        self.assertIs(driver.real, driver_mock2)
+        with mock.patch('time.sleep'):
+            driver_mock2 = mock.MagicMock(name='driver_mock2')
+            self.driver_mock.side_effect = (Exception("oops"), driver_mock2)
+            kwargs = {'user_id': 'foo'}
+            driver = self.new_driver(auth_kwargs=kwargs)
+            self.assertTrue(self.driver_mock.called)
+            self.assertIs(driver.real, driver_mock2)
 
 class RemotePollLoopActorTestMixin(ActorTestMixin):
     def build_monitor(self, *args, **kwargs):
