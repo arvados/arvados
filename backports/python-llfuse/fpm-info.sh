a9fa507c505ab702cae850f4ccd5aae26f6218a9
Joshua Randall <joshua.randall@sanger.ac.uk>
2016-02-03 11:48:55 +0000

    updates llfuse requirement and API to >=0.42

--- backports/python-llfuse/fpm-info.sh
+++ backports/python-llfuse/fpm-info.sh
@@ -3,14 +3,15 @@ case "$TARGET" in
         build_depends+=('fuse-devel')
         fpm_depends+=(glibc fuse-libs)
         ;;
+    ubuntu1204)
+        build_depends+=(libfuse2 libfuse-dev)
+        fpm_depends+=(libc6 python-contextlib2 'libfuse2 = 2.9.2-5' 'fuse = 2.9.2-5')
+        ;;
     debian* | ubuntu*)
         build_depends+=('libfuse-dev')
-        fpm_depends+=(libc6 libfuse2)
+        fpm_depends+=(libc6 'libfuse2 > 2.9.0' 'fuse > 2.9.0')
         ;;
 esac
 
 # FIXME: Remove this line after #6885 is done.
-fpm_args+=(--iteration 2)
-
-# FIXME: Remove once support for llfuse 0.42+ is in place
-fpm_args+=(-v 0.41.1)
+fpm_args+=(--iteration 3)
