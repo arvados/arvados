6f19089ca96e485f457932dce44256e46f1e3711
Peter Amstutz <peter.amstutz@curoverse.com>
2016-02-12 15:14:01 -0500

    Add arvbox support for overriding settings in application.yml no issue #

---
+++ arvbox/lib/arvbox/docker/application_yml_override.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import yaml
+
+try:
+    with open("application.yml.override") as f:
+        b = yaml.load(f)
+except IOError:
+    exit()
+
+with open("application.yml") as f:
+    a = yaml.load(f)
+
+def recursiveMerge(a, b):
+    if isinstance(a, dict) and isinstance(b, dict):
+        for k in b:
+            print k
+            a[k] = recursiveMerge(a.get(k), b[k])
+        return a
+    else:
+        return b
+
+with open("application.yml", "w") as f:
+    yaml.dump(recursiveMerge(a, b), f)
