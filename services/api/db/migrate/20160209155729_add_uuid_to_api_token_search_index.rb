b6afb1ac149e6771acd678ac6b4fd2e0afdedb0b
radhika <radhika@curoverse.com>
2016-02-15 12:47:22 -0500

    8079: add rescue to drop index

--- services/api/db/migrate/20160209155729_add_uuid_to_api_token_search_index.rb
+++ services/api/db/migrate/20160209155729_add_uuid_to_api_token_search_index.rb
@@ -1,13 +1,19 @@
 class AddUuidToApiTokenSearchIndex < ActiveRecord::Migration
   def up
-    remove_index :api_client_authorizations, :name => 'api_client_authorizations_search_index'
-	  add_index :api_client_authorizations,
+    begin
+      remove_index :api_client_authorizations, :name => 'api_client_authorizations_search_index'
+    rescue
+    end
+    add_index :api_client_authorizations,
               ["api_token", "created_by_ip_address", "last_used_by_ip_address", "default_owner_uuid", "uuid"],
               name: "api_client_authorizations_search_index"
   end
 
   def down
-    remove_index :api_client_authorizations, :name => 'api_client_authorizations_search_index'
+    begin
+      remove_index :api_client_authorizations, :name => 'api_client_authorizations_search_index'
+    rescue
+    end
 	  add_index :api_client_authorizations,
               ["api_token", "created_by_ip_address", "last_used_by_ip_address", "default_owner_uuid"],
               name: "api_client_authorizations_search_index"
