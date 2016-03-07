ea9d53175a0e7670ceb9bbeaf66cd6c89baa09dd
Tom Clegg <tom@curoverse.com>
2016-03-07 15:26:14 -0500

    8345: fix whitespace

--- services/fuse/arvados_fuse/__init__.py
+++ services/fuse/arvados_fuse/__init__.py
@@ -709,5 +709,5 @@ class Operations(llfuse.Operations):
     def fsync(self, fh, datasync):
         self.flush(fh)
 
-    def fsyncdir(self, fh, datasync): 
+    def fsyncdir(self, fh, datasync):
         self.flush(fh)
