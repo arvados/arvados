4e2763883588ac691da65ee316a52a052c002aa7
Tom Clegg <tom@curoverse.com>
2016-05-09 15:33:05 -0400

    8128: Add runtime tokens for containers, and locks for multiple dispatchers

--- services/api/app/controllers/arvados/v1/containers_controller.rb
+++ services/api/app/controllers/arvados/v1/containers_controller.rb
@@ -4,4 +4,11 @@ class Arvados::V1::ContainersController < ApplicationController
   accept_attribute_as_json :runtime_constraints, Hash
   accept_attribute_as_json :command, Array
 
+  def auth
+    if @object.locked_by_uuid != Thread.current[:api_client_authorization].uuid
+      raise ArvadosModel::PermissionDeniedError.new("Not locked by your token")
+    end
+    @object = @object.auth
+    show
+  end
 end
--- services/api/test/unit/container_test.rb
+++ services/api/test/unit/container_test.rb
@@ -1,22 +1,34 @@
 require 'test_helper'
 
 class ContainerTest < ActiveSupport::TestCase
-  def minimal_new
-    c = Container.new
-    c.command = ["echo", "foo"]
-    c.container_image = "img"
-    c.output_path = "/tmp"
-    c
-  end
+  include DbCurrentTime
 
-  def show_errors c
-    return lambda { c.errors.full_messages.inspect }
+  DEFAULT_ATTRS = {
+    command: ['echo', 'foo'],
+    container_image: 'img',
+    output_path: '/tmp',
+    priority: 1,
+  }
+
+  def minimal_new attrs={}
+    cr = ContainerRequest.new DEFAULT_ATTRS.merge(attrs)
+    act_as_user users(:active) do
+      cr.save!
+    end
+    c = Container.new DEFAULT_ATTRS.merge(attrs)
+    act_as_system_user do
+      c.save!
+      assert cr.update_attributes(container_uuid: c.uuid,
+                                  state: ContainerRequest::Committed,
+                                  ), show_errors(cr)
+    end
+    return c, cr
   end
 
   def check_illegal_updates c, bad_updates
     bad_updates.each do |u|
       refute c.update_attributes(u), u.inspect
-      refute c.valid?
+      refute c.valid?, u.inspect
       c.reload
     end
   end
@@ -28,6 +40,8 @@ class ContainerTest < ActiveSupport::TestCase
                               {environment: {"FOO" => "BAR"}},
                               {mounts: {"FOO" => "BAR"}},
                               {output_path: "/tmp3"},
+                              {locked_by_uuid: "zzzzz-gj3su-027z32aux8dg2s1"},
+                              {auth_uuid: "zzzzz-gj3su-017z32aux8dg2s1"},
                               {runtime_constraints: {"FOO" => "BAR"}}]
   end
 
@@ -39,7 +53,6 @@ class ContainerTest < ActiveSupport::TestCase
   def check_no_change_from_cancelled c
     check_illegal_modify c
     check_bogus_states c
-
     check_illegal_updates c, [{ priority: 3 },
                               { state: Container::Queued },
                               { state: Container::Locked },
@@ -49,13 +62,11 @@ class ContainerTest < ActiveSupport::TestCase
 
   test "Container create" do
     act_as_system_user do
-      c = minimal_new
-      c.environment = {}
-      c.mounts = {"BAR" => "FOO"}
-      c.output_path = "/tmp"
-      c.priority = 1
-      c.runtime_constraints = {}
-      c.save!
+      c, _ = minimal_new(environment: {},
+                      mounts: {"BAR" => "FOO"},
+                      output_path: "/tmp",
+                      priority: 1,
+                      runtime_constraints: {})
 
       check_illegal_modify c
       check_bogus_states c
@@ -67,89 +78,100 @@ class ContainerTest < ActiveSupport::TestCase
   end
 
   test "Container running" do
-    act_as_system_user do
-      c = minimal_new
-      c.save!
+    c, _ = minimal_new priority: 1
 
-      check_illegal_updates c, [{state: Container::Running},
-                                {state: Container::Complete}]
+    set_user_from_auth :dispatch1
+    check_illegal_updates c, [{state: Container::Running},
+                              {state: Container::Complete}]
 
-      c.update_attributes! state: Container::Locked
-      c.update_attributes! state: Container::Running
+    c.update_attributes! state: Container::Locked
+    c.update_attributes! state: Container::Running
 
-      check_illegal_modify c
-      check_bogus_states c
+    check_illegal_modify c
+    check_bogus_states c
 
-      check_illegal_updates c, [{state: Container::Queued}]
-      c.reload
+    check_illegal_updates c, [{state: Container::Queued}]
+    c.reload
 
-      c.update_attributes! priority: 3
-    end
+    c.update_attributes! priority: 3
   end
 
   test "Lock and unlock" do
-    act_as_system_user do
-      c = minimal_new
-      c.save!
-      assert_equal Container::Queued, c.state
+    c, cr = minimal_new priority: 0
 
-      refute c.update_attributes(state: Container::Running), "not locked"
-      c.reload
-      refute c.update_attributes(state: Container::Complete), "not locked"
-      c.reload
+    set_user_from_auth :dispatch1
+    assert_equal Container::Queued, c.state
 
-      assert c.update_attributes(state: Container::Locked), show_errors(c)
-      assert c.update_attributes(state: Container::Queued), show_errors(c)
+    refute c.update_attributes(state: Container::Locked), "no priority"
+    c.reload
+    assert cr.update_attributes priority: 1
 
-      refute c.update_attributes(state: Container::Running), "not locked"
-      c.reload
+    refute c.update_attributes(state: Container::Running), "not locked"
+    c.reload
+    refute c.update_attributes(state: Container::Complete), "not locked"
+    c.reload
 
-      assert c.update_attributes(state: Container::Locked), show_errors(c)
-      assert c.update_attributes(state: Container::Running), show_errors(c)
+    assert c.update_attributes(state: Container::Locked), show_errors(c)
+    assert c.locked_by_uuid
+    assert c.auth_uuid
 
-      refute c.update_attributes(state: Container::Locked), "already running"
-      c.reload
-      refute c.update_attributes(state: Container::Queued), "already running"
-      c.reload
+    assert c.update_attributes(state: Container::Queued), show_errors(c)
+    refute c.locked_by_uuid
+    refute c.auth_uuid
 
-      assert c.update_attributes(state: Container::Complete), show_errors(c)
-    end
+    refute c.update_attributes(state: Container::Running), "not locked"
+    c.reload
+    refute c.locked_by_uuid
+    refute c.auth_uuid
+
+    assert c.update_attributes(state: Container::Locked), show_errors(c)
+    assert c.update_attributes(state: Container::Running), show_errors(c)
+    assert c.locked_by_uuid
+    assert c.auth_uuid
+
+    auth_uuid_was = c.auth_uuid
+
+    refute c.update_attributes(state: Container::Locked), "already running"
+    c.reload
+    refute c.update_attributes(state: Container::Queued), "already running"
+    c.reload
+
+    assert c.update_attributes(state: Container::Complete), show_errors(c)
+    refute c.locked_by_uuid
+    refute c.auth_uuid
+
+    auth_exp = ApiClientAuthorization.find_by_uuid(auth_uuid_was).expires_at
+    assert_operator auth_exp, :<, db_current_time
   end
 
   test "Container queued cancel" do
-    act_as_system_user do
-      c = minimal_new
-      c.save!
-      assert c.update_attributes(state: Container::Cancelled), show_errors(c)
-      check_no_change_from_cancelled c
-    end
+    c, _ = minimal_new
+    set_user_from_auth :dispatch1
+    assert c.update_attributes(state: Container::Cancelled), show_errors(c)
+    check_no_change_from_cancelled c
   end
 
   test "Container locked cancel" do
-    act_as_system_user do
-      c = minimal_new
-      c.save!
-      assert c.update_attributes(state: Container::Locked), show_errors(c)
-      assert c.update_attributes(state: Container::Cancelled), show_errors(c)
-      check_no_change_from_cancelled c
-    end
+    c, _ = minimal_new
+    set_user_from_auth :dispatch1
+    assert c.update_attributes(state: Container::Locked), show_errors(c)
+    assert c.update_attributes(state: Container::Cancelled), show_errors(c)
+    check_no_change_from_cancelled c
   end
 
   test "Container running cancel" do
-    act_as_system_user do
-      c = minimal_new
-      c.save!
-      c.update_attributes! state: Container::Queued
-      c.update_attributes! state: Container::Locked
-      c.update_attributes! state: Container::Running
-      c.update_attributes! state: Container::Cancelled
-      check_no_change_from_cancelled c
-    end
+    c, _ = minimal_new
+    set_user_from_auth :dispatch1
+    c.update_attributes! state: Container::Queued
+    c.update_attributes! state: Container::Locked
+    c.update_attributes! state: Container::Running
+    c.update_attributes! state: Container::Cancelled
+    check_no_change_from_cancelled c
   end
 
   test "Container create forbidden for non-admin" do
     set_user_from_auth :active_trustedclient
-    c = minimal_new
+    c = Container.new DEFAULT_ATTRS
     c.environment = {}
     c.mounts = {"BAR" => "FOO"}
     c.output_path = "/tmp"
@@ -161,16 +183,14 @@ class ContainerTest < ActiveSupport::TestCase
   end
 
   test "Container only set exit code on complete" do
-    act_as_system_user do
-      c = minimal_new
-      c.save!
-      c.update_attributes! state: Container::Locked
-      c.update_attributes! state: Container::Running
+    c, _ = minimal_new
+    set_user_from_auth :dispatch1
+    c.update_attributes! state: Container::Locked
+    c.update_attributes! state: Container::Running
 
-      check_illegal_updates c, [{exit_code: 1},
-                                {exit_code: 1, state: Container::Cancelled}]
+    check_illegal_updates c, [{exit_code: 1},
+                              {exit_code: 1, state: Container::Cancelled}]
 
-      assert c.update_attributes(exit_code: 1, state: Container::Complete)
-    end
+    assert c.update_attributes(exit_code: 1, state: Container::Complete)
   end
 end
