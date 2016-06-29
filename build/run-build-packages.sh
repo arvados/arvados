5b0d660909c34b4b94ae2a9d45dccf2d5400fc7b
Peter Amstutz <peter.amstutz@curoverse.com>
2016-06-29 13:04:47 -0400

    9511: Bump cwltool dependency to get bugfix (81ff56f239de3ff0d17c8dec98d1a40f1e41cb7d) affecting bcbio run.

--- build/run-build-packages.sh
+++ build/run-build-packages.sh
@@ -466,7 +466,7 @@ fpm_build schema_salad "" "" python 1.12.20160610104117
 fpm_build ruamel.yaml "" "" python "" --python-setup-py-arguments "--single-version-externally-managed"
 
 # And for cwltool we have the same problem as for schema_salad. Ward, 2016-03-17
-fpm_build cwltool "" "" python 1.0.20160609160402
+fpm_build cwltool "" "" python 1.0.20160629140624
 
 # FPM eats the trailing .0 in the python-rdflib-jsonld package when built with 'rdflib-jsonld>=0.3.0'. Force the version. Ward, 2016-03-25
 fpm_build rdflib-jsonld "" "" python 0.3.0
