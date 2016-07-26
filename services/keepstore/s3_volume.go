e1f5b9c3c303502db7ef0e3c7a19c8edfbbb2183
Tom Clegg <tom@curoverse.com>
2016-07-26 16:20:30 -0400

    8555: gofmt

--- services/keepstore/s3_volume.go
+++ services/keepstore/s3_volume.go
@@ -592,7 +592,7 @@ func (v *S3Volume) EmptyTrash() {
 			continue
 		}
 		if trashT.Sub(recentT) < blobSignatureTTL {
-			if age := startT.Sub(recentT); age >= blobSignatureTTL - v.raceWindow {
+			if age := startT.Sub(recentT); age >= blobSignatureTTL-v.raceWindow {
 				// recent/loc is too old to protect
 				// loc from being Trashed again during
 				// the raceWindow that starts if we
