08262a4e18c0b3b65946878058e9546823b82535
Tom Clegg <tom@curoverse.com>
2016-08-19 11:14:03 -0400

    9799: Use regular (non-admin) user in Workbench websocket tests.

--- apps/workbench/test/integration/websockets_test.rb
+++ apps/workbench/test/integration/websockets_test.rb
@@ -6,20 +6,20 @@ class WebsocketTest < ActionDispatch::IntegrationTest
   end
 
   test "test page" do
-    visit(page_with_token("admin", "/websockets"))
+    visit(page_with_token("active", "/websockets"))
     fill_in("websocket-message-content", :with => "Stuff")
     click_button("Send")
     assert_text '"status":400'
   end
 
   test "test live logging" do
-    visit(page_with_token("admin", "/pipeline_instances/zzzzz-d1hrv-9fm8l10i9z2kqc6"))
+    visit(page_with_token("active", "/pipeline_instances/zzzzz-d1hrv-9fm8l10i9z2kqc6"))
     click_link("Log")
     assert_no_text '123 hello'
 
     api = ArvadosApiClient.new
 
-    Thread.current[:arvados_api_token] = @@API_AUTHS["admin"]['api_token']
+    use_token :active
     api.api("logs", "", {log: {
                 object_uuid: "zzzzz-d1hrv-9fm8l10i9z2kqc6",
                 event_type: "stderr",
@@ -32,14 +32,12 @@ class WebsocketTest < ActionDispatch::IntegrationTest
    ["jobs", api_fixture("jobs")['running']['uuid']],
    ["containers", api_fixture("containers")['running']['uuid']],
    ["container_requests", api_fixture("container_requests")['running']['uuid'], api_fixture("containers")['running']['uuid']],
-  ].each do |c|
-    test "test live logging scrolling #{c[0]}" do
+  ].each do |controller, uuid, log_uuid|
+    log_uuid = log_uuid || uuid
 
-      controller = c[0]
-      uuid = c[1]
-      log_uuid = c[2] || c[1]
+    test "test live logging scrolling for #{controller}" do
 
-      visit(page_with_token("admin", "/#{controller}/#{uuid}"))
+      visit(page_with_token("active", "/#{controller}/#{uuid}"))
       click_link("Log")
       assert_no_text '123 hello'
 
@@ -50,11 +48,12 @@ class WebsocketTest < ActionDispatch::IntegrationTest
         text << "#{i} hello\n"
       end
 
-      Thread.current[:arvados_api_token] = @@API_AUTHS["admin"]['api_token']
-      api.api("logs", "", {log: {
-                  object_uuid: log_uuid,
-                  event_type: "stderr",
-                  properties: {"text" => text}}})
+      use_token :dispatch1 do
+        api.api("logs", "", {log: {
+                    object_uuid: log_uuid,
+                    event_type: "stderr",
+                    properties: {"text" => text}}})
+      end
       assert_text '1000 hello'
 
       # First test that when we're already at the bottom of the page, it scrolls down
@@ -87,7 +86,7 @@ class WebsocketTest < ActionDispatch::IntegrationTest
   end
 
   test "pipeline instance arv-refresh-on-log-event" do
-    Thread.current[:arvados_api_token] = @@API_AUTHS["admin"]['api_token']
+    use_token :active
     # Do something and check that the pane reloads.
     p = PipelineInstance.create({state: "RunningOnServer",
                                   components: {
@@ -98,15 +97,17 @@ class WebsocketTest < ActionDispatch::IntegrationTest
                                   }
                                 })
 
-    visit(page_with_token("admin", "/pipeline_instances/#{p.uuid}"))
+    visit(page_with_token("active", "/pipeline_instances/#{p.uuid}"))
 
     assert_text 'Active'
     assert page.has_link? 'Pause'
     assert_no_text 'Complete'
     assert page.has_no_link? 'Re-run with latest'
 
-    p.state = "Complete"
-    p.save!
+    use_token :admin do
+      p.state = "Complete"
+      p.save!
+    end
 
     assert_no_text 'Active'
     assert page.has_no_link? 'Pause'
@@ -115,26 +116,28 @@ class WebsocketTest < ActionDispatch::IntegrationTest
   end
 
   test "job arv-refresh-on-log-event" do
-    Thread.current[:arvados_api_token] = @@API_AUTHS["admin"]['api_token']
+    use_token :active
     # Do something and check that the pane reloads.
     p = Job.where(uuid: api_fixture('jobs')['running_will_be_completed']['uuid']).results.first
 
-    visit(page_with_token("admin", "/jobs/#{p.uuid}"))
+    visit(page_with_token("active", "/jobs/#{p.uuid}"))
 
     assert_no_text 'complete'
     assert_no_text 'Re-run job'
 
-    p.state = "Complete"
-    p.save!
+    use_token :admin do
+      p.state = "Complete"
+      p.save!
+    end
 
     assert_text 'complete'
     assert_text 'Re-run job'
   end
 
   test "dashboard arv-refresh-on-log-event" do
-    Thread.current[:arvados_api_token] = @@API_AUTHS["admin"]['api_token']
+    use_token :active
 
-    visit(page_with_token("admin", "/"))
+    visit(page_with_token("active", "/"))
 
     assert_no_text 'test dashboard arv-refresh-on-log-event'
 
@@ -217,9 +220,9 @@ class WebsocketTest < ActionDispatch::IntegrationTest
   end
 
   test "test running job with just a few previous log records" do
-    Thread.current[:arvados_api_token] = @@API_AUTHS["admin"]['api_token']
+    use_token :active
     job = Job.where(uuid: api_fixture("jobs")['running']['uuid']).results.first
-    visit page_with_token("admin", "/jobs/#{job.uuid}")
+    visit page_with_token("active", "/jobs/#{job.uuid}")
 
     api = ArvadosApiClient.new
 
@@ -246,10 +249,10 @@ class WebsocketTest < ActionDispatch::IntegrationTest
   test "test running job with too many previous log records" do
     Rails.configuration.running_job_log_records_to_fetch = 5
 
-    Thread.current[:arvados_api_token] = @@API_AUTHS["admin"]['api_token']
+    use_token :active
     job = Job.where(uuid: api_fixture("jobs")['running']['uuid']).results.first
 
-    visit page_with_token("admin", "/jobs/#{job.uuid}")
+    visit page_with_token("active", "/jobs/#{job.uuid}")
 
     api = ArvadosApiClient.new
 
