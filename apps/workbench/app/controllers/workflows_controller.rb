4fb2e411c6fec1dad6472d0080810a9b8d267c0f
radhika <radhika@curoverse.com>
2016-08-29 22:03:51 -0400

    9824: include workflows in project#pipeline_templates tab.

--- apps/workbench/app/controllers/workflows_controller.rb
+++ apps/workbench/app/controllers/workflows_controller.rb
@@ -1,2 +1,6 @@
 class WorkflowsController < ApplicationController
+  skip_around_filter :require_thread_api_token, if: proc { |ctrl|
+    Rails.configuration.anonymous_user_token and
+    'show' == ctrl.action_name
+  }
 end
