b751f27e5dadf037003fe103bf170d6eef99154c
Lucas Di Pentima <lucas@curoverse.com>
2016-09-16 14:59:12 -0300

    10029: Added client_session_id to Workbench's client request log.

--- apps/workbench/config/initializers/lograge.rb
+++ apps/workbench/config/initializers/lograge.rb
@@ -3,7 +3,8 @@ ArvadosWorkbench::Application.configure do
   config.lograge.formatter = Lograge::Formatters::Logstash.new
   config.lograge.custom_options = lambda do |event|
     exceptions = %w(controller action format id)
-    params = event.payload[:params].except(*exceptions)
+    params = {client_session_id: Thread.current[:client_session_id]}.
+             merge(event.payload[:params].except(*exceptions))
     params_s = Oj.dump(params)
     if params_s.length > 1000
       { params_truncated: params_s[0..1000] + "[...]" }
