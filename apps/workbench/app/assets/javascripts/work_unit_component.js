85292497b7a89b9a0681fc38d307eea26681924c
radhika <radhika@curoverse.com>
2016-10-25 12:35:55 -0400

    10079: show spinner when a child component details are being displayed using ajax.

--- apps/workbench/app/assets/javascripts/work_unit_component.js
+++ apps/workbench/app/assets/javascripts/work_unit_component.js
@@ -6,6 +6,7 @@ $(document).
       }
 
       var content_div = href.find('.work-unit-component-detail-body');
+      content_div.html('<div class="spinner spinner-32px col-sm-1"></div>');
       var content_url = href.attr('content-url');
       var action_data = href.attr('action-data');
       $.ajax(content_url, {dataType: 'html', type: 'POST', data: {action_data: action_data}}).
