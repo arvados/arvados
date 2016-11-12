a4edc2a4a740d3908b907682a85e227eee80683b
Joshua C. Randall <jcrandall@alum.mit.edu>
2016-11-12 15:57:18 +0000

    Add Count to Go SDK ResourceListParams

--- sdk/go/arvados/resource_list.go
+++ sdk/go/arvados/resource_list.go
@@ -5,11 +5,13 @@ import "encoding/json"
 // ResourceListParams expresses which results are requested in a
 // list/index API.
 type ResourceListParams struct {
-	Select  []string `json:"select,omitempty"`
-	Filters []Filter `json:"filters,omitempty"`
-	Limit   *int     `json:"limit,omitempty"`
-	Offset  int      `json:"offset,omitempty"`
-	Order   string   `json:"order,omitempty"`
+	Select   []string `json:"select,omitempty"`
+	Filters  []Filter `json:"filters,omitempty"`
+	Limit    *int     `json:"limit,omitempty"`
+	Offset   int      `json:"offset,omitempty"`
+	Order    string   `json:"order,omitempty"`
+	Distinct bool     `json:"distinct,omitempty"`
+	Count    bool     `json:"count,omitempty"`
 }
 
 // A Filter restricts the set of records returned by a list/index API.
