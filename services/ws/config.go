1e094e23db422cabe013ecbf7a0b465c4b096e12
Tom Clegg <tom@curoverse.com>
2016-11-13 17:14:18 -0500

    8460: Add default config and -dump-config flag.

--- services/ws/config.go
+++ services/ws/config.go
@@ -15,6 +15,17 @@ type Config struct {
 
 func DefaultConfig() Config {
 	return Config{
+		Client: arvados.Client{
+			APIHost: "localhost:443",
+		},
+		Postgres: pgConfig{
+			"dbname":          "arvados_test",
+			"user":            "arvados",
+			"password":        "xyzzy",
+			"host":            "localhost",
+			"connect_timeout": "30",
+			"sslmode":         "disable",
+		},
 		ClientEventQueue: 64,
 	}
 }
--- services/ws/main.go
+++ services/ws/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -11,10 +12,22 @@ import (
 
 func main() {
 	configPath := flag.String("config", "/etc/arvados/ws/ws.yml", "`path` to config file")
+	dumpConfig := flag.Bool("dump-config", false, "show current configuration and exit")
 	cfg := DefaultConfig()
+	flag.Parse()
+
 	err := config.LoadFile(&cfg, *configPath)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if *dumpConfig {
+		txt, err := config.Dump(&cfg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Print(string(txt))
+		return
 	}
 
 	srv := &http.Server{
