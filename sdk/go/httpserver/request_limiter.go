f3c31e7c71f076f5feafbb3f14e210f4d0de9012
Tom Clegg <tom@curoverse.com>
2016-11-21 14:22:59 -0500

    10484: Report current client count.

--- sdk/go/httpserver/request_limiter.go
+++ sdk/go/httpserver/request_limiter.go
@@ -4,16 +4,40 @@ import (
 	"net/http"
 )
 
+// RequestCounter is an http.Handler that tracks the number of
+// requests in progress.
+type RequestCounter interface {
+	http.Handler
+
+	// Current() returns the number of requests in progress.
+	Current() int
+
+	// Max() returns the maximum number of concurrent requests
+	// that will be accepted.
+	Max() int
+}
+
 type limiterHandler struct {
 	requests chan struct{}
 	handler  http.Handler
 }
 
-func NewRequestLimiter(maxRequests int, handler http.Handler) http.Handler {
+// NewRequestLimiter returns a RequestCounter that delegates up to
+// maxRequests at a time to the given handler, and responds 503 to all
+// incoming requests beyond that limit.
+func NewRequestLimiter(maxRequests int, handler http.Handler) RequestCounter {
 	return &limiterHandler{
 		requests: make(chan struct{}, maxRequests),
 		handler:  handler,
 	}
+}
+
+func (h *limiterHandler) Current() int {
+	return len(h.requests)
+}
+
+func (h *limiterHandler) Max() int {
+	return cap(h.requests)
 }
 
 func (h *limiterHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
