13f7a114f9c03b1efa0d6f696a7f6034b6cc2677
Peter Amstutz <peter.amstutz@curoverse.com>
2016-12-09 14:18:18 -0500

    10576: Bugfix, set self.uuid on ArvadosContainer.

--- sdk/cwl/arvados_cwl/arvcontainer.py
+++ sdk/cwl/arvados_cwl/arvcontainer.py
@@ -118,7 +118,8 @@ class ArvadosContainer(object):
                 body=container_request
             ).execute(num_retries=self.arvrunner.num_retries)
 
-            self.arvrunner.processes[response["uuid"]] = self
+            self.uuid = response["uuid"]
+            self.arvrunner.processes[self.uuid] = self
 
             logger.info("Container request %s (%s) state is %s", self.name, response["uuid"], response["state"])
 
@@ -254,7 +255,7 @@ class RunnerContainer(Runner):
         ).execute(num_retries=self.arvrunner.num_retries)
 
         self.uuid = response["uuid"]
-        self.arvrunner.processes[response["uuid"]] = self
+        self.arvrunner.processes[self.uuid] = self
 
         logger.info("Submitted container %s", response["uuid"])
 
