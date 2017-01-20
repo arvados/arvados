c34b698a734537a9819289e3da76f8a97a32a7d9
Tom Clegg <tom@curoverse.com>
2017-01-20 17:03:24 -0500

    remove bindata, update name, add new bootstrap + mithril

--- services/boot/generate.go
+++ services/boot/generate.go
@@ -1,7 +1,7 @@
 //go:generate sh -c "which go-bindata 2>&1 >/dev/null || go get github.com/jteeuwen/go-bindata/..."
 //go:generate sh -c "which go-bindata-assetfs 2>&1 >/dev/null || go get github.com/elazarl/go-bindata-assetfs/..."
 //go:generate sh -c "[ -d node_modules ] || npm install"
-//go:generate sh -c "rm -r bindata.tmp && mkdir bindata.tmp"
+//go:generate sh -c "if [ -e bindata.tmp ]; then rm -r bindata.tmp; fi && mkdir bindata.tmp"
 //go:generate sh -c "npm run webpack ${WEBPACK_FLAGS:-p}"
 //go:generate sh -c "cp -rpL static/* bindata.tmp/"
 //go:generate go-bindata-assetfs -nometadata bindata.tmp/...
--- services/boot/js/index.js
+++ services/boot/js/index.js
@@ -1,2 +1,50 @@
 // application entry point
+window.jQuery = require('jquery')
+window.Tether = require('tether')
+require('bootstrap')
 require('./example.js')
+var m = require('mithril')
+var Stream = require('mithril/stream')
+
+var checklist = [
+    {
+        name: 'arvados-boot web gui',
+        api: null,
+        lastCheck: (new Date()).valueOf(),
+        error: Stream(null),
+        response: Stream('ok'),
+    },
+    {
+        name: 'arvados-boot web backend',
+        api: '/api/ping',
+    },
+    {
+        name: 'arvados-boot fail canary',
+        api: '/api/error',
+    },
+]
+
+checklist.map(function(check) {
+    if (!check.api) return
+    if (!check.response) check.response = Stream()
+    if (!check.error) check.error = Stream()
+    m.request({method: 'GET', url: check.api}).then(check.response).catch(check.error)
+})
+
+var Home = {
+    view: function(vnode) {
+        return m('.panel', checklist.map(function(check) {
+            return m('div.alert',
+                     {class: (!check.response() || check.error()) ? 'alert-danger' : 'alert-success'}, 
+                     [
+                         check.name,
+                         ': ',
+                         JSON.stringify(check.response()),
+                     ])
+        }))
+    }
+}
+
+m.route(document.getElementById('app'), '/', {
+    '/': Home,
+})
