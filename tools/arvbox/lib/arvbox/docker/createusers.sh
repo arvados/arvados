38bf86b7d9e256aaae50870174ae2cf6a84e8656
Peter Amstutz <peter.amstutz@curoverse.com>
2017-02-14 11:05:56 -0500

    Fix arvbox build.
    * Set up fuse groups separately.
    * Separate generating superuser token from writing file so as not to write file on failure.

--- tools/arvbox/lib/arvbox/docker/createusers.sh
+++ tools/arvbox/lib/arvbox/docker/createusers.sh
@@ -5,21 +5,19 @@ set -e -o pipefail
 if ! grep "^arvbox:" /etc/passwd >/dev/null 2>/dev/null ; then
     HOSTUID=$(ls -nd /usr/src/arvados | sed 's/ */ /' | cut -d' ' -f4)
     HOSTGID=$(ls -nd /usr/src/arvados | sed 's/ */ /' | cut -d' ' -f5)
-    FUSEGID=$(ls -nd /dev/fuse | sed 's/ */ /' | cut -d' ' -f5)
 
     mkdir -p /var/lib/arvados/git /var/lib/gems \
           /var/lib/passenger /var/lib/gopath /var/lib/pip
 
     groupadd --gid $HOSTGID --non-unique arvbox
-    groupadd --gid $FUSEGID --non-unique fuse
     groupadd --gid $HOSTGID --non-unique git
     useradd --home-dir /var/lib/arvados \
             --uid $HOSTUID --gid $HOSTGID \
             --non-unique \
-            --groups docker,fuse \
+            --groups docker \
             arvbox
     useradd --home-dir /var/lib/arvados/git --uid $HOSTUID --gid $HOSTGID --non-unique git
-    useradd --groups docker,fuse crunch
+    useradd --groups docker crunch
 
     chown arvbox:arvbox -R /usr/local /var/lib/arvados /var/lib/gems \
           /var/lib/passenger /var/lib/postgresql \
@@ -33,4 +31,13 @@ if ! grep "^arvbox:" /etc/passwd >/dev/null 2>/dev/null ; then
     chown crunch:crunch -R /tmp/crunch0 /tmp/crunch1
 
     echo "arvbox    ALL=(crunch) NOPASSWD: ALL" >> /etc/sudoers
+fi
+
+if ! grep "^fuse:" /etc/group >/dev/null 2>/dev/null ; then
+    if test -c /dev/fuse ; then
+        FUSEGID=$(ls -nd /dev/fuse | sed 's/ */ /' | cut -d' ' -f5)
+        groupadd --gid $FUSEGID --non-unique fuse
+        adduser arvbox fuse
+        adduser crunch fuse
+    fi
 fi
