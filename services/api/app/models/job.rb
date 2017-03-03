c7a44e530c2ecb036ac46d71a0306c6f64c439ee
Tom Clegg <tom@curoverse.com>
2017-03-03 00:17:43 -0500

    11168: Revert serialization change in order to avoid breaking job reuse.

--- services/api/app/models/job.rb
+++ services/api/app/models/job.rb
@@ -323,7 +323,7 @@ class Job < ArvadosModel
   protected
 
   def self.sorted_hash_digest h
-    Digest::MD5.hexdigest(SafeJSON.dump(deep_sort_hash(h)))
+    Digest::MD5.hexdigest(Oj.dump(deep_sort_hash(h)))
   end
 
   def foreign_key_attributes
