78ae065d149dff4cc1027f9c7941a9116f4ae51a
radhika <radhika@curoverse.com>
2017-04-10 12:00:27 -0400

    11365: "remove selected files" dropdown selection + collection lock button

--- apps/workbench/app/assets/javascripts/edit_collection.js
+++ apps/workbench/app/assets/javascripts/edit_collection.js
@@ -1,20 +1,31 @@
-$(document).ready(function(event) {
-  $(".btn-collection-file-control").addClass("disabled");
-  $(".tab-pane-Upload").addClass("disabled");
-  $("#Upload-tab").attr("data-toggle", "disabled");
-}).on('click', '.lock-collection-btn', function(event) {
-  classes = $(event.target).attr('class')
-  if(classes.includes("fa-lock")){
-    $(".lock-collection-btn").removeClass("fa-lock");
-    $(".lock-collection-btn").addClass("fa-unlock");
-    $(".btn-collection-file-control").removeClass("disabled");
-    $(".tab-pane-Upload").removeClass("disabled");
-    $("#Upload-tab").attr("data-toggle", "tab");
-  } else {
-    $(".lock-collection-btn").removeClass("fa-unlock");
-    $(".lock-collection-btn").addClass("fa-lock");
-    $(".btn-collection-file-control").addClass("disabled");
-    $(".tab-pane-Upload").addClass("disabled");
-    $("#Upload-tab").attr("data-toggle", "disabled");
-  }
-});
+// On loading of a collection, enable the "lock" button and
+// disable all file modification controls (upload, rename, delete)
+$(document).
+    ready(function(event) {
+        $(".btn-collection-file-control").addClass("disabled");
+        $(".tab-pane-Upload").addClass("disabled");
+        $("#Upload-tab").attr("data-toggle", "disabled");
+    }).
+    on('click', '.lock-collection-btn', function(event) {
+        classes = $(event.target).attr('class')
+
+        if(classes.includes("fa-lock")){
+            // About to unlock; warn the user
+            if (confirm("Adding, renaming, and deleting files changes the portalble data hash. Are you sure you want to unlock the collection?")) {
+                $(".lock-collection-btn").removeClass("fa-lock");
+                $(".lock-collection-btn").addClass("fa-unlock");
+                $(".btn-collection-file-control").removeClass("disabled");
+                $(".tab-pane-Upload").removeClass("disabled");
+                $("#Upload-tab").attr("data-toggle", "tab");
+            } else {
+                // User clicked "no" and so do not unlock
+            }
+        } else {
+            // Lock it back
+            $(".lock-collection-btn").removeClass("fa-unlock");
+            $(".lock-collection-btn").addClass("fa-lock");
+            $(".btn-collection-file-control").addClass("disabled");
+            $(".tab-pane-Upload").addClass("disabled");
+            $("#Upload-tab").attr("data-toggle", "disabled");
+        }
+    });
