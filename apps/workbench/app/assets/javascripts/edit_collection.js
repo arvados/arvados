bf55ee80e71c6503292a44fb377927f2ed908f9b
radhika <radhika@curoverse.com>
2017-04-16 21:20:11 -0400

    11465: add tooltips to disabled collection file controls

--- apps/workbench/app/assets/javascripts/edit_collection.js
+++ apps/workbench/app/assets/javascripts/edit_collection.js
@@ -2,10 +2,9 @@
 // disable all file modification controls (upload, rename, delete)
 $(document).
     ready(function(event) {
-        $(".btn-collection-file-rename").addClass("disabled");
-        $(".btn-collection-file-rename").attr("title", "Unlock collection to rename file");
-        $(".btn-collection-file-remove").addClass("disabled");
-        $(".btn-collection-file-remove").attr("title", "Unlock collection to remove file");
+        $(".btn-collection-file-control").addClass("disabled");
+        $(".btn-collection-rename-file-span").attr("title", "Unlock collection to rename file");
+        $(".btn-collection-remove-file-span").attr("title", "Unlock collection to remove file");
         $(".btn-remove-selected-files").attr("title", "Unlock collection to remove selected files");
         $(".tab-pane-Upload").addClass("disabled");
         $(".tab-pane-Upload").attr("title", "Unlock collection to upload files");
@@ -20,10 +19,9 @@ $(document).
                 $(".lock-collection-btn").removeClass("fa-lock");
                 $(".lock-collection-btn").addClass("fa-unlock");
                 $(".lock-collection-btn").attr("title", "Lock collection to prevent editing files");
-                $(".btn-collection-file-rename").removeClass("disabled");
-                $(".btn-collection-file-rename").attr("data-original-title", "Edit name or path or both for this file");
-                $(".btn-collection-file-remove").removeClass("disabled");
-                $(".btn-collection-file-remove").attr("data-original-title", "Remove this file");
+                $(".btn-collection-rename-file-span").attr("title", "");
+                $(".btn-collection-remove-file-span").attr("title", "");
+                $(".btn-collection-file-control").removeClass("disabled");
                 $(".btn-remove-selected-files").attr("title", "");
                 $(".tab-pane-Upload").removeClass("disabled");
                 $(".tab-pane-Upload").attr("data-original-title", "");
@@ -36,10 +34,9 @@ $(document).
             $(".lock-collection-btn").removeClass("fa-unlock");
             $(".lock-collection-btn").addClass("fa-lock");
             $(".lock-collection-btn").attr("title", "Unlock collection to edit files");
-            $(".btn-collection-file-rename").addClass("disabled");
-            $(".btn-collection-file-rename").attr("data-original-title", "Unlock collection to rename file");
-            $(".btn-collection-file-remove").addClass("disabled");
-            $(".btn-collection-file-remove").attr("data-original-title", "Unlock collection to remove file");
+            $(".btn-collection-rename-file-span").attr("title", "Unlock collection to rename file");
+            $(".btn-collection-remove-file-span").attr("title", "Unlock collection to remove file");
+            $(".btn-collection-file-control").addClass("disabled");
             $(".btn-remove-selected-files").attr("title", "Unlock collection to remove selected files");
             $(".tab-pane-Upload").addClass("disabled");
             $(".tab-pane-Upload").attr("data-original-title", "Unlock collection to upload files");
