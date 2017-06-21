f028cd05df30e4fba77d85743be1c509cd7adfdf
radhika <radhika@curoverse.com>
2017-06-21 12:47:35 -0400

    9426: update test assertion
    Arvados-DCO-1.1-Signed-off-by: Radhika Chippada <radhika@curoverse.com>

--- apps/workbench/test/controllers/collections_controller_test.rb
+++ apps/workbench/test/controllers/collections_controller_test.rb
@@ -814,7 +814,10 @@ class CollectionsControllerTest < ActionController::TestCase
     assert_equal false, response.body.include?("value for existing tag 1")
 
     updated_properties = Collection.find(collection['uuid']).properties
-    assert_equal new_tags, updated_properties[:tags]
+    assert_equal true, new_tags.keys.include?(:'new_tag1')
+    assert_equal true, new_tags.keys.include?(:'new_tag2')
+    assert_equal false, new_tags.keys.include?(:'existing tag 1')
+    assert_equal false, new_tags.keys.include?(:'existing tag 2')
     assert_equal true, updated_properties.keys.include?(:'some other property')
     assert_equal 'value for the other property', updated_properties[:'some other property']
   end
