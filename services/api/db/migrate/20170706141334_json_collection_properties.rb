5be901f5813fe9d026e2aa2a6a65d4ea4da4a001
Tom Clegg <tom@curoverse.com>
2017-07-11 10:25:16 -0400

    11908: Fix crash if index being deleted is already deleted.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@curoverse.com>

--- services/api/db/migrate/20170706141334_json_collection_properties.rb
+++ services/api/db/migrate/20170706141334_json_collection_properties.rb
@@ -2,7 +2,7 @@ require './db/migrate/20161213172944_full_text_search_indexes'
 
 class JsonCollectionProperties < ActiveRecord::Migration
   def up
-    ActiveRecord::Base.connection.execute 'DROP INDEX collections_full_text_search_idx'
+    ActiveRecord::Base.connection.execute 'DROP INDEX IF EXISTS collections_full_text_search_idx'
     ActiveRecord::Base.connection.execute 'ALTER TABLE collections ALTER COLUMN properties TYPE jsonb USING properties::jsonb'
     FullTextSearchIndexes.new.replace_index('collections')
   end
