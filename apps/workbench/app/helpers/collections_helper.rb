40e2b5dfa4de9d3f8bdbe01296f7e70b87c2446c
radhika <radhika@curoverse.com>
2017-07-20 10:30:42 -0400

    1195: case insensitive cwl file extension check
    Arvados-DCO-1.1-Signed-off-by: Radhika Chippada <radhika@curoverse.com>

--- apps/workbench/app/helpers/collections_helper.rb
+++ apps/workbench/app/helpers/collections_helper.rb
@@ -62,7 +62,7 @@ module CollectionsHelper
   # Check if collection preview is allowed for the given filename with extension
   #
   def preview_allowed_for file_name
-    return true if file_name.end_with?('.cwl') # unknown mime type, but we want to offer preview
+    return true if file_name.downcase.end_with?('.cwl') # unknown mime type, but we want to offer preview
 
     file_type = MIME::Types.type_for(file_name).first
     if file_type.nil?
