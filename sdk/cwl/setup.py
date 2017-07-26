6164f448f5828e1f4649fae4fe7866b8f082614c
Peter Amstutz <peter.amstutz@curoverse.com>
2017-07-26 11:11:24 -0400

    12037: Support CWL v1.0.1 spec.
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curoverse.com>

--- sdk/cwl/setup.py
+++ sdk/cwl/setup.py
@@ -51,8 +51,8 @@ setup(name='arvados-cwl-runner',
       # Note that arvados/build/run-build-packages.sh looks at this
       # file to determine what version of cwltool and schema-salad to build.
       install_requires=[
-          'cwltool==1.0.20170707200431',
-          'schema-salad==2.6.20170630075932',
+          'cwltool==1.0.20170723124118',
+          'schema-salad==2.6.20170712194300',
           'typing==3.5.3.0',
           'ruamel.yaml==0.13.7',
           'arvados-python-client>=0.1.20170526013812',
