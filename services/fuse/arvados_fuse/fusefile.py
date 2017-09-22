5c4cbf1ae8496930deb7e35c9880af05941cda75
Peter Amstutz <peter.amstutz@curoverse.com>
2017-09-22 11:21:07 -0400

    12276: Reduce number of spurious invalidations sent to kernel.
    * New policy to only send invalidations on objects that have a nonzero
      kernel reference count.
    * When clearing the contents of a CollectionDirectory, only invalidate the
      topmost directory entry, this should take care of invalidating all paths
      beneath it as well.
    * Don't send invalidate_inode() when deleting an inode (by definition, the
      inode is unreferenced by the kernel).
    * Remove allow_dirent_cache for now (this only relates to caching negative
      lookups, which we don't support).
    * Set attribute attr_timeout based on the polling period of the underlying
      object.
    * FuncToJSONFile sets allow_attr_cache = False instead of allow_dirent_cache =
      False (which is useless in this case).
    
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <pamstutz@veritasgenetics.com>

--- services/fuse/arvados_fuse/fusefile.py
+++ services/fuse/arvados_fuse/fusefile.py
@@ -122,12 +122,11 @@ class FuncToJSONFile(StringFile):
         super(FuncToJSONFile, self).__init__(parent_inode, "", 0)
         self.func = func
 
-        # invalidate_inode() and invalidate_entry() are asynchronous
-        # with no callback to wait for. In order to guarantee
-        # userspace programs don't get stale data that was generated
-        # before the last invalidate(), we must disallow dirent
+        # invalidate_inode() is asynchronous with no callback to wait for. In
+        # order to guarantee userspace programs don't get stale data that was
+        # generated before the last invalidate(), we must disallow inode
         # caching entirely.
-        self.allow_dirent_cache = False
+        self.allow_attr_cache = False
 
     def size(self):
         self._update()
