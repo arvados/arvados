0017c5ba7d966af1ea3a673f02f0f4102b34ff61
Tom Clegg <tclegg@veritasgenetics.com>
2017-09-26 15:39:00 -0400

    12032: Fix compatibility with PostgreSQL 9.4.
    9.4 doesn't have idle_in_transaction_session_timeout or row_security
    configs, so couldn't execute structure.sql.
    
    9.4 doesn't write "Dumped from/by version X", so 9.4 and 9.6 dev boxes
    want an edit war.
    
    refs #12032
    
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tclegg@veritasgenetics.com>

--- services/api/db/structure.sql
+++ services/api/db/structure.sql
@@ -2,20 +2,11 @@
 --
 -- SPDX-License-Identifier: AGPL-3.0
 
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 9.6.4
--- Dumped by pg_dump version 9.6.4
-
 SET statement_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
-SET row_security = off;
 
 --
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
