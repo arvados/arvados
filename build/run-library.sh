8f6613b3bb234877cb4b3a3afe4d4edaf43dc0bb
Fernando Monserrat <fernando.monserrat@gmail.com>
2017-10-05 15:50:54 -0300

    12389: send receive as param buildNumber as third digit Arvados-DCO-1.1-Signed-off-by: Fernando Monserrat <fmonserrat@veritasgenetics.com>

--- build/run-library.sh
+++ build/run-library.sh
@@ -46,10 +46,11 @@ version_from_latest_tag() {
   #get number parts and increase last one by 1
   vnum1=${version_bits[0]}
   vnum2=${version_bits[1]}
-  vnum3=$BUILD_NUMBER
+  vnum3=${BUILD_NUMBER}
   #create new tag
   echo "$vnum3"
-  echo "BUILDID: $BUILD_NUMBER"
+  echo "BUILDID: ${BUILD_NUMBER}"
+    echo "BUILDIDconpesos1: $1"
   new_version_tag="$vnum1.$vnum2.$vnum3"
   git rev-parse HEAD >Changelog
   echo "ver version: ${new_version_tag}"  
