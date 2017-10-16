54e8da57251023d4d7976c49f3481da5e1ce63e4
Fernando Monserrat <fernando.monserrat@gmail.com>
2017-10-16 14:53:31 -0300

    12389: try --exclude opt/rh/python33 Arvados-DCO-1.1-Signed-off-by: Fernando Monserrat <fmonserrat@veritasgenetics.com>

--- sdk/cwl/setup.py
+++ sdk/cwl/setup.py
@@ -22,8 +22,7 @@ except ImportError:
 versionfile = os.path.join(SETUP_DIR, "arvados_cwl/_version.py")
 try:
     gitinfo = subprocess.check_output(
-        ['git', 'log', '--first-parent', '--max-count=1',
-         '--format=format:%H', gittaggers.choose_version_from()]).strip()
+            ['git', 'describe', '--abbrev=0'])
     with open(versionfile, "w") as f:
         f.write("__version__ = '%s'\n" % gitinfo)
 except Exception as e:
