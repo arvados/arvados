9aeed914f8cb51d8677db063c6f6f2c021a7f9cb
Fernando Monserrat <fernando.monserrat@gmail.com>
2017-10-17 15:41:05 -0300

    12389: remove forth digit from build number Arvados-DCO-1.1-Signed-off-by: Fernando Monserrat <fmonserrat@veritasgenetics.com>

--- build/run-library.sh
+++ build/run-library.sh
@@ -47,9 +47,9 @@ version_from_latest_tag() {
   vnum1=${version_bits[0]}
   vnum2=${version_bits[1]}
   vnum3=${version_bits[2]}
-  vnum4=$(curl --silent https://ci.curoverse.com/job/new-versioning-build-packages-centos7/lastBuild/buildNumber)
+  #vnum4=$(curl --silent https://ci.curoverse.com/job/new-versioning-build-packages-centos7/lastBuild/buildNumber)
   #create new tag
-  new_version_tag="$vnum1.$vnum2.$vnum3-$vnum4"
+  new_version_tag="$vnum1.$vnum2.$vnum3"
   git rev-parse HEAD >Changelog
   echo "${new_version_tag}"   
 }
