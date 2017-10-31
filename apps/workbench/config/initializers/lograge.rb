0e12d049716003b08c4fc881343ebf14c3522b99
Tom Clegg <tclegg@veritasgenetics.com>
2017-10-30 23:07:52 -0400

    12167: Improve request-id tracking in Workbench.
    Include current request ID in API requests via X-Request-Id header
    instead of a form parameter.  (Also move Arvados API token from form
    to Authorization request header.)
    
    Always log request ID as its own field, instead of adding it to params
    (where it might get truncated before being logged).
    
    Use new request ID format: "req-" + 20-random-alphanum-chars.
    
    Use an around_filter to set/clear the current request ID in one place,
    instead of setting it in controller code and clearing it in logging
    code.
    
    Return request ID to browser via X-Request-Id header.
    
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tclegg@veritasgenetics.com>

--- apps/workbench/config/initializers/lograge.rb
+++ apps/workbench/config/initializers/lograge.rb
@@ -6,15 +6,20 @@ ArvadosWorkbench::Application.configure do
   config.lograge.enabled = true
   config.lograge.formatter = Lograge::Formatters::Logstash.new
   config.lograge.custom_options = lambda do |event|
+    payload = {
+      request_id: event.payload[:request_id],
+    }
+    # Also log params (minus the pseudo-params added by Rails). But if
+    # params is huge, don't log the whole thing, just hope we get the
+    # most useful bits in truncate(json(params)).
     exceptions = %w(controller action format id)
-    params = {current_request_id: Thread.current[:current_request_id]}.
-             merge(event.payload[:params].except(*exceptions))
+    params = event.payload[:params].except(*exceptions)
     params_s = Oj.dump(params)
-    Thread.current[:current_request_id] = nil # Clear for next request
     if params_s.length > 1000
-      { params_truncated: params_s[0..1000] + "[...]" }
+      payload[:params_truncated] = params_s[0..1000] + "[...]"
     else
-      { params: params }
+      payload[:params] = params
     end
+    payload
   end
 end
