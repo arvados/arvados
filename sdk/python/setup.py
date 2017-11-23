03e384ee8ff2986805554a0b2451e9f96b5bb36f
Fernando Monserrat <fernando.monserrat@gmail.com>
2017-11-23 11:27:24 -0300

    12389: new build for Roche including changes from master up to 197d9db51a\nArvados-DCO-1.1-Signed-off-by: Fernando Monserrat <fmonserrat@veritasgenetics.com>

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -24,7 +24,7 @@ if '--short-tests-only' in sys.argv:
     sys.argv.remove('--short-tests-only')
 
 setup(name='arvados-python-client',
-      version="1.1.0",
+      version="1.1.1",
       description='Arvados client library',
       long_description=open(README).read(),
       author='Arvados',
--- services/nodemanager/setup.py
+++ services/nodemanager/setup.py
@@ -19,7 +19,7 @@ except ImportError:
     tagger = egg_info_cmd.egg_info
 
 setup(name='arvados-node-manager',
-      version="1.1.0",
+      version="1.1.1",
       description='Arvados compute node manager',
       long_description=open(README).read(),
       author='Arvados',
