0ddcedfe047540bb1e415f19a57f02d9ae30b8f0
Fernando Monserrat <fernando.monserrat@gmail.com>
2017-12-01 13:08:38 -0300

    12771: fix for add version as param Arvados-DCO-1.1-Signed-off-by: Fernando Monserrat <fmonserrat@veritasgenetics.com>

--- build/run-library.sh
+++ build/run-library.sh
@@ -39,8 +39,8 @@ format_last_commit_here() {
 }
 
 version_from_latest_tag() {
-  if [[ $VERNO != "" ]] ; then
-      new_version_tag=$VERSION
+  if [ -n "$VERNO" ]; then
+      new_version_tag=$VERNO
       echo "${new_version_tag}" 
   else
       # Generates a version number from the git log latest tag
