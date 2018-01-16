b017ae29973838673d66d9f9da4a07f9852d9476
Tom Clegg <tclegg@veritasgenetics.com>
2018-01-16 14:32:33 -0500

    12479: Fix per-tag strict mode.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tclegg@veritasgenetics.com>

--- apps/workbench/app/assets/javascripts/components/edit_tags.js
+++ apps/workbench/app/assets/javascripts/components/edit_tags.js
@@ -37,12 +37,14 @@ window.SelectOrAutocomplete = {
             minChars: 0,
             autoFirst: true,
         })
+        vnode.state.create = vnode.attrs.create
+        vnode.state.options = vnode.attrs.options
         // Option is selected from the list.
         $(vnode.dom).on('awesomplete-selectcomplete', function(event) {
             vnode.attrs.value(this.value)
         })
         $(vnode.dom).on('change', function(event) {
-            if (!vnode.attrs.create && !(this.value in vnode.attrs.options)) {
+            if (!vnode.state.create && !(this.value in vnode.state.options)) {
                 this.value = vnode.attrs.value()
             } else {
                 if (vnode.attrs.value() !== this.value) {
@@ -59,6 +61,8 @@ window.SelectOrAutocomplete = {
     },
     onupdate: function(vnode) {
         vnode.state.awesomplete.list = vnode.attrs.options
+        vnode.state.create = vnode.attrs.create
+        vnode.state.options = vnode.attrs.options
     },
 }
 
