1ea42486b1badd6ebd1a0071f3dd7bc1ec41b920
Fernando Monserrat <fernando.monserrat@gmail.com>
2018-03-12 13:05:56 -0300

    13200:test new job with timestamp only removing hash Arvados-DCO-1.1-Signed-off-by: Fernando Monserrat <fmonserrat@veritasgenetics.com>

--- build/run-library.sh
+++ build/run-library.sh
@@ -46,19 +46,12 @@ version_from_git() {
     # dev/prerelease, output a version number based on the git log for
     # the current working directory.
     if [[ -n "$ARVADOS_BUILDING_VERSION" ]]; then
-        echo "$ARVADOS_BUILDING_VERSION"
-        return
+      local git_ts git_hash prefix
+      declare $(format_last_commit_here "git_ts=%ct")
+      echo "$ARVADOS_BUILDING_VERSION.$(date -ud "@$git_ts" +%Y%m%d%H%M%S)"
+      return
     fi
 
-    local git_ts git_hash prefix
-    if [[ -n "$1" ]] ; then
-        prefix="$1"
-    else
-        prefix="0.1"
-    fi
-
-    declare $(format_last_commit_here "git_ts=%ct")
-    echo "${prefix}.$(date -ud "@$git_ts" +%Y%m%d%H%M%S)"
 }
 
 nohash_version_from_git() {
