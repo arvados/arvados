5667012a090b6c561cdf0f84554d4673b2020954
Fernando Monserrat <fernando.monserrat@gmail.com>
2018-03-16 13:24:17 -0300

    13200:fix python gittagers version with latest git tag and timestamp Arvados-DCO-1.1-Signed-off-by: Fernando Monserrat <fmonserrat@veritasgenetics.com>

--- services/nodemanager/setup.py
+++ services/nodemanager/setup.py
@@ -15,7 +15,7 @@ README = os.path.join(SETUP_DIR, 'README.rst')
 tagger = egg_info_cmd.egg_info
 version = os.environ.get("ARVADOS_BUILDING_VERSION")
 if not version:
-    version = ""
+    version = "0.1"
     try:
         import gittaggers
         tagger = gittaggers.EggInfoFromGit
