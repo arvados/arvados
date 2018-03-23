7a38d6b19120b3284533196ff3c94c2043826aef
Fernando Monserrat <fernando.monserrat@gmail.com>
2018-03-23 13:48:30 -0300

    13200:test to remove prefix on python Arvados-DCO-1.1-Signed-off-by: Fernando Monserrat <fmonserrat@veritasgenetics.com>

--- build/run-library.sh
+++ build/run-library.sh
@@ -391,8 +391,6 @@ fpm_build () {
           python=python2.7
           set -- "$@" --python-bin python2.7 \
               --python-easyinstall "$EASY_INSTALL2" \
-              --python-package-name-prefix "$PYTHON2_PKG_PREFIX" \
-              --prefix "$PYTHON2_PREFIX" \
               --python-install-lib "$PYTHON2_INSTALL_LIB" \
               --python-install-data . \
               --exclude "${PYTHON2_INSTALL_LIB#/}/tests" \
@@ -407,8 +405,6 @@ fpm_build () {
           python=python3
           set -- "$@" --python-bin python3 \
               --python-easyinstall "$EASY_INSTALL3" \
-              --python-package-name-prefix "$PYTHON3_PKG_PREFIX" \
-              --prefix "$PYTHON3_PREFIX" \
               --python-install-lib "$PYTHON3_INSTALL_LIB" \
               --python-install-data . \
               --exclude "${PYTHON3_INSTALL_LIB#/}/tests" \
