62908a022cf7fc2fe7bf81e9e97f92bd7ef2912c
Fuad Muhic <muhic.fuad@gmail.com>
2018-03-27 11:11:18 +0200

    Removed obsolated methods from HttpRequest class and updated tests to reflect that change.
    Arvados-DCO-1.1-Signed-off-by: Fuad Muhic <fmuhic@capeannenterprises.com>

--- sdk/R/R/HttpRequest.R
+++ sdk/R/R/HttpRequest.R
@@ -15,21 +15,6 @@ HttpRequest <- R6::R6Class(
             self$validVerbs <- c("GET", "POST", "PUT", "DELETE", "PROPFIND", "MOVE")
         },
 
-        execute = function(verb, url, headers = NULL, body = NULL, query = NULL,
-                           limit = NULL, offset = NULL, retryTimes = 0)
-        {
-            if(!(verb %in% self$validVerbs))
-                stop("Http verb is not valid.")
-
-            headers  <- httr::add_headers(unlist(headers))
-            urlQuery <- self$createQuery(query, limit, offset)
-            url      <- paste0(url, urlQuery)
-
-            # times = 1 regular call + numberOfRetries
-            response <- httr::RETRY(verb, url = url, body = body,
-                                    config = headers, times = retryTimes + 1)
-        },
-
         exec = function(verb, url, headers = NULL, body = NULL, queryParams = NULL,
                         retryTimes = 0)
         {
@@ -37,7 +22,7 @@ HttpRequest <- R6::R6Class(
                 stop("Http verb is not valid.")
 
             headers  <- httr::add_headers(unlist(headers))
-            urlQuery <- self$genQuery(queryParams)
+            urlQuery <- self$createQuery(queryParams)
             url      <- paste0(url, urlQuery)
 
             # times = 1 regular call + numberOfRetries
@@ -45,24 +30,7 @@ HttpRequest <- R6::R6Class(
                                     config = headers, times = retryTimes + 1)
         },
 
-        createQuery = function(filters, limit, offset)
-        {
-            finalQuery <- NULL
-
-            finalQuery <- c(finalQuery, private$createFiltersQuery(filters))
-            finalQuery <- c(finalQuery, private$createLimitQuery(limit))
-            finalQuery <- c(finalQuery, private$createOffsetQuery(offset))
-
-            finalQuery <- finalQuery[!is.null(finalQuery)]
-            finalQuery <- paste0(finalQuery, collapse = "&")
-
-            if(finalQuery != "")
-                finalQuery <- paste0("/?", finalQuery)
-
-            finalQuery
-        },
-
-        genQuery = function(queryParams)
+        createQuery = function(queryParams)
         {
             queryParams <- Filter(Negate(is.null), queryParams)
 
@@ -83,52 +51,6 @@ HttpRequest <- R6::R6Class(
             }
 
             return("")
-        }
-    ),
-
-    private = list(
-
-        createFiltersQuery = function(filters)
-        {
-            if(!is.null(filters))
-            {
-                filters <- RListToPythonList(filters, ",")
-                encodedQuery <- URLencode(filters, reserved = T, repeated = T)
-
-                return(paste0("filters=", encodedQuery))
-            }
-
-            return(NULL)
-        },
-
-        createLimitQuery = function(limit)
-        {
-            if(!is.null(limit))
-            {
-                limit <- suppressWarnings(as.numeric(limit))
-
-                if(is.na(limit))
-                    stop("Limit must be a numeric type.")
-                
-                return(paste0("limit=", limit))
-            }
-
-            return(NULL)
-        },
-
-        createOffsetQuery = function(offset)
-        {
-            if(!is.null(offset))
-            {
-                offset <- suppressWarnings(as.numeric(offset))
-
-                if(is.na(offset))
-                    stop("Offset must be a numeric type.")
-                
-                return(paste0("offset=", offset))
-            }
-
-            return(NULL)
         }
     ),
 
