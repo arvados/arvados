3d0c6eb03b4147bdda952aeca0cb933c1636242b
Peter Amstutz <pamstutz@veritasgenetics.com>
2018-04-03 09:15:51 -0400

    Add missing file refs #13301
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <pamstutz@veritasgenetics.com>

---
+++ sdk/cwl/tests/wf/check_mem.py
@@ -0,0 +1,12 @@
+import arvados
+import sys
+import os
+
+if "JOB_UUID" in os.environ:
+    requested = arvados.api().jobs().get(uuid=os.environ["JOB_UUID"]).execute()["runtime_constraints"]["min_ram_mb_per_node"]
+else:
+    requested = arvados.api().containers().current().execute()["runtime_constraints"]["ram"]/(1024*1024)
+
+print("Requested %d expected %d" % (requested, int(sys.argv[1])))
+
+exit(0 if requested == int(sys.argv[1]) else 1)
