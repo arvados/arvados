ab241553f88f9806aa4fe13756c318c0d754e5cc
Fernando Monserrat <fernando.monserrat@gmail.com>
2018-04-04 18:58:42 -0300

    13200: remove_version from python packages Arvados-DCO-1.1-Signed-off-by: Fernando Monserrat <fmonserrat@veritasgenetics.com>

--- services/nodemanager/arvnodeman/_version.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) The Arvados Authors. All rights reserved.
-#
-# SPDX-License-Identifier: AGPL-3.0
-
-import pkg_resources
-
-__version__ = pkg_resources.require('arvados-node-manager')[0].version
