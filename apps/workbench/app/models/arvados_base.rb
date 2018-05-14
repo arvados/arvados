d6e33993b181510069bf285c159acb8094e9edf8
Lucas Di Pentima <ldipentima@veritasgenetics.com>
2018-05-14 11:17:35 -0300

    12737: Upgrade workbench from rails 4.1 to 4.2 (WIP)
    Serialization isn't working properly, Hash & Array columns happily receive
    these types but return their text versions when accessed.
    
    Arvados-DCO-1.1-Signed-off-by: Lucas Di Pentima <ldipentima@veritasgenetics.com>

--- apps/workbench/app/models/arvados_base.rb
+++ apps/workbench/app/models/arvados_base.rb
@@ -62,10 +62,10 @@ class ArvadosBase < ActiveRecord::Base
 
   def self.columns
     return @columns if @columns.andand.any?
-    @columns = []
+    columns = []
     @attribute_info ||= {}
     schema = arvados_api_client.discovery[:schemas][self.to_s.to_sym]
-    return @columns if schema.nil?
+    return columns if schema.nil?
     schema[:properties].each do |k, coldef|
       case k
       when :etag, :kind
@@ -73,10 +73,10 @@ class ArvadosBase < ActiveRecord::Base
       else
         if coldef[:type] == coldef[:type].downcase
           # boolean, integer, etc.
-          @columns << column(k, coldef[:type].to_sym)
+          columns << column(k, coldef[:type].to_sym)
         else
           # Hash, Array
-          @columns << column(k, :text)
+          columns << column(k, :text, default)
           serialize k, coldef[:type].constantize
         end
         define_method k do
@@ -90,11 +90,16 @@ class ArvadosBase < ActiveRecord::Base
         @attribute_info[k] = coldef
       end
     end
-    @columns
+    @columns = columns
   end
 
   def self.column(name, sql_type = nil, default = nil, null = true)
-    ActiveRecord::ConnectionAdapters::Column.new(name.to_s, default, sql_type.to_s, null)
+    if sql_type == :datetime
+      cast_type = "ActiveRecord::Type::DateTime".constantize.new
+    else
+      cast_type = "ActiveRecord::Type::#{sql_type.to_s.camelize}".constantize.new
+    end
+    ActiveRecord::ConnectionAdapters::Column.new(name.to_s, default, cast_type, sql_type.to_s, null)
   end
 
   def self.attribute_info
