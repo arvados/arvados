abf519da836b635eac4cf1f728615472169ba032
Daniel Kos <daniel.kos@contractors.roche.com>
2018-06-06 08:44:10 +0200

    Fix tests passing
    Feature #13535
    
    Arvados-DCO-1.1-Signed-off-by: Daniel Kos <daniel.kos@contractors.roche.com>:

--- src/views/workbench/workbench.test.tsx
+++ src/views/workbench/workbench.test.tsx
@@ -8,10 +8,19 @@ import Workbench from '../../views/workbench/workbench';
 import { Provider } from "react-redux";
 import configureStore from "../../store/store";
 import createBrowserHistory from "history/createBrowserHistory";
+import { ConnectedRouter } from "react-router-redux";
+
+const history = createBrowserHistory();
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     const store = configureStore({ projects: [], router: { location: null } }, createBrowserHistory());
-    ReactDOM.render(<Provider store={store}><Workbench/></Provider>, div);
+    ReactDOM.render(
+        <Provider store={store}>
+            <ConnectedRouter history={history}>
+                <Workbench/>
+            </ConnectedRouter>
+        </Provider>,
+    div);
     ReactDOM.unmountComponentAtNode(div);
 });
