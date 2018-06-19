7966796b929e29fd647ed9d7839406d30921c52b
Daniel Kos <daniel.kos@contractors.roche.com>
2018-06-19 08:55:52 +0200

    Added collection service instance
    Feature #13632
    
    Arvados-DCO-1.1-Signed-off-by: Daniel Kos <daniel.kos@contractors.roche.com>

--- src/services/services.ts
+++ src/services/services.ts
@@ -4,6 +4,8 @@
 
 import AuthService from "./auth-service/auth-service";
 import ProjectService from "./project-service/project-service";
+import CollectionService from "./collection-service/collection-service";
 
 export const authService = new AuthService();
 export const projectService = new ProjectService();
+export const collectionService = new CollectionService();
