acf6467a572a10268e8cd8687d9ea5864f729639
Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>
2018-07-17 10:03:59 +0200

    deleted unnecessary code
    Feature #13781
    
    Arvados-DCO-1.1-Signed-off-by: Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>

--- src/views-components/dialog-create/dialog-project-create.tsx
+++ src/views-components/dialog-create/dialog-project-create.tsx
@@ -8,7 +8,7 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import { Button, StyleRulesCallback, WithStyles, withStyles, CircularProgress, Typography } from '@material-ui/core';
+import { Button, StyleRulesCallback, WithStyles, withStyles, CircularProgress } from '@material-ui/core';
 
 import Validator from '../../utils/dialog-validator';
 
