99e5151440dd831a295313f4f2e86da5922806f7
Daniel Kos <daniel.kos@contractors.roche.com>
2018-07-23 15:31:01 +0200

    Remove default export
    Feature #13781
    
    Arvados-DCO-1.1-Signed-off-by: Daniel Kos <daniel.kos@contractors.roche.com>

--- src/views-components/dialog-create/dialog-project-create.tsx
+++ src/views-components/dialog-create/dialog-project-create.tsx
@@ -64,64 +64,65 @@ interface TextFieldProps {
     meta?: any;
 }
 
-class DialogProjectCreate extends React.Component<DialogProjectProps & WithStyles<CssRules>> {
-    render() {
-        const { classes, open, handleClose, handleSubmit, onSubmit, submitting } = this.props;
-
-        return (
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                disableBackdropClick={true}
-                disableEscapeKeyDown={true}>
-                <div className={classes.dialog}>
-                    <form onSubmit={handleSubmit((data: any) => onSubmit(data))}>
-                        <DialogTitle id="form-dialog-title" className={classes.dialogTitle}>Create a project</DialogTitle>
-                        <DialogContent className={classes.formContainer}>
-                            <Field name="name"
-                                component={this.renderTextField}
-                                floatinglabeltext="Project Name"
-                                validate={NAME}
-                                className={classes.textField}
-                                label="Project Name" />
-                            <Field name="description"
-                                component={this.renderTextField}
-                                floatinglabeltext="Description - optional"
-                                validate={DESCRIPTION}
-                                className={classes.textField}
-                                label="Description - optional" />
-                        </DialogContent>
-                        <DialogActions className={classes.dialogActions}>
-                            <Button onClick={handleClose} className={classes.button} color="primary" disabled={submitting}>CANCEL</Button>
-                            <Button type="submit"
-                                className={classes.lastButton}
-                                color="primary"
-                                disabled={submitting}
-                                variant="contained">
-                                CREATE A PROJECT
-                            </Button>
-                            {submitting && <CircularProgress size={20} className={classes.createProgress} />}
-                        </DialogActions>
-                    </form>
-                </div>
-            </Dialog>
-        );
-    }
-
-    renderTextField = ({ input, label, meta: { touched, error }, ...custom }: TextFieldProps) => (
-        <TextField
-            helperText={touched && error}
-            label={label}
-            className={this.props.classes.textField}
-            error={touched && !!error}
-            autoComplete='off'
-            {...input}
-            {...custom}
-        />
-    )
-}
-
-export default compose(
+export const DialogProjectCreate = compose(
     reduxForm({ form: 'projectCreateDialog' }),
-    withStyles(styles)
-)(DialogProjectCreate);
+    withStyles(styles))(
+    class DialogProjectCreate extends React.Component<DialogProjectProps & WithStyles<CssRules>> {
+        render() {
+            const { classes, open, handleClose, handleSubmit, onSubmit, submitting } = this.props;
+
+            return (
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    disableBackdropClick={true}
+                    disableEscapeKeyDown={true}>
+                    <div className={classes.dialog}>
+                        <form onSubmit={handleSubmit((data: any) => onSubmit(data))}>
+                            <DialogTitle id="form-dialog-title" className={classes.dialogTitle}>Create a
+                                project</DialogTitle>
+                            <DialogContent className={classes.formContainer}>
+                                <Field name="name"
+                                       component={this.renderTextField}
+                                       floatinglabeltext="Project Name"
+                                       validate={NAME}
+                                       className={classes.textField}
+                                       label="Project Name"/>
+                                <Field name="description"
+                                       component={this.renderTextField}
+                                       floatinglabeltext="Description - optional"
+                                       validate={DESCRIPTION}
+                                       className={classes.textField}
+                                       label="Description - optional"/>
+                            </DialogContent>
+                            <DialogActions className={classes.dialogActions}>
+                                <Button onClick={handleClose} className={classes.button} color="primary"
+                                        disabled={submitting}>CANCEL</Button>
+                                <Button type="submit"
+                                        className={classes.lastButton}
+                                        color="primary"
+                                        disabled={submitting}
+                                        variant="contained">
+                                    CREATE A PROJECT
+                                </Button>
+                                {submitting && <CircularProgress size={20} className={classes.createProgress}/>}
+                            </DialogActions>
+                        </form>
+                    </div>
+                </Dialog>
+            );
+        }
+
+        renderTextField = ({ input, label, meta: { touched, error }, ...custom }: TextFieldProps) => (
+            <TextField
+                helperText={touched && error}
+                label={label}
+                className={this.props.classes.textField}
+                error={touched && !!error}
+                autoComplete='off'
+                {...input}
+                {...custom}
+            />
+        )
+    }
+);
