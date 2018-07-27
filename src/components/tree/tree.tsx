42de1303354a30d004095003d6143deaa3f62992
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-07-27 11:30:25 +0200

    Extends tree component with checkboxes
    Feature #13855
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/components/tree/tree.tsx
+++ src/components/tree/tree.tsx
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: AGPL-3.0
 
 import * as React from 'react';
-import { List, ListItem, ListItemIcon, Collapse } from "@material-ui/core";
+import { List, ListItem, ListItemIcon, Collapse, Checkbox } from "@material-ui/core";
 import { StyleRulesCallback, withStyles, WithStyles } from '@material-ui/core/styles';
 import { ReactElement } from "react";
 import CircularProgress from '@material-ui/core/CircularProgress';
@@ -12,11 +12,23 @@ import * as classnames from "classnames";
 import { ArvadosTheme } from '../../common/custom-theme';
 import { SidePanelRightArrowIcon } from '../icon/icon';
 
-type CssRules = 'list' | 'active' | 'loader' | 'toggableIconContainer' | 'iconClose' | 'iconOpen' | 'toggableIcon';
+type CssRules = 'list'
+    | 'listItem'
+    | 'active'
+    | 'loader'
+    | 'toggableIconContainer'
+    | 'iconClose'
+    | 'renderContainer'
+    | 'iconOpen'
+    | 'toggableIcon'
+    | 'checkbox';
 
 const styles: StyleRulesCallback<CssRules> = (theme: ArvadosTheme) => ({
     list: {
         padding: '3px 0px'
+    },
+    listItem: {
+        padding: '3px 0px',
     },
     loader: {
         position: 'absolute',
@@ -26,10 +38,13 @@ const styles: StyleRulesCallback<CssRules> = (theme: ArvadosTheme) => ({
     toggableIconContainer: {
         color: theme.palette.grey["700"],
         height: '14px',
-        position: 'absolute'
+        width: '14px',
     },
     toggableIcon: {
         fontSize: '14px'
+    },
+    renderContainer: {
+        flex: 1
     },
     active: {
         color: theme.palette.primary.main,
@@ -40,6 +55,12 @@ const styles: StyleRulesCallback<CssRules> = (theme: ArvadosTheme) => ({
     iconOpen: {
         transition: 'all 0.1s ease',
         transform: 'rotate(90deg)',
+    },
+    checkbox: {
+        width: theme.spacing.unit * 3,
+        height: theme.spacing.unit * 3,
+        margin: `0 ${theme.spacing.unit}px`,
+        color: theme.palette.grey["500"]
     }
 });
 
@@ -54,6 +75,7 @@ export interface TreeItem<T> {
     id: string;
     open: boolean;
     active: boolean;
+    selected?: boolean;
     status: TreeItemStatus;
     items?: Array<TreeItem<T>>;
 }
@@ -65,6 +87,8 @@ interface TreeProps<T> {
     toggleItemActive: (id: string, status: TreeItemStatus) => void;
     level?: number;
     onContextMenu: (event: React.MouseEvent<HTMLElement>, item: TreeItem<T>) => void;
+    showSelection?: boolean;
+    onSelectionChange?: (event: React.ChangeEvent<HTMLInputElement>, item: TreeItem<T>) => void;
 }
 
 export const Tree = withStyles(styles)(
@@ -72,11 +96,11 @@ export const Tree = withStyles(styles)(
         render(): ReactElement<any> {
             const level = this.props.level ? this.props.level : 0;
             const { classes, render, toggleItemOpen, items, toggleItemActive, onContextMenu } = this.props;
-            const { list, loader, toggableIconContainer } = classes;
+            const { list, listItem, loader, toggableIconContainer, renderContainer } = classes;
             return <List component="div" className={list}>
                 {items && items.map((it: TreeItem<T>, idx: number) =>
                     <div key={`item/${level}/${idx}`}>
-                        <ListItem button className={list} style={{ paddingLeft: (level + 1) * 20 }}
+                        <ListItem button className={listItem} style={{ paddingLeft: (level + 1) * 20 }}
                             onClick={() => toggleItemActive(it.id, it.status)}
                             onContextMenu={this.handleRowContextMenu(it)}>
                             {it.status === TreeItemStatus.PENDING ?
@@ -87,11 +111,20 @@ export const Tree = withStyles(styles)(
                                     {it.status !== TreeItemStatus.INITIAL && it.items && it.items.length === 0 ? <span /> : <SidePanelRightArrowIcon />}
                                 </ListItemIcon>
                             </i>
-                            {render(it, level)}
+                            {this.props.showSelection &&
+                                <Checkbox
+                                    checked={it.selected}
+                                    className={classes.checkbox}
+                                    color="primary"
+                                    onChange={this.handleCheckboxChange(it)} />}
+                            <div className={renderContainer}>
+                                {render(it, level)}
+                            </div>
                         </ListItem>
                         {it.items && it.items.length > 0 &&
                             <Collapse in={it.open} timeout="auto" unmountOnExit>
                                 <Tree
+                                    showSelection={this.props.showSelection}
                                     items={it.items}
                                     render={render}
                                     toggleItemOpen={toggleItemOpen}
@@ -115,5 +148,14 @@ export const Tree = withStyles(styles)(
         handleRowContextMenu = (item: TreeItem<T>) =>
             (event: React.MouseEvent<HTMLElement>) =>
                 this.props.onContextMenu(event, item)
+
+        handleCheckboxChange = (item: TreeItem<T>) => {
+            const { onSelectionChange } = this.props;
+            return onSelectionChange
+                ? (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => {
+                    onSelectionChange(event, item);
+                }
+                : undefined;
+        }
     }
 );
