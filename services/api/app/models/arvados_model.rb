19f787c5b5833623eee4d6cb88ddb909a5add7fc
Colin Nolan <colin-nolan@users.noreply.github.com>
2018-07-30 16:57:59 +0100

    Corrects logic to determine if audit logging is enabled.
    Arvados-DCO-1.1-Signed-off-by: Colin Nolan <colin.nolan@sanger.ac.uk>

--- services/api/app/models/arvados_model.rb
+++ services/api/app/models/arvados_model.rb
@@ -764,8 +764,8 @@ class ArvadosModel < ActiveRecord::Base
   end
 
   def is_audit_logging_enabled?
-    return Rails.configuration.max_audit_log_age == 0 &&
-           Rails.configuration.max_audit_log_delete_batch > 0
+    return !(Rails.configuration.max_audit_log_age.to_i == 0 &&
+             Rails.configuration.max_audit_log_delete_batch.to_i > 0)
   end
 
   def log_start_state
