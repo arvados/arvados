922a67467d2b7cc9995bbde095d9c2fa5b4518a9
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-08-02 15:39:21 +0200

    Pass closeDialog action into component wrapped withDialog
    Feature #13855
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/store/dialog/with-dialog.ts
+++ src/store/dialog/with-dialog.ts
@@ -5,17 +5,29 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { DialogState } from './dialog-reducer';
+import { Dispatch } from 'redux';
+import { dialogActions } from './dialog-actions';
 
 export type WithDialog<T> = {
     open: boolean;
     data?: T;
 };
 
+export type WithDialogActions = {
+    closeDialog: () => void;
+};
+
 export const withDialog = (id: string) =>
-    <T>(component: React.ComponentType<WithDialog<T>>) =>
-        connect(mapStateToProps(id))(component);
+    <T>(component: React.ComponentType<WithDialog<T> & WithDialogActions>) =>
+        connect(mapStateToProps(id), mapDispatchToProps(id))(component);
 
 export const mapStateToProps = (id: string) => <T>(state: { dialog: DialogState }): WithDialog<T> => {
     const dialog = state.dialog[id];
     return dialog ? dialog : { open: false };
 };
+
+export const mapDispatchToProps = (id: string) => (dispatch: Dispatch): WithDialogActions => ({
+    closeDialog: () => {
+        dispatch(dialogActions.CLOSE_DIALOG({ id }));
+    }
+});
