37421f3062f84e7a5ce81606eab758a43b987b27
Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>
2018-08-17 16:50:37 +0200

    cr changes v2
    Feature #13902
    
    Arvados-DCO-1.1-Signed-off-by: Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>

--- src/views-components/move-to-dialog/move-to-dialog.tsx
+++ src/views-components/move-to-dialog/move-to-dialog.tsx
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0
 
-import { Dispatch } from "redux";
+import { Dispatch, compose } from "redux";
 import { withDialog } from "../../store/dialog/with-dialog";
 import { dialogActions } from "../../store/dialog/dialog-actions";
 import { MoveToDialog } from "../../components/move-to-dialog/move-to-dialog";
@@ -12,15 +12,16 @@ export const MOVE_TO_DIALOG = 'moveToDialog';
 
 export const openMoveToDialog = () =>
     (dispatch: Dispatch) => {
-        dispatch(dialogActions.OPEN_DIALOG({ id: MOVE_TO_DIALOG, data: {}}));
+        dispatch(dialogActions.OPEN_DIALOG({ id: MOVE_TO_DIALOG, data: {} }));
     };
 
-export const [MoveToProjectDialog] = [MoveToDialog]
-    .map(withDialog(MOVE_TO_DIALOG))
-    .map(reduxForm({
+export const MoveToProjectDialog = compose(
+    withDialog(MOVE_TO_DIALOG),
+    reduxForm({
         form: MOVE_TO_DIALOG,
         onSubmit: (data, dispatch) => {
             dispatch(startSubmit(MOVE_TO_DIALOG));
             setTimeout(() => dispatch(stopSubmit(MOVE_TO_DIALOG, { name: 'Invalid path' })), 2000);
         }
-    }));
+    })
+)(MoveToDialog);
