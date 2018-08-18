c1787278642d2ef02234ea27f94aaae9cdd52d40
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-08-18 11:20:35 +0200

    Implement rename dialog usinf FormDialog
    Feature #14015
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/views-components/rename-file-dialog/rename-file-dialog.tsx
+++ src/views-components/rename-file-dialog/rename-file-dialog.tsx
@@ -2,11 +2,14 @@
 //
 // SPDX-License-Identifier: AGPL-3.0
 
-import { Dispatch } from "redux";
-import { reduxForm, reset, startSubmit, stopSubmit } from "redux-form";
-import { withDialog } from "~/store/dialog/with-dialog";
+import * as React from 'react';
+import { Dispatch, compose } from 'redux';
+import { reduxForm, reset, startSubmit, stopSubmit, InjectedFormProps, Field } from 'redux-form';
+import { withDialog, WithDialogProps } from '~/store/dialog/with-dialog';
 import { dialogActions } from "~/store/dialog/dialog-actions";
-import { RenameDialog } from "~/components/rename-dialog/rename-dialog";
+import { FormDialog } from '~/components/form-dialog/form-dialog';
+import { DialogContentText } from '@material-ui/core';
+import { TextField } from '~/components/text-field/text-field';
 
 export const RENAME_FILE_DIALOG = 'renameFileDialog';
 
@@ -16,13 +19,30 @@ export const openRenameFileDialog = (originalName: string) =>
         dispatch(dialogActions.OPEN_DIALOG({ id: RENAME_FILE_DIALOG, data: originalName }));
     };
 
-export const [RenameFileDialog] = [RenameDialog]
-    .map(withDialog(RENAME_FILE_DIALOG))
-    .map(reduxForm({
+export const RenameFileDialog = compose(
+    withDialog(RENAME_FILE_DIALOG),
+    reduxForm({
         form: RENAME_FILE_DIALOG,
         onSubmit: (data, dispatch) => {
             dispatch(startSubmit(RENAME_FILE_DIALOG));
             // TODO: call collection file renaming action here
             setTimeout(() => dispatch(stopSubmit(RENAME_FILE_DIALOG, { name: 'Invalid name' })), 2000);
         }
-    }));
+    })
+)((props: WithDialogProps<string> & InjectedFormProps<{ name: string }>) =>
+    <FormDialog
+        dialogTitle='Rename'
+        formFields={RenameDialogFormFields}
+        submitLabel='Ok'
+        {...props}
+    />);
+
+const RenameDialogFormFields = (props: WithDialogProps<string>) => <>
+    <DialogContentText>
+        {`Please, enter a new name for ${props.data}`}
+    </DialogContentText>
+    <Field
+        name='name'
+        component={TextField}
+    />
+</>;
