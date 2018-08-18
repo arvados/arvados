5a98bccbd21405c97baa35c288898fac16c2d567
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-08-18 11:22:15 +0200

    Create getDialog selector
    Feature #14015
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/store/dialog/dialog-reducer.ts
+++ src/store/dialog/dialog-reducer.ts
@@ -6,9 +6,9 @@ import { DialogAction, dialogActions } from "./dialog-actions";
 
 export type DialogState = Record<string, Dialog>;
 
-export interface Dialog {
+export interface Dialog <T = any> {
     open: boolean;
-    data: any;
+    data: T;
 }
 
 export const dialogReducer = (state: DialogState = {}, action: DialogAction) =>
@@ -20,3 +20,5 @@ export const dialogReducer = (state: DialogState = {}, action: DialogAction) =>
         default: () => state,
     });
 
+export const getDialog = <T>(state: DialogState, id: string) => 
+    state[id] ? state[id] as Dialog<T> : undefined;
