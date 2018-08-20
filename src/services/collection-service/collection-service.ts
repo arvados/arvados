80a9ee470d066aeb95e84338c8f57d558f715113
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-08-20 10:48:51 +0200

    Add comment explainig use of for loop
    Feature #14013
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/services/collection-service/collection-service.ts
+++ src/services/collection-service/collection-service.ts
@@ -33,6 +33,7 @@ export class CollectionService extends CommonResourceService<CollectionResource>
     }
 
     async uploadFiles(collectionUuid: string, files: File[], onProgress?: UploadProgress) {
+        // files have to be uploaded sequentially
         for (let idx = 0; idx < files.length; idx++) {
             await this.uploadFile(collectionUuid, files[idx], idx, onProgress);
         }
