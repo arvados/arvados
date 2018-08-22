b478a2dc99c373634dffd5a1110f0fa3a53866ef
Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>
2018-08-22 14:12:26 +0200

    cr changes
    Feature #13988
    
    Arvados-DCO-1.1-Signed-off-by: Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>

--- src/views-components/project-copy-dialog/project-copy-dialog.tsx
+++ src/views-components/project-copy-dialog/project-copy-dialog.tsx
@@ -7,11 +7,13 @@ import { InjectedFormProps, Field } from 'redux-form';
 import { WithDialogProps } from '~/store/dialog/with-dialog';
 import { FormDialog } from '~/components/form-dialog/form-dialog';
 import { ProjectTreePickerField } from '~/views-components/project-tree-picker/project-tree-picker';
-import { COPY_NAME_VALIDATION, MAKE_A_COPY_VALIDATION } from '~/validators/validators';
+import { COPY_NAME_VALIDATION, COPY_PROJECT_VALIDATION } from '~/validators/validators';
 import { TextField } from "~/components/text-field/text-field";
 import { ProjectCopyFormDialogData } from "~/store/project-copy-dialog/project-copy-dialog";
 
-export const ProjectCopyFormDialog = (props: WithDialogProps<string> & InjectedFormProps<ProjectCopyFormDialogData>) =>
+type ProjectCopyFormDialogProps = WithDialogProps<string> & InjectedFormProps<ProjectCopyFormDialogData>;
+
+export const ProjectCopyFormDialog = (props: ProjectCopyFormDialogProps) =>
     <FormDialog
         dialogTitle='Make a copy'
         formFields={ProjectCopyFields}
@@ -19,20 +21,14 @@ export const ProjectCopyFormDialog = (props: WithDialogProps<string> & InjectedF
         {...props}
     />;
 
-const ProjectCopyFields = () => <div>
-    <ProjectCopyNameField />
-    <ProjectCopyDialogFields />
-</div>;
-
-const ProjectCopyNameField = () =>
+const ProjectCopyFields = () => <span>
     <Field
         name='name'
         component={TextField}
         validate={COPY_NAME_VALIDATION}
-        label="Enter a new name for the copy" />;
-
-const ProjectCopyDialogFields = () =>
+        label="Enter a new name for the copy" />
     <Field
         name="projectUuid"
         component={ProjectTreePickerField}
-        validate={MAKE_A_COPY_VALIDATION} />;
+        validate={COPY_PROJECT_VALIDATION} />
+</span>;
