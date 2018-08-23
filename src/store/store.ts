e9a65be7cd7d5889d885794bf46ac3c6277eca3c
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-08-23 14:53:00 +0200

    Create store for custom properties
    Feature #14102
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,79 +3,44 @@
 // SPDX-License-Identifier: AGPL-3.0
 
 import { createStore, applyMiddleware, compose, Middleware, combineReducers, Store, Action, Dispatch } from 'redux';
-import { routerMiddleware, routerReducer, RouterState } from "react-router-redux";
+import { routerMiddleware, routerReducer } from "react-router-redux";
 import thunkMiddleware from 'redux-thunk';
 import { History } from "history";
 
-import { projectsReducer, ProjectState } from "./project/project-reducer";
-import { sidePanelReducer, SidePanelState } from './side-panel/side-panel-reducer';
-import { authReducer, AuthState } from "./auth/auth-reducer";
-import { dataExplorerReducer, DataExplorerState } from './data-explorer/data-explorer-reducer';
-import { detailsPanelReducer, DetailsPanelState } from './details-panel/details-panel-reducer';
-import { contextMenuReducer, ContextMenuState } from './context-menu/context-menu-reducer';
+import { projectsReducer } from "./project/project-reducer";
+import { authReducer } from "./auth/auth-reducer";
+import { dataExplorerReducer } from './data-explorer/data-explorer-reducer';
+import { detailsPanelReducer } from './details-panel/details-panel-reducer';
+import { contextMenuReducer } from './context-menu/context-menu-reducer';
 import { reducer as formReducer } from 'redux-form';
-import { FavoritesState, favoritesReducer } from './favorites/favorites-reducer';
-import { snackbarReducer, SnackbarState } from './snackbar/snackbar-reducer';
-import { CollectionPanelFilesState } from './collection-panel/collection-panel-files/collection-panel-files-state';
+import { favoritesReducer } from './favorites/favorites-reducer';
+import { snackbarReducer } from './snackbar/snackbar-reducer';
 import { collectionPanelFilesReducer } from './collection-panel/collection-panel-files/collection-panel-files-reducer';
 import { dataExplorerMiddleware } from "./data-explorer/data-explorer-middleware";
 import { FAVORITE_PANEL_ID } from "./favorite-panel/favorite-panel-action";
 import { PROJECT_PANEL_ID } from "./project-panel/project-panel-action";
 import { ProjectPanelMiddlewareService } from "./project-panel/project-panel-middleware-service";
 import { FavoritePanelMiddlewareService } from "./favorite-panel/favorite-panel-middleware-service";
-import { CollectionPanelState, collectionPanelReducer } from './collection-panel/collection-panel-reducer';
-import { DialogState, dialogReducer } from './dialog/dialog-reducer';
-import { CollectionsState, collectionsReducer } from './collections/collections-reducer';
+import { collectionPanelReducer } from './collection-panel/collection-panel-reducer';
+import { dialogReducer } from './dialog/dialog-reducer';
+import { collectionsReducer } from './collections/collections-reducer';
 import { ServiceRepository } from "~/services/services";
 import { treePickerReducer } from './tree-picker/tree-picker-reducer';
-import { TreePicker } from './tree-picker/tree-picker';
-import { ResourcesState } from '~/store/resources/resources';
 import { resourcesReducer } from '~/store/resources/resources-reducer';
+import { propertiesReducer } from './properties/properties-reducer';
+import { RootState } from './store';
 
 const composeEnhancers =
     (process.env.NODE_ENV === 'development' &&
         window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
     compose;
 
-export interface RootState {
-    auth: AuthState;
-    projects: ProjectState;
-    collections: CollectionsState;
-    router: RouterState;
-    dataExplorer: DataExplorerState;
-    sidePanel: SidePanelState;
-    collectionPanel: CollectionPanelState;
-    detailsPanel: DetailsPanelState;
-    contextMenu: ContextMenuState;
-    favorites: FavoritesState;
-    snackbar: SnackbarState;
-    collectionPanelFiles: CollectionPanelFilesState;
-    dialog: DialogState;
-    treePicker: TreePicker;
-    resources: ResourcesState;
-}
+export type RootState = ReturnType<ReturnType<typeof createRootReducer>>;
 
 export type RootStore = Store<RootState, Action> & { dispatch: Dispatch<any> };
 
 export function configureStore(history: History, services: ServiceRepository): RootStore {
-    const rootReducer = combineReducers({
-        auth: authReducer(services),
-        projects: projectsReducer,
-        collections: collectionsReducer,
-        router: routerReducer,
-        dataExplorer: dataExplorerReducer,
-        sidePanel: sidePanelReducer,
-        collectionPanel: collectionPanelReducer,
-        detailsPanel: detailsPanelReducer,
-        contextMenu: contextMenuReducer,
-        form: formReducer,
-        favorites: favoritesReducer,
-        snackbar: snackbarReducer,
-        collectionPanelFiles: collectionPanelFilesReducer,
-        dialog: dialogReducer,
-        treePicker: treePickerReducer,
-        resources: resourcesReducer,
-    });
+    const rootReducer = createRootReducer(services);
 
     const projectPanelMiddleware = dataExplorerMiddleware(
         new ProjectPanelMiddlewareService(services, PROJECT_PANEL_ID)
@@ -93,3 +58,22 @@ export function configureStore(history: History, services: ServiceRepository): R
     const enhancer = composeEnhancers(applyMiddleware(...middlewares));
     return createStore(rootReducer, enhancer);
 }
+
+const createRootReducer = (services: ServiceRepository) => combineReducers({
+    auth: authReducer(services),
+    projects: projectsReducer,
+    collections: collectionsReducer,
+    router: routerReducer,
+    dataExplorer: dataExplorerReducer,
+    collectionPanel: collectionPanelReducer,
+    detailsPanel: detailsPanelReducer,
+    contextMenu: contextMenuReducer,
+    form: formReducer,
+    favorites: favoritesReducer,
+    snackbar: snackbarReducer,
+    collectionPanelFiles: collectionPanelFilesReducer,
+    dialog: dialogReducer,
+    treePicker: treePickerReducer,
+    resources: resourcesReducer,
+    properties: propertiesReducer,
+});
