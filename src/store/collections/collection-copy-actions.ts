47eb02af0c2c7bc4f0f852400308033c72322af8
Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>
2018-08-24 10:12:41 +0200

    cr changes
    Feature #13897
    
    Arvados-DCO-1.1-Signed-off-by: Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>

--- src/store/collections/collection-copy-actions.ts
+++ src/store/collections/collection-copy-actions.ts
@@ -12,7 +12,7 @@ import { getCommonResourceServiceError, CommonResourceServiceError } from '~/com
 import { snackbarActions } from '~/store/snackbar/snackbar-actions';
 import { projectPanelActions } from '~/store/project-panel/project-panel-action';
 
-export const COLLECTION_COPY_DIALOG = 'projectCopy';
+export const COLLECTION_COPY_FORM_NAME = 'projectCopy';
 
 export interface CollectionCopyFormDialogData {
     name: string;
@@ -24,27 +24,27 @@ export const openCollectionCopyDialog = (resource: { name: string, uuid: string 
     (dispatch: Dispatch) => {
         dispatch<any>(resetPickerProjectTree());
         const initialData: CollectionCopyFormDialogData = { name: `Copy of: ${resource.name}`, ownerUuid: '', uuid: resource.uuid };
-        dispatch<any>(initialize(COLLECTION_COPY_DIALOG, initialData));
-        dispatch(dialogActions.OPEN_DIALOG({ id: COLLECTION_COPY_DIALOG, data: {} }));
+        dispatch<any>(initialize(COLLECTION_COPY_FORM_NAME, initialData));
+        dispatch(dialogActions.OPEN_DIALOG({ id: COLLECTION_COPY_FORM_NAME, data: {} }));
     };
 
 export const copyCollection = (resource: CollectionCopyFormDialogData) =>
     async (dispatch: Dispatch, getState: () => RootState, services: ServiceRepository) => {
-        dispatch(startSubmit(COLLECTION_COPY_DIALOG));
+        dispatch(startSubmit(COLLECTION_COPY_FORM_NAME));
         try {
             const collection = await services.collectionService.get(resource.uuid);
             const uuidKey = 'uuid';
             delete collection[uuidKey];
             await services.collectionService.create({ ...collection, ownerUuid: resource.ownerUuid, name: resource.name });
             dispatch(projectPanelActions.REQUEST_ITEMS());
-            dispatch(dialogActions.CLOSE_DIALOG({ id: COLLECTION_COPY_DIALOG }));
+            dispatch(dialogActions.CLOSE_DIALOG({ id: COLLECTION_COPY_FORM_NAME }));
             dispatch(snackbarActions.OPEN_SNACKBAR({ message: 'Collection has been copied', hideDuration: 2000 }));
         } catch (e) {
             const error = getCommonResourceServiceError(e);
             if (error === CommonResourceServiceError.UNIQUE_VIOLATION) {
-                dispatch(stopSubmit(COLLECTION_COPY_DIALOG, { ownerUuid: 'A collection with the same name already exists in the target project.' }));
+                dispatch(stopSubmit(COLLECTION_COPY_FORM_NAME, { ownerUuid: 'A collection with the same name already exists in the target project.' }));
             } else {
-                dispatch(dialogActions.CLOSE_DIALOG({ id: COLLECTION_COPY_DIALOG }));
+                dispatch(dialogActions.CLOSE_DIALOG({ id: COLLECTION_COPY_FORM_NAME }));
                 dispatch(snackbarActions.OPEN_SNACKBAR({ message: 'Could not copy the collection', hideDuration: 2000 }));
             }
         }
--- src/views-components/dialog-forms/copy-collection-dialog.ts
+++ src/views-components/dialog-forms/copy-collection-dialog.ts
@@ -5,13 +5,13 @@
 import { compose } from "redux";
 import { withDialog } from "~/store/dialog/with-dialog";
 import { reduxForm } from 'redux-form';
-import { COLLECTION_COPY_DIALOG, CollectionCopyFormDialogData, copyCollection } from '~/store/collections/collection-copy-actions';
+import { COLLECTION_COPY_FORM_NAME, CollectionCopyFormDialogData, copyCollection } from '~/store/collections/collection-copy-actions';
 import { DialogCopy } from "~/views-components/dialog-copy/dialog-copy";
 
 export const CopyCollectionDialog = compose(
-    withDialog(COLLECTION_COPY_DIALOG),
+    withDialog(COLLECTION_COPY_FORM_NAME),
     reduxForm<CollectionCopyFormDialogData>({
-        form: COLLECTION_COPY_DIALOG,
+        form: COLLECTION_COPY_FORM_NAME,
         onSubmit: (data, dispatch) => {
             dispatch(copyCollection(data));
         }
