9f4bca6531631fc5ed21435f31db36720d58512b
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-08-28 14:24:16 +0200

    Update const name
    Feature #14119
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/views-components/dialog-forms/collection-partial-copy-dialog.ts
+++ src/views-components/dialog-forms/collection-partial-copy-dialog.ts
@@ -5,14 +5,14 @@
 import { compose } from "redux";
 import { reduxForm } from 'redux-form';
 import { withDialog, } from '~/store/dialog/with-dialog';
-import { CollectionPartialCopyFormData, doCollectionPartialCopy, COLLECTION_PARTIAL_COPY_DIALOG } from '~/store/collections/collection-partial-copy-actions';
+import { CollectionPartialCopyFormData, doCollectionPartialCopy, COLLECTION_PARTIAL_COPY_FORM_NAME } from '~/store/collections/collection-partial-copy-actions';
 import { CollectionPartialCopyDialog as Dialog } from "~/views-components/dialog-copy/collection-partial-copy-dialog";
 
 
 export const CollectionPartialCopyDialog = compose(
-    withDialog(COLLECTION_PARTIAL_COPY_DIALOG),
+    withDialog(COLLECTION_PARTIAL_COPY_FORM_NAME),
     reduxForm({
-        form: COLLECTION_PARTIAL_COPY_DIALOG,
+        form: COLLECTION_PARTIAL_COPY_FORM_NAME,
         onSubmit: (data: CollectionPartialCopyFormData, dispatch) => {
             dispatch(doCollectionPartialCopy(data));
         }
