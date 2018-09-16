3fd7d22747e43fd810c1eace852ae5b184b06bde
Daniel Kos <daniel.kos@contractors.roche.com>
2018-09-16 23:14:20 +0200

    Reactivate withProgress
    Feature #14186
    
    Arvados-DCO-1.1-Signed-off-by: Daniel Kos <daniel.kos@contractors.roche.com>

--- src/store/progress-indicator/with-progress.ts
+++ src/store/progress-indicator/with-progress.ts
@@ -1,20 +1,20 @@
-// // Copyright (C) The Arvados Authors. All rights reserved.
-// //
-// // SPDX-License-Identifier: AGPL-3.0
+// Copyright (C) The Arvados Authors. All rights reserved.
 //
-// import * as React from 'react';
-// import { connect } from 'react-redux';
-// import { RootState } from '~/store/store';
-//
-// export type WithProgressStateProps = {
-//     started: boolean;
-// };
-//
-// export const withProgress = (id: string) =>
-//     (component: React.ComponentType<WithProgressStateProps>) =>
-//         connect(mapStateToProps(id))(component);
-//
-// export const mapStateToProps = (id: string) => (state: RootState): WithProgressStateProps => {
-//     const progress = state.progressIndicator[id];
-//     return progress;
-// };
+// SPDX-License-Identifier: AGPL-3.0
+
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { RootState } from '~/store/store';
+
+export type WithProgressStateProps = {
+    working: boolean;
+};
+
+export const withProgress = (id: string) =>
+    (component: React.ComponentType<WithProgressStateProps>) =>
+        connect(mapStateToProps(id))(component);
+
+export const mapStateToProps = (id: string) => (state: RootState): WithProgressStateProps => {
+    const progress = state.progressIndicator.find(p => p.id === id);
+    return { working: progress ? progress.working : false };
+};
