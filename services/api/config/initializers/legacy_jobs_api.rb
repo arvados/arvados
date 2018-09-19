985bfe119f34bbf39a7007bee1fc1c03db9ab8bf
Peter Amstutz <pamstutz@veritasgenetics.com>
2018-09-19 15:15:11 -0400

    10865: Fix query for empty jobs table
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <pamstutz@veritasgenetics.com>

--- services/api/config/initializers/legacy_jobs_api.rb
+++ services/api/config/initializers/legacy_jobs_api.rb
@@ -8,9 +8,11 @@
 require_relative 'load_config.rb'
 
 Server::Application.configure do
+  # Use exec_query because the Job model isn't loaded yet
+  # it returns the string '0', not converted to integer.
   if Rails.configuration.enable_legacy_jobs_api == false ||
      (Rails.configuration.enable_legacy_jobs_api == "auto" &&
-      ActiveRecord::Base.connection.exec_query("select count(*) from jobs")[0] == 0)
+      ActiveRecord::Base.connection.exec_query('select count(*) from jobs').first['count'] == '0')
     Rails.configuration.disable_api_methods = ["jobs.create",
                                                "pipeline_instances.create",
                                                "pipeline_templates.create",
