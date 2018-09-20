ba54c26be4078cb32d78947837f630a4404a93ae
Peter Amstutz <pamstutz@veritasgenetics.com>
2018-09-20 12:57:40 -0400

    10865: Add test for check_enable_legacy_jobs_api
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <pamstutz@veritasgenetics.com>

--- services/api/config/initializers/legacy_jobs_api.rb
+++ services/api/config/initializers/legacy_jobs_api.rb
@@ -7,38 +7,8 @@
 # classes.
 require_relative 'load_config.rb'
 
+require 'enable_jobs_api'
+
 Server::Application.configure do
-  # Use exec_query because the Job model isn't loaded yet
-  # it returns the string '0', not converted to integer.
-  if Rails.configuration.enable_legacy_jobs_api == false ||
-     (Rails.configuration.enable_legacy_jobs_api == "auto" &&
-      ActiveRecord::Base.connection.exec_query('select count(*) from jobs').first['count'] == '0')
-    Rails.configuration.disable_api_methods = ["jobs.create",
-                                               "pipeline_instances.create",
-                                               "pipeline_templates.create",
-                                               "jobs.get",
-                                               "pipeline_instances.get",
-                                               "pipeline_templates.get",
-                                               "jobs.list",
-                                               "pipeline_instances.list",
-                                               "pipeline_templates.list",
-                                               "jobs.index",
-                                               "pipeline_instances.index",
-                                               "pipeline_templates.index",
-                                               "jobs.update",
-                                               "pipeline_instances.update",
-                                               "pipeline_templates.update",
-                                               "jobs.queue",
-                                               "jobs.queue_size",
-                                               "job_tasks.create",
-                                               "job_tasks.get",
-                                               "job_tasks.list",
-                                               "job_tasks.index",
-                                               "job_tasks.update",
-                                               "jobs.show",
-                                               "pipeline_instances.show",
-                                               "pipeline_templates.show",
-                                               "jobs.show",
-                                               "job_tasks.show"]
-  end
+  check_enable_legacy_jobs_api
 end
---
+++ services/api/lib/enable_jobs_api.rb
@@ -0,0 +1,39 @@
+# Copyright (C) The Arvados Authors. All rights reserved.
+#
+# SPDX-License-Identifier: AGPL-3.0
+
+Disable_jobs_api_method_list = ["jobs.create",
+                                               "pipeline_instances.create",
+                                               "pipeline_templates.create",
+                                               "jobs.get",
+                                               "pipeline_instances.get",
+                                               "pipeline_templates.get",
+                                               "jobs.list",
+                                               "pipeline_instances.list",
+                                               "pipeline_templates.list",
+                                               "jobs.index",
+                                               "pipeline_instances.index",
+                                               "pipeline_templates.index",
+                                               "jobs.update",
+                                               "pipeline_instances.update",
+                                               "pipeline_templates.update",
+                                               "jobs.queue",
+                                               "jobs.queue_size",
+                                               "job_tasks.create",
+                                               "job_tasks.get",
+                                               "job_tasks.list",
+                                               "job_tasks.index",
+                                               "job_tasks.update",
+                                               "jobs.show",
+                                               "pipeline_instances.show",
+                                               "pipeline_templates.show",
+                                               "jobs.show",
+                                               "job_tasks.show"]
+
+def check_enable_legacy_jobs_api
+  if Rails.configuration.enable_legacy_jobs_api == false ||
+     (Rails.configuration.enable_legacy_jobs_api == "auto" &&
+      ActiveRecord::Base.connection.exec_query("select count(*) from jobs").first["count"] == "0")
+    Rails.configuration.disable_api_methods = Disable_jobs_api_method_list
+  end
+end
