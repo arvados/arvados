f0553f71caddcf44371c616e7177c2716b6bcb88
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-10-01 11:26:53 +0200

    Populate form with default values
    Feature #13863
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/views/run-process-panel/run-process-inputs-form.tsx
+++ src/views/run-process-panel/run-process-inputs-form.tsx
@@ -11,6 +11,7 @@ import { StringCommandInputParameter, FloatCommandInputParameter, File } from '.
 import { FloatInput } from '~/views/run-process-panel/inputs/float-input';
 import { BooleanInput } from './inputs/boolean-input';
 import { FileInput } from './inputs/file-input';
+import { connect } from 'react-redux';
 
 const RUN_PROCESS_INPUTS_FORM = 'runProcessInputsForm';
 
@@ -18,26 +19,32 @@ export interface RunProcessInputFormProps {
     inputs: CommandInputParameter[];
 }
 
-export const RunProcessInputsForm = reduxForm<any, RunProcessInputFormProps>({
-    form: RUN_PROCESS_INPUTS_FORM
-})((props: InjectedFormProps & RunProcessInputFormProps) =>
-    <form>
-        {props.inputs.map(input => {
-            switch (true) {
-                case input.type === CWLType.BOOLEAN:
-                    return <BooleanInput key={input.id} input={input as BooleanCommandInputParameter} />;
-                case input.type === CWLType.INT:
-                case input.type === CWLType.LONG:
-                    return <IntInput key={input.id} input={input as IntCommandInputParameter} />;
-                case input.type === CWLType.FLOAT:
-                case input.type === CWLType.DOUBLE:
-                    return <FloatInput key={input.id} input={input as FloatCommandInputParameter} />;
-                case input.type === CWLType.STRING:
-                    return <StringInput key={input.id} input={input as StringCommandInputParameter} />;
-                case input.type === CWLType.FILE:
-                    return <FileInput key={input.id} input={input as FileCommandInputParameter} />;
-                default:
-                    return null;
-            }
-        })}
-    </form>);
+export const RunProcessInputsForm = connect(
+    (_: any, props: RunProcessInputFormProps) => ({
+        initialValues: props.inputs.reduce(
+            (values, input) => ({ ...values, [input.id]: input.default }),
+            {}),
+    }))(
+        reduxForm<any, RunProcessInputFormProps>({
+            form: RUN_PROCESS_INPUTS_FORM
+        })((props: InjectedFormProps & RunProcessInputFormProps) =>
+            <form>
+                {props.inputs.map(input => {
+                    switch (true) {
+                        case input.type === CWLType.BOOLEAN:
+                            return <BooleanInput key={input.id} input={input as BooleanCommandInputParameter} />;
+                        case input.type === CWLType.INT:
+                        case input.type === CWLType.LONG:
+                            return <IntInput key={input.id} input={input as IntCommandInputParameter} />;
+                        case input.type === CWLType.FLOAT:
+                        case input.type === CWLType.DOUBLE:
+                            return <FloatInput key={input.id} input={input as FloatCommandInputParameter} />;
+                        case input.type === CWLType.STRING:
+                            return <StringInput key={input.id} input={input as StringCommandInputParameter} />;
+                        case input.type === CWLType.FILE:
+                            return <FileInput key={input.id} input={input as FileCommandInputParameter} />;
+                        default:
+                            return null;
+                    }
+                })}
+            </form>));
