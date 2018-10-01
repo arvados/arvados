cfff00494c45cb6f8a53373a64d72dbd2fe8cd85
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-10-01 10:02:19 +0200

    Create DecimalInput for storing decimal separator info
    Feature #13863
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/views/run-process-panel/inputs/float-input.tsx
+++ src/views/run-process-panel/inputs/float-input.tsx
@@ -4,10 +4,9 @@
 
 import * as React from 'react';
 import { getInputLabel, FloatCommandInputParameter } from '~/models/workflow';
-import { Field } from 'redux-form';
+import { Field, WrappedFieldProps } from 'redux-form';
 import { TextField } from '~/components/text-field/text-field';
 import { isNumber } from '~/validators/is-number';
-import { toNumber } from 'lodash';
 export interface FloatInputProps {
     input: FloatCommandInputParameter;
 }
@@ -15,8 +14,32 @@ export const FloatInput = ({ input }: FloatInputProps) =>
     <Field
         name={input.id}
         label={getInputLabel(input)}
-        component={TextField}
-        parse={value => toNumber(value)}
+        component={DecimalInput}
+        parse={parseFloat}
         format={value => isNaN(value) ? '' : JSON.stringify(value)}
         validate={[isNumber]} />;
 
+
+class DecimalInput extends React.Component<WrappedFieldProps & { label?: string }> {
+    state = {
+        endsWithDecimalSeparator: false,
+    };
+
+    handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const [base, fraction] = event.target.value.split('.');
+        this.setState({ endsWithDecimalSeparator: fraction === '' });
+        this.props.input.onChange(event);
+    }
+
+    render() {
+        const props = {
+            ...this.props,
+            input: {
+                ...this.props.input,
+                value: this.props.input.value + (this.state.endsWithDecimalSeparator ? '.' : ''),
+                onChange: this.handleChange,
+            },
+        };
+        return <TextField {...props} />;
+    }
+}
