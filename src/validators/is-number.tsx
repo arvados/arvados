b1e94c2980f863debc47501a88b50e6e3597ae31
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-10-01 10:06:41 +0200

    Fix isNumber validator
    Feature #13863
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/validators/is-number.tsx
+++ src/validators/is-number.tsx
@@ -6,5 +6,5 @@ import { isNumber as isNum } from 'lodash';
 const ERROR_MESSAGE = 'This field must be a number';
 
 export const isNumber = (value: any) => {
-    return isNum(value) ? undefined : ERROR_MESSAGE;
+    return !isNaN(value) && isNum(value) ? undefined : ERROR_MESSAGE;
 };
