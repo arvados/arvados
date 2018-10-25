62f6c1f0be5ebd4911ca0891e2b68db4c1e07a9a
Tom Clegg <tclegg@veritasgenetics.com>
2018-10-25 15:52:21 -0400

    14407: Fix "too much recursion" error.
    The recursion error comes from evaluate_script() trying to serialize
    the magical jQuery response. We don't need to see that response so
    we can use execute_script() instead.
    
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tclegg@veritasgenetics.com>

--- apps/workbench/test/integration_helper.rb
+++ apps/workbench/test/integration_helper.rb
@@ -103,14 +103,14 @@ module AssertDomEvent
   # DOM element.
   def assert_triggers_dom_event events, target='body'
     magic = 'received-dom-event-' + rand(2**30).to_s(36)
-    page.evaluate_script <<eos
+    page.execute_script <<eos
       $('#{target}').one('#{events}', function() {
         $('body').addClass('#{magic}');
       });
 eos
     yield
     assert_selector "body.#{magic}"
-    page.evaluate_script "$('body').removeClass('#{magic}');";
+    page.execute_script "$('body').removeClass('#{magic}');";
   end
 end
 
