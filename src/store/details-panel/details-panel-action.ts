1eb2cc60cbf40d86db2304e11a678c3aa51af321
Pawel Kromplewski <pawel.kromplewski@contractors.roche.com>
2018-11-14 13:52:41 +0100

    Use one const for action and component
    Feature #14420
    
    Arvados-DCO-1.1-Signed-off-by: Pawel Kromplewski <pawel.kromplewski@contractors.roche.com>

--- src/store/details-panel/details-panel-action.ts
+++ src/store/details-panel/details-panel-action.ts
@@ -5,6 +5,8 @@
 import { unionize, ofType, UnionOf } from '~/common/unionize';
 import { Dispatch } from 'redux';
 
+export const SLIDE_TIMEOUT = 500;
+
 export const detailsPanelActions = unionize({
     TOGGLE_DETAILS_PANEL: ofType<{}>(),
     LOAD_DETAILS_PANEL: ofType<string>()
@@ -17,9 +19,8 @@ export const loadDetailsPanel = (uuid: string) => detailsPanelActions.LOAD_DETAI
 export const toggleDetailsPanel = () => (dispatch: Dispatch) => {
     // because of material-ui issue resizing details panel breaks tabs.
     // triggering window resize event fixes that.
-    const detailsPanelAnimationDuration = 500;
     setTimeout(() => {
         window.dispatchEvent(new Event('resize'));
-    }, detailsPanelAnimationDuration);
+    }, SLIDE_TIMEOUT);
     dispatch(detailsPanelActions.TOGGLE_DETAILS_PANEL());
 };
