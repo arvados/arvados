9da0da6012b6c4b80db90fdd5862d37ad2113aa4
Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>
2018-11-14 11:44:13 +0100

    cr changes
    Feature #14461
    
    Arvados-DCO-1.1-Signed-off-by: Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>

--- src/views-components/process-remove-dialog/process-remove-dialog.tsx
+++ src/views-components/process-remove-dialog/process-remove-dialog.tsx
@@ -2,19 +2,24 @@
 //
 // SPDX-License-Identifier: AGPL-3.0
 
-import { Dispatch } from "redux";
+import { Dispatch, compose } from 'redux';
 import { connect } from "react-redux";
 import { ConfirmationDialog } from "~/components/confirmation-dialog/confirmation-dialog";
 import { withDialog, WithDialogProps } from "~/store/dialog/with-dialog";
-import { removeProcessPermanently, REMOVE_PROCESS_DIALOG } from "~/store/processes/processes-actions";
+import { removeProcessPermanently, REMOVE_PROCESS_DIALOG } from '~/store/processes/processes-actions';
 
- const mapDispatchToProps = (dispatch: Dispatch, props: WithDialogProps<any>) => ({
+const mapStateToProps = (props: WithDialogProps<string>) => ({
+    ...props,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch, props: WithDialogProps<any>) => ({
     onConfirm: () => {
         props.closeDialog();
         dispatch<any>(removeProcessPermanently(props.data.uuid));
     }
 });
 
- export const [RemoveProcessDialog] = [ConfirmationDialog]
-    .map(connect(null, mapDispatchToProps) as any)
-    .map(withDialog(REMOVE_PROCESS_DIALOG)); 
+export const RemoveProcessDialog = compose(
+    withDialog(REMOVE_PROCESS_DIALOG),
+    connect(mapStateToProps, mapDispatchToProps)
+)(ConfirmationDialog);
