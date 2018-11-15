dd00886a1098e82a70b814d7d4c2f30d3e6e69c7
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-11-15 10:47:40 +0100

    Apply pickerId to MoveProjectDialog
    Feature #14470
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/views-components/dialog-forms/move-project-dialog.ts
+++ src/views-components/dialog-forms/move-project-dialog.ts
@@ -2,7 +2,6 @@
 //
 // SPDX-License-Identifier: AGPL-3.0
 
-import * as React from 'react';
 import { compose } from "redux";
 import { withDialog } from "~/store/dialog/with-dialog";
 import { reduxForm } from 'redux-form';
@@ -10,6 +9,7 @@ import { PROJECT_MOVE_FORM_NAME } from '~/store/projects/project-move-actions';
 import { MoveToFormDialogData } from '~/store/move-to-dialog/move-to-dialog';
 import { DialogMoveTo } from '~/views-components/dialog-move/dialog-move-to';
 import { moveProject } from '~/store/workbench/workbench-actions';
+import { pickerId } from '~/store/tree-picker/picker-id';
 
 export const MoveProjectDialog = compose(
     withDialog(PROJECT_MOVE_FORM_NAME),
@@ -19,8 +19,6 @@ export const MoveProjectDialog = compose(
             dispatch(moveProject(data));
         }
     }),
-)(
-    (...params: Parameters<typeof DialogMoveTo>) =>
-        <DialogMoveTo {...params[0]} pickerId={PROJECT_MOVE_FORM_NAME} />
-);
+    pickerId(PROJECT_MOVE_FORM_NAME),
+)(DialogMoveTo);
 
