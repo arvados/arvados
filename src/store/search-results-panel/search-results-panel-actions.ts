745374f29652ebb8720f5303a403f680d86f971f
Janicki Artur <artur.janicki@contractors.roche.com>
2018-11-28 12:01:22 +0100

    refs #14534 Breadcrumbs are not updated after searching
    Arvados-DCO-1.1-Signed-off-by: Janicki Artur <artur.janicki@contractors.roche.com>

--- src/store/search-results-panel/search-results-panel-actions.ts
+++ src/store/search-results-panel/search-results-panel-actions.ts
@@ -6,11 +6,13 @@ import { Dispatch } from 'redux';
 import { RootState } from '~/store/store';
 import { ServiceRepository } from '~/services/services';
 import { bindDataExplorerActions } from '~/store/data-explorer/data-explorer-action';
+import { setBreadcrumbs } from '~/store/breadcrumbs/breadcrumbs-actions';
 
 export const SEARCH_RESULTS_PANEL_ID = "searchResultsPanel";
 export const searchResultsPanelActions = bindDataExplorerActions(SEARCH_RESULTS_PANEL_ID);
 
 export const loadSearchResultsPanel = () =>
     (dispatch: Dispatch, getState: () => RootState, services: ServiceRepository) => {
+        dispatch(setBreadcrumbs([{ label: 'Search results' }]));
         dispatch(searchResultsPanelActions.REQUEST_ITEMS());
     };
