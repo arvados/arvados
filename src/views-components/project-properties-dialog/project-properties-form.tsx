52bc782204edbbf55ec837a23029eee6c40629fb
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-11-28 12:47:16 +0100

    Add margin to ProjectPropertiesForm
    Feature #14393
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/views-components/project-properties-dialog/project-properties-form.tsx
+++ src/views-components/project-properties-dialog/project-properties-form.tsx
@@ -5,6 +5,9 @@
 import { reduxForm, reset } from 'redux-form';
 import { PROJECT_PROPERTIES_FORM_NAME, createProjectProperty } from '~/store/details-panel/details-panel-action';
 import { ResourcePropertiesForm, ResourcePropertiesFormData } from '~/views-components/resource-properties-form/resource-properties-form';
+import { withStyles } from '@material-ui/core';
+
+const Form = withStyles(({ spacing }) => ({ container: { marginBottom: spacing.unit * 2 } }))(ResourcePropertiesForm);
 
 export const ProjectPropertiesForm = reduxForm<ResourcePropertiesFormData>({
     form: PROJECT_PROPERTIES_FORM_NAME,
@@ -12,4 +15,4 @@ export const ProjectPropertiesForm = reduxForm<ResourcePropertiesFormData>({
         dispatch<any>(createProjectProperty(data));
         dispatch(reset(PROJECT_PROPERTIES_FORM_NAME));
     }
-})(ResourcePropertiesForm);
+})(Form);
