486d4d2abb7329ea90fb31b9901c767d69b4ef37
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-11-28 12:48:59 +0100

    Add margin to CollectionTagForm
    Feature #14393
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/views/collection-panel/collection-tag-form.tsx
+++ src/views/collection-panel/collection-tag-form.tsx
@@ -5,6 +5,9 @@
 import { reduxForm, reset } from 'redux-form';
 import { createCollectionTag, COLLECTION_TAG_FORM_NAME } from '~/store/collection-panel/collection-panel-action';
 import { ResourcePropertiesForm, ResourcePropertiesFormData } from '~/views-components/resource-properties-form/resource-properties-form';
+import { withStyles } from '@material-ui/core';
+
+const Form = withStyles(({ spacing }) => ({ container: { marginBottom: spacing.unit * 2 } }))(ResourcePropertiesForm);
 
 export const CollectionTagForm = reduxForm<ResourcePropertiesFormData>({
     form: COLLECTION_TAG_FORM_NAME,
@@ -12,4 +15,4 @@ export const CollectionTagForm = reduxForm<ResourcePropertiesFormData>({
         dispatch<any>(createCollectionTag(data));
         dispatch(reset(COLLECTION_TAG_FORM_NAME));
     }
-})(ResourcePropertiesForm);
+})(Form);
