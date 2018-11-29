e3232103918da984d2d8198a08abe389af15aeb1
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-11-29 13:49:57 +0100

    Extract BooleanInput's change handler
    Feature #14524
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/views/run-process-panel/inputs/boolean-input.tsx
+++ src/views/run-process-panel/inputs/boolean-input.tsx
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: AGPL-3.0
 
 import * as React from 'react';
+import { memoize } from 'lodash/fp';
 import { BooleanCommandInputParameter } from '~/models/workflow';
 import { Field } from 'redux-form';
 import { Switch } from '@material-ui/core';
@@ -30,5 +31,9 @@ const Input = (props: GenericInputProps) =>
     <Switch
         color='primary'
         checked={props.input.value}
-        onChange={() => props.input.onChange(props.input.value)} 
+        onChange={handleChange(props.input.onChange, props.input.value)}
         disabled={props.commandInput.disabled} />;
+
+const handleChange = memoize(
+    (onChange: (value: string) => void, value: string) => () => onChange(value)
+);
