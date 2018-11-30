13e4f194c414887746db9255da3fb406b94d857f
Peter Amstutz <pamstutz@veritasgenetics.com>
2018-11-30 15:32:27 -0500

    14558: Version dependency fixes.
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <pamstutz@veritasgenetics.com>

--- build/package-testing/test-package-python27-python-arvados-cwl-runner.sh
+++ build/package-testing/test-package-python27-python-arvados-cwl-runner.sh
@@ -3,6 +3,10 @@
 #
 # SPDX-License-Identifier: AGPL-3.0
 
+set -e
+
+arvados-cwl-runner --version
+
 exec python <<EOF
 import arvados_cwl
 print "arvados-cwl-runner version", arvados_cwl.__version__
