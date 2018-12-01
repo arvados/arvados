2ab2ec271e09c54f6e7e1d4c3adbd1d76e56f1cd
Tom Clegg <tclegg@veritasgenetics.com>
2018-12-01 16:11:28 -0500

    14560: Only index the first 1M bytes of ft-searchable data.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tclegg@veritasgenetics.com>

---
+++ services/api/db/migrate/20180917200000_replace_full_text_indexes.rb
@@ -0,0 +1,14 @@
+# Copyright (C) The Arvados Authors. All rights reserved.
+#
+# SPDX-License-Identifier: AGPL-3.0
+
+require './db/migrate/20161213172944_full_text_search_indexes'
+
+class ReplaceFullTextIndexes < ActiveRecord::Migration
+  def up
+    FullTextSearchIndexes.new.up
+  end
+
+  def down
+  end
+end
