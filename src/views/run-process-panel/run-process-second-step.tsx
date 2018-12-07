3fdf49aeaf054284ec59e18885e66f798777ada9
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-12-07 18:25:43 +0100

    Add WorkflowPresetSelect to RunProcessSecondStepForm
    Feature #14490
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/views/run-process-panel/run-process-second-step.tsx
+++ src/views/run-process-panel/run-process-second-step.tsx
@@ -6,23 +6,38 @@ import * as React from 'react';
 import { Grid, Button } from '@material-ui/core';
 import { RunProcessBasicForm, RUN_PROCESS_BASIC_FORM } from './run-process-basic-form';
 import { RunProcessInputsForm } from '~/views/run-process-panel/run-process-inputs-form';
-import { CommandInputParameter } from '~/models/workflow';
+import { CommandInputParameter, WorkflowResource } from '~/models/workflow';
 import { connect } from 'react-redux';
 import { RootState } from '~/store/store';
 import { isValid } from 'redux-form';
 import { RUN_PROCESS_INPUTS_FORM } from './run-process-inputs-form';
 import { RunProcessAdvancedForm } from './run-process-advanced-form';
 import { createSelector, createStructuredSelector } from 'reselect';
+import { WorkflowPresetSelect } from '~/views/run-process-panel/workflow-preset-select';
+import { runProcessPanelActions } from '~/store/run-process-panel/run-process-panel-actions';
 
 export interface RunProcessSecondStepFormDataProps {
     inputs: CommandInputParameter[];
+    workflow?: WorkflowResource;
+    presets?: WorkflowResource[];
+    selectedPreset?: WorkflowResource;
     valid: boolean;
 }
 
 export interface RunProcessSecondStepFormActionProps {
     goBack: () => void;
     runProcess: () => void;
+    onPresetChange: (preset: WorkflowResource) => void;
 }
+
+const selectedWorkflowSelector = (state: RootState) =>
+    state.runProcessPanel.selectedWorkflow;
+
+const presetsSelector = (state: RootState) =>
+    state.runProcessPanel.presets;
+
+const selectedPresetSelector = (state: RootState) =>
+    state.runProcessPanel.selectedPreset;
 
 const inputsSelector = (state: RootState) =>
     state.runProcessPanel.inputs;
@@ -33,13 +48,22 @@ const validSelector = (state: RootState) =>
 const mapStateToProps = createStructuredSelector({
     inputs: inputsSelector,
     valid: validSelector,
+    workflow: selectedWorkflowSelector,
+    presets: presetsSelector,
+    selectedPreset: selectedPresetSelector,
 });
 
 export type RunProcessSecondStepFormProps = RunProcessSecondStepFormDataProps & RunProcessSecondStepFormActionProps;
-export const RunProcessSecondStepForm = connect(mapStateToProps)(
-    ({ inputs, valid, goBack, runProcess }: RunProcessSecondStepFormProps) =>
+export const RunProcessSecondStepForm = connect(mapStateToProps, { onPresetChange: runProcessPanelActions.SELECT_WORKFLOW_PRESET })(
+    ({ inputs, workflow, selectedPreset, presets, onPresetChange, valid, goBack, runProcess }: RunProcessSecondStepFormProps) =>
         <Grid container spacing={16}>
             <Grid item xs={12}>
+                <Grid item xs={6}>
+                    {workflow && selectedPreset && presets &&
+                        < WorkflowPresetSelect
+                            {...{ workflow, selectedPreset, presets, onChange: onPresetChange }} />
+                    }
+                </Grid>
                 <RunProcessBasicForm />
                 <RunProcessInputsForm inputs={inputs} />
                 <RunProcessAdvancedForm />
