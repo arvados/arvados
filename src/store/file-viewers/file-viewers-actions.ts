4105d31a5aba13507832d6c86c0154116d857ea5
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-12-12 18:01:50 +0100

    Handle  errors while fetching fileViewersConfig
    Feature #13540
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/store/file-viewers/file-viewers-actions.ts
+++ src/store/file-viewers/file-viewers-actions.ts
@@ -5,11 +5,17 @@
 import { Dispatch } from 'redux';
 import { ServiceRepository } from '~/services/services';
 import { propertiesActions } from '~/store/properties/properties-actions';
-import { FILE_VIEWERS_PROPERTY_NAME } from '~/store/file-viewers/file-viewers-selectors';
+import { FILE_VIEWERS_PROPERTY_NAME, DEFAULT_FILE_VIEWERS } from '~/store/file-viewers/file-viewers-selectors';
+import { FileViewerList } from '~/models/file-viewers-config';
 
 export const loadFileViewersConfig = async (dispatch: Dispatch, _: {}, { fileViewersConfig }: ServiceRepository) => {
-
-    const config = await fileViewersConfig.get();
+    
+    let config: FileViewerList;
+    try{
+        config = await fileViewersConfig.get();
+    } catch (e){
+        config = DEFAULT_FILE_VIEWERS;
+    }
 
     dispatch(propertiesActions.SET_PROPERTY({
         key: FILE_VIEWERS_PROPERTY_NAME,
