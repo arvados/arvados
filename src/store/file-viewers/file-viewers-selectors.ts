e9e0cc24a86e55e7b6421b352800ff29f201a29d
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-12-13 14:57:57 +0100

    Clean up DEFAULT_FILE_VIEWERS
    Feature #13540
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/store/file-viewers/file-viewers-selectors.ts
+++ src/store/file-viewers/file-viewers-selectors.ts
@@ -7,8 +7,6 @@ import { FileViewerList } from '~/models/file-viewers-config';
 
 export const FILE_VIEWERS_PROPERTY_NAME = 'fileViewers';
 
-export const DEFAULT_FILE_VIEWERS: FileViewerList = [
-    { name: 'Test viewer', url: 'http://viewer.com', filePathParam: 'filePath' },
-];
+export const DEFAULT_FILE_VIEWERS: FileViewerList = [];
 export const getFileViewers = (state: PropertiesState) =>
     getProperty<FileViewerList>(FILE_VIEWERS_PROPERTY_NAME)(state) || DEFAULT_FILE_VIEWERS;
