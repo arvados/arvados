5017989693137cabe7f00a00c51db35625f9b55f
Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>
2018-12-27 09:36:04 +0100

    login-as-someone
    Feature #14565
    
    Arvados-DCO-1.1-Signed-off-by: Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>

--- src/store/users/users-actions.ts
+++ src/store/users/users-actions.ts
@@ -12,6 +12,7 @@ import { snackbarActions, SnackbarKind } from '~/store/snackbar/snackbar-actions
 import { UserResource } from "~/models/user";
 import { getResource } from '~/store/resources/resources';
 import { navigateToProject, navigateToUsers, navigateToRootProject } from "~/store/navigation/navigation-action";
+import { saveApiToken } from '~/store/auth/auth-action';
 
 export const USERS_PANEL_ID = 'usersPanel';
 export const USER_ATTRIBUTES_DIALOG = 'userAttributesDialog';
@@ -44,7 +45,16 @@ export const openSetupShellAccount = (uuid: string) =>
         const { resources } = getState();
         const user = getResource<UserResource>(uuid)(resources);
         const virtualMachines = await services.virtualMachineService.list();
+        dispatch(dialogActions.CLOSE_DIALOG({ id: USER_MANAGE_DIALOG }));
         dispatch(dialogActions.OPEN_DIALOG({ id: SETUP_SHELL_ACCOUNT_DIALOG, data: { user, ...virtualMachines } }));
+    };
+
+export const loginAs = (uuid: string) => 
+    async (dispatch: Dispatch, getState: () => RootState, services: ServiceRepository) => {
+        const client = await services.apiClientAuthorizationService.get(uuid);
+        dispatch<any>(saveApiToken(client.apiToken));
+        location.reload();
+        dispatch<any>(navigateToRootProject);
     };
 
 export const openUserCreateDialog = () =>
--- src/views-components/user-dialog/manage-dialog.tsx
+++ src/views-components/user-dialog/manage-dialog.tsx
@@ -9,7 +9,7 @@ import { withDialog } from '~/store/dialog/with-dialog';
 import { WithStyles, withStyles } from '@material-ui/core/styles';
 import { ArvadosTheme } from '~/common/custom-theme';
 import { compose, Dispatch } from "redux";
-import { USER_MANAGE_DIALOG, openSetupShellAccount } from "~/store/users/users-actions";
+import { USER_MANAGE_DIALOG, openSetupShellAccount, loginAs } from "~/store/users/users-actions";
 import { UserResource } from "~/models/user";
 import { connect } from "react-redux";
 
@@ -28,10 +28,12 @@ interface UserManageDataProps {
 
 interface UserManageActionProps {
     openSetupShellAccount: (uuid: string) => void;
+    loginAs: (uuid: string) => void;
 }
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-    openSetupShellAccount: (uuid: string) => dispatch<any>(openSetupShellAccount(uuid))
+    openSetupShellAccount: (uuid: string) => dispatch<any>(openSetupShellAccount(uuid)),
+    loginAs: (uuid: string) => dispatch<any>(loginAs(uuid))
 });
 
 type UserManageProps = UserManageDataProps & UserManageActionProps & WithStyles<CssRules>;
@@ -50,13 +52,13 @@ export const UserManageDialog = compose(
                     <Typography variant="body2" className={props.classes.spacing}>
                         As an admin, you can log in as this user. When you’ve finished, you will need to log out and log in again with your own account.
                     </Typography>
-                    <Button variant="contained" color="primary">
+                    <Button variant="contained" color="primary" onClick={() => props.loginAs(props.data.uuid)}>
                         {`LOG IN AS ${props.data.firstName} ${props.data.lastName}`}
                     </Button>
                     <Typography variant="body2" className={props.classes.spacing}>
                         As an admin, you can setup a shell account for this user. The login name is automatically generated from the user's e-mail address.
                     </Typography>
-                    <Button variant="contained" color="primary" onClick={() => props.openSetupShellAccount(props.data.uuid)}>
+                    <Button variant="contained" color="primary" onClick={() => props.openSetupShellAccount(props.data.uuid)} disabled>
                         {`SETUP SHELL ACCOUNT FOR ${props.data.firstName} ${props.data.lastName}`}
                     </Button>
                 </DialogContent>
