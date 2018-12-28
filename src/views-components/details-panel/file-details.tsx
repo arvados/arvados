cdad04a31b572aa401e3f5d1fdc2c0f69d6e2d08
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-12-28 12:21:28 +0100

    Display correct icon in FileDetails
    Feature #14684
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/views-components/details-panel/file-details.tsx
+++ src/views-components/details-panel/file-details.tsx
@@ -3,14 +3,15 @@
 // SPDX-License-Identifier: AGPL-3.0
 
 import * as React from 'react';
-import { ProjectIcon } from '~/components/icon/icon';
 import { DetailsData } from "./details-data";
 import { CollectionFile, CollectionDirectory } from '~/models/collection-file';
+import { getIcon } from '~/components/file-tree/file-tree-item';
 
 export class FileDetails extends DetailsData<CollectionFile | CollectionDirectory> {
 
-    getIcon(className?: string){
-        return <ProjectIcon className={className} />;
+    getIcon(className?: string) {
+        const Icon = getIcon(this.item.type);
+        return <Icon className={className} />;
     }
 
     getDetails() {
