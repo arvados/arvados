ff073f6450dc3f6551586aab015583e39abb0a41
Peter Amstutz <pamstutz@veritasgenetics.com>
2019-01-08 13:30:47 -0500

    Make redux devtools optional for development build refs #14660
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <pamstutz@veritasgenetics.com>

--- src/store/store.ts
+++ src/store/store.ts
@@ -61,7 +61,8 @@ import { ApiClientAuthorizationMiddlewareService } from '~/store/api-client-auth
 
 const composeEnhancers =
     (process.env.NODE_ENV === 'development' &&
-        window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({trace: true, traceLimit: 25})) ||
+     window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({trace: true, traceLimit: 25})) ||
     compose;
 
 export type RootState = ReturnType<ReturnType<typeof createRootReducer>>;
