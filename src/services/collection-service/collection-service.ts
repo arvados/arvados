75a77f7bf1b104d58e1d2bcfdb7e0d76a59a2909
Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>
2019-02-13 12:27:44 +0100

    refs #14671-download-file-from-collection-asks-for-auth
    Arvados-DCO-1.1-Signed-off-by: Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>

--- src/services/collection-service/collection-service.ts
+++ src/services/collection-service/collection-service.ts
@@ -54,9 +54,14 @@ export class CollectionService extends TrashableResourceService<CollectionResour
         const baseUrl = this.webdavClient.defaults.baseURL.endsWith('/')
             ? this.webdavClient.defaults.baseURL.slice(0, -1)
             : this.webdavClient.defaults.baseURL;
+            const apiToken = this.authService.getApiToken();
+            const splittedApiToken = apiToken ? apiToken.split('/') : [];
+            const userApiToken = `/t=${splittedApiToken[2]}/`;
+            const splittedPrevFileUrl = file.url.split('/');
+            const url = `${baseUrl}/${splittedPrevFileUrl[1]}${userApiToken}${splittedPrevFileUrl[2]}`;
         return {
             ...file,
-            url: baseUrl + file.url
+            url
         };
     }
 
