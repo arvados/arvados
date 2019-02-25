28cfe1015a00073a89726ef39b9ac04733ea49cc
Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>
2019-02-25 11:09:20 +0100

    download-selected
    Feature #14856
    
    Arvados-DCO-1.1-Signed-off-by: Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>

--- src/views-components/context-menu/action-sets/collection-files-action-set.ts
+++ src/views-components/context-menu/action-sets/collection-files-action-set.ts
@@ -5,6 +5,7 @@
 import { ContextMenuActionSet } from "~/views-components/context-menu/context-menu-action-set";
 import { collectionPanelFilesAction, openMultipleFilesRemoveDialog } from "~/store/collection-panel/collection-panel-files/collection-panel-files-actions";
 import { openCollectionPartialCopyDialog } from '~/store/collections/collection-partial-copy-actions';
+import { DownloadCollectionFileAction } from "~/views-components/context-menu/actions/download-collection-file-action";
 
 export const collectionFilesActionSet: ContextMenuActionSet = [[{
     name: "Select all",
@@ -22,7 +23,7 @@ export const collectionFilesActionSet: ContextMenuActionSet = [[{
         dispatch(openMultipleFilesRemoveDialog());
     }
 }, {
-    name: "Download selected",
+    component: DownloadCollectionFileAction,
     execute: () => { return; }
 }, {
     name: "Create a new collection with selected",
