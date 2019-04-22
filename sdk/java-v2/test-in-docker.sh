30298819bc92b5ec90c8544bee7e102cb64e888d
Fernando Monserrat <fernando.monserrat@gmail.com>
2019-04-22 12:56:03 -0300

    issue #15041 Split test and upload. uploadArchives passed as parameter to publish artifact  \nArvados-DCO-1.1-Signed-off-by: Fernando Monserrat <fmonserrat@veritasgenetics.com>

--- sdk/java-v2/test-in-docker.sh
+++ sdk/java-v2/test-in-docker.sh
@@ -45,4 +45,4 @@ else
     build_version="$(version_from_git)"
 fi
 #UID=$(id -u) # UID is read-only on many systems
-exec docker run --rm --user $UID -v $PWD:$PWD -w $PWD gradle /bin/sh -c 'gradle clean && gradle test && gradle jar install uploadArchives -Pversion='"$build_version" --info --stacktrace
+exec docker run --rm --user $UID -v $PWD:$PWD -w $PWD gradle /bin/sh -c 'gradle clean && gradle test && gradle jar install '"$gradle_upload"
