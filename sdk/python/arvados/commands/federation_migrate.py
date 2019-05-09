2d6908242ec0726e76878e0316d6334fe91c2555
Peter Amstutz <pamstutz@veritasgenetics.com>
2019-05-09 10:59:56 -0400

    15061: Move federation migrate script into sdk
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <pamstutz@veritasgenetics.com>

--- sdk/python/arvados/commands/federation_migrate.py
+++ sdk/python/arvados/commands/federation_migrate.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (C) The Arvados Authors. All rights reserved.
 #
-# SPDX-License-Identifier: AGPL-3.0
+# SPDX-License-Identifier: Apache-2.0
 
 import arvados
 import arvados.util
