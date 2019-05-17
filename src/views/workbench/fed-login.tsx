cd10c61ba9a77ca1d4dea853dacc350c729cab9f
Peter Amstutz <pamstutz@veritasgenetics.com>
2019-05-17 16:26:13 -0400

    Fix federated login, only create iframes for local user refs #15064
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <pamstutz@veritasgenetics.com>

--- src/views/workbench/fed-login.tsx
+++ src/views/workbench/fed-login.tsx
@@ -13,7 +13,7 @@ import { Config } from '~/common/config';
 export interface FedLoginProps {
     user?: User;
     apiToken?: string;
-    homeCluster: string;
+    localCluster: string;
     remoteHostsConfig: { [key: string]: Config };
 }
 
@@ -21,20 +21,20 @@ const mapStateToProps = ({ auth }: RootState) => ({
     user: auth.user,
     apiToken: auth.apiToken,
     remoteHostsConfig: auth.remoteHostsConfig,
-    homeCluster: auth.homeCluster,
+    localCluster: auth.localCluster,
 });
 
 export const FedLogin = connect(mapStateToProps)(
     class extends React.Component<FedLoginProps> {
         render() {
-            const { apiToken, user, homeCluster, remoteHostsConfig } = this.props;
-            if (!apiToken || !user || !user.uuid.startsWith(homeCluster)) {
+            const { apiToken, user, localCluster, remoteHostsConfig } = this.props;
+            if (!apiToken || !user || !user.uuid.startsWith(localCluster)) {
                 return <></>;
             }
             const [, tokenUuid, token] = apiToken.split("/");
             return <div id={"fedtoken-iframe-div"}>
                 {Object.keys(remoteHostsConfig)
-                    .map((k) => k !== homeCluster &&
+                    .map((k) => k !== localCluster &&
                         <iframe key={k} src={`${remoteHostsConfig[k].workbench2Url}/fedtoken?api_token=${getSaltedToken(k, tokenUuid, token)}`} style={{
                             height: 0,
                             width: 0,
