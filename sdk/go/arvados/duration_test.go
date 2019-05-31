6e73eff3926a2e7345333edd02531e8e6fbe15ef
Tom Clegg <tclegg@veritasgenetics.com>
2019-05-31 16:15:05 -0400

    15003: Format durations as "336h", not "336h0m0s".
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tclegg@veritasgenetics.com>

---
+++ sdk/go/arvados/duration_test.go
@@ -0,0 +1,45 @@
+// Copyright (C) The Arvados Authors. All rights reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package arvados
+
+import (
+	"encoding/json"
+	"time"
+
+	check "gopkg.in/check.v1"
+)
+
+var _ = check.Suite(&DurationSuite{})
+
+type DurationSuite struct{}
+
+func (s *DurationSuite) TestMarshalJSON(c *check.C) {
+	var d struct {
+		D Duration
+	}
+	err := json.Unmarshal([]byte(`{"D":"1.234s"}`), &d)
+	c.Check(err, check.IsNil)
+	c.Check(d.D, check.Equals, Duration(time.Second+234*time.Millisecond))
+	buf, err := json.Marshal(d)
+	c.Check(string(buf), check.Equals, `{"D":"1.234s"}`)
+
+	for _, trial := range []struct {
+		seconds int
+		out     string
+	}{
+		{30, "30s"},
+		{60, "1m"},
+		{120, "2m"},
+		{150, "2m30s"},
+		{3600, "1h"},
+		{7201, "2h1s"},
+		{360600, "100h10m"},
+		{360610, "100h10m10s"},
+	} {
+		buf, err := json.Marshal(Duration(time.Duration(trial.seconds) * time.Second))
+		c.Check(err, check.IsNil)
+		c.Check(string(buf), check.Equals, `"`+trial.out+`"`)
+	}
+}
