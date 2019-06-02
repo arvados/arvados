2f9771027fa0b1096b1eb8d450d7abf95356ee20
Ward Vandewege <wvandewege@veritasgenetics.com>
2019-06-02 13:23:29 -0400

    If a version number is supplied to the build command, make sure to report that version number in the web interface (top left).
    No issue #
    
    Arvados-DCO-1.1-Signed-off-by: Ward Vandewege <wvandewege@veritasgenetics.com>

--- src/common/app-info.ts
+++ src/common/app-info.ts
@@ -3,7 +3,11 @@
 // SPDX-License-Identifier: AGPL-3.0
 
 export const getBuildInfo = (): string => {
-    const getBuildNumber = "BN-" + (process.env.REACT_APP_BUILD_NUMBER || "dev");
-    const getGitCommit = "GIT-" + (process.env.REACT_APP_GIT_COMMIT || "latest").substr(0, 7);
-    return getBuildNumber + " / " + getGitCommit;
+    if (process.env.REACT_APP_VERSION) {
+      return "v" + process.env.REACT_APP_VERSION;
+    } else {
+      const getBuildNumber = "BN-" + (process.env.REACT_APP_BUILD_NUMBER || "dev");
+      const getGitCommit = "GIT-" + (process.env.REACT_APP_GIT_COMMIT || "latest").substr(0, 7);
+      return getBuildNumber + " / " + getGitCommit;
+    }
 };
