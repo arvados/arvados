c12b6b673802dcca6b6eb5d3fc5b10c151b2db76
Peter Amstutz <pamstutz@veritasgenetics.com>
2019-07-03 16:18:11 -0400

    14812: We can't log the error, might as well make it fatal.
    Rails.logger isn't initialized at this point.
    
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <pamstutz@veritasgenetics.com>

--- apps/workbench/lib/config_validators.rb
+++ apps/workbench/lib/config_validators.rb
@@ -5,18 +5,16 @@
 require 'uri'
 
 module ConfigValidators
-    def self.validate_wb2_url_config
-        if Rails.configuration.Services.Workbench2.ExternalURL != URI("")
-          if !Rails.configuration.Services.Workbench2.ExternalURL.is_a?(URI::HTTP)
-            Rails.logger.warn("workbench2_url config is not an HTTP URL: #{Rails.configuration.Services.Workbench2.ExternalURL}")
-            Rails.configuration.Services.Workbench2.ExternalURL = URI("")
-          elsif /.*[\/]{2,}$/.match(Rails.configuration.Services.Workbench2.ExternalURL.to_s)
-            Rails.logger.warn("workbench2_url config shouldn't have multiple trailing slashes: #{Rails.configuration.Services.Workbench2.ExternalURL}")
-            Rails.configuration.Services.Workbench2.ExternalURL = URI("")
-          else
-            return true
-          end
-        end
-        return false
+  def self.validate_wb2_url_config
+    if Rails.configuration.Services.Workbench2.ExternalURL != URI("")
+      if !Rails.configuration.Services.Workbench2.ExternalURL.is_a?(URI::HTTP)
+        raise "workbench2_url config is not an HTTP URL: #{Rails.configuration.Services.Workbench2.ExternalURL}"
+      elsif /.*[\/]{2,}$/.match(Rails.configuration.Services.Workbench2.ExternalURL.to_s)
+        raise "workbench2_url config shouldn't have multiple trailing slashes: #{Rails.configuration.Services.Workbench2.ExternalURL}"
+      else
+        return true
+      end
     end
+    return false
+  end
 end
