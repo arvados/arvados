4d56f9b913fcf41fbf89bf5016463b5353fa3a9f
Peter Amstutz <pamstutz@veritasgenetics.com>
2019-08-06 15:20:47 -0400

    15133: Delete crunch_scripts, start clearing out API server
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <pamstutz@veritasgenetics.com>

--- services/api/app/controllers/arvados/v1/job_tasks_controller.rb
+++ services/api/app/controllers/arvados/v1/job_tasks_controller.rb
@@ -4,4 +4,9 @@
 
 class Arvados::V1::JobTasksController < ApplicationController
   accept_attribute_as_json :parameters, Hash
+
+  def create
+    return send_error("Unsupported legacy jobs API",
+                      status: 400)
+  end
 end
