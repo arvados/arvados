7410ffda247fc84a9e650dc441dd415f483cfa5e
Peter Amstutz <pamstutz@veritasgenetics.com>
2019-08-22 14:47:44 -0400

    15583: Set with_count('none') on wb page notification tests
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <pamstutz@veritasgenetics.com>

--- apps/workbench/app/controllers/application_controller.rb
+++ apps/workbench/app/controllers/application_controller.rb
@@ -782,7 +782,7 @@ class ApplicationController < ActionController::Base
 
   @@notification_tests.push lambda { |controller, current_user|
     return nil if Rails.configuration.Services.WebShell.ExternalURL != URI("")
-    AuthorizedKey.limit(1).where(authorized_user_uuid: current_user.uuid).each do
+    AuthorizedKey.limit(1).with_count('none').where(authorized_user_uuid: current_user.uuid).each do
       return nil
     end
     return lambda { |view|
@@ -791,7 +791,7 @@ class ApplicationController < ActionController::Base
   }
 
   @@notification_tests.push lambda { |controller, current_user|
-    Collection.limit(1).where(created_by: current_user.uuid).each do
+    Collection.limit(1).with_count('none').where(created_by: current_user.uuid).each do
       return nil
     end
     return lambda { |view|
@@ -801,7 +801,7 @@ class ApplicationController < ActionController::Base
 
   @@notification_tests.push lambda { |controller, current_user|
     if PipelineInstance.api_exists?(:index)
-      PipelineInstance.limit(1).where(created_by: current_user.uuid).each do
+      PipelineInstance.limit(1).with_count('none').where(created_by: current_user.uuid).each do
         return nil
       end
     else
