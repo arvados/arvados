d26d87bbe23b31f2838c3eaea26699c829223792
Lucas Di Pentima <lucas@di-pentima.com.ar>
2019-10-31 11:12:32 -0300

    15067: Uses new vocabulary example. Temporarily forces to load it for testing.
    The design of this new format was produced at:
    
    https://dev.arvados.org/issues/15071
    
    Arvados-DCO-1.1-Signed-off-by: Lucas Di Pentima <ldipentima@veritasgenetics.com>

--- src/common/config.ts
+++ src/common/config.ts
@@ -102,6 +102,9 @@ remove the entire ${varName} entry from ${WORKBENCH_CONFIG_URL}`);
                 else {
                     vocabularyUrl = clusterConfigJSON.Workbench.VocabularyURL || "/vocabulary-example.json";
                 }
+                // FIXME: The following line is for dev testing purposes
+                vocabularyUrl = "/vocabulary-example.json";
+
                 config.vocabularyUrl = vocabularyUrl;
 
                 config.rootUrl = clusterConfigJSON.Services.Controller.ExternalURL;
