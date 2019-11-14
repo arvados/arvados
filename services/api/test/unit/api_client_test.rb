acb8ea09b9d699bb5c955a5279a0aca2e0906c39
Peter Amstutz <pamstutz@veritasgenetics.com>
2019-11-13 21:36:27 -0500

    15828: Configured workbench is a "trusted client" by default
    Sidestep an installation sinkhole that almost everyone seems to fall into.
    
    This makes it so it is no longer necessary to explicitly set
    "is_trusted" on an api_client record for the system configured
    Workbench instances to solve the "client cannot manipulate other's
    tokens" permission error.
    
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <pamstutz@veritasgenetics.com>

--- services/api/test/unit/api_client_test.rb
+++ services/api/test/unit/api_client_test.rb
@@ -5,7 +5,27 @@
 require 'test_helper'
 
 class ApiClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include CurrentApiClient
+
+  test "configured workbench is trusted" do
+    Rails.configuration.Services.Workbench1.ExternalURL = URI("http://wb1.example.com")
+    Rails.configuration.Services.Workbench2.ExternalURL = URI("https://wb2.example.com:443")
+
+    act_as_system_user do
+      [["http://wb0.example.com", false],
+       ["http://wb1.example.com", true],
+       ["http://wb2.example.com", false],
+       ["https://wb2.example.com", true],
+       ["https://wb2.example.com/", true],
+      ].each do |pfx, result|
+        a = ApiClient.create(url_prefix: pfx, is_trusted: false)
+        assert_equal result, a.is_trusted
+      end
+
+      a = ApiClient.create(url_prefix: "http://example.com", is_trusted: true)
+      a.save!
+      a.reload
+      assert a.is_trusted
+    end
+  end
 end
