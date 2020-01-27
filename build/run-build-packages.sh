bc4f35573b38ccde0756209057c8bce6947a38bc
Peter Amstutz <peter.amstutz@curii.com>
2020-01-27 13:31:49 -0500

    Add python3 build of arvados-cwl-runner
    no issue #
    
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- build/run-build-packages.sh
+++ build/run-build-packages.sh
@@ -325,6 +325,9 @@ fpm_build_virtualenv "arvados-python-client" "sdk/python"
 # Arvados cwl runner
 fpm_build_virtualenv "arvados-cwl-runner" "sdk/cwl"
 
+# Arvados cwl runner - Python3 package
+fpm_build_virtualenv "arvados-cwl-runner" "sdk/cwl" "python3"
+
 # The PAM module
 fpm_build_virtualenv "libpam-arvados" "sdk/pam"
 
