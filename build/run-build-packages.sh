74c70bf8c41ac135976f30784a4b5a3ab85dbe47
Ward Vandewege <ward@jhvc.com>
2020-02-02 10:55:17 -0500

    Update maintainer and vendor for our OS packages.
    No issue #
    
    Arvados-DCO-1.1-Signed-off-by: Ward Vandewege <ward@jhvc.com>

--- build/run-build-packages.sh
+++ build/run-build-packages.sh
@@ -38,8 +38,8 @@ EOF
 # set to --no-cache-dir to disable pip caching
 CACHE_FLAG=
 
-MAINTAINER="Ward Vandewege <wvandewege@veritasgenetics.com>"
-VENDOR="Veritas Genetics, Inc."
+MAINTAINER="Arvados Package Maintainers <packaging@arvados.org>"
+VENDOR="The Arvados Project"
 
 # End of user configuration
 
