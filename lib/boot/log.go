d527d35430978b42448388dad241b57851fda335
Tom Clegg <tom@tomclegg.ca>
2020-02-19 10:24:48 -0500

    15954: Fix up logging prefixes.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@tomclegg.ca>

--- lib/boot/log.go
+++ lib/boot/log.go
@@ -19,11 +19,13 @@ func (lp *logPrefixer) Write(p []byte) (int, error) {
 	if len(p) == 0 {
 		return 0, nil
 	}
+	var out []byte
 	if !lp.did {
-		lp.Writer.Write(lp.Prefix)
-		lp.did = p[len(p)-1] != '\n'
+		out = append(out, lp.Prefix...)
 	}
-	out := append(bytes.Replace(p[:len(p)-1], []byte("\n"), append([]byte("\n"), lp.Prefix...), -1), p[len(p)-1])
+	lp.did = p[len(p)-1] != '\n'
+	out = append(out, bytes.Replace(p[:len(p)-1], []byte("\n"), append([]byte("\n"), lp.Prefix...), -1)...)
+	out = append(out, p[len(p)-1])
 	_, err := lp.Writer.Write(out)
 	if err != nil {
 		return 0, err
