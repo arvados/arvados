0761f57a7703655bdbfd77c261f3016592fb7f8b
Lucas Di Pentima <lucas@di-pentima.com.ar>
2020-03-24 18:03:34 -0300

    16263: Adds test exposing a bug on filter unmarshalling.
    Arvados-DCO-1.1-Signed-off-by: Lucas Di Pentima <lucas@di-pentima.com.ar>

--- sdk/go/arvados/resource_list_test.go
+++ sdk/go/arvados/resource_list_test.go
@@ -34,3 +34,40 @@ func TestMarshalFiltersWithNil(t *testing.T) {
 		t.Errorf("Encoded as %q, expected %q", buf, expect)
 	}
 }
+
+func TestUnmarshalFiltersWithNil(t *testing.T) {
+	buf := []byte(`["modified_at","=",null]`)
+	f := &Filter{}
+	err := f.UnmarshalJSON(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expect := Filter{Attr: "modified_at", Operator: "=", Operand: nil}
+	if f.Attr != expect.Attr || f.Operator != expect.Operator || f.Operand != expect.Operand {
+		t.Errorf("Decoded as %q, expected %q", f, expect)
+	}
+}
+
+func TestMarshalFiltersWithBoolean(t *testing.T) {
+	buf, err := json.Marshal([]Filter{
+		{Attr: "is_active", Operator: "=", Operand: true}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expect := []byte(`[["is_active","=",true]]`); 0 != bytes.Compare(buf, expect) {
+		t.Errorf("Encoded as %q, expected %q", buf, expect)
+	}
+}
+
+func TestUnmarshalFiltersWithBoolean(t *testing.T) {
+	buf := []byte(`["is_active","=",true]`)
+	f := &Filter{}
+	err := f.UnmarshalJSON(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expect := Filter{Attr: "is_active", Operator: "=", Operand: true}
+	if f.Attr != expect.Attr || f.Operator != expect.Operator || f.Operand != expect.Operand {
+		t.Errorf("Decoded as %q, expected %q", f, expect)
+	}
+}
