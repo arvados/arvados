d9b06eb06c0ac38af922f102da0b8e405bb40f82
Tom Clegg <tom@tomclegg.ca>
2020-06-04 14:49:32 -0400

    16427: Return error instead of ok bool from util funcs.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@tomclegg.ca>

--- lib/undelete/cmd.go
+++ lib/undelete/cmd.go
@@ -6,6 +6,7 @@ package undelete
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -112,18 +113,18 @@ type undeleter struct {
 	logger  logrus.FieldLogger
 }
 
+var errNotFound = errors.New("not found")
+
 // Return the timestamp of the newest copy of blk on svc. Second
 // return value is false if blk is not on svc at all, or an error
 // occurs.
-func (und undeleter) newestMtime(logger logrus.FieldLogger, blk string, svc arvados.KeepService) (time.Time, bool) {
+func (und undeleter) newestMtime(logger logrus.FieldLogger, blk string, svc arvados.KeepService) (time.Time, error) {
 	found, err := svc.Index(und.client, blk)
 	if err != nil {
 		logger.WithError(err).Warn("error getting index")
-		return time.Time{}, false
-	}
-	if len(found) == 0 {
-		logger.Debug("not found")
-		return time.Time{}, false
+		return time.Time{}, err
+	} else if len(found) == 0 {
+		return time.Time{}, errNotFound
 	}
 	var latest time.Time
 	for _, ent := range found {
@@ -133,38 +134,40 @@ func (und undeleter) newestMtime(logger logrus.FieldLogger, blk string, svc arva
 		}
 	}
 	logger.WithField("latest", latest).Debug("found")
-	return latest, true
+	return latest, nil
 }
+
+var errTouchIneffective = errors.New("(BUG?) touch succeeded but had no effect -- reported timestamp is still too old")
 
 // Ensure the given block exists on the given server and won't be
 // eligible for trashing until after our chosen deadline (blobsigexp).
-// Returns false if the block doesn't exist on the given server, has
-// an old timestamp and can't be updated, or any error occurred.
-// Reports errors via logger.
+// Returns an error if the block doesn't exist on the given server, or
+// has an old timestamp and can't be updated.  Reports errors via
+// logger.
 //
 // After we decide a block is "safe" (whether or not we had to untrash
 // it), keep-balance might notice that it's currently unreferenced and
 // decide to trash it, all before our recovered collection gets
 // saved. But if the block's timestamp is more recent than blobsigttl,
 // keepstore will refuse to trash it even if told to by keep-balance.
-func (und undeleter) ensureSafe(ctx context.Context, logger logrus.FieldLogger, blk string, svc arvados.KeepService, blobsigttl time.Duration, blobsigexp time.Time) bool {
-	if latest, ok := und.newestMtime(logger, blk, svc); !ok {
-		return false
+func (und undeleter) ensureSafe(ctx context.Context, logger logrus.FieldLogger, blk string, svc arvados.KeepService, blobsigttl time.Duration, blobsigexp time.Time) error {
+	if latest, err := und.newestMtime(logger, blk, svc); err != nil {
+		return err
 	} else if latest.Add(blobsigttl).After(blobsigexp) {
-		return true
+		return nil
 	}
 	if err := svc.Touch(ctx, und.client, blk); err != nil {
-		logger.WithError(err).Warn("error updating timestamp")
-		return false
+		return fmt.Errorf("error updating timestamp: %s", err)
 	}
 	logger.Debug("updated timestamp")
-	if latest, ok := und.newestMtime(logger, blk, svc); !ok {
-		return false
+	if latest, err := und.newestMtime(logger, blk, svc); err == errNotFound {
+		return fmt.Errorf("(BUG?) touch succeeded, but then block did not appear in index")
+	} else if err != nil {
+		return err
 	} else if latest.Add(blobsigttl).After(blobsigexp) {
-		return true
+		return nil
 	} else {
-		logger.WithField("latest", latest).Error("BUG? touch return success, but newest reported timestamp is still too old")
-		return false
+		return errTouchIneffective
 	}
 }
 
@@ -225,7 +228,12 @@ func (und undeleter) RecoverManifest(mtxt string) (string, error) {
 							}
 							logger.Info("untrashed")
 						}
-						if und.ensureSafe(ctx, logger, blk, svc, blobsigttl, blobsigexp) {
+						err := und.ensureSafe(ctx, logger, blk, svc, blobsigttl, blobsigexp)
+						if err == errNotFound {
+							logger.Debug(err)
+						} else if err != nil {
+							logger.Error(err)
+						} else {
 							blkFound[idx] = true
 							continue nextblk
 						}
