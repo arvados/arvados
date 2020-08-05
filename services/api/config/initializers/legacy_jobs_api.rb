bb0c95e59ed5bda44837042e5768732f9bd822a2
Lucas Di Pentima <lucas@di-pentima.com.ar>
2020-08-05 08:58:40 -0300

    16470: Avoids crashing when running rake db:create. WIP
    Arvados-DCO-1.1-Signed-off-by: Lucas Di Pentima <lucas@di-pentima.com.ar>

--- services/api/config/initializers/legacy_jobs_api.rb
+++ services/api/config/initializers/legacy_jobs_api.rb
@@ -9,7 +9,12 @@
 require 'enable_jobs_api'
 
 Rails.application.configure do
-  if ActiveRecord::Base.connected? and ActiveRecord::Base.connection.tables.include?('jobs')
-    check_enable_legacy_jobs_api
+  begin
+    if ActiveRecord::Base.connection.tables.include?('jobs')
+      check_enable_legacy_jobs_api
+    end
+  rescue ActiveRecord::NoDatabaseError
+    # Since rails 5.2, all initializers are run by rake tasks (like db:create),
+    # see: https://github.com/rails/rails/issues/32870
   end
 end
