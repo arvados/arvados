bc11ee32eeb31c63a3fb99819087d2def0988789
Tom Clegg <tom@tomclegg.ca>
2020-09-01 10:16:47 -0400

    16790: Avoid returning empty NextMarker tag in list responses.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@tomclegg.ca>

--- services/keep-web/s3_test.go
+++ services/keep-web/s3_test.go
@@ -411,6 +411,26 @@ func (s *IntegrationSuite) TestS3ListNoCommonPrefixes(c *check.C) {
 	c.Check(string(buf), check.Not(check.Matches), `(?ms).*CommonPrefixes.*`)
 }
 
+// If there is no delimiter in the request, or the results are not
+// truncated, the NextMarker XML tag should not appear in the response
+// body.
+func (s *IntegrationSuite) TestS3ListNoNextMarker(c *check.C) {
+	stage := s.s3setup(c)
+	defer stage.teardown(c)
+
+	for _, query := range []string{"prefix=e&delimiter=/", ""} {
+		req, err := http.NewRequest("GET", stage.collbucket.URL("/"), nil)
+		c.Assert(err, check.IsNil)
+		req.Header.Set("Authorization", "AWS "+arvadostest.ActiveTokenV2+":none")
+		req.URL.RawQuery = query
+		resp, err := http.DefaultClient.Do(req)
+		c.Assert(err, check.IsNil)
+		buf, err := ioutil.ReadAll(resp.Body)
+		c.Assert(err, check.IsNil)
+		c.Check(string(buf), check.Not(check.Matches), `(?ms).*NextMarker.*`)
+	}
+}
+
 func (s *IntegrationSuite) TestS3CollectionList(c *check.C) {
 	stage := s.s3setup(c)
 	defer stage.teardown(c)
