d371cc9924105d7db4389edbe3a765e759a080f8
Peter Amstutz <peter.amstutz@curii.com>
2020-09-22 17:15:19 -0400

    Tweak database seeds so the reset-to-fixture API works
    refs #16811
    
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- services/api/app/models/database_seeds.rb
+++ services/api/app/models/database_seeds.rb
@@ -7,17 +7,18 @@ require 'update_permissions'
 class DatabaseSeeds
   extend CurrentApiClient
   def self.install
-    system_user
-    system_group
-    all_users_group
-    anonymous_group
-    anonymous_group_read_permission
-    anonymous_user
-    system_root_token_api_client
-    public_project_group
-    public_project_read_permission
-    empty_collection
-    refresh_permissions
+    batch_update_permissions do
+      system_user
+      system_group
+      all_users_group
+      anonymous_group
+      anonymous_group_read_permission
+      anonymous_user
+      system_root_token_api_client
+      public_project_group
+      public_project_read_permission
+      empty_collection
+    end
     refresh_trashed
   end
 end
