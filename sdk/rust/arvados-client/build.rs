18b9da6fd752c372aec5e7c9403f7eed98f6ed30
andy-thomason <andy@andythomason.com>
2020-10-01 08:18:10 +0100

    Parameterise build.rs

--- sdk/rust/arvados-client/build.rs
+++ sdk/rust/arvados-client/build.rs
@@ -1,13 +1,20 @@
 
 
+const INPUT_FILENAME : &str = "arvados-api.json";
+const OUTPUT_FILENAME : &str = "arvados-api.rs";
 
+/// Call the arvados-api-generator crate to generate arvados-api.rs
+/// This will be included in lib.rs.
+/// OUT_DIR is target/build/arvados_client_<hash>
 fn main() {
     use std::path::Path;
     let out_dir = std::env::var_os("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("arvados-api.rs");
-    let src_path = Path::new("arvados-api.json");
+
+    let dest_path = Path::new(&out_dir).join(OUTPUT_FILENAME);
+    let src_path = Path::new(INPUT_FILENAME);
+
     let src_file = std::fs::File::open(src_path).unwrap();
     let dest_file = std::fs::File::create(dest_path).unwrap();
+
     arvados_api_generator::convert(src_file, dest_file).unwrap();
 }
-
