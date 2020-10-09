1021d0b3c6bef69d30871093ce535987f2e6fdfa
Javier Bértoli <jbertoli@curii.com>
2020-10-09 13:00:51 -0300

    fix(salt): temporal fix for database setup issue
    plus some linting
    
    refs #16471 and #16992
    Arvados-DCO-1.1-Signed-off-by: Javier Bértoli <jbertoli@curii.com>

--- tools/salt-install/bin/provision.sh
+++ tools/salt-install/bin/provision.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 # Copyright (C) The Arvados Authors. All rights reserved.
 #
-# SPDX-License-Identifier: AGPL-3.0
+# SPDX-License-Identifier: CC-BY-SA-3.0
 
 # If you want to test arvados in a single host, you can run this script, which
 # will install it using salt masterless
@@ -26,7 +27,7 @@ F_DIR="/srv/formulas"
 ##pillars
 P_DIR="/srv/pillars"
 # In vagrant, we can use the shared dir
-P_DIR="/vagrant/salt_pillars"
+# P_DIR="/vagrant/salt_pillars"
 
 sudo apt-get update
 sudo apt-get install -y curl git
@@ -62,6 +63,7 @@ cat > ${S_DIR}/top.sls << EOFTSLS
 base:
   '*':
     - example_add_snakeoil_certs
+    - locale
     - nginx.passenger
     - postgres
     - docker
@@ -72,7 +74,8 @@ cat > ${P_DIR}/top.sls << EOFPSLS
 base:
   '*':
     - arvados
-    - nginx_api_configuration	
+    - locale
+    - nginx_api_configuration
     - nginx_controller_configuration
     - nginx_keepproxy_configuration
     - nginx_keepweb_configuration
@@ -86,7 +89,7 @@ EOFPSLS
 
 # Get the formula and dependencies
 cd ${F_DIR} || exit 1
-for f in postgres arvados nginx docker; do
+for f in postgres arvados nginx docker locale; do
   git clone https://github.com/netmanagers/${f}-formula.git
 done
 
@@ -96,25 +99,31 @@ if [ "x${BRANCH}" != "x" ]; then
   cd -
 fi
 
-sed "s/example.net/${DOMAIN}/g; s/name: fixme/name: ${CLUSTER}/g" \
+sed "s/example.net/${DOMAIN}/g; s/fixme/${CLUSTER}/g" \
   ${F_DIR}/arvados-formula/test/salt/pillar/arvados_dev.sls > ${P_DIR}/arvados.sls
 
-# Replace domain name in the example pillars
+# Replace cluster and domain name in the example pillars
 for f in ${F_DIR}/arvados-formula/test/salt/pillar/examples/*; do
-  sed "s/example.net/${DOMAIN}/g" ${f} > ${P_DIR}/$(basename ${f})
+  sed "s/example.net/${DOMAIN}/g; s/fixme/${CLUSTER}/g" \
+  ${f} > ${P_DIR}/$(basename ${f})
 done
 
 # # Copy arvados' pillar.example file to the pillars dir, so it's used
 # sed "s/example.net/${DOMAIN}/g" ${F_DIR}/arvados-formula/pillar.example > ${P_DIR}/arvados.sls
-# 
+#
 # # Replace domain name in the example pillars
 # for f in ${F_DIR}/arvados-formula/test/salt/pillar/examples/*; do
 #   sed "s/example.net/${DOMAIN}/g" ${f} > ${P_DIR}/$(basename ${f})
 # done
-# 
+#
 # Let's write a /etc/hosts file that points all the hosts to localhost
 
-echo "127.0.0.2 api keep keep0 collections download ws workbench workbench2 api.${CLUSTER}.${DOMAIN} keep.${CLUSTER}.${DOMAIN} keep0.${CLUSTER}.${DOMAIN} collections.${CLUSTER}.${DOMAIN} download.${CLUSTER}.${DOMAIN} ws.${CLUSTER}.${DOMAIN} workbench.${CLUSTER}.${DOMAIN} workbench2.${CLUSTER}.${DOMAIN}" >> /etc/hosts
+echo "127.0.0.2 api keep keep0 collections download ws workbench workbench2 ${CLUSTER}.${DOMAIN} api.${CLUSTER}.${DOMAIN} keep.${CLUSTER}.${DOMAIN} keep0.${CLUSTER}.${DOMAIN} collections.${CLUSTER}.${DOMAIN} download.${CLUSTER}.${DOMAIN} ws.${CLUSTER}.${DOMAIN} workbench.${CLUSTER}.${DOMAIN} workbench2.${CLUSTER}.${DOMAIN}" >> /etc/hosts
+
+# FIXME! Test to see if arvados-api-server progresses without issues
+cat > /root/.psqlrc << EOF
+\pset pager off
+EOF
 
 # Now run the install
 salt-call --local state.apply -l debug
