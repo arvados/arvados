a5ea2cce9d99fbfa52dc34d4ba18dd5338d724a0
Daniel Kutyła <daniel.kutyla@contractors.roche.com>
2020-11-12 18:11:38 +0100

    16005: Removed unused functions
    Arvados-DCO-1.1-Signed-off-by: Daniel Kutyła <daniel.kutyla@contractors.roche.com>

--- src/common/redirect-to.ts
+++ src/common/redirect-to.ts
@@ -5,12 +5,10 @@
 import { Config } from './config';
 
 const REDIRECT_TO_KEY = 'redirectTo';
-export const REDIRECT_TO_APPLY_TO_PATH = 'redirectToApplyToPath';
 
 export const storeRedirects = () => {
     let redirectUrl;
     const { location: { href }, localStorage } = window;
-    const applyToPath = href.indexOf(REDIRECT_TO_APPLY_TO_PATH) > -1;
 
     if (href.indexOf(REDIRECT_TO_KEY) > -1) {
         redirectUrl = href.split(`${REDIRECT_TO_KEY}=`)[1];
@@ -18,10 +16,6 @@ export const storeRedirects = () => {
 
     if (localStorage && redirectUrl) {
         localStorage.setItem(REDIRECT_TO_KEY, redirectUrl);
-
-        if (applyToPath) {
-            localStorage.setItem(REDIRECT_TO_APPLY_TO_PATH, 'true');
-        }
     }
 };
 
@@ -32,18 +26,10 @@ export const handleRedirects = (token: string, config: Config) => {
     if (localStorage && localStorage.getItem(REDIRECT_TO_KEY)) {
         const redirectUrl = localStorage.getItem(REDIRECT_TO_KEY);
         localStorage.removeItem(REDIRECT_TO_KEY);
-        const applyToPath = localStorage.getItem(REDIRECT_TO_APPLY_TO_PATH);
 
         if (redirectUrl) {
-            if (applyToPath === 'true') {
-                localStorage.removeItem(REDIRECT_TO_APPLY_TO_PATH);
-                setTimeout(() => {
-                    window.location.pathname = redirectUrl;
-                }, 0);
-            } else {
-                const sep = redirectUrl.indexOf("?") > -1 ? "&" : "?";
-                window.location.href = `${keepWebServiceUrl}${redirectUrl}${sep}api_token=${token}`;
-            }
+            const sep = redirectUrl.indexOf("?") > -1 ? "&" : "?";
+            window.location.href = `${keepWebServiceUrl}${redirectUrl}${sep}api_token=${token}`;
         }
     }
 };
