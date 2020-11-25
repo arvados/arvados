152c62fd0cde25378ee0d2d10c86946dcec8c138
Peter Amstutz <peter.amstutz@curii.com>
2020-11-25 15:42:08 -0500

    17072: Bump cwltool version for unbound 'result' fix
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- sdk/cwl/setup.py
+++ sdk/cwl/setup.py
@@ -39,7 +39,7 @@ setup(name='arvados-cwl-runner',
       # file to determine what version of cwltool and schema-salad to
       # build.
       install_requires=[
-          'cwltool==3.0.20200807132242',
+          'cwltool==3.0.20201121085451',
           'schema-salad==7.0.20200612160654',
           'arvados-python-client{}'.format(pysdk_dep),
           'setuptools',
