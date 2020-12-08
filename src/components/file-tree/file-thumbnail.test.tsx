2d401fdbf50b0e980548ee8098116b595a475bef
Daniel Kutyła <daniel.kutyla@contractors.roche.com>
2020-12-08 22:15:47 +0100

    16812: Tests improvements
    Arvados-DCO-1.1-Signed-off-by: Daniel Kutyła <daniel.kutyla@contractors.roche.com>

--- src/components/file-tree/file-thumbnail.test.tsx
+++ src/components/file-tree/file-thumbnail.test.tsx
@@ -21,13 +21,13 @@ describe("<FileThumbnail />", () => {
         file = {
             name: 'test-image',
             type: CollectionFileType.FILE,
-            url: 'http://test.com/c=test-hash/t=test-token/test-token2/test-token3/test-image.jpg',
+            url: 'http://example.com/c=zzzzz-4zz18-0123456789abcde/t=v2/zzzzz-gj3su-0123456789abcde/xxxxxxtokenxxxxx/test-image.jpg',
             size: 300
         };
     });
 
     it("renders file thumbnail with proper src", () => {
         const fileThumbnail = shallow(<FileThumbnail file={file} />);
-        expect(fileThumbnail.html()).toBe('<img class="Component-thumbnail-1" alt="test-image" src="http://test.com/c=test-hash/test-image.jpg?api_token=test-token/test-token2/test-token3"/>');
+        expect(fileThumbnail.html()).toBe('<img class="Component-thumbnail-1" alt="test-image" src="http://example.com/c=zzzzz-4zz18-0123456789abcde/test-image.jpg?api_token=v2/zzzzz-gj3su-0123456789abcde/xxxxxxtokenxxxxx"/>');
     });
 });
