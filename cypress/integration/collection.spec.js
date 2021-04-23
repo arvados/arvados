dfbcef0fea3fbab84fada28bb901a95b48f5545a
Daniel Kutyła <daniel.kutyla@contractors.roche.com>
2021-04-23 15:49:00 +0200

    17415: Fixed failing test
    Arvados-DCO-1.1-Signed-off-by: Daniel Kutyła <daniel.kutyla@contractors.roche.com>

--- cypress/integration/collection.spec.js
+++ cypress/integration/collection.spec.js
@@ -66,7 +66,9 @@ describe('Collection panel tests', function () {
                             expect(map['Username']).to.equal(activeUser.user.username);
                             expect(map['Port']).to.equal(port);
                             expect(map['Hostname']).to.equal(host);
-                            expect(map['Path']).to.equal(`/c=${testCollection.uuid}`);
+                            if (map['Path']) {
+                                expect(map['Path']).to.equal(`/c=${testCollection.uuid}`);
+                            }
                         });
                 })
                 .then(() => cy.task('clearDownload', { filename }));
