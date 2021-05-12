de3b8d1fa96ec9a5d28b05dade868abf8132904b
Lucas Di Pentima <lucas.dipentima@curii.com>
2021-05-12 17:54:06 -0300

    17637: Fixes supurious "not found" error when trashing project being viewed.
    Also, fixed error snackbar when an untrash operation fails.
    Also, removed superfluous "about to do X..." snackbar messages.
    
    Arvados-DCO-1.1-Signed-off-by: Lucas Di Pentima <lucas.dipentima@curii.com>

--- src/store/trash/trash-actions.ts
+++ src/store/trash/trash-actions.ts
@@ -8,41 +8,44 @@ import { ServiceRepository } from "~/services/services";
 import { snackbarActions, SnackbarKind } from "~/store/snackbar/snackbar-actions";
 import { trashPanelActions } from "~/store/trash-panel/trash-panel-action";
 import { activateSidePanelTreeItem, loadSidePanelTreeProjects } from "~/store/side-panel-tree/side-panel-tree-actions";
-import { projectPanelActions } from "~/store/project-panel/project-panel-action";
+import { getProjectPanelCurrentUuid, projectPanelActions } from "~/store/project-panel/project-panel-action";
 import { ResourceKind } from "~/models/resource";
-import { navigateToTrash } from '~/store/navigation/navigation-action';
+import { navigateTo, navigateToTrash } from '~/store/navigation/navigation-action';
 import { matchCollectionRoute } from '~/routes/routes';
 
 export const toggleProjectTrashed = (uuid: string, ownerUuid: string, isTrashed: boolean) =>
     async (dispatch: Dispatch, getState: () => RootState, services: ServiceRepository): Promise<any> => {
+        let errorMessage = '';
+        let successMessage = '';
         try {
             if (isTrashed) {
-                dispatch(snackbarActions.OPEN_SNACKBAR({ message: "Restoring from trash...", kind: SnackbarKind.INFO }));
+                errorMessage = "Could not restore project from trash";
+                successMessage = "Restored from trash";
                 await services.groupsService.untrash(uuid);
                 dispatch<any>(activateSidePanelTreeItem(uuid));
                 dispatch(trashPanelActions.REQUEST_ITEMS());
-                dispatch(snackbarActions.OPEN_SNACKBAR({
-                    message: "Restored from trash",
-                    hideDuration: 2000,
-                    kind: SnackbarKind.SUCCESS
-                }));
             } else {
-                dispatch(snackbarActions.OPEN_SNACKBAR({ message: "Moving to trash...", kind: SnackbarKind.INFO }));
+                errorMessage = "Could not move project to trash";
+                successMessage = "Added to trash";
                 await services.groupsService.trash(uuid);
-                dispatch(projectPanelActions.REQUEST_ITEMS());
+                if (getProjectPanelCurrentUuid(getState()) === uuid) {
+                    dispatch<any>(navigateTo(ownerUuid));
+                } else {
+                    dispatch(projectPanelActions.REQUEST_ITEMS());
+                }
                 dispatch<any>(loadSidePanelTreeProjects(ownerUuid));
-                dispatch(snackbarActions.OPEN_SNACKBAR({
-                    message: "Added to trash",
-                    hideDuration: 2000,
-                    kind: SnackbarKind.SUCCESS
-                }));
             }
         } catch (e) {
             dispatch(snackbarActions.OPEN_SNACKBAR({
-                message: "Could not move project to trash",
+                message: errorMessage,
                 kind: SnackbarKind.ERROR
             }));
         }
+        dispatch(snackbarActions.OPEN_SNACKBAR({
+            message: successMessage,
+            hideDuration: 2000,
+            kind: SnackbarKind.SUCCESS
+        }));
     };
 
 export const toggleCollectionTrashed = (uuid: string, isTrashed: boolean) =>
