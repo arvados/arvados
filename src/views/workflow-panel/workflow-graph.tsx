f616027fe6cc593b7aec49e38c8d9203a3a35aa4
Lucas Di Pentima <lucas.dipentima@curii.com>
2021-06-24 20:12:40 -0300

    17782: Adds missing copyright headers.
    Arvados-DCO-1.1-Signed-off-by: Lucas Di Pentima <lucas.dipentima@curii.com>

--- src/views/workflow-panel/workflow-graph.tsx
+++ src/views/workflow-panel/workflow-graph.tsx
@@ -1,3 +1,7 @@
+// Copyright (C) The Arvados Authors. All rights reserved.
+//
+// SPDX-License-Identifier: AGPL-3.0
+
 import * as React from 'react';
 import { WorkflowResource } from "~/models/workflow";
 import { WorkflowFactory } from "cwlts/models";
