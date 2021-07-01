a858a15370220c57fc1feb3c6f50655244193e33
Ward Vandewege <ward@curii.com>
2021-07-01 14:00:01 -0400

    The bleeding edge branch is now called 'main'.
    refs #17817
    
    Arvados-DCO-1.1-Signed-off-by: Ward Vandewege <ward@curii.com>

--- build/version-at-commit.sh
+++ build/version-at-commit.sh
@@ -14,12 +14,12 @@ devsuffix="~dev"
 #
 # 1. commit is directly tagged.  print that.
 #
-# 2. commit is on master or a development branch, the nearest tag is older
-#    than commit where this branch joins master.
+# 2. commit is on main or a development branch, the nearest tag is older
+#    than commit where this branch joins main.
 #    -> take greatest version tag in repo X.Y.Z and assign X.(Y+1).0
 #
 # 3. commit is on a release branch, the nearest tag is newer
-#    than the commit where this branch joins master.
+#    than the commit where this branch joins main.
 #    -> take nearest tag X.Y.Z and assign X.Y.(Z+1)
 
 tagged=$(git tag --points-at "$commit")
@@ -28,13 +28,13 @@ if [[ -n "$tagged" ]] ; then
     echo $tagged
 else
     # 1. get the nearest tag with 'git describe'
-    # 2. get the merge base between this commit and master
+    # 2. get the merge base between this commit and main
     # 3. if the tag is an ancestor of the merge base,
     #    (tag is older than merge base) increment minor version
     #    else, tag is newer than merge base, so increment point version
 
     nearest_tag=$(git describe --tags --abbrev=0 --match "$versionglob" "$commit")
-    merge_base=$(git merge-base origin/master "$commit")
+    merge_base=$(git merge-base origin/main "$commit")
 
     if git merge-base --is-ancestor "$nearest_tag" "$merge_base" ; then
         # x.(y+1).0.devTIMESTAMP, where x.y.z is the newest version that does not contain $commit
