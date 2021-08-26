9ed0a21813117caf5c6bae73c09a0d725564743b
Tom Clegg <tom@curii.com>
2021-08-26 00:04:26 -0400

    17217: Update tests, remove unused code.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@curii.com>

--- lib/controller/server_test.go
+++ lib/controller/server_test.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 
 	"git.arvados.org/arvados.git/sdk/go/arvados"
 	"git.arvados.org/arvados.git/sdk/go/arvadostest"
@@ -39,6 +40,8 @@ func newServerFromIntegrationTestEnv(c *check.C) *httpserver.Server {
 		PostgreSQL: integrationTestCluster().PostgreSQL,
 	}}
 	handler.Cluster.TLS.Insecure = true
+	handler.Cluster.Collections.BlobSigningKey = arvadostest.BlobSigningKey
+	handler.Cluster.Collections.BlobSigningTTL = arvados.Duration(time.Hour * 24 * 14)
 	arvadostest.SetServiceURL(&handler.Cluster.Services.RailsAPI, "https://"+os.Getenv("ARVADOS_TEST_API_HOST"))
 	arvadostest.SetServiceURL(&handler.Cluster.Services.Controller, "http://localhost:/")
 
