33765b48e350abcd844ea9db4c718f006a6a3d7b
Ward Vandewege <ward@jhvc.com>
2021-09-07 14:06:47 -0400

    18112: Ubuntu 18.04 comes with bundler version 1.16.1, which doesn't handle our current Gemfiles properly. Make our Ubuntu 18.04 distribution packages for arvados-api-server and arvados-workbench conflict with the ruby-bundler package to work around this issue. Our post-install scripts install the proper version of bundler.
    Arvados-DCO-1.1-Signed-off-by: Ward Vandewege <ward@curii.com>

--- apps/workbench/fpm-info.sh
+++ apps/workbench/fpm-info.sh
@@ -6,6 +6,10 @@ case "$TARGET" in
     centos*)
         fpm_depends+=(git bison make automake gcc gcc-c++ graphviz shared-mime-info)
         ;;
+    ubuntu1804)
+        fpm_depends+=(git g++ bison zlib1g-dev make graphviz shared-mime-info)
+        fpm_conflicts+=(ruby-bundler)
+        ;;
     debian* | ubuntu*)
         fpm_depends+=(git g++ bison zlib1g-dev make graphviz shared-mime-info)
         ;;
