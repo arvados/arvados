93131b473366ae5442103511d455d1ed5b73bcab
Peter Amstutz <peter.amstutz@curii.com>
2021-10-19 21:11:05 -0400

    18260: Update when we're going to actually run the workflow
    Update cwltool version for bugfixes.
    
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- sdk/cwl/setup.py
+++ sdk/cwl/setup.py
@@ -39,8 +39,8 @@ setup(name='arvados-cwl-runner',
       # file to determine what version of cwltool and schema-salad to
       # build.
       install_requires=[
-          'cwltool==3.1.20211019185001',
-          'schema-salad==8.2.20210918131710',
+          'cwltool==3.1.20211020155521',
+          'schema-salad==8.2.20211020114435',
           'arvados-python-client{}'.format(pysdk_dep),
           'setuptools',
           'ciso8601 >= 2.0.0',
