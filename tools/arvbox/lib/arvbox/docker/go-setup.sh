1f558e0d8688511f50a308521624aed6b6e136d2
Peter Amstutz <peter.amstutz@curii.com>
2022-03-11 11:50:43 -0500

    Add "go mod vendor" so switching branches in arvbox dev works better
    no issue #
    
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- tools/arvbox/lib/arvbox/docker/go-setup.sh
+++ tools/arvbox/lib/arvbox/docker/go-setup.sh
@@ -13,6 +13,7 @@ fi
 
 if [[ ! -f /usr/local/bin/arvados-server ]]; then
   $RUNSU flock /var/lib/gopath/gopath.lock go mod download
+  $RUNSU flock /var/lib/gopath/gopath.lock go mod vendor
   $RUNSU flock /var/lib/gopath/gopath.lock go install git.arvados.org/arvados.git/cmd/arvados-server
   $RUNSU flock /var/lib/gopath/gopath.lock install $GOPATH/bin/arvados-server /usr/local/bin
 fi
