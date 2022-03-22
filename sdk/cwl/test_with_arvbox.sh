603cad6467e3d186c19e0b7092efb7cd716007d0
Peter Amstutz <peter.amstutz@curii.com>
2022-03-22 14:37:43 -0400

    test_with_arvbox pass through -j3  refs #18899
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- sdk/cwl/test_with_arvbox.sh
+++ sdk/cwl/test_with_arvbox.sh
@@ -166,7 +166,7 @@ if [[ "$suite" = "integration" ]] ; then
    cd /usr/src/arvados/sdk/cwl/tests
    exec ./arvados-tests.sh $@
 elif [[ "$suite" = "conformance-v1.2" ]] ; then
-   exec cwltest --tool arvados-cwl-runner --test conformance_tests.yaml -N307 -- \$EXTRA
+   exec cwltest --tool arvados-cwl-runner --test conformance_tests.yaml -N307 $@ -- \$EXTRA
 else
    exec ./run_test.sh RUNNER=arvados-cwl-runner EXTRA="\$EXTRA" $@
 fi
