fe0c81c57f073e69bd334bd456046d88bd5f58df
Tom Clegg <tom@curii.com>
2022-03-24 10:20:45 -0400

    18700: Add /etc/hosts entry so ExternalURLs resolve in container.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@curii.com>

--- cmd/arvados-package/install.go
+++ cmd/arvados-package/install.go
@@ -93,6 +93,7 @@ rm /etc/apt/sources.list.d/arvados-local.list
 			"--env=domain="+opts.Live,
 			"--env=bootargs=",
 			"--publish=:4430-4450:4430-4450",
+			"--add-host="+opts.Live+":0.0.0.0",
 			"--volume=/var/lib/acme/live/"+opts.Live+":/var/lib/acme/live/"+opts.Live+":ro")
 	} else {
 		cmd.Args = append(cmd.Args,
