32b402114038bc6761c04c370afad786dbbb3125
Tom Clegg <tom@curii.com>
2022-07-22 14:54:32 -0400

    19281: Use embedded hello-world.tar as default diagnostics image.
    Ensures diagnostics work even if no docker images have been uploaded
    yet and docker.io wasn't installed at "init" time.
    
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@curii.com>

--- lib/install/init.go
+++ lib/install/init.go
@@ -402,29 +402,6 @@ func (initcmd *initCommand) RunCommand(prog string, args []string, stdin io.Read
 		fmt.Fprintln(stderr, "...looks good")
 	}
 
-	if out, err := exec.CommandContext(ctx, "docker", "version").CombinedOutput(); err == nil && strings.Contains(string(out), "\nServer:\n") {
-		fmt.Fprintln(stderr, "loading alpine docker image for diagnostics...")
-		cmd := exec.CommandContext(ctx, "docker", "pull", "alpine")
-		cmd.Stdout = stderr
-		cmd.Stderr = stderr
-		err = cmd.Run()
-		if err != nil {
-			err = fmt.Errorf("%v: %w", cmd.Args, err)
-			return 1
-		}
-		cmd = exec.CommandContext(ctx, "arv", "sudo", "keep", "docker", "alpine")
-		cmd.Stdout = stderr
-		cmd.Stderr = stderr
-		err = cmd.Run()
-		if err != nil {
-			err = fmt.Errorf("%v: %w", cmd.Args, err)
-			return 1
-		}
-		fmt.Fprintln(stderr, "...done")
-	} else {
-		fmt.Fprintln(stderr, "docker is not installed -- skipping step of downloading 'alpine' image")
-	}
-
 	fmt.Fprintf(stderr, `
 Setup complete. Next steps:
 * run 'arv sudo diagnostics'
