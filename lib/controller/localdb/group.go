d3efc21c6aa0f31988b2e7936f24e6f1941791e7
Tom Clegg <tom@curii.com>
2022-09-23 15:23:07 -0400

    19388: Trigger activity logs on group/project APIs, too.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@curii.com>

--- lib/controller/localdb/group.go
+++ lib/controller/localdb/group.go
@@ -13,6 +13,7 @@ import (
 // GroupCreate defers to railsProxy for everything except vocabulary
 // checking.
 func (conn *Conn) GroupCreate(ctx context.Context, opts arvados.CreateOptions) (arvados.Group, error) {
+	conn.logActivity(ctx)
 	err := conn.checkProperties(ctx, opts.Attrs["properties"])
 	if err != nil {
 		return arvados.Group{}, err
@@ -24,9 +25,15 @@ func (conn *Conn) GroupCreate(ctx context.Context, opts arvados.CreateOptions) (
 	return resp, nil
 }
 
+func (conn *Conn) GroupGet(ctx context.Context, opts arvados.GetOptions) (arvados.Group, error) {
+	conn.logActivity(ctx)
+	return conn.railsProxy.GroupGet(ctx, opts)
+}
+
 // GroupUpdate defers to railsProxy for everything except vocabulary
 // checking.
 func (conn *Conn) GroupUpdate(ctx context.Context, opts arvados.UpdateOptions) (arvados.Group, error) {
+	conn.logActivity(ctx)
 	err := conn.checkProperties(ctx, opts.Attrs["properties"])
 	if err != nil {
 		return arvados.Group{}, err
@@ -36,4 +43,14 @@ func (conn *Conn) GroupUpdate(ctx context.Context, opts arvados.UpdateOptions) (
 		return resp, err
 	}
 	return resp, nil
+}
+
+func (conn *Conn) GroupList(ctx context.Context, opts arvados.ListOptions) (arvados.GroupList, error) {
+	conn.logActivity(ctx)
+	return conn.railsProxy.GroupList(ctx, opts)
+}
+
+func (conn *Conn) GroupDelete(ctx context.Context, opts arvados.DeleteOptions) (arvados.Group, error) {
+	conn.logActivity(ctx)
+	return conn.railsProxy.GroupDelete(ctx, opts)
 }
