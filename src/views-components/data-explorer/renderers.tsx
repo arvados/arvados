3277922ef580144a55f34e80992ca40b9fb66146
Lisa Knox <lisaknox83@gmail.com>
2022-12-01 16:51:28 -0500

    removed testing lorem ipsum Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- src/views-components/data-explorer/renderers.tsx
+++ src/views-components/data-explorer/renderers.tsx
@@ -804,10 +804,6 @@ export const ResourceFileCount = connect(
 export const ResourceDescription = connect(
     (state: RootState, props: { uuid: string }) => {
         const resource = getResource<GroupContentsResource>(props.uuid)(state.resources);
-        //testing---------------
-        // const containerRequestDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        // if (resource && !resource.description && resource.kind === ResourceKind.PROCESS) resource.description = containerRequestDescription
-        //testing---------------
         return { description: resource ? resource.description : '' };
     })((props: { description: string }) => renderDescription(props.description));
 
