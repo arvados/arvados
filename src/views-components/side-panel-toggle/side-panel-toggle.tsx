234df49c86d0d1aece8aa9264578eac515b42eff
Peter Amstutz <peter.amstutz@curii.com>
2022-12-21 16:39:59 -0500

    Use SVG icon for collapse panel toggle
    refs #19434
    
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- src/views-components/side-panel-toggle/side-panel-toggle.tsx
+++ src/views-components/side-panel-toggle/side-panel-toggle.tsx
@@ -3,53 +3,53 @@
 // SPDX-License-Identifier: AGPL-3.0
 
 import React from 'react';
-import { Tooltip, IconButton  } from '@material-ui/core';
+import { Tooltip, IconButton } from '@material-ui/core';
 import { connect } from 'react-redux';
 import { toggleSidePanel } from "store/side-panel/side-panel-action";
 import { RootState } from 'store/store';
 
 type collapseButtonProps = {
-  isCollapsed: boolean;
-  toggleSidePanel: (collapsedState: boolean) => void
+    isCollapsed: boolean;
+    toggleSidePanel: (collapsedState: boolean) => void
 }
 
 export const COLLAPSE_ICON_SIZE = 35
 
-const SidePanelToggle = (props: collapseButtonProps) =>{ 
-  const collapseButtonIconStyles = {
-    root: {
-      width: `${COLLAPSE_ICON_SIZE}px`,
-      height: `${COLLAPSE_ICON_SIZE}px`,
-      marginTop: '0.4rem'
-    },
-    icon: {
-      width: '1.5rem',
-      height: '1.5rem',
-      marginTop: '0.5rem'
-    },
-  }
+const SidePanelToggle = (props: collapseButtonProps) => {
+    const collapseButtonIconStyles = {
+        root: {
+            width: `${COLLAPSE_ICON_SIZE}px`,
+            height: `${COLLAPSE_ICON_SIZE}px`,
+            marginTop: '0.4rem'
+        },
+        icon: {
+            height: '1.5rem',
+            width: '3rem',
+            opacity: '0.6',
+        },
+    }
 
-  return <Tooltip disableFocusListener title="Toggle Side Panel">
-            <IconButton style={collapseButtonIconStyles.root} onClick={()=>{props.toggleSidePanel(props.isCollapsed)}}>
-              <div>
-              {props.isCollapsed? 
-                <img style={{...collapseButtonIconStyles.icon, marginLeft: '0.25rem'}} src='/arrow-to-right.png'/>
-                :
-                <img style={{...collapseButtonIconStyles.icon, marginRight: '0.5rem'}} src='/arrow-to-left.png'/>}
-              </div>
-            </IconButton>
-          </Tooltip>
+    return <Tooltip disableFocusListener title="Toggle Side Panel">
+        <IconButton style={collapseButtonIconStyles.root} onClick={() => { props.toggleSidePanel(props.isCollapsed) }}>
+            <div>
+                {props.isCollapsed ?
+                    <img style={{ ...collapseButtonIconStyles.icon, transform: "rotate(180deg)" }} src='/collapseLHS-New.svg#svgView(preserveAspectRatio(none))' />
+                    :
+                    <img style={{ ...collapseButtonIconStyles.icon, }} src='/collapseLHS-New.svg#svgView(preserveAspectRatio(none))' />}
+            </div>
+        </IconButton>
+    </Tooltip>
 };
 
 const mapStateToProps = (state: RootState) => {
-  return {
-    isCollapsed: state.sidePanel.collapsedState
-  }
+    return {
+        isCollapsed: state.sidePanel.collapsedState
+    }
 }
 
-  const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch) => {
     return {
-        toggleSidePanel: (collapsedState)=>{
+        toggleSidePanel: (collapsedState) => {
             return dispatch(toggleSidePanel(collapsedState))
         }
     }
