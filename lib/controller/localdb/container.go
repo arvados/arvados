dcb3df6be978eef367e0d4fda95ccd71e0281749
Tom Clegg <tom@curii.com>
2023-03-09 16:38:24 -0500

    20183: Explain priority update strategy in comments.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@curii.com>

--- lib/controller/localdb/container.go
+++ lib/controller/localdb/container.go
@@ -56,6 +56,10 @@ func (conn *Conn) containerPriorityUpdate(ctx context.Context, log logrus.FieldL
 	if err != nil {
 		return fmt.Errorf("getdb: %w", err)
 	}
+	// Stage 1: Fix containers that have priority>0 but should
+	// have priority=0 because there are no active
+	// container_requests (unfinished, priority>0) associated with
+	// them.
 	res, err := db.ExecContext(ctx, `
 		UPDATE containers
 		SET priority=0
@@ -73,6 +77,16 @@ func (conn *Conn) containerPriorityUpdate(ctx context.Context, log logrus.FieldL
 	} else if rows > 0 {
 		log.Infof("found %d containers with priority>0 and no active requests, updated to priority=0", rows)
 	}
+
+	// Stage 2: Fix containers that have priority=0 but should
+	// have priority>0 because there are active container_requests
+	// (priority>0, unfinished, and not children of cancelled
+	// containers).
+	//
+	// Fixing here means calling out to RailsAPI to compute the
+	// correct priority for the contianer and (if needed)
+	// propagate that change to child containers.
+
 	// In this loop we look for a single container that needs
 	// fixing, call out to Rails to fix it, and repeat until we
 	// don't find any more.
