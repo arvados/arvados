1cabb7a57d7810ce0b0012351eb97ee2fb222082
Lucas Di Pentima <lucas.dipentima@curii.com>
2023-05-11 11:15:11 -0300

    20482: Pins terraform and AWS provider versions.
    Arvados-DCO-1.1-Signed-off-by: Lucas Di Pentima <lucas.dipentima@curii.com>

--- tools/salt-install/terraform/aws/services/main.tf
+++ tools/salt-install/terraform/aws/services/main.tf
@@ -3,9 +3,11 @@
 # SPDX-License-Identifier: CC-BY-SA-3.0
 
 terraform {
+  required_version = "~> 1.3.0"
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "~> 4.38.0"
     }
   }
 }
