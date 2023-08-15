59cc53cbed401e6b5ad750d992f64a8f5c83c6bf
Stephen Smith <stephen@curii.com>
2023-08-15 19:29:56 -0400

    20219: Try to fix tests on jenkins
    Arvados-DCO-1.1-Signed-off-by: Stephen Smith <stephen@curii.com>

--- cypress/integration/process.spec.js
+++ cypress/integration/process.spec.js
@@ -431,6 +431,11 @@ describe('Process tests', function() {
                 ['echo', 'hello world'],
                 false, 'Committed')
             .then(function(containerRequest) {
+                // Create empty log file before loading process page
+                cy.appendLog(adminUser.token, containerRequest.uuid, "stdout.txt", [
+                    ""
+                ])
+
                 cy.loginAs(activeUser);
                 cy.goToPath(`/processes/${containerRequest.uuid}`);
                 cy.get('[data-cy=process-details]').should('contain', crName);
@@ -442,7 +447,7 @@ describe('Process tests', function() {
                 cy.appendLog(adminUser.token, containerRequest.uuid, "stdout.txt", [
                     "2023-07-18T20:14:48.128642814Z hello world"
                 ]).then(() => {
-                    cy.get('[data-cy=process-logs]', {timeout: 10000})
+                    cy.get('[data-cy=process-logs]', {timeout: 7000})
                         .should('not.contain', 'No logs yet')
                         .and('contain', 'hello world');
                 });
@@ -451,7 +456,7 @@ describe('Process tests', function() {
                 cy.appendLog(adminUser.token, containerRequest.uuid, "stderr.txt", [
                     "2023-07-18T20:14:49.128642814Z hello new line"
                 ]).then(() => {
-                    cy.get('[data-cy=process-logs]', {timeout: 10000})
+                    cy.get('[data-cy=process-logs]', {timeout: 7000})
                         .should('not.contain', 'No logs yet')
                         .and('contain', 'hello new line');
                 });
