3c0213756adf7694a993db1e6dc87568ed89c74d
Tom Clegg <tom@curii.com>
2023-09-21 11:49:27 -0400

    20300: Fix missing imports in initializers.
    Autoload no longer works in initializers.
    
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@curii.com>

--- services/api/config/initializers/authorization.rb
+++ services/api/config/initializers/authorization.rb
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: AGPL-3.0
 
+require_relative "../../app/middlewares/arvados_api_token"
+
 Server::Application.configure do
   config.middleware.delete ActionDispatch::RemoteIp
   config.middleware.insert 0, ActionDispatch::RemoteIp
