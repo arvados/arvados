d719195a223a00b1f7706f4f734e0cd59618e54e
Brett Smith <brett.smith@curii.com>
2023-11-07 22:43:42 -0500

    21021: Send logout requests to the issuing cluster in a federation
    User agents may not follow the redirect sent by the previous
    implementation, meaning the logout process never really completes: the
    token gets deleted locally, but then can be revived when it gets
    re-checked against the issuing cluster. Address this by having the local
    controller make a logout request to the issuing cluster directly.
    
    Arvados-DCO-1.1-Signed-off-by: Brett Smith <brett.smith@curii.com>

--- lib/controller/federation/login_test.go
+++ lib/controller/federation/login_test.go
@@ -8,10 +8,8 @@ import (
 	"context"
 	"net/url"
 
-	"git.arvados.org/arvados.git/lib/ctrlctx"
 	"git.arvados.org/arvados.git/sdk/go/arvados"
 	"git.arvados.org/arvados.git/sdk/go/arvadostest"
-	"git.arvados.org/arvados.git/sdk/go/auth"
 	check "gopkg.in/check.v1"
 )
 
@@ -38,42 +36,5 @@ func (s *LoginSuite) TestDeferToLoginCluster(c *check.C) {
 		c.Check(target.Query().Get("remote"), check.Equals, remote)
 		_, remotePresent := target.Query()["remote"]
 		c.Check(remotePresent, check.Equals, remote != "")
-	}
-}
-
-func (s *LoginSuite) TestLogout(c *check.C) {
-	otherOrigin := arvados.URL{Scheme: "https", Host: "app.example.com", Path: "/"}
-	otherURL := "https://app.example.com/foo"
-	s.cluster.Services.Workbench1.ExternalURL = arvados.URL{Scheme: "https", Host: "workbench1.example.com"}
-	s.cluster.Services.Workbench2.ExternalURL = arvados.URL{Scheme: "https", Host: "workbench2.example.com"}
-	s.cluster.Login.TrustedClients = map[arvados.URL]struct{}{otherOrigin: {}}
-	s.addHTTPRemote(c, "zhome", &arvadostest.APIStub{})
-	s.cluster.Login.LoginCluster = "zhome"
-	// s.fed is already set by SetUpTest, but we need to
-	// reinitialize with the above config changes.
-	s.fed = New(s.ctx, s.cluster, nil, (&ctrlctx.DBConnector{PostgreSQL: s.cluster.PostgreSQL}).GetDB)
-
-	for _, trial := range []struct {
-		token    string
-		returnTo string
-		target   string
-	}{
-		{token: "", returnTo: "", target: s.cluster.Services.Workbench2.ExternalURL.String()},
-		{token: "", returnTo: otherURL, target: otherURL},
-		{token: "zzzzzzzzzzzzzzzzzzzzz", returnTo: otherURL, target: otherURL},
-		{token: "v2/zzzzz-aaaaa-aaaaaaaaaaaaaaa/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", returnTo: otherURL, target: otherURL},
-		{token: "v2/zhome-aaaaa-aaaaaaaaaaaaaaa/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", returnTo: otherURL, target: "http://" + s.cluster.RemoteClusters["zhome"].Host + "/logout?" + url.Values{"return_to": {otherURL}}.Encode()},
-	} {
-		c.Logf("trial %#v", trial)
-		ctx := s.ctx
-		if trial.token != "" {
-			ctx = auth.NewContext(ctx, &auth.Credentials{Tokens: []string{trial.token}})
-		}
-		resp, err := s.fed.Logout(ctx, arvados.LogoutOptions{ReturnTo: trial.returnTo})
-		c.Assert(err, check.IsNil)
-		c.Logf("  RedirectLocation %q", resp.RedirectLocation)
-		target, err := url.Parse(resp.RedirectLocation)
-		c.Check(err, check.IsNil)
-		c.Check(target.String(), check.Equals, trial.target)
 	}
 }
