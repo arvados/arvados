7fd0ba13716b814331fb08e6843a720a47f86ce9
Lisa Knox <lisaknox83@gmail.com>
2023-12-18 09:55:59 -0500

    21128: added freeze button disabling Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- src/store/projects/project-lock-actions.ts
+++ src/store/projects/project-lock-actions.ts
@@ -7,25 +7,31 @@ import { ServiceRepository } from "services/services";
 import { projectPanelActions } from "store/project-panel/project-panel-action-bind";
 import { loadResource } from "store/resources/resources-actions";
 import { RootState } from "store/store";
+import { MultiSelectMenuActionNames } from "views-components/multiselect-toolbar/ms-menu-actions";
+import { addDisabledButton, removeDisabledButton } from "store/multiselect/multiselect-actions";
 
 export const freezeProject = (uuid: string) => async (dispatch: Dispatch, getState: () => RootState, services: ServiceRepository) => {
+    dispatch<any>(addDisabledButton(MultiSelectMenuActionNames.FREEZE_PROJECT))
     const userUUID = getState().auth.user!.uuid;
-
+    
     const updatedProject = await services.projectService.update(uuid, {
         frozenByUuid: userUUID,
     });
-
+    
     dispatch(projectPanelActions.REQUEST_ITEMS());
     dispatch<any>(loadResource(uuid, false));
+    dispatch<any>(removeDisabledButton(MultiSelectMenuActionNames.FREEZE_PROJECT))
     return updatedProject;
 };
 
 export const unfreezeProject = (uuid: string) => async (dispatch: Dispatch, getState: () => RootState, services: ServiceRepository) => {
+    dispatch<any>(addDisabledButton(MultiSelectMenuActionNames.FREEZE_PROJECT))
     const updatedProject = await services.projectService.update(uuid, {
         frozenByUuid: null,
     });
 
     dispatch(projectPanelActions.REQUEST_ITEMS());
     dispatch<any>(loadResource(uuid, false));
+    dispatch<any>(removeDisabledButton(MultiSelectMenuActionNames.FREEZE_PROJECT))
     return updatedProject;
 };
