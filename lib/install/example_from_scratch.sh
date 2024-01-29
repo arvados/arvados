121107e5b11edf4eb9a5a571fa99d7aea9a731eb
Brett Smith <brett.smith@curii.com>
2024-01-29 11:57:22 -0500

    21361: Modernize test script
    * Update for Debian 11.
    * Assume #16053 has been merged, as it has been for a while.
    
    Arvados-DCO-1.1-Signed-off-by: Brett Smith <brett.smith@curii.com>

--- lib/install/example_from_scratch.sh
+++ lib/install/example_from_scratch.sh
@@ -6,16 +6,14 @@
 
 set -e -o pipefail
 
-# Starting with a base debian buster system, like "docker run -it
-# debian:10"...
+# Starting with a base debian bullseye system, like "docker run -it
+# debian:11"...
 
 apt update
 apt upgrade
 apt install --no-install-recommends build-essential ca-certificates git golang
 git clone https://git.arvados.org/arvados.git
-cd arvados
-[[ -e lib/install ]] || git checkout origin/16053-install-deps
-cd cmd/arvados-server
+cd arvados/cmd/arvados-server
 go run ./cmd/arvados-server install -type test
-pg_isready || pg_ctlcluster 11 main start # only needed if there's no init process (as in docker)
+pg_isready || pg_ctlcluster 13 main start # only needed if there's no init process (as in docker)
 build/run-tests.sh
