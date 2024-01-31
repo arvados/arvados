17647c7f12eae3557dfefbe8432c785360630d3d
Lisa Knox <lisaknox83@gmail.com>
2024-01-31 15:00:58 -0500

    21427: changed spec to allow for delay in toolbar transition Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/cypress/integration/process.spec.js
+++ services/workbench2/cypress/integration/process.spec.js
@@ -123,6 +123,7 @@ describe("Process tests", function () {
                 cy.get("[data-cy=side-panel-tree]").contains("Home Projects").click();
                 cy.waitForDom()
                 cy.get('[data-cy=data-table-row]').contains(containerRequest.name).should('exist').parent().parent().parent().parent().click()
+                cy.waitForDom()
                 cy.get('[data-cy=multiselect-button]').should('have.length', msButtonTooltips.length)
                 for (let i = 0; i < msButtonTooltips.length; i++) {
                     cy.get('[data-cy=multiselect-button]').eq(i).trigger('mouseover');
