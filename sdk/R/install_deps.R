561561ecadfa2fc2ed36aaf09bb98be382051e7b
Brett Smith <brett.smith@curii.com>
2024-02-15 17:07:04 -0500

    21361: Remove dependency pin for Debian 10
    Arvados-DCO-1.1-Signed-off-by: Brett Smith <brett.smith@curii.com>

--- sdk/R/install_deps.R
+++ sdk/R/install_deps.R
@@ -16,10 +16,7 @@ if (!requireNamespace("markdown")) {
   install.packages("markdown")
 }
 if (!requireNamespace("XML")) {
-  # XML 3.99-0.4 depends on R >= 4.0.0, but we run tests on debian
-  # stable (10) with R 3.5.2 so we install an older version from
-  # source.
-  install.packages("https://cran.r-project.org/src/contrib/Archive/XML/XML_3.99-0.3.tar.gz", repos=NULL, type="source")
+  install.packages("XML")
 }
 
 devtools::install_dev_deps()
