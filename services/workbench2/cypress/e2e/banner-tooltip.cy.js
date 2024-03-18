ffe0c69733abbf34adca1c6d1c9dd15cb3904ccb
Lisa Knox <lisaknox83@gmail.com>
2024-03-18 14:13:45 -0400

    21600: removed tests to test spec itself Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/cypress/e2e/banner-tooltip.cy.js
+++ services/workbench2/cypress/e2e/banner-tooltip.cy.js
@@ -68,33 +68,33 @@ describe('Banner / tooltip tests', function () {
     });
 
     it('should re-show the banner', () => {
-        cy.loginAs(adminUser);
+        // cy.loginAs(adminUser);
 
-        cy.get('[data-cy=confirmation-dialog-ok-btn]').click();
+        // cy.get('[data-cy=confirmation-dialog-ok-btn]').click();
 
-        cy.get('[title=Notifications]').click({ force: true });
-        cy.get('[data-cy=restore-banner-li]').click({ force: true });
+        // cy.get('[title=Notifications]').click({ force: true });
+        // cy.get('[data-cy=restore-banner-li]').click({ force: true });
 
-        cy.get('[data-cy=confirmation-dialog-ok-btn]').should('be.visible');
+        // cy.get('[data-cy=confirmation-dialog-ok-btn]').should('be.visible');
     });
 
 
     it('should show tooltips and remove tooltips as localStorage key is present', () => {
-        cy.loginAs(adminUser);
+        // cy.loginAs(adminUser);
 
-        cy.get('[data-cy=side-panel-tree]').then(($el) => {
-            const el = $el.get(0) //native DOM element
-            expect(el._tippy).to.not.be.undefined;
-        });
+        // cy.get('[data-cy=side-panel-tree]').then(($el) => {
+        //     const el = $el.get(0) //native DOM element
+        //     expect(el._tippy).to.not.be.undefined;
+        // });
 
-        cy.get('[data-cy=confirmation-dialog-ok-btn]').click();
+        // cy.get('[data-cy=confirmation-dialog-ok-btn]').click();
 
-        cy.get('[title=Notifications]').click();
-        cy.get('[data-cy=disable-tooltip-toggle]').click({ force: true });
+        // cy.get('[title=Notifications]').click();
+        // cy.get('[data-cy=disable-tooltip-toggle]').click({ force: true });
 
-        cy.get('[data-cy=side-panel-tree]').then(($el) => {
-            const el = $el.get(0) //native DOM element
-            expect(el._tippy).to.be.undefined;
-        });
+        // cy.get('[data-cy=side-panel-tree]').then(($el) => {
+        //     const el = $el.get(0) //native DOM element
+        //     expect(el._tippy).to.be.undefined;
+        // });
     });
 });
