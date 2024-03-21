1a7b50a8cbb56342845df954cbb07c16c2e5722c
Tom Clegg <tom@curii.com>
2024-03-21 15:11:41 -0400

    Fix open-ended bundler version in ruby install recipe.
    refs #21463
    
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@curii.com>

--- lib/install/deps.go
+++ lib/install/deps.go
@@ -351,7 +351,7 @@ make install
 if [[ "$rubyversion" > "3" ]]; then
   /var/lib/arvados/bin/gem update --no-document --system 3.4.21
 fi
-/var/lib/arvados/bin/gem install bundler --no-document
+/var/lib/arvados/bin/gem install bundler:`+inst.BundlerVersion+` --no-document
 `, stdout, stderr)
 		if err != nil {
 			return 1
