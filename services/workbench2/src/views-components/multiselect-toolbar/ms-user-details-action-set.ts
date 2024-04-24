9e82519ebc14c20423cb3eb712ef7581f609dc1f
Lisa Knox <lisaknox83@gmail.com>
2024-04-24 14:07:03 -0400

    21224: moved openuserattributes to resolve circular dep Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/src/views-components/multiselect-toolbar/ms-user-details-action-set.ts
+++ services/workbench2/src/views-components/multiselect-toolbar/ms-user-details-action-set.ts
@@ -4,10 +4,24 @@
 
 import { AdvancedIcon, AttributesIcon, UserPanelIcon } from 'components/icon/icon';
 import { openAdvancedTabDialog } from 'store/advanced-tab/advanced-tab';
-import { openUserAttributes } from 'store/users/users-actions';
 import { navigateToUserProfile } from 'store/navigation/navigation-action';
 import { ContextMenuActionNames } from "views-components/context-menu/context-menu-action-set";
 import { MultiSelectMenuActionSet } from './ms-menu-actions';
+import { Dispatch } from 'redux';
+import { RootState } from 'store/store';
+import { ServiceRepository } from 'services/services';
+import { dialogActions } from 'store/dialog/dialog-actions';
+import { UserResource } from 'models/user';
+import { getResource } from 'store/resources/resources';
+
+const USER_ATTRIBUTES_DIALOG = 'userAttributesDialog';
+
+const msOpenUserAttributes = (uuid: string) =>
+    (dispatch: Dispatch, getState: () => RootState, services: ServiceRepository) => {
+        const { resources } = getState();
+        const data = getResource<UserResource>(uuid)(resources);
+        dispatch(dialogActions.OPEN_DIALOG({ id: USER_ATTRIBUTES_DIALOG, data }));
+    };
 
 export const UserDetailsActionSet: MultiSelectMenuActionSet= [
     [
@@ -17,7 +31,7 @@ export const UserDetailsActionSet: MultiSelectMenuActionSet= [
             hasAlts: false,
             isForMulti: false,
             execute: (dispatch, resources) => {
-                dispatch<any>(openUserAttributes(resources[0].uuid));
+                dispatch<any>(msOpenUserAttributes(resources[0].uuid));
             },
         },
         {
