5dfd9a57f9d65ea91295e8502cf7f6e748fc4010
Tom Clegg <tom@curii.com>
2024-05-01 09:39:29 -0400

    21611: Remove log-to-logs-table code.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@curii.com>

--- lib/crunchrun/logging.go
+++ lib/crunchrun/logging.go
@@ -5,373 +5,82 @@
 package crunchrun
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io"
 	"log"
-	"regexp"
-	"strings"
-	"sync"
 	"time"
-
-	"git.arvados.org/arvados.git/sdk/go/arvadosclient"
 )
 
-// Timestamper is the signature for a function that takes a timestamp and
-// return a formated string value.
-type Timestamper func(t time.Time) string
+// rfc3339NanoFixed is a fixed-width version of time.RFC3339Nano.
+const rfc3339NanoFixed = "2006-01-02T15:04:05.000000000Z07:00"
 
-// Logging plumbing:
-//
-// ThrottledLogger.Logger -> ThrottledLogger.Write ->
-// ThrottledLogger.buf -> ThrottledLogger.flusher ->
-// ArvLogWriter.Write -> CollectionFileWriter.Write | Api.Create
-//
-// For stdout/stderr ReadWriteLines additionally runs as a goroutine to pull
-// data from the stdout/stderr Reader and send to the Logger.
-
-// ThrottledLogger accepts writes, prepends a timestamp to each line of the
-// write, and periodically flushes to a downstream writer.  It supports the
-// "Logger" and "WriteCloser" interfaces.
-type ThrottledLogger struct {
-	*log.Logger
-	buf *bytes.Buffer
-	sync.Mutex
-	writer   io.WriteCloser
-	flush    chan struct{}
-	stopped  chan struct{}
-	stopping chan struct{}
-	Timestamper
-	Immediate    *log.Logger
-	pendingFlush bool
+// prefixer wraps an io.Writer, inserting a string returned by
+// prefixFunc at the beginning of each line.
+type prefixer struct {
+	writer     io.Writer
+	prefixFunc func() string
+	unfinished bool // true if the most recent write ended with a non-newline char
 }
 
-// RFC3339NanoFixed is a fixed-width version of time.RFC3339Nano.
-const RFC3339NanoFixed = "2006-01-02T15:04:05.000000000Z07:00"
-
-// RFC3339Timestamp formats t as RFC3339NanoFixed.
-func RFC3339Timestamp(t time.Time) string {
-	return t.Format(RFC3339NanoFixed)
+// newTimestamper wraps an io.Writer, inserting an RFC3339NanoFixed
+// timestamp at the beginning of each line.
+func newTimestamper(w io.Writer) *prefixer {
+	return &prefixer{
+		writer:     w,
+		prefixFunc: func() string { return time.Now().UTC().Format(rfc3339NanoFixed + " ") },
+	}
 }
 
-// Write prepends a timestamp to each line of the input data and
-// appends to the internal buffer. Each line is also logged to
-// tl.Immediate, if tl.Immediate is not nil.
-func (tl *ThrottledLogger) Write(p []byte) (n int, err error) {
-	tl.Mutex.Lock()
-	defer tl.Mutex.Unlock()
-
-	if tl.buf == nil {
-		tl.buf = &bytes.Buffer{}
+// newStringPrefixer wraps an io.Writer, inserting the given string at
+// the beginning of each line. The given string should include a
+// trailing space for readability.
+func newStringPrefixer(w io.Writer, s string) *prefixer {
+	return &prefixer{
+		writer:     w,
+		prefixFunc: func() string { return s },
 	}
+}
 
-	now := tl.Timestamper(time.Now().UTC())
-	sc := bufio.NewScanner(bytes.NewBuffer(p))
-	for err == nil && sc.Scan() {
-		out := fmt.Sprintf("%s %s\n", now, sc.Bytes())
-		if tl.Immediate != nil {
-			tl.Immediate.Print(out[:len(out)-1])
+func (tp *prefixer) Write(p []byte) (n int, err error) {
+	for len(p) > 0 && err == nil {
+		if !tp.unfinished {
+			_, err = io.WriteString(tp.writer, tp.prefixFunc())
+			if err != nil {
+				return
+			}
 		}
-		_, err = io.WriteString(tl.buf, out)
-	}
-	if err == nil {
-		err = sc.Err()
-		if err == nil {
-			n = len(p)
+		newline := bytes.IndexRune(p, '\n')
+		var nn int
+		if newline < 0 {
+			tp.unfinished = true
+			nn, err = tp.writer.Write(p)
+			p = nil
+		} else {
+			tp.unfinished = false
+			nn, err = tp.writer.Write(p[:newline+1])
+			p = p[nn:]
 		}
+		n += nn
 	}
-
-	if int64(tl.buf.Len()) >= crunchLogBytesPerEvent {
-		// Non-blocking send.  Try send a flush if it is ready to
-		// accept it.  Otherwise do nothing because a flush is already
-		// pending.
-		select {
-		case tl.flush <- struct{}{}:
-		default:
-		}
-	}
-
 	return
 }
 
-// Periodically check the current buffer; if not empty, send it on the
-// channel to the goWriter goroutine.
-func (tl *ThrottledLogger) flusher() {
-	ticker := time.NewTicker(time.Duration(crunchLogSecondsBetweenEvents))
-	defer ticker.Stop()
-	for stopping := false; !stopping; {
-		select {
-		case <-tl.stopping:
-			// flush tl.buf and exit the loop
-			stopping = true
-		case <-tl.flush:
-		case <-ticker.C:
-		}
+// logWriter adds log.Logger methods to an io.Writer.
+type logWriter struct {
+	io.Writer
+	*log.Logger
+}
 
-		var ready *bytes.Buffer
-
-		tl.Mutex.Lock()
-		ready, tl.buf = tl.buf, &bytes.Buffer{}
-		tl.Mutex.Unlock()
-
-		if ready != nil && ready.Len() > 0 {
-			tl.writer.Write(ready.Bytes())
-		}
+func newLogWriter(w io.Writer) *logWriter {
+	return &logWriter{
+		Writer: w,
+		Logger: log.New(w, "", 0),
 	}
-	close(tl.stopped)
 }
 
-// Close the flusher goroutine and wait for it to complete, then close the
-// underlying Writer.
-func (tl *ThrottledLogger) Close() error {
-	select {
-	case <-tl.stopping:
-		// already stopped
-	default:
-		close(tl.stopping)
-	}
-	<-tl.stopped
-	return tl.writer.Close()
-}
-
-const (
-	// MaxLogLine is the maximum length of stdout/stderr lines before they are split.
-	MaxLogLine = 1 << 12
-)
-
-// ReadWriteLines reads lines from a reader and writes to a Writer, with long
-// line splitting.
-func ReadWriteLines(in io.Reader, writer io.Writer, done chan<- bool) {
-	reader := bufio.NewReaderSize(in, MaxLogLine)
-	var prefix string
-	for {
-		line, isPrefix, err := reader.ReadLine()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			writer.Write([]byte(fmt.Sprintln("error reading container log:", err)))
-		}
-		var suffix string
-		if isPrefix {
-			suffix = "[...]\n"
-		}
-
-		if prefix == "" && suffix == "" {
-			writer.Write(line)
-		} else {
-			writer.Write([]byte(fmt.Sprint(prefix, string(line), suffix)))
-		}
-
-		// Set up prefix for following line
-		if isPrefix {
-			prefix = "[...]"
-		} else {
-			prefix = ""
-		}
-	}
-	done <- true
-}
-
-// NewThrottledLogger creates a new thottled logger that
-//   - prepends timestamps to each line, and
-//   - batches log messages and only calls the underlying Writer
-//     at most once per "crunchLogSecondsBetweenEvents" seconds.
-func NewThrottledLogger(writer io.WriteCloser) *ThrottledLogger {
-	tl := &ThrottledLogger{}
-	tl.flush = make(chan struct{}, 1)
-	tl.stopped = make(chan struct{})
-	tl.stopping = make(chan struct{})
-	tl.writer = writer
-	tl.Logger = log.New(tl, "", 0)
-	tl.Timestamper = RFC3339Timestamp
-	go tl.flusher()
-	return tl
-}
-
-// Log throttling rate limiting config parameters
-var crunchLimitLogBytesPerJob int64 = 67108864
-var crunchLogThrottleBytes int64 = 65536
-var crunchLogThrottlePeriod time.Duration = time.Second * 60
-var crunchLogThrottleLines int64 = 1024
-var crunchLogPartialLineThrottlePeriod time.Duration = time.Second * 5
-var crunchLogBytesPerEvent int64 = 4096
-var crunchLogSecondsBetweenEvents = time.Second
 var crunchLogUpdatePeriod = time.Hour / 2
 var crunchLogUpdateSize = int64(1 << 25)
-
-// ArvLogWriter is an io.WriteCloser that processes each write by
-// writing it through to another io.WriteCloser (typically a
-// CollectionFileWriter) and creating an Arvados log entry.
-type ArvLogWriter struct {
-	ArvClient     IArvadosClient
-	UUID          string
-	loggingStream string
-	writeCloser   io.WriteCloser
-
-	// for rate limiting
-	bytesLogged                  int64
-	logThrottleResetTime         time.Time
-	logThrottleLinesSoFar        int64
-	logThrottleBytesSoFar        int64
-	logThrottleBytesSkipped      int64
-	logThrottleIsOpen            bool
-	logThrottlePartialLineNextAt time.Time
-	logThrottleFirstPartialLine  bool
-	bufToFlush                   bytes.Buffer
-	bufFlushedAt                 time.Time
-	closing                      bool
-}
-
-func (arvlog *ArvLogWriter) Write(p []byte) (int, error) {
-	// Write to the next writer in the chain (a file in Keep)
-	var err1 error
-	if arvlog.writeCloser != nil {
-		_, err1 = arvlog.writeCloser.Write(p)
-	}
-
-	// write to API after checking rate limit
-	now := time.Now()
-
-	if now.After(arvlog.logThrottleResetTime) {
-		// It has been more than throttle_period seconds since the last
-		// checkpoint; so reset the throttle
-		if arvlog.logThrottleBytesSkipped > 0 {
-			arvlog.bufToFlush.WriteString(fmt.Sprintf("%s Skipped %d bytes of log\n", RFC3339Timestamp(now.UTC()), arvlog.logThrottleBytesSkipped))
-		}
-
-		arvlog.logThrottleResetTime = now.Add(crunchLogThrottlePeriod)
-		arvlog.logThrottleBytesSoFar = 0
-		arvlog.logThrottleLinesSoFar = 0
-		arvlog.logThrottleBytesSkipped = 0
-		arvlog.logThrottleIsOpen = true
-	}
-
-	lines := bytes.Split(p, []byte("\n"))
-
-	for _, line := range lines {
-		// Short circuit the counting code if we're just going to throw
-		// away the data anyway.
-		if !arvlog.logThrottleIsOpen {
-			arvlog.logThrottleBytesSkipped += int64(len(line))
-			continue
-		} else if len(line) == 0 {
-			continue
-		}
-
-		// check rateLimit
-		logOpen, msg := arvlog.rateLimit(line, now)
-		if logOpen {
-			arvlog.bufToFlush.WriteString(string(msg) + "\n")
-		}
-	}
-
-	if (int64(arvlog.bufToFlush.Len()) >= crunchLogBytesPerEvent ||
-		(now.Sub(arvlog.bufFlushedAt) >= crunchLogSecondsBetweenEvents) ||
-		arvlog.closing) && (arvlog.bufToFlush.Len() > 0) {
-		// write to API
-		lr := arvadosclient.Dict{"log": arvadosclient.Dict{
-			"object_uuid": arvlog.UUID,
-			"event_type":  arvlog.loggingStream,
-			"properties":  map[string]string{"text": arvlog.bufToFlush.String()}}}
-		err2 := arvlog.ArvClient.Create("logs", lr, nil)
-
-		arvlog.bufToFlush = bytes.Buffer{}
-		arvlog.bufFlushedAt = now
-
-		if err1 != nil || err2 != nil {
-			return 0, fmt.Errorf("%s ; %s", err1, err2)
-		}
-	}
-
-	return len(p), nil
-}
-
-// Close the underlying writer
-func (arvlog *ArvLogWriter) Close() (err error) {
-	arvlog.closing = true
-	arvlog.Write([]byte{})
-	if arvlog.writeCloser != nil {
-		err = arvlog.writeCloser.Close()
-		arvlog.writeCloser = nil
-	}
-	return err
-}
-
-var lineRegexp = regexp.MustCompile(`^\S+ (.*)`)
-
-// Test for hard cap on total output and for log throttling. Returns whether
-// the log line should go to output or not. Returns message if limit exceeded.
-func (arvlog *ArvLogWriter) rateLimit(line []byte, now time.Time) (bool, []byte) {
-	message := ""
-	lineSize := int64(len(line))
-
-	if arvlog.logThrottleIsOpen {
-		matches := lineRegexp.FindStringSubmatch(string(line))
-
-		if len(matches) == 2 && strings.HasPrefix(matches[1], "[...]") && strings.HasSuffix(matches[1], "[...]") {
-			// This is a partial line.
-
-			if arvlog.logThrottleFirstPartialLine {
-				// Partial should be suppressed.  First time this is happening for this line so provide a message instead.
-				arvlog.logThrottleFirstPartialLine = false
-				arvlog.logThrottlePartialLineNextAt = now.Add(crunchLogPartialLineThrottlePeriod)
-				arvlog.logThrottleBytesSkipped += lineSize
-				return true, []byte(fmt.Sprintf("%s Rate-limiting partial segments of long lines to one every %d seconds.",
-					RFC3339Timestamp(now.UTC()), crunchLogPartialLineThrottlePeriod/time.Second))
-			} else if now.After(arvlog.logThrottlePartialLineNextAt) {
-				// The throttle period has passed.  Update timestamp and let it through.
-				arvlog.logThrottlePartialLineNextAt = now.Add(crunchLogPartialLineThrottlePeriod)
-			} else {
-				// Suppress line.
-				arvlog.logThrottleBytesSkipped += lineSize
-				return false, line
-			}
-		} else {
-			// Not a partial line so reset.
-			arvlog.logThrottlePartialLineNextAt = time.Time{}
-			arvlog.logThrottleFirstPartialLine = true
-		}
-
-		arvlog.bytesLogged += lineSize
-		arvlog.logThrottleBytesSoFar += lineSize
-		arvlog.logThrottleLinesSoFar++
-
-		if arvlog.bytesLogged > crunchLimitLogBytesPerJob {
-			message = fmt.Sprintf("%s Exceeded log limit %d bytes (crunch_limit_log_bytes_per_job). Log will be truncated.",
-				RFC3339Timestamp(now.UTC()), crunchLimitLogBytesPerJob)
-			arvlog.logThrottleResetTime = now.Add(time.Duration(365 * 24 * time.Hour))
-			arvlog.logThrottleIsOpen = false
-
-		} else if arvlog.logThrottleBytesSoFar > crunchLogThrottleBytes {
-			remainingTime := arvlog.logThrottleResetTime.Sub(now)
-			message = fmt.Sprintf("%s Exceeded rate %d bytes per %d seconds (crunch_log_throttle_bytes). Logging will be silenced for the next %d seconds.",
-				RFC3339Timestamp(now.UTC()), crunchLogThrottleBytes, crunchLogThrottlePeriod/time.Second, remainingTime/time.Second)
-			arvlog.logThrottleIsOpen = false
-
-		} else if arvlog.logThrottleLinesSoFar > crunchLogThrottleLines {
-			remainingTime := arvlog.logThrottleResetTime.Sub(now)
-			message = fmt.Sprintf("%s Exceeded rate %d lines per %d seconds (crunch_log_throttle_lines), logging will be silenced for the next %d seconds.",
-				RFC3339Timestamp(now.UTC()), crunchLogThrottleLines, crunchLogThrottlePeriod/time.Second, remainingTime/time.Second)
-			arvlog.logThrottleIsOpen = false
-
-		}
-	}
-
-	if !arvlog.logThrottleIsOpen {
-		// Don't log anything if any limit has been exceeded. Just count lossage.
-		arvlog.logThrottleBytesSkipped += lineSize
-	}
-
-	if message != "" {
-		// Yes, write to logs, but use our "rate exceeded" message
-		// instead of the log message that exceeded the limit.
-		message += " A complete log is still being written to Keep, and will be available when the job finishes."
-		return true, []byte(message)
-	}
-	return arvlog.logThrottleIsOpen, line
-}
 
 // load the rate limit discovery config parameters
 func loadLogThrottleParams(clnt IArvadosClient) {
@@ -394,13 +103,6 @@ func loadLogThrottleParams(clnt IArvadosClient) {
 		}
 	}
 
-	loadInt64(&crunchLimitLogBytesPerJob, "crunchLimitLogBytesPerJob")
-	loadInt64(&crunchLogThrottleBytes, "crunchLogThrottleBytes")
-	loadDuration(&crunchLogThrottlePeriod, "crunchLogThrottlePeriod")
-	loadInt64(&crunchLogThrottleLines, "crunchLogThrottleLines")
-	loadDuration(&crunchLogPartialLineThrottlePeriod, "crunchLogPartialLineThrottlePeriod")
-	loadInt64(&crunchLogBytesPerEvent, "crunchLogBytesPerEvent")
-	loadDuration(&crunchLogSecondsBetweenEvents, "crunchLogSecondsBetweenEvents")
 	loadInt64(&crunchLogUpdateSize, "crunchLogUpdateSize")
 	loadDuration(&crunchLogUpdatePeriod, "crunchLogUpdatePeriod")
 
--- lib/crunchrun/logging_test.go
+++ lib/crunchrun/logging_test.go
@@ -13,26 +13,25 @@ import (
 	"time"
 
 	"git.arvados.org/arvados.git/sdk/go/arvados"
-	"git.arvados.org/arvados.git/sdk/go/arvadosclient"
 	. "gopkg.in/check.v1"
 	check "gopkg.in/check.v1"
 )
 
+// newTestTimestamper wraps an io.Writer, inserting a predictable
+// RFC3339NanoFixed timestamp at the beginning of each line.
+func newTestTimestamper(w io.Writer) *prefixer {
+	count := 0
+	return &prefixer{
+		writer: w,
+		prefixFunc: func() string {
+			count++
+			return fmt.Sprintf("2015-12-29T15:51:45.%09dZ ", count)
+		},
+	}
+}
+
 type LoggingTestSuite struct {
 	client *arvados.Client
-}
-
-type TestTimestamper struct {
-	count int
-}
-
-func (stamper *TestTimestamper) Timestamp(t time.Time) string {
-	stamper.count++
-	t, err := time.ParseInLocation(time.RFC3339Nano, fmt.Sprintf("2015-12-29T15:51:45.%09dZ", stamper.count), t.Location())
-	if err != nil {
-		panic(err)
-	}
-	return RFC3339Timestamp(t)
 }
 
 // Gocheck boilerplate
@@ -48,26 +47,20 @@ func (s *LoggingTestSuite) TestWriteLogs(c *C) {
 	api := &ArvTestClient{}
 	kc := &KeepTestClient{}
 	defer kc.Close()
-	cr, err := NewContainerRunner(s.client, api, kc, "zzzzz-zzzzzzzzzzzzzzz")
+	cr, err := NewContainerRunner(s.client, api, kc, "zzzzz-dz642-zzzzzzzzzzzzzzz")
 	c.Assert(err, IsNil)
-	cr.CrunchLog.Timestamper = (&TestTimestamper{}).Timestamp
+	f, err := cr.openLogFile("crunch-run")
+	c.Assert(err, IsNil)
+	cr.CrunchLog = newLogWriter(newTestTimestamper(f))
 
 	cr.CrunchLog.Print("Hello world!")
 	cr.CrunchLog.Print("Goodbye")
-	cr.CrunchLog.Close()
 
-	c.Check(api.Calls, Equals, 1)
+	c.Check(api.Calls, Equals, 0)
 
-	mt, err := cr.LogCollection.MarshalManifest(".")
-	c.Check(err, IsNil)
-	c.Check(mt, Equals, ". 74561df9ae65ee9f35d5661d42454264+83 0:83:crunch-run.txt\n")
-
-	logtext := "2015-12-29T15:51:45.000000001Z Hello world!\n" +
-		"2015-12-29T15:51:45.000000002Z Goodbye\n"
-
-	c.Check(api.Content[0]["log"].(arvadosclient.Dict)["event_type"], Equals, "crunch-run")
-	c.Check(api.Content[0]["log"].(arvadosclient.Dict)["properties"].(map[string]string)["text"], Equals, logtext)
-	s.checkWroteBlock(c, kc, "74561df9ae65ee9f35d5661d42454264+83", logtext)
+	logs := logFileContent(c, cr, "crunch-run.txt")
+	c.Check(logs, Matches, `....-..-..T..:..:..\..........Z Hello world!\n`+
+		`....-..-..T..:..:..\..........Z Goodbye\n`)
 }
 
 func (s *LoggingTestSuite) TestWriteLogsLarge(c *C) {
@@ -79,59 +72,34 @@ func (s *LoggingTestSuite) TestWriteLogsLarge(c *C) {
 	defer kc.Close()
 	cr, err := NewContainerRunner(s.client, api, kc, "zzzzz-zzzzzzzzzzzzzzz")
 	c.Assert(err, IsNil)
-	cr.CrunchLog.Timestamper = (&TestTimestamper{}).Timestamp
-	cr.CrunchLog.Immediate = nil
-
+	f, err := cr.openLogFile("crunch-run")
+	c.Assert(err, IsNil)
+	cr.CrunchLog = newLogWriter(newTestTimestamper(f))
 	for i := 0; i < 2000000; i++ {
 		cr.CrunchLog.Printf("Hello %d", i)
 	}
 	cr.CrunchLog.Print("Goodbye")
-	cr.CrunchLog.Close()
 
-	c.Check(api.Calls > 0, Equals, true)
-	c.Check(api.Calls < 2000000, Equals, true)
+	logs := logFileContent(c, cr, "crunch-run.txt")
+	c.Check(strings.Count(logs, "\n"), Equals, 2000001)
+	// Redact most of the logs except the start/end for the regexp
+	// match -- otherwise, when the regexp fails, gocheck spams
+	// the test logs with tens of megabytes of quoted strings.
+	c.Assert(len(logs) > 10000, Equals, true)
+	c.Check(logs[:500]+"\n...\n"+logs[len(logs)-500:], Matches, `(?ms)2015-12-29T15:51:45.000000001Z Hello 0
+2015-12-29T15:51:45.000000002Z Hello 1
+2015-12-29T15:51:45.000000003Z Hello 2
+2015-12-29T15:51:45.000000004Z Hello 3
+.*
+2015-12-29T15:51:45.001999998Z Hello 1999997
+2015-12-29T15:51:45.001999999Z Hello 1999998
+2015-12-29T15:51:45.002000000Z Hello 1999999
+2015-12-29T15:51:45.002000001Z Goodbye
+`)
 
 	mt, err := cr.LogCollection.MarshalManifest(".")
 	c.Check(err, IsNil)
 	c.Check(mt, Equals, ". 9c2c05d1fae6aaa8af85113ba725716d+67108864 80b821383a07266c2a66a4566835e26e+21780065 0:88888929:crunch-run.txt\n")
-}
-
-func (s *LoggingTestSuite) TestWriteMultipleLogs(c *C) {
-	api := &ArvTestClient{}
-	kc := &KeepTestClient{}
-	defer kc.Close()
-	cr, err := NewContainerRunner(s.client, api, kc, "zzzzz-zzzzzzzzzzzzzzz")
-	c.Assert(err, IsNil)
-	ts := &TestTimestamper{}
-	cr.CrunchLog.Timestamper = ts.Timestamp
-	w, err := cr.NewLogWriter("stdout")
-	c.Assert(err, IsNil)
-	stdout := NewThrottledLogger(w)
-	stdout.Timestamper = ts.Timestamp
-
-	cr.CrunchLog.Print("Hello world!")
-	stdout.Print("Doing stuff")
-	cr.CrunchLog.Print("Goodbye")
-	stdout.Print("Blurb")
-	cr.CrunchLog.Close()
-	stdout.Close()
-
-	logText := make(map[string]string)
-	for _, content := range api.Content {
-		log := content["log"].(arvadosclient.Dict)
-		logText[log["event_type"].(string)] += log["properties"].(map[string]string)["text"]
-	}
-
-	c.Check(logText["crunch-run"], Equals, `2015-12-29T15:51:45.000000001Z Hello world!
-2015-12-29T15:51:45.000000003Z Goodbye
-`)
-	c.Check(logText["stdout"], Equals, `2015-12-29T15:51:45.000000002Z Doing stuff
-2015-12-29T15:51:45.000000004Z Blurb
-`)
-
-	mt, err := cr.LogCollection.MarshalManifest(".")
-	c.Check(err, IsNil)
-	c.Check(mt, Equals, ". 48f9023dc683a850b1c9b482b14c4b97+163 0:83:crunch-run.txt 83:80:stdout.txt\n")
 }
 
 func (s *LoggingTestSuite) TestLogUpdate(c *C) {
@@ -149,14 +117,13 @@ func (s *LoggingTestSuite) TestLogUpdate(c *C) {
 		api := &ArvTestClient{}
 		kc := &KeepTestClient{}
 		defer kc.Close()
-		cr, err := NewContainerRunner(s.client, api, kc, "zzzzz-zzzzzzzzzzzzzzz")
+		cr, err := NewContainerRunner(s.client, api, kc, "zzzzz-dz642-zzzzzzzzzzzzzzz")
 		c.Assert(err, IsNil)
-		ts := &TestTimestamper{}
-		cr.CrunchLog.Timestamper = ts.Timestamp
-		w, err := cr.NewLogWriter("stdout")
+		f, err := cr.openLogFile("crunch-run")
 		c.Assert(err, IsNil)
-		stdout := NewThrottledLogger(w)
-		stdout.Timestamper = ts.Timestamp
+		cr.CrunchLog = newLogWriter(newTestTimestamper(f))
+		stdout, err := cr.openLogFile("stdout")
+		c.Assert(err, IsNil)
 
 		c.Check(cr.logUUID, Equals, "")
 		cr.CrunchLog.Printf("Hello %1000s", "space")
@@ -165,73 +132,16 @@ func (s *LoggingTestSuite) TestLogUpdate(c *C) {
 		}
 		c.Check(cr.logUUID, Not(Equals), "")
 		cr.CrunchLog.Print("Goodbye")
-		fmt.Fprint(stdout, "Goodbye\n")
-		cr.CrunchLog.Close()
-		stdout.Close()
-		w.Close()
+		fmt.Fprintln(stdout, "Goodbye")
+
+		c.Check(logFileContent(c, cr, "crunch-run.txt"), Matches, `....-..-..T..:..:............Z Hello  {995}space\n`+
+			`....-..-..T..:..:............Z Goodbye\n`)
+		c.Check(logFileContent(c, cr, "stdout.txt"), Matches, `Goodbye\n`)
 
 		mt, err := cr.LogCollection.MarshalManifest(".")
 		c.Check(err, IsNil)
-		// Block packing depends on whether there's an update
-		// between the two Goodbyes -- either way the first
-		// block will be 4dc76.
-		c.Check(mt, Matches, `. 4dc76e0a212bfa30c39d76d8c16da0c0\+1038 (afc503bc1b9a828b4bb543cb629e936c\+78|90699dc22545cd74a0664303f70bc05a\+39 276b49339fd5203d15a93ff3de11bfb9\+39) 0:1077:crunch-run.txt 1077:39:stdout.txt\n`)
+		c.Check(mt, Matches, `. 4dc76e0a212bfa30c39d76d8c16da0c0\+1038 5be52044a8c51e7b62dd62be07872968\+47 0:1077:crunch-run.txt 1077:8:stdout.txt\n`)
 	}
-}
-
-func (s *LoggingTestSuite) TestWriteLogsWithRateLimitThrottleBytes(c *C) {
-	s.testWriteLogsWithRateLimit(c, "crunchLogThrottleBytes", 50, 65536, "Exceeded rate 50 bytes per 60 seconds")
-}
-
-func (s *LoggingTestSuite) TestWriteLogsWithRateLimitThrottleLines(c *C) {
-	s.testWriteLogsWithRateLimit(c, "crunchLogThrottleLines", 1, 1024, "Exceeded rate 1 lines per 60 seconds")
-}
-
-func (s *LoggingTestSuite) TestWriteLogsWithRateLimitThrottleBytesPerEvent(c *C) {
-	s.testWriteLogsWithRateLimit(c, "crunchLimitLogBytesPerJob", 50, 67108864, "Exceeded log limit 50 bytes (crunch_limit_log_bytes_per_job)")
-}
-
-func (s *LoggingTestSuite) TestWriteLogsWithZeroBytesPerJob(c *C) {
-	s.testWriteLogsWithRateLimit(c, "crunchLimitLogBytesPerJob", 0, 67108864, "Exceeded log limit 0 bytes (crunch_limit_log_bytes_per_job)")
-}
-
-func (s *LoggingTestSuite) testWriteLogsWithRateLimit(c *C, throttleParam string, throttleValue int, throttleDefault int, expected string) {
-	discoveryMap[throttleParam] = float64(throttleValue)
-	defer func() {
-		discoveryMap[throttleParam] = float64(throttleDefault)
-	}()
-
-	api := &ArvTestClient{}
-	kc := &KeepTestClient{}
-	defer kc.Close()
-	cr, err := NewContainerRunner(s.client, api, kc, "zzzzz-zzzzzzzzzzzzzzz")
-	c.Assert(err, IsNil)
-	cr.CrunchLog.Timestamper = (&TestTimestamper{}).Timestamp
-
-	cr.CrunchLog.Print("Hello world!")
-	cr.CrunchLog.Print("Goodbye")
-	cr.CrunchLog.Close()
-
-	c.Check(api.Calls, Equals, 1)
-
-	mt, err := cr.LogCollection.MarshalManifest(".")
-	c.Check(err, IsNil)
-	c.Check(mt, Equals, ". 74561df9ae65ee9f35d5661d42454264+83 0:83:crunch-run.txt\n")
-
-	logtext := "2015-12-29T15:51:45.000000001Z Hello world!\n" +
-		"2015-12-29T15:51:45.000000002Z Goodbye\n"
-
-	c.Check(api.Content[0]["log"].(arvadosclient.Dict)["event_type"], Equals, "crunch-run")
-	stderrLog := api.Content[0]["log"].(arvadosclient.Dict)["properties"].(map[string]string)["text"]
-	c.Check(true, Equals, strings.Contains(stderrLog, expected))
-	s.checkWroteBlock(c, kc, "74561df9ae65ee9f35d5661d42454264+83", logtext)
-}
-
-func (s *LoggingTestSuite) checkWroteBlock(c *C, kc *KeepTestClient, locator, expect string) {
-	buf := make([]byte, len([]byte(expect))+1)
-	n, err := kc.ReadAt(locator, buf, 0)
-	c.Check(err, IsNil)
-	c.Check(string(buf[:n]), Equals, expect)
 }
 
 type filterSuite struct{}
