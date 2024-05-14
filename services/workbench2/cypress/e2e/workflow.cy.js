e92b3c613b2dfebb06eab7668c14f53f8d823415
Lisa Knox <lisaknox83@gmail.com>
2024-05-14 13:38:36 -0400

    21535: added timeouts to test spec Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/cypress/e2e/workflow.cy.js
+++ services/workbench2/cypress/e2e/workflow.cy.js
@@ -256,10 +256,8 @@ describe('Registered workflow panel tests', function() {
         cy.get('[data-cy=data-table-multiselect-popover]').click();
         cy.get('[data-cy=multiselect-popover-All]').click();
 
-        cy.waitForDom().get('[data-cy=multiselect-button]', {timeout: 10000})
-            .should('exist')
-            .should('have.length', '1')
-            .trigger('mouseover');
+        cy.waitForDom().get('[data-cy=multiselect-button]', {timeout: 10000}).should('be.visible')
+        cy.get('[data-cy=multiselect-button]', {timeout: 10000}).should('have.length', '1').trigger('mouseover');
         cy.get('body').contains('Delete Workflow', {timeout: 10000}).should('exist')
         cy.get('[data-cy=multiselect-button]').eq(0).click();
         
