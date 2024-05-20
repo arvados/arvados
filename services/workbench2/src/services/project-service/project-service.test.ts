eb277a8dcfd0c5c4c7a06ae2c2a45effb6dcaeee
Lisa Knox <lisaknox83@gmail.com>
2024-05-20 11:33:29 -0400

    21720: added type assertions for AxiosInstance post
    Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/src/services/project-service/project-service.test.ts
+++ services/workbench2/src/services/project-service/project-service.test.ts
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0
 
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 import { ProjectService } from "./project-service";
 import { FilterBuilder } from "services/api/filter-builder";
 import { ApiActions } from "services/api/api-actions";
@@ -15,7 +15,7 @@ describe("CommonResourceService", () => {
     };
 
     it(`#create has groupClass set to "project"`, async () => {
-        axiosInstance.post = jest.fn(() => Promise.resolve({ data: {} }));
+        axiosInstance.post = jest.fn(() => Promise.resolve({ data: {} })) as AxiosInstance['post'];
         const projectService = new ProjectService(axiosInstance, actions);
         const resource = await projectService.create({ name: "nameValue" });
         expect(axiosInstance.post).toHaveBeenCalledWith("/groups", {
@@ -27,7 +27,7 @@ describe("CommonResourceService", () => {
     });
 
     it("#list has groupClass filter set by default", async () => {
-        axiosInstance.get = jest.fn(() => Promise.resolve({ data: {} }));
+        axiosInstance.get = jest.fn(() => Promise.resolve({ data: {} })) as AxiosInstance['post'];
         const projectService = new ProjectService(axiosInstance, actions);
         const resource = await projectService.list();
         expect(axiosInstance.get).toHaveBeenCalledWith("/groups", {
