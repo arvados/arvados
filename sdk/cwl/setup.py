de6342dbb0832783f7279fa1cbc78e65e80f1db4
Peter Amstutz <peter.amstutz@curii.com>
2024-05-23 18:54:17 -0400

    Pin pydot version to fix version conflict with pyparsing
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- sdk/cwl/setup.py
+++ sdk/cwl/setup.py
@@ -50,6 +50,9 @@ setup(name='arvados-cwl-runner',
 
           # lxml 5.2 dropped support for Python 3.7
           'lxml<5.2; python_version<"3.8"',
+
+          # pydot 2.0 requires pyparsing>=3 which creates a conflict
+          'pydot<2',
       ],
       data_files=[
           ('share/doc/arvados-cwl-runner', ['LICENSE-2.0.txt', 'README.rst']),
