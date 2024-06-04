5823c9f23cbf23e86d653b6ffb6b84515864f5c9
Lisa Knox <lisaknox83@gmail.com>
2024-06-04 14:00:53 -0400

    21762: added window timeout to test spec
    Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/cypress/e2e/search.cy.js
+++ services/workbench2/cypress/e2e/search.cy.js
@@ -301,7 +301,7 @@ describe("Search tests", function () {
             cy.get("[data-cy=context-menu]").within(() => {
                 cy.contains("Copy link to clipboard").click();
                 cy.waitForDom();
-                cy.window().then(win =>
+                cy.window({ timeout: 15000 }).then(win =>
                     win.navigator.clipboard.readText().then(text => {
                         expect(text).to.equal(`https://wb2.xxxxx.fakecluster.tld/collections/${federatedColUuid}`);
                     })
