4a9e5c926179b3ab8ff7231904e2db93d42149e0
Lisa Knox <lisa.knox@curii.com>
2024-06-06 13:46:49 -0400

    21762: added trycatch to observer callback
    Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/src/views-components/side-panel/side-panel.tsx
+++ services/workbench2/src/views-components/side-panel/side-panel.tsx
@@ -60,14 +60,18 @@ export const SidePanel = withStyles(styles)(
             if (!splitPane) return;
 
             const observerCallback: ResizeObserverCallback = (entries: ResizeObserverEntry[]) => {
-                //prevents potential infinite resize triggers
-                window.requestAnimationFrame((): void | undefined => {
-                  if (!Array.isArray(entries) || !entries.length) {
-                    return;
-                  }
-                    const width = entries[0].contentRect.width
-                    props.setCurrentSideWidth(width)
-                });
+                try {
+                    //prevents potential infinite resize triggers
+                    window.requestAnimationFrame((): void | undefined => {
+                    if (!Array.isArray(entries) || !entries.length) {
+                        return;
+                    }
+                        const width = entries[0].contentRect.width
+                        props.setCurrentSideWidth(width)
+                    });
+                } catch (error) {
+                    console.error('Error in resize observer callback', error);
+                }
               };
 
             const observer = new ResizeObserver(observerCallback)
