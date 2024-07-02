bc3578056656d5bbe884d5586a95d775ebec109d
Peter Amstutz <peter.amstutz@curii.com>
2024-07-02 15:33:58 -0400

    21121: Add missing file and cluster-activity cwl
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- tools/cluster-activity/make_docker.sh
+++ tools/cluster-activity/make_docker.sh
@@ -19,7 +19,7 @@ VERSION=$(python3 arvados_version.py)
 rm -f docker/*.tar.gz
 cp ../../sdk/python/dist/arvados-python-client-${SDK_VERSION}.tar.gz \
    ../crunchstat-summary/dist/crunchstat_summary-${CRUNCHSTAT_VERSION}.tar.gz \
-   dist/arvados-user-activity-${VERSION}.tar.gz \
+   dist/arvados-cluster-activity-${VERSION}.tar.gz \
    docker/
 cd docker
 docker build -t arvados/cluster-activity:$VERSION .
