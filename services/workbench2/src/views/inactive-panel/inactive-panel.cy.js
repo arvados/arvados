631277c087e64c3db81b75fa0b3ae06ee6b6e403
Lisa Knox <lisa.knox@curii.com>
2024-07-03 10:02:44 -0400

    21720:
    * updated inactive-panel test
    
    Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/src/views/inactive-panel/inactive-panel.cy.js
+++ services/workbench2/src/views/inactive-panel/inactive-panel.cy.js
@@ -3,25 +3,12 @@
 // SPDX-License-Identifier: AGPL-3.0
 
 import React from 'react';
-import { mount, configure } from 'enzyme';
-import Adapter from "enzyme-adapter-react-16";
 import { CustomTheme } from 'common/custom-theme';
-import { InactivePanelStateProps, CssRules, InactivePanelRoot } from './inactive-panel';
-import { ThemeProvider, Theme, StyledEngineProvider } from '@mui/material';
-
-import { StyledComponentProps } from '@mui/styles';
-
-
-declare module '@mui/styles/defaultTheme' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends Theme {}
-}
-
-
-configure({ adapter: new Adapter() });
+import { InactivePanelRoot } from './inactive-panel';
+import { ThemeProvider, StyledEngineProvider } from '@mui/material';
 
 describe('InactivePanel', () => {
-    let props: InactivePanelStateProps & StyledComponentProps<CssRules>;
+    let props;
 
     beforeEach(() => {
         props = {
@@ -41,7 +28,7 @@ describe('InactivePanel', () => {
         const expectedLinkAccountText = 'If you would like to use this login to access another account click "Link Account"';
 
         // when
-        const wrapper = mount(
+        cy.mount(
             <StyledEngineProvider injectFirst>
                 <ThemeProvider theme={CustomTheme}>
                     <InactivePanelRoot {...props} />
@@ -50,8 +37,8 @@ describe('InactivePanel', () => {
             );
 
         // then
-        expect(wrapper.find('p').first().text()).toContain(expectedMessage);
-        expect(wrapper.find('p').at(1).text()).toContain(expectedLinkAccountText);
+        cy.get('p').eq(0).contains(expectedMessage);
+        cy.get('p').eq(1).contains(expectedLinkAccountText);
     })
 
     it('should render content and link account warning on LoginCluster federations', () => {
@@ -61,7 +48,7 @@ describe('InactivePanel', () => {
         const expectedLinkAccountText = 'If you would like to use this login to access another account, please contact your administrator';
 
         // when
-        const wrapper = mount(
+        cy.mount(
             <StyledEngineProvider injectFirst>
                 <ThemeProvider theme={CustomTheme}>
                     <InactivePanelRoot {...props} />
@@ -70,7 +57,7 @@ describe('InactivePanel', () => {
             );
 
         // then
-        expect(wrapper.find('p').first().text()).toContain(expectedMessage);
-        expect(wrapper.find('p').at(1).text()).toContain(expectedLinkAccountText);
+        cy.get('p').eq(0).contains(expectedMessage);
+        cy.get('p').eq(1).contains(expectedLinkAccountText);
     })
 });
