55bacf0220b5682c83086eae571769db42755845
Peter Amstutz <peter.amstutz@curii.com>
2024-07-03 15:27:04 -0400

    21121: Use separate dygraphs.js for crunchstat and cluster summary
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- tools/cluster-activity/arvados_cluster_activity/report.py
+++ tools/cluster-activity/arvados_cluster_activity/report.py
@@ -21,134 +21,6 @@ from crunchstat_summary.summarizer import Task
 
 from arvados_cluster_activity.prometheus import get_metric_usage, get_data_usage
 
-sortablejs = """
-/**
- * sortable v3.2.3
- *
- * https://www.npmjs.com/package/sortable-tablesort
- * https://github.com/tofsjonas/sortable
- *
- * Makes html tables sortable, No longer ie9+ 😢
- *
- * Styling is done in css.
- *
- * Copyleft 2017 Jonas Earendel
- *
- * This is free and unencumbered software released into the public domain.
- *
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
- *
- * In jurisdictions that recognize copyright laws, the author or authors
- * of this software dedicate any and all copyright interest in the
- * software to the public domain. We make this dedication for the benefit
- * of the public at large and to the detriment of our heirs and
- * successors. We intend this dedication to be an overt act of
- * relinquishment in perpetuity of all present and future rights to this
- * software under copyright law.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- *
- * For more information, please refer to <http://unlicense.org>
- *
- */
-document.addEventListener('click', function (e) {
-    try {
-        // allows for elements inside TH
-        function findElementRecursive(element, tag) {
-            return element.nodeName === tag ? element : findElementRecursive(element.parentNode, tag);
-        }
-        var ascending_table_sort_class = 'asc';
-        var no_sort_class = 'no-sort';
-        var null_last_class = 'n-last';
-        var table_class_name = 'sortable';
-        var alt_sort_1 = e.shiftKey || e.altKey;
-        var element = findElementRecursive(e.target, 'TH');
-        var tr = element.parentNode;
-        var thead = tr.parentNode;
-        var table = thead.parentNode;
-        function getValue(element) {
-            var _a;
-            var value = alt_sort_1 ? element.dataset.sortAlt : (_a = element.dataset.sort) !== null && _a !== void 0 ? _a : element.textContent;
-            return value;
-        }
-        if (thead.nodeName === 'THEAD' && // sortable only triggered in `thead`
-            table.classList.contains(table_class_name) &&
-            !element.classList.contains(no_sort_class) // .no-sort is now core functionality, no longer handled in CSS
-        ) {
-            var column_index_1;
-            var nodes = tr.cells;
-            var tiebreaker_1 = +element.dataset.sortTbr;
-            // Reset thead cells and get column index
-            for (var i = 0; i < nodes.length; i++) {
-                if (nodes[i] === element) {
-                    column_index_1 = +element.dataset.sortCol || i;
-                }
-                else {
-                    nodes[i].setAttribute('aria-sort', 'none');
-                }
-            }
-            var direction = 'descending';
-            if (element.getAttribute('aria-sort') === 'descending' ||
-                (table.classList.contains(ascending_table_sort_class) && element.getAttribute('aria-sort') !== 'ascending')) {
-                direction = 'ascending';
-            }
-            // Update the `th` class accordingly
-            element.setAttribute('aria-sort', direction);
-            var reverse_1 = direction === 'ascending';
-            var sort_null_last_1 = table.classList.contains(null_last_class);
-            var compare_1 = function (a, b, index) {
-                var x = getValue(b.cells[index]);
-                var y = getValue(a.cells[index]);
-                if (sort_null_last_1) {
-                    if (x === '' && y !== '') {
-                        return -1;
-                    }
-                    if (y === '' && x !== '') {
-                        return 1;
-                    }
-                }
-                // Before comparing, clean up formatted numbers that may have a leading dollar sign and/or commas.
-                x = x.replace("$", "").replace(",", "");
-                y = y.replace("$", "").replace(",", "");
-                var temp = +x - +y;
-                var bool = isNaN(temp) ? x.localeCompare(y) : temp;
-                return reverse_1 ? -bool : bool;
-            };
-            // loop through all tbodies and sort them
-            for (var i = 0; i < table.tBodies.length; i++) {
-                var org_tbody = table.tBodies[i];
-                // Put the array rows in an array, so we can sort them...
-                var rows = [].slice.call(org_tbody.rows, 0);
-                // Sort them using Array.prototype.sort()
-                rows.sort(function (a, b) {
-                    var bool = compare_1(a, b, column_index_1);
-                    return bool === 0 && !isNaN(tiebreaker_1) ? compare_1(a, b, tiebreaker_1) : bool;
-                });
-                // Make an empty clone
-                var clone_tbody = org_tbody.cloneNode();
-                // Put the sorted rows inside the clone
-                clone_tbody.append.apply(clone_tbody, rows);
-                // And finally replace the unsorted tbody with the sorted one
-                table.replaceChild(clone_tbody, org_tbody);
-            }
-        }
-        // eslint-disable-next-line no-unused-vars
-    }
-    catch (error) {
-        // console.log(error)
-    }
-});
-"""
-
 sortablecss = """
 @charset "UTF-8";
 .sortable thead th:not(.no-sort) {
@@ -257,6 +129,14 @@ containers_category = 'Concurrent running containers'
 storage_category = 'Storage Usage'
 
 class ReportChart(crunchstat_summary.dygraphs.DygraphsChart):
+    def __init__(self, label, summarizers):
+        super(ReportChart, self).__init__(label, summarizers)
+        self.label = label
+        self.summarizers = summarizers
+        self.STYLE = '\n'.join((self.STYLE, sortablecss))
+
+    JSASSETS = ['dygraphs.js', 'sortable.js']
+
     def sections(self):
         return [
             {
@@ -271,9 +151,10 @@ class ReportChart(crunchstat_summary.dygraphs.DygraphsChart):
             for s in self.summarizers]
 
     def js(self):
-        return 'var chartdata = {};\n{}'.format(
+        return 'var chartdata = {};\n{}{}'.format(
             json.dumps(self.sections(), default=date_export).replace('"@', '').replace('@"', '').replace('\\"', '"'),
-            '\n'.join([pkg_resources.resource_string('crunchstat_summary', jsa).decode('utf-8') for jsa in self.JSASSETS]))
+            '\n'.join([pkg_resources.resource_string('crunchstat_summary', 'synchronizer.js').decode('utf-8')]),
+            '\n'.join([pkg_resources.resource_string('arvados_cluster_activity', jsa).decode('utf-8') for jsa in self.JSASSETS]))
 
     def _collate_data(self, tasks, stats):
         data = []
@@ -472,16 +353,12 @@ class ClusterActivityReport(object):
 
         bottomhtml.append(
             """
-            <style>{style}</style>
-            <script>{script}</script>
             <a id="Active_Projects"><h2>Active Projects</h2></a>
             <table class='sortable'>
             <thead><tr><th>Project</th> <th>Users</th> <th>Active</th> <th>Compute usage (hours)</th> <th>Compute cost</th> </tr></thead>
             <tbody><tr>{projects}</tr></tbody>
             </table>
-            """.format(projects="</tr>\n<tr>".join("""<td><a href="#{name}">{name}</a></td>{rest}""".format(name=prj.name, rest=prj.tablerow) for k, prj in projectlist),
-                       script=sortablejs,
-                       style=sortablecss))
+            """.format(projects="</tr>\n<tr>".join("""<td><a href="#{name}">{name}</a></td>{rest}""".format(name=prj.name, rest=prj.tablerow) for k, prj in projectlist)))
 
         for k, prj in projectlist:
             wfsum = []
