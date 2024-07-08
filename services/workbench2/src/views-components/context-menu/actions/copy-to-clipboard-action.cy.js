a47785a3f99a18405094c91a3e1716bfa67cf8e3
Lisa Knox <lisa.knox@curii.com>
2024-07-08 12:17:20 -0400

    21720:
    * updated copy-to-clipboard test
    
    Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/src/views-components/context-menu/actions/copy-to-clipboard-action.cy.js
+++ services/workbench2/src/views-components/context-menu/actions/copy-to-clipboard-action.cy.js
@@ -3,34 +3,33 @@
 // SPDX-License-Identifier: AGPL-3.0
 
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+// import { shallow, configure } from 'enzyme';
 import { ListItem } from "@mui/material";
-import Adapter from 'enzyme-adapter-react-16';
+// import Adapter from 'enzyme-adapter-react-16';
 import { CopyToClipboardAction } from './copy-to-clipboard-action';
 
-configure({ adapter: new Adapter() });
+// configure({ adapter: new Adapter() });
 
-jest.mock('copy-to-clipboard', () => jest.fn());
+// jest.mock('copy-to-clipboard', () => jest.fn());
 
 describe('CopyToClipboardAction', () => {
     let props;
 
     beforeEach(() => {
         props = {
-            onClick: jest.fn(),
+            onClick: cy.stub().as('onClick'),
             href: 'https://collections.example.com/c=zzzzz-4zz18-k0hamvtwyit6q56/t=xxxxxxxx/LIMS/1.html',
         };
     });
 
     it('should render properly and handle click', () => {
         // when
-        const wrapper = shallow(<CopyToClipboardAction {...props} />);
-        wrapper.find(ListItem).simulate('click');
+        cy.mount(<CopyToClipboardAction {...props} />);
+
+        // check
+        cy.contains('Copy link to clipboard').click();
 
         // then
-        expect(wrapper).not.toBeUndefined();
-
-        // and
-        expect(props.onClick).toHaveBeenCalled();
+        cy.get('@onClick').should('have.been.called');
     });
 });
