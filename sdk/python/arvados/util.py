626f005fc6a8ccb0efc28e6c151f57590e2da7d2
Brett Smith <brett.smith@curii.com>
2024-07-12 11:35:29 -0400

    21935: Move arvados.util._deprecated to arvados._internal
    Arvados-DCO-1.1-Signed-off-by: Brett Smith <brett.smith@curii.com>

--- sdk/python/arvados/util.py
+++ sdk/python/arvados/util.py
@@ -11,7 +11,6 @@ import dataclasses
 import enum
 import errno
 import fcntl
-import functools
 import hashlib
 import httplib2
 import itertools
@@ -24,7 +23,6 @@ import shlex
 import stat
 import subprocess
 import sys
-import warnings
 
 import arvados.errors
 
@@ -79,58 +77,6 @@ user_uuid_pattern = re.compile(r'[a-z0-9]{5}-tpzed-[a-z0-9]{15}')
 """Regular expression to match any Arvados user UUID"""
 
 logger = logging.getLogger('arvados')
-
-def _deprecated(version=None, preferred=None):
-    """Mark a callable as deprecated in the SDK
-
-    This will wrap the callable to emit as a DeprecationWarning
-    and add a deprecation notice to its docstring.
-
-    If the following arguments are given, they'll be included in the
-    notices:
-
-    * preferred: str | None --- The name of an alternative that users should
-      use instead.
-
-    * version: str | None --- The version of Arvados when the callable is
-      scheduled to be removed.
-    """
-    if version is None:
-        version = ''
-    else:
-        version = f' and scheduled to be removed in Arvados {version}'
-    if preferred is None:
-        preferred = ''
-    else:
-        preferred = f' Prefer {preferred} instead.'
-    def deprecated_decorator(func):
-        fullname = f'{func.__module__}.{func.__qualname__}'
-        parent, _, name = fullname.rpartition('.')
-        if name == '__init__':
-            fullname = parent
-        warning_msg = f'{fullname} is deprecated{version}.{preferred}'
-        @functools.wraps(func)
-        def deprecated_wrapper(*args, **kwargs):
-            warnings.warn(warning_msg, DeprecationWarning, 2)
-            return func(*args, **kwargs)
-        # Get func's docstring without any trailing newline or empty lines.
-        func_doc = re.sub(r'\n\s*$', '', func.__doc__ or '')
-        match = re.search(r'\n([ \t]+)\S', func_doc)
-        indent = '' if match is None else match.group(1)
-        warning_doc = f'\n\n{indent}.. WARNING:: Deprecated\n{indent}   {warning_msg}'
-        # Make the deprecation notice the second "paragraph" of the
-        # docstring if possible. Otherwise append it.
-        docstring, count = re.subn(
-            rf'\n[ \t]*\n{indent}',
-            f'{warning_doc}\n\n{indent}',
-            func_doc,
-            count=1,
-        )
-        if not count:
-            docstring = f'{func_doc.lstrip()}{warning_doc}'
-        deprecated_wrapper.__doc__ = docstring
-        return deprecated_wrapper
-    return deprecated_decorator
 
 @dataclasses.dataclass
 class _BaseDirectorySpec:
