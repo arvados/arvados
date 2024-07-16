73ca698f33cc07fee2e293d86ba529f37975d947
Lisa Knox <lisa.knox@curii.com>
2024-07-16 14:52:10 -0400

    21720:
    * updated models/tree test
    
    Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/src/models/tree.cy.js
+++ services/workbench2/src/models/tree.cy.js
@@ -4,10 +4,10 @@
 
 import * as Tree from './tree';
 import { initTreeNode } from './tree';
-import { pipe } from 'lodash/fp';
+import { pipe, isEqual } from 'lodash/fp';
 
 describe('Tree', () => {
-    let tree: Tree.Tree<string>;
+    let tree;
 
     beforeEach(() => {
         tree = Tree.createTree();
@@ -15,15 +15,15 @@ describe('Tree', () => {
 
     it('sets new node', () => {
         const newTree = Tree.setNode(initTreeNode({ id: 'Node 1', value: 'Value 1' }))(tree);
-        expect(Tree.getNode('Node 1')(newTree)).toEqual(initTreeNode({ id: 'Node 1', value: 'Value 1' }));
+        expect(isEqual(Tree.getNode('Node 1')(newTree), initTreeNode({ id: 'Node 1', value: 'Value 1' }))).to.equal(true);
     });
 
     it('appends a subtree', () => {
         const newTree = Tree.setNode(initTreeNode({ id: 'Node 1', value: 'Value 1' }))(tree);
         const subtree = Tree.setNode(initTreeNode({ id: 'Node 2', value: 'Value 2' }))(Tree.createTree());
         const mergedTree = Tree.appendSubtree('Node 1', subtree)(newTree);
-        expect(Tree.getNode('Node 1')(mergedTree)).toBeDefined();
-        expect(Tree.getNode('Node 2')(mergedTree)).toBeDefined();
+        expect(Tree.getNode('Node 1')(mergedTree)).to.not.equal('undefined');
+        expect(Tree.getNode('Node 2')(mergedTree)).to.not.equal('undefined');
     });
 
     it('adds new node reference to parent children', () => {
@@ -32,10 +32,10 @@ describe('Tree', () => {
             Tree.setNode(initTreeNode({ id: 'Node 2', parent: 'Node 1', value: 'Value 2' })),
         )(tree);
 
-        expect(Tree.getNode('Node 1')(newTree)).toEqual({
+        expect(isEqual(Tree.getNode('Node 1')(newTree), {
             ...initTreeNode({ id: 'Node 1', parent: '', value: 'Value 1' }),
             children: ['Node 2']
-        });
+        })).to.equal(true);
     });
 
     it('gets node ancestors', () => {
@@ -44,7 +44,7 @@ describe('Tree', () => {
             initTreeNode({ id: 'Node 2', parent: 'Node 1', value: 'Value 1' }),
             initTreeNode({ id: 'Node 3', parent: 'Node 2', value: 'Value 1' }),
         ].reduce((tree, node) => Tree.setNode(node)(tree), tree);
-        expect(Tree.getNodeAncestorsIds('Node 3')(newTree)).toEqual(['Node 1', 'Node 2']);
+        expect(isEqual(Tree.getNodeAncestorsIds('Node 3')(newTree), ['Node 1', 'Node 2'])).to.equal(true);
     });
 
     it('gets node descendants', () => {
@@ -55,7 +55,7 @@ describe('Tree', () => {
             initTreeNode({ id: 'Node 3', parent: 'Node 1', value: 'Value 1' }),
             initTreeNode({ id: 'Node 3.1', parent: 'Node 3', value: 'Value 1' }),
         ].reduce((tree, node) => Tree.setNode(node)(tree), tree);
-        expect(Tree.getNodeDescendantsIds('Node 1')(newTree)).toEqual(['Node 2', 'Node 3', 'Node 2.1', 'Node 3.1']);
+        expect(isEqual(Tree.getNodeDescendantsIds('Node 1')(newTree), ['Node 2', 'Node 3', 'Node 2.1', 'Node 3.1'])).to.equal(true);
     });
 
     it('gets root descendants', () => {
@@ -66,7 +66,7 @@ describe('Tree', () => {
             initTreeNode({ id: 'Node 3', parent: 'Node 1', value: 'Value 1' }),
             initTreeNode({ id: 'Node 3.1', parent: 'Node 3', value: 'Value 1' }),
         ].reduce((tree, node) => Tree.setNode(node)(tree), tree);
-        expect(Tree.getNodeDescendantsIds('')(newTree)).toEqual(['Node 1', 'Node 2', 'Node 3', 'Node 2.1', 'Node 3.1']);
+        expect(isEqual(Tree.getNodeDescendantsIds('')(newTree), ['Node 1', 'Node 2', 'Node 3', 'Node 2.1', 'Node 3.1'])).to.equal(true);
     });
 
     it('gets node children', () => {
@@ -77,7 +77,7 @@ describe('Tree', () => {
             initTreeNode({ id: 'Node 3', parent: 'Node 1', value: 'Value 1' }),
             initTreeNode({ id: 'Node 3.1', parent: 'Node 3', value: 'Value 1' }),
         ].reduce((tree, node) => Tree.setNode(node)(tree), tree);
-        expect(Tree.getNodeChildrenIds('Node 1')(newTree)).toEqual(['Node 2', 'Node 3']);
+        expect(isEqual(Tree.getNodeChildrenIds('Node 1')(newTree), ['Node 2', 'Node 3'])).to.equal(true);
     });
 
     it('gets root children', () => {
@@ -88,7 +88,7 @@ describe('Tree', () => {
             initTreeNode({ id: 'Node 3', parent: '', value: 'Value 1' }),
             initTreeNode({ id: 'Node 3.1', parent: 'Node 3', value: 'Value 1' }),
         ].reduce((tree, node) => Tree.setNode(node)(tree), tree);
-        expect(Tree.getNodeChildrenIds('')(newTree)).toEqual(['Node 1', 'Node 3']);
+        expect(isEqual(Tree.getNodeChildrenIds('')(newTree), ['Node 1', 'Node 3'])).to.equal(true);
     });
 
     it('maps tree', () => {
@@ -96,8 +96,8 @@ describe('Tree', () => {
             initTreeNode({ id: 'Node 1', parent: '', value: 'Value 1' }),
             initTreeNode({ id: 'Node 2', parent: 'Node 1', value: 'Value 2' }),
         ].reduce((tree, node) => Tree.setNode(node)(tree), tree);
-        const mappedTree = Tree.mapTreeValues<string, number>(value => parseInt(value.split(' ')[1], 10))(newTree);
-        expect(Tree.getNode('Node 2')(mappedTree)).toEqual(initTreeNode({ id: 'Node 2', parent: 'Node 1', value: 2 }));
+        const mappedTree = Tree.mapTreeValues(value => parseInt(value.split(' ')[1], 10))(newTree);
+        expect(isEqual(Tree.getNode('Node 2')(mappedTree), initTreeNode({ id: 'Node 2', parent: 'Node 1', value: 2 }))).to.equal(true);
     });
 
     it('expands node ancestor chains', () => {
@@ -120,14 +120,14 @@ describe('Tree', () => {
             'Node 2.1', // Expands 2
         )(newTree);
 
-        expect(Tree.getNode('Root Node 1')(expandedTree)?.expanded).toEqual(true);
-        expect(Tree.getNode('Node 1.1')(expandedTree)?.expanded).toEqual(true);
-        expect(Tree.getNode('Node 1.1.1')(expandedTree)?.expanded).toEqual(false);
-        expect(Tree.getNode('Node 1.2')(expandedTree)?.expanded).toEqual(false);
-        expect(Tree.getNode('Root Node 2')(expandedTree)?.expanded).toEqual(true);
-        expect(Tree.getNode('Node 2.1')(expandedTree)?.expanded).toEqual(false);
-        expect(Tree.getNode('Node 2.1.1')(expandedTree)?.expanded).toEqual(false);
-        expect(Tree.getNode('Root Node 3')(expandedTree)?.expanded).toEqual(false);
-        expect(Tree.getNode('Node 3.1')(expandedTree)?.expanded).toEqual(false);
+        expect(Tree.getNode('Root Node 1')(expandedTree)?.expanded).to.equal(true);
+        expect(Tree.getNode('Node 1.1')(expandedTree)?.expanded).to.equal(true);
+        expect(Tree.getNode('Node 1.1.1')(expandedTree)?.expanded).to.equal(false);
+        expect(Tree.getNode('Node 1.2')(expandedTree)?.expanded).to.equal(false);
+        expect(Tree.getNode('Root Node 2')(expandedTree)?.expanded).to.equal(true);
+        expect(Tree.getNode('Node 2.1')(expandedTree)?.expanded).to.equal(false);
+        expect(Tree.getNode('Node 2.1.1')(expandedTree)?.expanded).to.equal(false);
+        expect(Tree.getNode('Root Node 3')(expandedTree)?.expanded).to.equal(false);
+        expect(Tree.getNode('Node 3.1')(expandedTree)?.expanded).to.equal(false);
     });
 });
