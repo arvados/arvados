4efc886e70b1fc0950958b2f49a53417cedaa8a7
Lisa Knox <lisa.knox@curii.com>
2024-07-19 09:17:28 -0400

    21720:
    * updated collection-action-set test
    
    Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/src/views-components/context-menu/action-sets/collection-action-set.cy.js
+++ services/workbench2/src/views-components/context-menu/action-sets/collection-action-set.cy.js
@@ -4,32 +4,36 @@
 
 import { collectionActionSet, readOnlyCollectionActionSet } from "./collection-action-set";
 
+const containsActionSubSet = (mainSet, subSet) => {
+    const mainNames = mainSet.map(action => action.name)
+    const subNames = subSet.map(action => action.name)
+    return subNames.every(name => mainNames.includes(name));
+}
+
 describe('collection-action-set', () => {
     const flattCollectionActionSet = collectionActionSet.reduce((prev, next) => prev.concat(next), []);
     const flattReadOnlyCollectionActionSet = readOnlyCollectionActionSet.reduce((prev, next) => prev.concat(next), []);
     describe('collectionActionSet', () => {
         it('should not be empty', () => {
             // then
-            expect(flattCollectionActionSet.length).toBeGreaterThan(0);
+            expect(flattCollectionActionSet).to.have.length.greaterThan(0);
         });
 
         it('should contain readOnlyCollectionActionSet items', () => {
             // then
-            expect(flattCollectionActionSet)
-                .toEqual(expect.arrayContaining(flattReadOnlyCollectionActionSet));
+            expect(containsActionSubSet(flattCollectionActionSet, flattReadOnlyCollectionActionSet)).to.be.true;
         })
     });
 
     describe('readOnlyCollectionActionSet', () => {
         it('should not be empty', () => {
             // then
-            expect(flattReadOnlyCollectionActionSet.length).toBeGreaterThan(0);
+            expect(flattReadOnlyCollectionActionSet).to.have.length.greaterThan(0);
         });
 
         it('should not contain collectionActionSet items', () => {
             // then
-            expect(flattReadOnlyCollectionActionSet)
-                .not.toEqual(expect.arrayContaining(flattCollectionActionSet));
+            expect(containsActionSubSet(flattReadOnlyCollectionActionSet, flattCollectionActionSet)).to.be.false;
         })
     });
 });
