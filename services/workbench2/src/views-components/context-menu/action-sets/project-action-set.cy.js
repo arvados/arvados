147da911e801b6d5324696c215553e04af8843de
Lisa Knox <lisa.knox@curii.com>
2024-07-19 10:04:21 -0400

    21720:
    * updated project-action-set test
    
    Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/src/views-components/context-menu/action-sets/project-action-set.cy.js
+++ services/workbench2/src/views-components/context-menu/action-sets/project-action-set.cy.js
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: AGPL-3.0
 
 import { filterGroupActionSet, projectActionSet, readOnlyProjectActionSet } from "./project-action-set";
+import { containsActionSubSet } from "../../../cypress/utils/contains-action-subset";
 
 describe('project-action-set', () => {
     const flattProjectActionSet = projectActionSet.reduce((prev, next) => prev.concat(next), []);
@@ -12,39 +13,36 @@ describe('project-action-set', () => {
     describe('projectActionSet', () => {
         it('should not be empty', () => {
             // then
-            expect(flattProjectActionSet.length).toBeGreaterThan(0);
+            expect(flattProjectActionSet).to.have.length.greaterThan(0);
         });
 
         it('should contain readOnlyProjectActionSet items', () => {
             // then
-            expect(flattProjectActionSet)
-                .toEqual(expect.arrayContaining(flattReadOnlyProjectActionSet));
+            expect(containsActionSubSet(flattProjectActionSet, flattReadOnlyProjectActionSet)).to.be.true;
         })
     });
 
     describe('readOnlyProjectActionSet', () => {
         it('should not be empty', () => {
             // then
-            expect(flattReadOnlyProjectActionSet.length).toBeGreaterThan(0);
+            expect(flattReadOnlyProjectActionSet).to.have.length.greaterThan(0);
         });
 
         it('should not contain projectActionSet items', () => {
             // then
-            expect(flattReadOnlyProjectActionSet)
-                .not.toEqual(expect.arrayContaining(flattProjectActionSet));
+            expect(containsActionSubSet(flattReadOnlyProjectActionSet, flattProjectActionSet)).to.be.false;
         })
     });
 
     describe('filterGroupActionSet', () => {
         it('should not be empty', () => {
             // then
-            expect(flattFilterGroupActionSet.length).toBeGreaterThan(0);
+            expect(flattFilterGroupActionSet).to.have.length.greaterThan(0);
         });
 
         it('should not contain projectActionSet items', () => {
             // then
-            expect(flattFilterGroupActionSet)
-                .not.toEqual(expect.arrayContaining(flattProjectActionSet));
+            expect(containsActionSubSet(flattFilterGroupActionSet, flattProjectActionSet)).to.be.false;
         })
     });
 });
