73d78618ae340fb6f78c5060b41ff3071cb5c1ea
Lisa Knox <lisa.knox@curii.com>
2024-07-19 13:39:52 -0400

    21720:
    * updated favorite-service test
    
    Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/src/services/favorite-service/favorite-service.cy.js
+++ services/workbench2/src/services/favorite-service/favorite-service.cy.js
@@ -6,59 +6,74 @@ import { LinkService } from "../link-service/link-service";
 import { GroupsService } from "../groups-service/groups-service";
 import { FavoriteService } from "./favorite-service";
 import { LinkClass } from "models/link";
-import { mockResourceService } from "services/common-service/common-resource-service.test";
 import { FilterBuilder } from "services/api/filter-builder";
+import axios from "axios";
+import { isEqual } from "lodash";
 
 describe("FavoriteService", () => {
 
-    let linkService: LinkService;
-    let groupService: GroupsService;
+    let linkService;
+    let groupService;
+
+    const mockListArgs = {
+        filters: [],
+        limit: undefined,
+        offset: undefined,
+        order: undefined,
+    };
+
+    const mockContentsArgs = {
+        limit: undefined,
+        offset: undefined,
+        order: undefined,
+        filters: [],
+        recursive: true
+    };
 
     beforeEach(() => {
-        linkService = mockResourceService(LinkService);
-        groupService = mockResourceService(GroupsService);
+        linkService = new LinkService(axios, []);
+        groupService = new GroupsService(axios, []);
     });
 
     it("marks resource as favorite", async () => {
-        linkService.create = jest.fn().mockReturnValue(Promise.resolve({ uuid: "newUuid" }));
+        linkService.create = cy.stub().returns(Promise.resolve({ uuid: "newUuid" })).as("create");
         const favoriteService = new FavoriteService(linkService, groupService);
-
         const newFavorite = await favoriteService.create({ userUuid: "userUuid", resource: { uuid: "resourceUuid", name: "resource" } });
 
-        expect(linkService.create).toHaveBeenCalledWith({
+        cy.get("@create").should("be.calledWith", {
             ownerUuid: "userUuid",
             tailUuid: "userUuid",
             headUuid: "resourceUuid",
             linkClass: LinkClass.STAR,
             name: "resource"
         });
-        expect(newFavorite.uuid).toEqual("newUuid");
+        expect(newFavorite.uuid).to.equal("newUuid");
 
     });
 
     it("unmarks resource as favorite", async () => {
-        const list = jest.fn().mockReturnValue(Promise.resolve({ items: [{ uuid: "linkUuid" }] }));
+        const list = cy.stub().returns(Promise.resolve({ items: [{ uuid: "linkUuid" }] })).as("list");
         const filters = new FilterBuilder()
             .addEqual('owner_uuid', "userUuid")
             .addEqual('head_uuid', "resourceUuid")
             .addEqual('link_class', LinkClass.STAR);
         linkService.list = list;
-        linkService.delete = jest.fn().mockReturnValue(Promise.resolve({ uuid: "linkUuid" }));
+        linkService.delete = cy.stub().returns(Promise.resolve({ uuid: "linkUuid" })).as("delete");
         const favoriteService = new FavoriteService(linkService, groupService);
 
         const newFavorite = await favoriteService.delete({ userUuid: "userUuid", resourceUuid: "resourceUuid" });
 
-        expect(list.mock.calls[0][0].filters).toEqual(filters.getFilters());
-        expect(linkService.delete).toHaveBeenCalledWith("linkUuid");
-        expect(newFavorite[0].uuid).toEqual("linkUuid");
+        cy.get("@list").should("be.calledWith", { filters: filters.getFilters() });
+        cy.get("@delete").should("be.calledWith", "linkUuid");
+        expect(newFavorite[0].uuid).to.equal("linkUuid");
     });
 
     it("lists favorite resources", async () => {
-        const list = jest.fn().mockReturnValue(Promise.resolve({ items: [{ headUuid: "headUuid" }] }));
+        const list = cy.stub().returns(Promise.resolve({ items: [{ headUuid: "headUuid" }] })).as("list");
         const listFilters = new FilterBuilder()
             .addEqual('owner_uuid', "userUuid")
             .addEqual('link_class', LinkClass.STAR);
-        const contents = jest.fn().mockReturnValue(Promise.resolve({ items: [{ uuid: "resourceUuid" }] }));
+        const contents = cy.stub().returns(Promise.resolve({ items: [{ uuid: "resourceUuid" }] })).as("contents");
         const contentFilters = new FilterBuilder().addIn('uuid', ["headUuid"]);
         linkService.list = list;
         groupService.contents = contents;
@@ -66,14 +81,13 @@ describe("FavoriteService", () => {
 
         const favorites = await favoriteService.list("userUuid");
 
-        expect(list.mock.calls[0][0].filters).toEqual(listFilters.getFilters());
-        expect(contents.mock.calls[0][0]).toEqual("userUuid");
-        expect(contents.mock.calls[0][1].filters).toEqual(contentFilters.getFilters());
-        expect(favorites).toEqual({ items: [{ uuid: "resourceUuid" }] });
+        cy.get("@list").should("be.calledWith", { ...mockListArgs, filters: listFilters.getFilters() });
+        cy.get("@contents").should("be.calledWith", "userUuid", { ...mockContentsArgs,  filters: contentFilters.getFilters() });
+        expect(isEqual(favorites, { items: [{ uuid: "resourceUuid" }] })).to.equal(true);
     });
 
     it("checks if resources are present in favorites", async () => {
-        const list = jest.fn().mockReturnValue(Promise.resolve({ items: [{ headUuid: "foo" }] }));
+        const list = cy.stub().returns(Promise.resolve({ items: [{ headUuid: "foo" }] })).as("list");
         const listFilters = new FilterBuilder()
             .addIn("head_uuid", ["foo", "oof"])
             .addEqual("owner_uuid", "userUuid")
@@ -83,8 +97,8 @@ describe("FavoriteService", () => {
 
         const favorites = await favoriteService.checkPresenceInFavorites("userUuid", ["foo", "oof"]);
 
-        expect(list.mock.calls[0][0].filters).toEqual(listFilters.getFilters());
-        expect(favorites).toEqual({ foo: true, oof: false });
+        cy.get("@list").should("be.calledWith", { filters: listFilters.getFilters() });
+        expect(isEqual(favorites, { foo: true, oof: false })).to.be.true;
     });
 
 });
