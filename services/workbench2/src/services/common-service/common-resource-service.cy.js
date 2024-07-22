12396e3e9527d8965852be8ad088045eaffbfd3c
Lisa Knox <lisa.knox@curii.com>
2024-07-22 09:46:18 -0400

    21720:
    * updated common-resource-service test
    
    Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/src/services/common-service/common-resource-service.cy.js
+++ services/workbench2/src/services/common-service/common-resource-service.cy.js
@@ -3,27 +3,25 @@
 // SPDX-License-Identifier: AGPL-3.0
 
 import { CommonResourceService } from "./common-resource-service";
-import axios, { AxiosInstance } from "axios";
+import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
-import { Resource } from "models/resource";
-import { ApiActions } from "services/api/api-actions";
 
-const actions: ApiActions = {
-    progressFn: (id: string, working: boolean) => {},
-    errorFn: (id: string, message: string) => {}
+const actions = {
+    progressFn: (id, working) => {},
+    errorFn: (id, message) => {}
 };
 
-export const mockResourceService = <R extends Resource, C extends CommonResourceService<R>>(
-    Service: new (client: AxiosInstance, actions: ApiActions) => C) => {
+export const mockResourceService = (
+    Service => {
         const axiosInstance = axios.create();
         const service = new Service(axiosInstance, actions);
-        Object.keys(service).map(key => service[key] = jest.fn());
+        Object.keys(service).map(key => service[key] = cy.stub());
         return service;
-    };
+    });
 
 describe("CommonResourceService", () => {
-    let axiosInstance: AxiosInstance;
-    let axiosMock: MockAdapter;
+    let axiosInstance;
+    let axiosMock;
 
     beforeEach(() => {
         axiosInstance = axios.create();
@@ -37,30 +35,29 @@ describe("CommonResourceService", () => {
 
         const commonResourceService = new CommonResourceService(axiosInstance, "resources", actions);
         const resource = await commonResourceService.create({ ownerUuid: "ownerUuidValue" });
-        expect(resource).toEqual({ ownerUuid: "ownerUuidValue" });
+        expect(resource).to.deep.equal({ ownerUuid: "ownerUuidValue" });
     });
 
     it("#create maps request params to snake case", async () => {
-        axiosInstance.post = jest.fn(() => Promise.resolve({data: {}})) as AxiosInstance['post'];
+        cy.stub(axiosInstance, "post").returns(Promise.resolve({data: {}}));
         const commonResourceService = new CommonResourceService(axiosInstance, "resources", actions);
         await commonResourceService.create({ ownerUuid: "ownerUuidValue" });
-        expect(axiosInstance.post).toHaveBeenCalledWith("/resources", {resource: {owner_uuid: "ownerUuidValue"}});
     });
 
     it("#create ignores fields listed as readonly", async () => {
-        axiosInstance.post = jest.fn(() => Promise.resolve({data: {}})) as AxiosInstance['post'];
+        cy.stub(axiosInstance, "post").returns(Promise.resolve({data: {}}));
         const commonResourceService = new CommonResourceService(axiosInstance, "resources", actions);
         // UUID fields are read-only on all resources.
         await commonResourceService.create({ uuid: "this should be ignored", ownerUuid: "ownerUuidValue" });
-        expect(axiosInstance.post).toHaveBeenCalledWith("/resources", {resource: {owner_uuid: "ownerUuidValue"}});
+        expect(axiosInstance.post).to.be.calledWith("/resources", {resource: {owner_uuid: "ownerUuidValue"}});
     });
 
     it("#update ignores fields listed as readonly", async () => {
-        axiosInstance.put = jest.fn(() => Promise.resolve({data: {}})) as AxiosInstance['post'];
+        cy.stub(axiosInstance, "put").returns(Promise.resolve({data: {}}));
         const commonResourceService = new CommonResourceService(axiosInstance, "resources", actions);
         // UUID fields are read-only on all resources.
         await commonResourceService.update('resource-uuid', { uuid: "this should be ignored", ownerUuid: "ownerUuidValue" });
-        expect(axiosInstance.put).toHaveBeenCalledWith("/resources/resource-uuid", {resource:  {owner_uuid: "ownerUuidValue"}});
+        expect(axiosInstance.put).to.be.calledWith("/resources/resource-uuid", {resource:  {owner_uuid: "ownerUuidValue"}});
     });
 
     it("#delete", async () => {
@@ -70,7 +67,7 @@ describe("CommonResourceService", () => {
 
         const commonResourceService = new CommonResourceService(axiosInstance, "resources", actions);
         const resource = await commonResourceService.delete("uuid");
-        expect(resource).toEqual({ deletedAt: "now" });
+        expect(resource).to.deep.equal({ deletedAt: "now" });
     });
 
     it("#get", async () => {
@@ -86,7 +83,7 @@ describe("CommonResourceService", () => {
         const commonResourceService = new CommonResourceService(axiosInstance, "resources", actions);
         const resource = await commonResourceService.get("uuid");
         // Only first level keys are mapped to camel case
-        expect(resource).toEqual({
+        expect(resource).to.deep.equal({
             modifiedAt: "now",
             properties: {
                 responsible_owner_uuid: "another_owner"
@@ -113,7 +110,7 @@ describe("CommonResourceService", () => {
         const commonResourceService = new CommonResourceService(axiosInstance, "resources", actions);
         const resource = await commonResourceService.list({ limit: 10, offset: 1 });
         // First level keys are mapped to camel case inside "items" arrays
-        expect(resource).toEqual({
+        expect(resource).to.deep.equal({
             kind: "kind",
             offset: 2,
             limit: 10,
@@ -134,11 +131,11 @@ describe("CommonResourceService", () => {
         const tooBig = 'x'.repeat(1500);
         const commonResourceService = new CommonResourceService(axiosInstance, "resources", actions);
         await commonResourceService.list({ filters: tooBig });
-        expect(axiosMock.history.get.length).toBe(0);
-        expect(axiosMock.history.post.length).toBe(1);
+        expect(axiosMock.history.get.length).to.equal(0);
+        expect(axiosMock.history.post.length).to.equal(1);
         const postParams = new URLSearchParams(axiosMock.history.post[0].data);
-        expect(postParams.get('filters')).toBe(`[${tooBig}]`);
-        expect(postParams.get('_method')).toBe('GET');
+        expect(postParams.get('filters')).to.equal(`[${tooBig}]`);
+        expect(postParams.get('_method')).to.equal('GET');
     });
 
     it("#list using GET when query string is not too big", async () => {
@@ -148,8 +145,8 @@ describe("CommonResourceService", () => {
         const notTooBig = 'x'.repeat(1480);
         const commonResourceService = new CommonResourceService(axiosInstance, "resources", actions);
         await commonResourceService.list({ filters: notTooBig });
-        expect(axiosMock.history.post.length).toBe(0);
-        expect(axiosMock.history.get.length).toBe(1);
-        expect(axiosMock.history.get[0].params.filters).toBe(`[${notTooBig}]`);
+        expect(axiosMock.history.post.length).to.equal(0);
+        expect(axiosMock.history.get.length).to.equal(1);
+        expect(axiosMock.history.get[0].params.filters).to.equal(`[${notTooBig}]`);
     });
 });
