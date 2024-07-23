bd6fad3555ea95c6e4ecf7b67785bd629252cd01
Peter Amstutz <peter.amstutz@curii.com>
2024-07-23 13:24:57 -0400

    21121: Clean up make_docker.sh
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- tools/cluster-activity/make_docker.sh
+++ tools/cluster-activity/make_docker.sh
@@ -1,25 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 # Copyright (C) The Arvados Authors. All rights reserved.
 #
 # SPDX-License-Identifier: AGPL-3.0
-set -ex
 
-cd ../../sdk/python
-python3 setup.py sdist
-SDK_VERSION=$(python3 arvados_version.py)
+read -rd "\000" helpmessage <<EOF
+Build an arvados/cluster-activity Docker image from local git tree.
 
-cd ../../tools/crunchstat-summary
-python3 setup.py sdist
-CRUNCHSTAT_VERSION=$(python3 arvados_version.py)
+Intended for use by developers working on arvados/cluster-activity or
+and need to run a crunch job with a custom package version.
 
-cd ../../tools/cluster-activity
-python3 setup.py sdist
-VERSION=$(python3 arvados_version.py)
+Syntax:
+        WORKSPACE=/path/to/arvados $(basename $0)
 
-rm -f docker/*.tar.gz
-cp ../../sdk/python/dist/arvados-python-client-${SDK_VERSION}.tar.gz \
-   ../crunchstat-summary/dist/crunchstat_summary-${CRUNCHSTAT_VERSION}.tar.gz \
-   dist/arvados-cluster-activity-${VERSION}.tar.gz \
-   docker/
-cd docker
-docker build -t arvados/cluster-activity:$VERSION .
+WORKSPACE=path         Path to the Arvados source tree to build packages from
+
+EOF
+
+set -e
+
+if [[ -z "$WORKSPACE" ]] ; then
+    export WORKSPACE=$(readlink -f $(dirname $0)/../..)
+    echo "Using WORKSPACE $WORKSPACE"
+fi
+
+context_dir="$(mktemp --directory --tmpdir dev-jobs.XXXXXXXX)"
+trap 'rm -rf "$context_dir"' EXIT INT TERM QUIT
+
+cluster_activity_version=$(cd $WORKSPACE/tools/cluster-activity && python3 arvados_version.py)
+
+for src_dir in "$WORKSPACE/sdk/python" "$WORKSPACE/tools/crunchstat-summary" "$WORKSPACE/tools/cluster-activity" ; do
+    env -C "$src_dir" python3 setup.py sdist --dist-dir="$context_dir"
+done
+
+set -x
+docker build --no-cache \
+       -f "$WORKSPACE/tools/cluster-activity/cluster-activity.dockerfile" \
+       -t arvados/cluster-activity:$cluster_activity_version \
+       "$context_dir"
