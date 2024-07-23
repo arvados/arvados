3bfdcc73428bcec779d0585c640b94cf4d420f13
Stephen Smith <stephen@curii.com>
2024-07-23 15:27:32 -0400

    17074: Organize groups panel service
    Arvados-DCO-1.1-Signed-off-by: Stephen Smith <stephen@curii.com>

--- services/workbench2/src/store/groups-panel/groups-panel-middleware-service.ts
+++ services/workbench2/src/store/groups-panel/groups-panel-middleware-service.ts
@@ -7,7 +7,7 @@ import { DataExplorerMiddlewareService, listResultsToDataExplorerItemsMeta, data
 import { RootState } from "store/store";
 import { ServiceRepository } from "services/services";
 import { snackbarActions, SnackbarKind } from 'store/snackbar/snackbar-actions';
-import { getDataExplorer, getSortColumn } from "store/data-explorer/data-explorer-reducer";
+import { DataExplorer, getDataExplorer, getSortColumn } from "store/data-explorer/data-explorer-reducer";
 import { GroupsPanelActions } from 'store/groups-panel/groups-panel-actions';
 import { FilterBuilder } from 'services/api/filter-builder';
 import { updateResources } from 'store/resources/resources-actions';
@@ -15,11 +15,44 @@ import { OrderBuilder, OrderDirection } from 'services/api/order-builder';
 import { GroupResource, GroupClass } from 'models/group';
 import { SortDirection } from 'components/data-table/data-column';
 import { progressIndicatorActions } from "store/progress-indicator/progress-indicator-actions";
+import { ListArguments } from "services/common-service/common-service";
 
 export class GroupsPanelMiddlewareService extends DataExplorerMiddlewareService {
     constructor(private services: ServiceRepository, id: string) {
         super(id);
     }
+
+    getOrder = (dataExplorer: DataExplorer) => {
+        const sortColumn = getSortColumn<GroupResource>(dataExplorer);
+        const order = new OrderBuilder<GroupResource>();
+        if (sortColumn && sortColumn.sort) {
+            const sortDirection = sortColumn.sort.direction === SortDirection.ASC ? OrderDirection.ASC : OrderDirection.DESC;
+
+            // Use createdAt as a secondary sort column so we break ties consistently.
+            return order
+                .addOrder(sortDirection, sortColumn.sort.field)
+                .addOrder(OrderDirection.DESC, "createdAt")
+                .getOrder();
+        } else {
+            return order.getOrder();
+        }
+    };
+
+    getFilters(dataExplorer: DataExplorer): string {
+        return new FilterBuilder()
+            .addEqual('group_class', GroupClass.ROLE)
+            .addILike('name', dataExplorer.searchValue)
+            .getFilters();
+    }
+
+    getParams(dataExplorer: DataExplorer): ListArguments {
+        return {
+            ...dataExplorerToListParams(dataExplorer),
+            filters: this.getFilters(dataExplorer),
+            order: this.getOrder(dataExplorer),
+        };
+    }
+
     async requestItems(api: MiddlewareAPI<Dispatch, RootState>) {
         const dataExplorer = getDataExplorer(api.getState().dataExplorer, this.getId());
         if (!dataExplorer) {
@@ -27,32 +60,14 @@ export class GroupsPanelMiddlewareService extends DataExplorerMiddlewareService 
         } else {
             try {
                 api.dispatch(progressIndicatorActions.START_WORKING(this.getId()));
-                const sortColumn = getSortColumn<GroupResource>(dataExplorer);
-                const order = new OrderBuilder<GroupResource>();
-                if (sortColumn && sortColumn.sort) {
-                    const direction =
-                        sortColumn.sort.direction === SortDirection.ASC
-                            ? OrderDirection.ASC
-                            : OrderDirection.DESC;
-                    order.addOrder(direction, sortColumn.sort.field);
-                }
-                const filters = new FilterBuilder()
-                    .addEqual('group_class', GroupClass.ROLE)
-                    .addILike('name', dataExplorer.searchValue)
-                    .getFilters();
-                const groups = await this.services.groupsService
-                    .list({
-                        ...dataExplorerToListParams(dataExplorer),
-                        filters,
-                        order: order.getOrder(),
-                    });
+                const groups = await this.services.groupsService.list(this.getParams(dataExplorer));
                 api.dispatch(updateResources(groups.items));
                 api.dispatch(GroupsPanelActions.SET_ITEMS({
                     ...listResultsToDataExplorerItemsMeta(groups),
                     items: groups.items.map(item => item.uuid),
                 }));
 
-                // Get group member count
+                // Get group member counts
                 groups.items.map(group => (
                     this.services.permissionService.list({
                         limit: 0,
