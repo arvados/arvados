fd434c97e981b6018c1eaeae871204e3d31651db
Stephen Smith <stephen@curii.com>
2024-07-23 21:00:36 -0400

    17074: Cleanup and refactor trash middleware
    Arvados-DCO-1.1-Signed-off-by: Stephen Smith <stephen@curii.com>

--- services/workbench2/src/store/trash-panel/trash-panel-middleware-service.ts
+++ services/workbench2/src/store/trash-panel/trash-panel-middleware-service.ts
@@ -15,7 +15,7 @@ import { FilterBuilder } from "services/api/filter-builder";
 import { trashPanelActions } from "./trash-panel-action";
 import { Dispatch, MiddlewareAPI } from "redux";
 import { OrderBuilder, OrderDirection } from "services/api/order-builder";
-import { GroupContentsResource, GroupContentsResourcePrefix } from "services/groups-service/groups-service";
+import { ContentsArguments, GroupContentsResource, GroupContentsResourcePrefix } from "services/groups-service/groups-service";
 import { TrashPanelColumnNames } from "views/trash-panel/trash-panel";
 import { updateFavorites } from "store/favorites/favorites-actions";
 import { updatePublicFavorites } from 'store/public-favorites/public-favorites-actions';
@@ -36,38 +36,15 @@ export class TrashPanelMiddlewareService extends DataExplorerMiddlewareService {
 
     async requestItems(api: MiddlewareAPI<Dispatch, RootState>) {
         const dataExplorer = api.getState().dataExplorer[this.getId()];
-        const columns = dataExplorer.columns as DataColumns<string, CollectionResource>;
-
-        const typeFilters = serializeResourceTypeFilters(getDataExplorerColumnFilters(columns, TrashPanelColumnNames.TYPE));
-
-        const otherFilters = new FilterBuilder()
-            .addILike("name", dataExplorer.searchValue, GroupContentsResourcePrefix.COLLECTION)
-            // .addILike("name", dataExplorer.searchValue, GroupContentsResourcePrefix.PROCESS)
-            .addILike("name", dataExplorer.searchValue, GroupContentsResourcePrefix.PROJECT)
-            .addEqual("is_trashed", true)
-            .getFilters();
-
-        const filters = joinFilters(
-            typeFilters,
-            otherFilters,
-        );
 
         const userUuid = getUserUuid(api.getState());
         if (!userUuid) { return; }
         try {
             api.dispatch(progressIndicatorActions.START_WORKING(this.getId()));
             const listResults = await this.services.groupsService
-                .contents('', {
-                    ...dataExplorerToListParams(dataExplorer),
-                    order: getOrder(dataExplorer),
-                    filters,
-                    recursive: true,
-                    includeTrash: true
-                });
-            api.dispatch(progressIndicatorActions.PERSIST_STOP_WORKING(this.getId()));
+                .contents('', getParams(dataExplorer));
 
             const items = listResults.items.map(it => it.uuid);
-
             api.dispatch(trashPanelActions.SET_ITEMS({
                 ...listResultsToDataExplorerItemsMeta(listResults),
                 items
@@ -76,7 +53,6 @@ export class TrashPanelMiddlewareService extends DataExplorerMiddlewareService {
             api.dispatch<any>(updatePublicFavorites(items));
             api.dispatch(updateResources(listResults.items));
         } catch (e) {
-            api.dispatch(progressIndicatorActions.PERSIST_STOP_WORKING(this.getId()));
             api.dispatch(trashPanelActions.SET_ITEMS({
                 items: [],
                 itemsAvailable: 0,
@@ -84,6 +60,8 @@ export class TrashPanelMiddlewareService extends DataExplorerMiddlewareService {
                 rowsPerPage: dataExplorer.rowsPerPage
             }));
             api.dispatch(couldNotFetchTrashContents());
+        } finally {
+            api.dispatch(progressIndicatorActions.PERSIST_STOP_WORKING(this.getId()));
         }
         api.dispatch<any>(removeDisabledButton(ContextMenuActionNames.MOVE_TO_TRASH))
     }
@@ -107,6 +85,30 @@ const getOrder = (dataExplorer: DataExplorer) => {
         return order.getOrder();
     }
 };
+
+const getFilters = (dataExplorer: DataExplorer) => {
+    const columns = dataExplorer.columns as DataColumns<string, CollectionResource>;
+    const typeFilters = serializeResourceTypeFilters(getDataExplorerColumnFilters(columns, TrashPanelColumnNames.TYPE));
+
+    const otherFilters = new FilterBuilder()
+        .addILike("name", dataExplorer.searchValue, GroupContentsResourcePrefix.COLLECTION)
+        .addILike("name", dataExplorer.searchValue, GroupContentsResourcePrefix.PROJECT)
+        .addEqual("is_trashed", true)
+        .getFilters();
+
+    return joinFilters(
+        typeFilters,
+        otherFilters,
+    );
+};
+
+const getParams = (dataExplorer: DataExplorer): ContentsArguments => ({
+    ...dataExplorerToListParams(dataExplorer),
+    order: getOrder(dataExplorer),
+    filters: getFilters(dataExplorer),
+    recursive: true,
+    includeTrash: true,
+});
 
 const couldNotFetchTrashContents = () =>
     snackbarActions.OPEN_SNACKBAR({
