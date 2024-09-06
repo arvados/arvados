7d75776fd6377100862f911da968193177d12007
Lisa Knox <lisa.knox@curii.com>
2024-09-06 10:09:29 -0400

    21720: converted tree-picker test to cypress
    Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/src/views-components/tree-picker/tree-picker.cy.js
+++ services/workbench2/src/views-components/tree-picker/tree-picker.cy.js
@@ -4,12 +4,9 @@
 
 import React from "react";
 import Axios from "axios";
-import Adapter from "enzyme-adapter-react-16";
-import { configure, mount } from "enzyme";
 import { mockConfig } from "common/config";
-import { ServiceRepository, createServices } from "services/services";
+import { createServices } from "services/services";
 import { createBrowserHistory } from "history";
-import { ApiActions } from "services/api/api-actions";
 import { Provider } from "react-redux";
 import { configureStore } from "store/store";
 import { TreePicker } from "./tree-picker";
@@ -17,135 +14,141 @@ import { initUserProject, receiveTreePickerData, extractGroupContentsNodeData } 
 import { authActions } from "store/auth/auth-action";
 import { ResourceKind } from "models/resource";
 import { updateResources } from "store/resources/resources-actions";
-
-configure({ adapter: new Adapter() });
+import { CustomTheme } from "common/custom-theme";
+import { ThemeProvider } from "@mui/material";
 
 describe('<TreePicker />', () => {
-    // let store;
-    // let services: ServiceRepository;
-    // const axiosInst = Axios.create({ headers: {} });
-    // const config: any = {};
-    // const actions: ApiActions = {
-    //     progressFn: (id: string, working: boolean) => { },
-    //     errorFn: (id: string, message: string) => { }
-    // };
-    // const TEST_PICKER_ID = 'testPickerId';
-    // const fakeUser = {
-    //     email: "test@test.com",
-    //     firstName: "John",
-    //     lastName: "Doe",
-    //     uuid: "zzzzz-tpzed-xurymjxw79nv3jz",
-    //     ownerUuid: "ownerUuid",
-    //     username: "username",
-    //     prefs: {},
-    //     isAdmin: false,
-    //     isActive: true,
-    //     canWrite: false,
-    //     canManage: false,
-    // };
-    // const renderItem = (item) => (
-    //     <li data-id={item.id}>{item.data.name}</li>
-    // );
+    let store;
+    let services;
+    const axiosInst = Axios.create({ headers: {} });
+    const config = {};
+    const actions = {
+        progressFn: (id, working) => { },
+        errorFn: (id, message) => { }
+    };
+    const TEST_PICKER_ID = 'testPickerId';
+    const fakeUser = {
+        email: "test@test.com",
+        firstName: "John",
+        lastName: "Doe",
+        uuid: "zzzzz-tpzed-xurymjxw79nv3jz",
+        ownerUuid: "ownerUuid",
+        username: "username",
+        prefs: {},
+        isAdmin: false,
+        isActive: true,
+        canWrite: false,
+        canManage: false,
+    };
+    const renderItem = (item) => (
+        <li data-id={item.id}>{item.data.name}</li>
+    );
 
-    // beforeEach(() => {
-    //     services = createServices(mockConfig({}), actions, axiosInst);
-    //     store = configureStore(createBrowserHistory(), services, config);
-    //     store.dispatch(authActions.USER_DETAILS_SUCCESS(fakeUser));
-    //     store.dispatch(initUserProject(TEST_PICKER_ID));
-    // });
+    beforeEach(() => {
+        services = createServices(mockConfig({}), actions, axiosInst);
+        store = configureStore(createBrowserHistory(), services, config);
+        store.dispatch(authActions.USER_DETAILS_SUCCESS(fakeUser));
+        store.dispatch(initUserProject(TEST_PICKER_ID));
+    });
 
-    // it("renders tree picker with initial home project state", () => {
-    //     let treePicker = mount(
-    //         <Provider store={store}>
-    //             <TreePicker
-    //                 pickerId={TEST_PICKER_ID}
-    //                 render={renderItem}
-    //                 onContextMenu={() => {}}
-    //                 toggleItemOpen={() => {}}
-    //                 toggleItemActive={() => {}}
-    //                 toggleItemSelection={() => {}}
-    //             />
-    //         </Provider>);
+    it("renders tree picker with initial home project state", () => {
+        cy.mount(
+            <Provider store={store}>
+              <ThemeProvider theme={CustomTheme}>
+                <TreePicker
+                    pickerId={TEST_PICKER_ID}
+                    render={renderItem}
+                    onContextMenu={() => {}}
+                    toggleItemOpen={() => {}}
+                    toggleItemActive={() => {}}
+                    toggleItemSelection={() => {}}
+                />
+                </ThemeProvider>
+            </Provider>);
 
-    //     expect(treePicker.find(`li[data-id="${fakeUser.uuid}"]`).text()).toBe('Home Projects');
-    // });
+        cy.get(`li[data-id="${fakeUser.uuid}"]`).should('have.text', 'Home Projects');
+    });
 
-    // it("displays item loaded into treePicker store", () => {
-    //     const fakeProject = {
-    //         uuid: "zzzzz-j7d0g-111111111111111",
-    //         name: "FakeProject",
-    //         kind: ResourceKind.PROJECT,
-    //     };
+    it("displays item loaded into treePicker store", () => {
+        const fakeProject = {
+            uuid: "zzzzz-j7d0g-111111111111111",
+            name: "FakeProject",
+            kind: ResourceKind.PROJECT,
+        };
 
-    //     store.dispatch(receiveTreePickerData({
-    //         id: fakeUser.uuid,
-    //         pickerId: TEST_PICKER_ID,
-    //         data: [fakeProject],
-    //         extractNodeData: extractGroupContentsNodeData(false)
-    //     }));
+        store.dispatch(receiveTreePickerData({
+            id: fakeUser.uuid,
+            pickerId: TEST_PICKER_ID,
+            data: [fakeProject],
+            extractNodeData: extractGroupContentsNodeData(false)
+        }));
 
-    //     let treePicker = mount(
-    //         <Provider store={store}>
-    //             <TreePicker
-    //                 pickerId={TEST_PICKER_ID}
-    //                 render={renderItem}
-    //                 onContextMenu={() => {}}
-    //                 toggleItemOpen={() => {}}
-    //                 toggleItemActive={() => {}}
-    //                 toggleItemSelection={() => {}}
-    //             />
-    //         </Provider>);
+        cy.mount(
+            <Provider store={store}>
+              <ThemeProvider theme={CustomTheme}>
+                <TreePicker
+                    pickerId={TEST_PICKER_ID}
+                    render={renderItem}
+                    onContextMenu={() => {}}
+                    toggleItemOpen={() => {}}
+                    toggleItemActive={() => {}}
+                    toggleItemSelection={() => {}}
+                />
+                </ThemeProvider>
+            </Provider>);
 
-    //     expect(treePicker.find(`[data-id="${fakeUser.uuid}"]`).text()).toBe('Home Projects');
-    //     expect(treePicker.find(`[data-id="${fakeProject.uuid}"]`).text()).toBe('FakeProject');
-    // });
+        cy.get(`li[data-id="${fakeUser.uuid}"]`).should('have.text', 'Home Projects');
+        cy.get(`[data-id="${fakeProject.uuid}"]`).should('have.text', 'FakeProject');
+    });
 
-    // it("preserves treenode name when exists in resources", () => {
-    //     const treeProjectResource = {
-    //         uuid: "zzzzz-j7d0g-111111111111111",
-    //         name: "FakeProject",
-    //         kind: ResourceKind.PROJECT,
-    //     };
-    //     const treeProjectResource2 = {
-    //         uuid: "zzzzz-j7d0g-222222222222222",
-    //         name: "",
-    //         kind: ResourceKind.PROJECT,
-    //     };
+    it("preserves treenode name when exists in resources", () => {
+        const treeProjectResource = {
+            uuid: "zzzzz-j7d0g-111111111111111",
+            name: "FakeProject",
+            kind: ResourceKind.PROJECT,
+        };
+        const treeProjectResource2 = {
+            uuid: "zzzzz-j7d0g-222222222222222",
+            name: "",
+            kind: ResourceKind.PROJECT,
+        };
 
-    //     const storeProjectResource = {
-    //         ...treeProjectResource,
-    //         name: "StoreProjectName",
-    //         description: "Test description",
-    //     };
-    //     const storeProjectResource2 = {
-    //         ...treeProjectResource2,
-    //         name: "StoreProjectName2",
-    //         description: "Test description",
-    //     };
+        const storeProjectResource = {
+            ...treeProjectResource,
+            name: "StoreProjectName",
+            description: "Test description",
+        };
+        const storeProjectResource2 = {
+            ...treeProjectResource2,
+            name: "StoreProjectName2",
+            description: "Test description",
+        };
 
-    //     store.dispatch(updateResources([storeProjectResource, storeProjectResource2]));
-    //     store.dispatch(receiveTreePickerData({
-    //         id: fakeUser.uuid,
-    //         pickerId: TEST_PICKER_ID,
-    //         data: [treeProjectResource, treeProjectResource2],
-    //         extractNodeData: extractGroupContentsNodeData(false)
-    //     }));
+        store.dispatch(updateResources([storeProjectResource, storeProjectResource2]));
+        store.dispatch(receiveTreePickerData({
+            id: fakeUser.uuid,
+            pickerId: TEST_PICKER_ID,
+            data: [treeProjectResource, treeProjectResource2],
+            extractNodeData: extractGroupContentsNodeData(false)
+        }));
 
-    //     let treePicker = mount(
-    //         <Provider store={store}>
-    //             <TreePicker
-    //                 pickerId={TEST_PICKER_ID}
-    //                 render={renderItem}
-    //                 onContextMenu={() => {}}
-    //                 toggleItemOpen={() => {}}
-    //                 toggleItemActive={() => {}}
-    //                 toggleItemSelection={() => {}}
-    //             />
-    //         </Provider>);
+        cy.mount(
+            <Provider store={store}>
+              <ThemeProvider theme={CustomTheme}>
+                <TreePicker
+                    pickerId={TEST_PICKER_ID}
+                    render={renderItem}
+                    onContextMenu={() => {}}
+                    toggleItemOpen={() => {}}
+                    toggleItemActive={() => {}}
+                    toggleItemSelection={() => {}}
+                />
+                </ThemeProvider>
+            </Provider>);
 
-    //     expect(treePicker.find(`[data-id="${fakeUser.uuid}"]`).text()).toBe('Home Projects');
-    //     expect(treePicker.find(`[data-id="${treeProjectResource.uuid}"]`).text()).toBe('FakeProject');
-    //     expect(treePicker.find(`[data-id="${treeProjectResource2.uuid}"]`).text()).toBe('');
-    // });
+        cy.get(`[data-id="${fakeUser.uuid}"]`).should('have.text', 'Home Projects');
+        cy.get(`[data-id="${treeProjectResource.uuid}"]`).should('have.text', 'FakeProject');
+        cy.get(`[data-id="${treeProjectResource2.uuid}"]`).should('have.text', '');
+    });
 
 });
