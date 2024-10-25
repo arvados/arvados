74edc1db8f9420ac7c4ed3b6c18e55d44a1dcbcd
Lisa Knox <lisa.knox@curii.com>
2024-10-25 11:46:25 -0400

    15798: added sorting to ensure consistent coloring
    Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/src/store/auth/cluster-badges.ts
+++ services/workbench2/src/store/auth/cluster-badges.ts
@@ -5,6 +5,7 @@
 import { Dispatch } from 'redux';
 import { Session } from 'models/session';
 import { propertiesActions } from 'store/properties/properties-actions';
+import { sortByProperty } from 'common/array-utils';
 
 export type ClusterBadge = {
     text: string,
@@ -14,10 +15,12 @@ export type ClusterBadge = {
 
 export const initClusterBadges = (sessions: Session[]) => (dispatch: Dispatch) => {
 
+    const sortedSessions = [...sessions].sort(sortByProperty('name'));
+
     const bgColors = [
-        '#2e8b57', // sea green
         '#000fd0', // royal blue
         '#fb6b1c', // orange
+        '#2e8b57', // sea green
         '#580082', // purple
         '#733e07', // brown
         '#961e0a', // dark red
@@ -26,10 +29,10 @@ export const initClusterBadges = (sessions: Session[]) => (dispatch: Dispatch) =
         '#c1802f', // tan
         '#1e90ff', // light blue
         '#972be2', // violet
-        '#ecc700', // mustard yellow
+        '#baa844', // mustard yellow
     ];
-
-    const badges: ClusterBadge[] = sessions.map((session, i) => {
+    
+    const badges: ClusterBadge[] = sortedSessions.map((session, i) => {
         const color = bgColors[i % bgColors.length];
         return {
             text: session.clusterId,
