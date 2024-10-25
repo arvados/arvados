9108953b2d86508eccc4778fd51ac0e21f087101
Lisa Knox <lisa.knox@curii.com>
2024-10-25 11:32:19 -0400

    15798: set up cluster badge renderer
    Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/src/store/auth/cluster-badges.ts
+++ services/workbench2/src/store/auth/cluster-badges.ts
@@ -6,24 +6,30 @@ import { Dispatch } from 'redux';
 import { Session } from 'models/session';
 import { propertiesActions } from 'store/properties/properties-actions';
 
+export type ClusterBadge = {
+    text: string,
+    color: string,
+    backgroundColor: string
+}
+
 export const initClusterBadges = (sessions: Session[]) => (dispatch: Dispatch) => {
 
     const bgColors = [
-        '000fd0', // royal blue
-        'fb6b1c', // orange
-        '580082', // purple
-        '733e07', // brown
-        '2e8b57', // sea green
-        '961e0a', // dark red
-        'ff49b4', // pink
-        '00c6c9', // turquoise
-        'c1802f', // tan
-        '1e90ff', // light blue
-        '972be2', // violet
-        'ecc700', // mustard yellow
+        '#2e8b57', // sea green
+        '#000fd0', // royal blue
+        '#fb6b1c', // orange
+        '#580082', // purple
+        '#733e07', // brown
+        '#961e0a', // dark red
+        '#ff49b4', // pink
+        '#00c6c9', // turquoise
+        '#c1802f', // tan
+        '#1e90ff', // light blue
+        '#972be2', // violet
+        '#ecc700', // mustard yellow
     ];
 
-    const badges = sessions.map((session, i) => {
+    const badges: ClusterBadge[] = sessions.map((session, i) => {
         const color = bgColors[i % bgColors.length];
         return {
             text: session.clusterId,
@@ -32,6 +38,5 @@ export const initClusterBadges = (sessions: Session[]) => (dispatch: Dispatch) =
         };
     });
 
-    console.log(badges);
     dispatch(propertiesActions.SET_PROPERTY({ key: 'clusterBadges', value: badges }));
 };
