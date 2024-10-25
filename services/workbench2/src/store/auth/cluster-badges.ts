58a530c79750cba2a9fbcf91381a58b20ed689d3
Lisa Knox <lisa.knox@curii.com>
2024-10-25 10:34:37 -0400

    15798: added cluster colors to redux store
    Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

---
+++ services/workbench2/src/store/auth/cluster-badges.ts
@@ -0,0 +1,37 @@
+// Copyright (C) The Arvados Authors. All rights reserved.
+//
+// SPDX-License-Identifier: AGPL-3.0
+
+import { Dispatch } from 'redux';
+import { Session } from 'models/session';
+import { propertiesActions } from 'store/properties/properties-actions';
+
+export const initClusterBadges = (sessions: Session[]) => (dispatch: Dispatch) => {
+
+    const bgColors = [
+        '000fd0', // royal blue
+        'fb6b1c', // orange
+        '580082', // purple
+        '733e07', // brown
+        '2e8b57', // sea green
+        '961e0a', // dark red
+        'ff49b4', // pink
+        '00c6c9', // turquoise
+        'c1802f', // tan
+        '1e90ff', // light blue
+        '972be2', // violet
+        'ecc700', // mustard yellow
+    ];
+
+    const badges = sessions.map((session, i) => {
+        const color = bgColors[i % bgColors.length];
+        return {
+            text: session.clusterId,
+            color: '#fff',
+            backgroundColor: color,
+        };
+    });
+
+    console.log(badges);
+    dispatch(propertiesActions.SET_PROPERTY({ key: 'clusterBadges', value: badges }));
+};
