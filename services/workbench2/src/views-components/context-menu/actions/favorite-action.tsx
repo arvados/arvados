f294f432d27497a1c2a088cf535d328842c617a6
Lisa Knox <lisa.knox@curii.com>
2025-01-22 10:40:38 -0500

    22051: updated favorite action to use component
    Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/workbench2/src/views-components/context-menu/actions/favorite-action.tsx
+++ services/workbench2/src/views-components/context-menu/actions/favorite-action.tsx
@@ -3,28 +3,52 @@
 // SPDX-License-Identifier: AGPL-3.0
 
 import React from "react";
-import { ListItemIcon, ListItemText, ListItem } from "@mui/material";
+import { ListItemIcon, ListItemText, ListItem, Tooltip } from "@mui/material";
 import { AddFavoriteIcon, RemoveFavoriteIcon } from "components/icon/icon";
 import { connect } from "react-redux";
 import { RootState } from "store/store";
+import { FavoritesState } from "store/favorites/favorites-reducer";
+import { ContextMenuResource } from "store/context-menu/context-menu-actions";
 
-const mapStateToProps = (state: RootState, props: { onClick: () => {} }) => ({
-    isFavorite: state.contextMenu.resource !== undefined && state.favorites[state.contextMenu.resource.uuid] === true,
-    onClick: props.onClick
+const toolbarIconClass = {
+    width: '1rem',
+    marginLeft: '-0.5rem',
+    marginTop: '0.15rem',
+}
+
+const mapStateToProps = (state: RootState) => ({
+    contextMenuResource: state.contextMenu.resource,
+    selectedResourceUuid: state.selectedResourceUuid,
+    favorites: state.favorites,
 });
 
-export const ToggleFavoriteAction = connect(mapStateToProps)((props: { isFavorite: boolean, onClick: () => void }) =>
-    <ListItem
-        button
-        onClick={props.onClick}>
-        <ListItemIcon>
-            {props.isFavorite
-                ? <RemoveFavoriteIcon />
-                : <AddFavoriteIcon />}
-        </ListItemIcon>
-        <ListItemText style={{ textDecoration: 'none' }}>
-            {props.isFavorite
-                ? <>Remove from favorites</>
-                : <>Add to favorites</>}
-        </ListItemText>
-    </ListItem >);
+type ToggleFavoriteActionProps = {
+    isInToolbar?: boolean,
+    contextMenuResource?: ContextMenuResource,
+    selectedResourceUuid?: string,
+    favorites: FavoritesState,
+    onClick: () => void
+}
+
+export const ToggleFavoriteAction = connect(mapStateToProps)((props: ToggleFavoriteActionProps) => {
+    const faveResourceUuid = props.isInToolbar ? props.selectedResourceUuid : props.contextMenuResource?.uuid;
+    const isFavorite = faveResourceUuid !== undefined && props.favorites[faveResourceUuid] === true;
+
+    return <Tooltip title={isFavorite ? "Remove from favorites" : "Add to favorites"}>
+        <ListItem
+            button
+            onClick={props.onClick}>
+            <ListItemIcon style={props.isInToolbar ? toolbarIconClass : {}}>
+                {isFavorite
+                    ? <RemoveFavoriteIcon />
+                    : <AddFavoriteIcon />}
+            </ListItemIcon>
+            {!props.isInToolbar &&
+                <ListItemText style={{ textDecoration: 'none' }}>
+                    {isFavorite
+                        ? <>Remove from favorites</>
+                        : <>Add to favorites</>}
+                </ListItemText>}
+        </ListItem >
+    </Tooltip>
+});
