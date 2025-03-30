1525ee42301bd64f63304721c4d6cf6f2ee7d200
Stephen Smith <stephen@curii.com>
2025-03-30 19:38:01 -0400

    22395: Remove unused toggleProjectTrashed
    Arvados-DCO-1.1-Signed-off-by: Stephen Smith <stephen@curii.com>

--- services/workbench2/src/store/trash/trash-actions.ts
+++ services/workbench2/src/store/trash/trash-actions.ts
@@ -5,89 +5,19 @@
 import { Dispatch } from "redux";
 import { RootState } from "store/store";
 import { ServiceRepository } from "services/services";
-import { snackbarActions, SnackbarKind } from "store/snackbar/snackbar-actions";
 import { trashPanelActions } from "store/trash-panel/trash-panel-action";
 import { activateSidePanelTreeItem, loadSidePanelTreeProjects, SidePanelTreeCategory } from "store/side-panel-tree/side-panel-tree-actions";
 import { projectPanelDataActions } from "store/project-panel/project-panel-action-bind";
 import { sharedWithMePanelActions } from "store/shared-with-me-panel/shared-with-me-panel-actions";
 import { extractUuidKind, ResourceKind } from "models/resource";
-import { navigateTo, navigateToTrash } from "store/navigation/navigation-action";
+import { navigateTo } from "store/navigation/navigation-action";
 import { matchFavoritesRoute, matchProjectRoute, matchSharedWithMeRoute, matchTrashRoute } from "routes/routes";
 import { ContextMenuActionNames } from "views-components/context-menu/context-menu-action-set";
 import { addDisabledButton } from "store/multiselect/multiselect-actions";
 import { showGroupedCommonResourceResultSnackbars, updateResources } from "store/resources/resources-actions";
-import { GroupResource } from "models/group";
 import { favoritePanelActions } from "store/favorite-panel/favorite-panel-action";
 import { CommonResourceServiceError } from "services/common-service/common-resource-service";
 import _ from "lodash";
-
-export const toggleProjectTrashed =
-    (uuid: string, ownerUuid: string, isTrashed: boolean, isMulti: boolean) =>
-        async (dispatch: Dispatch, getState: () => RootState, services: ServiceRepository): Promise<any> => {
-            let errorMessage = "";
-            let successMessage = "";
-            let toggledResource: GroupResource | undefined = undefined;
-            dispatch<any>(addDisabledButton(ContextMenuActionNames.MOVE_TO_TRASH));
-            try {
-                if (isTrashed) {
-                    errorMessage = "Could not restore project from trash";
-                    successMessage = "Restored project from trash";
-                    toggledResource = await services.groupsService.untrash(uuid);
-                    if (toggledResource) {
-                        // Resource store must be updated with trashed flag for favorites tree to hide trashed
-                        await dispatch<any>(updateResources([toggledResource]));
-                    }
-                    dispatch<any>(isMulti || !toggledResource ? navigateToTrash : navigateTo(uuid));
-                    dispatch<any>(activateSidePanelTreeItem(uuid));
-                    dispatch<any>(loadSidePanelTreeProjects(SidePanelTreeCategory.FAVORITES));
-                } else {
-                    errorMessage = "Could not move project to trash";
-                    successMessage = "Added project to trash";
-                    toggledResource = await services.groupsService.trash(uuid);
-                    if (toggledResource) {
-                        // Resource store must be updated with trashed flag for favorites tree to hide trashed
-                        await dispatch<any>(updateResources([toggledResource]));
-                    }
-                    // Refresh favorites tree after trash/untrash
-                    // must be await to avoid race conditions with the next loadSidePanelTreeProjects
-                    await dispatch<any>(loadSidePanelTreeProjects(SidePanelTreeCategory.FAVORITES));
-                    dispatch<any>(loadSidePanelTreeProjects(ownerUuid));
-
-                    const { location } = getState().router;
-                    if (matchSharedWithMeRoute(location ? location.pathname : "")) {
-                        dispatch(sharedWithMePanelActions.REQUEST_ITEMS());
-                    }
-                    else {
-                        dispatch<any>(navigateTo(ownerUuid));
-                    }
-                }
-                if (toggledResource) {
-                        dispatch(
-                        snackbarActions.OPEN_SNACKBAR({
-                            message: successMessage,
-                            hideDuration: 2000,
-                            kind: SnackbarKind.SUCCESS,
-                        })
-                    );
-                }
-            } catch (e) {
-                if (e.status === 422) {
-                    dispatch(
-                        snackbarActions.OPEN_SNACKBAR({
-                            message: "Could not restore project from trash: Duplicate name at destination",
-                            kind: SnackbarKind.ERROR,
-                        })
-                    );
-                } else {
-                    dispatch(
-                        snackbarActions.OPEN_SNACKBAR({
-                            message: errorMessage,
-                            kind: SnackbarKind.ERROR,
-                        })
-                    );
-                }
-            }
-        };
 
 /**
  * Toggles the trash status of an array of UUIDS based on the current isTrashed status
