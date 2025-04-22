ff5613a55b98dfe1e6714385cd710ec2d83c3c9a
Stephen Smith <stephen@curii.com>
2025-04-22 12:47:28 -0400

    22394: Fix prefs page styling and display project tab options as row
    Arvados-DCO-1.1-Signed-off-by: Stephen Smith <stephen@curii.com>

--- services/workbench2/src/views/user-preferences-panel/user-preferences-panel-root.tsx
+++ services/workbench2/src/views/user-preferences-panel/user-preferences-panel-root.tsx
@@ -21,36 +21,23 @@ import { ResourcesState } from 'store/resources/resources';
 import { ProjectPanelTabLabels } from 'store/project-panel/project-panel-action';
 import { RadioField } from 'components/radio-field/radio-field';
 
-type CssRules = 'root' | 'emptyRoot' | 'gridItem' | 'label' | 'readOnlyValue' | 'title' | 'actions';
+type CssRules = 'root' | 'gridItem' | 'label' | 'title';
 
 const styles: CustomStyleRulesCallback<CssRules> = (theme: ArvadosTheme) => ({
     root: {
         width: '100%',
         overflow: 'auto'
     },
-    emptyRoot: {
-        width: '100%',
-        overflow: 'auto',
-        padding: theme.spacing(4),
-    },
     gridItem: {
-        height: 45,
         marginBottom: 20
     },
     label: {
         fontSize: '0.8rem',
         color: theme.palette.grey['600']
     },
-    readOnlyValue: {
-        fontSize: '0.875rem',
-    },
     title: {
         fontSize: '1.1rem',
     },
-    actions: {
-        display: 'flex',
-        justifyContent: 'flex-end'
-    }
 });
 
 export interface UserPreferencesPanelRootDataProps {
@@ -62,24 +49,49 @@ export interface UserPreferencesPanelRootDataProps {
 
 type UserPreferencesPanelRootProps = InjectedFormProps<{}> & UserPreferencesPanelRootDataProps & DispatchProp & WithStyles<CssRules>;
 
+const ProjectPanelDefaultTabOptions = Object.keys(ProjectPanelTabLabels).map((key) => ({
+    key: ProjectPanelTabLabels[key],
+    value: ProjectPanelTabLabels[key],
+}));
+
 export const UserPreferencesPanelRoot = withStyles(styles)(
     class extends React.Component<UserPreferencesPanelRootProps> {
         render() {
             return (
                 <Paper className={this.props.classes.root}>
-                    <CardContent>
-                        <Grid container justifyContent="space-between">
-                            <Grid item>
-                                <Typography className={this.props.classes.title}>
-                                    User Preferences
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                        <form onSubmit={this.props.handleSubmit} data-cy="profile-form">
+                    <form onSubmit={this.props.handleSubmit} data-cy="preferences-form">
+                        <CardContent>
                             <Grid container spacing={3}>
+                                <Grid item>
+                                    <Typography className={this.props.classes.title}>
+                                        Project Settings
+                                    </Typography>
+                                </Grid>
+                                <Grid item className={this.props.classes.gridItem} sm={12}>
+                                    <InputLabel className={this.props.classes.label} htmlFor="prefs.wb.default_project_tab">Default Project Tab</InputLabel>
+                                    <Field
+                                        id="prefs.wb.default_project_tab"
+                                        name="prefs.wb.default_project_tab"
+                                        component={RadioField as any}
+                                        items={ProjectPanelDefaultTabOptions}
+                                        flexRowDirection
+                                    />
+                                </Grid>
+                                <Grid item sm={12}>
+                                    <Grid container direction="row" justifyContent="flex-end">
+                                        <Button color="primary" onClick={this.props.reset} disabled={this.props.isPristine}>Discard changes</Button>
+                                        <Button
+                                            color="primary"
+                                            variant="contained"
+                                            type="submit"
+                                            disabled={this.props.isPristine || this.props.invalid || this.props.submitting}>
+                                            Save changes
+                                        </Button>
+                                    </Grid>
+                                </Grid>
                             </Grid>
-                        </form >
-                    </CardContent>
+                        </CardContent>
+                    </form >
                 </Paper >
             );
         }
