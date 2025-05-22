36aee5b080f7ebc0b013992f566da8d3e5ece0fe
Lucas Di Pentima <lucas.dipentima@curii.com>
2025-05-22 12:29:03 -0300

    22779: Points installer to newer arvados-formula commit
    This new commit implements a-d-c service envvars overrides to
    avoid it to use any AWS credentials from ~/.aws/ and only
    rely on instance profile permissions.
    
    Arvados-DCO-1.1-Signed-off-by: Lucas Di Pentima <lucas.dipentima@curii.com>

--- tools/salt-install/provision.sh
+++ tools/salt-install/provision.sh
@@ -287,7 +287,7 @@ VERSION="latest"
 SALT_VERSION="3006"
 
 # Other formula versions we depend on
-ARVADOS_TAG="eed69116c1bcaf46474b7ff7c93383eb855695b4"
+ARVADOS_TAG="928762f4c2b3d47ce15b33fe1e8528b7e5a527e9"
 POSTGRES_TAG="a809e03bad115bbdf24ad347e2dc9a52e144c31f"
 POSTGRES_URL="https://github.com/arvados/postgres-formula.git"
 NGINX_TAG="v2.8.1"
