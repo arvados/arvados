9b594d9f8624e903eb3fd841801df0250c9d47f5
Tom Clegg <tom@curii.com>
2025-06-24 10:22:29 -0400

    22974: Override Rack::QueryParser default request size limit.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@curii.com>

--- services/api/config/boot.rb
+++ services/api/config/boot.rb
@@ -1,3 +1,14 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
+# Setting an environment variable before loading rack is the only way
+# to change rack's request size limit for an urlencoded POST body.
+# Rack::QueryParser accepts an initialization argument to override the
+# default, but rack only ever uses its global default_parser, and
+# there is no facility for overriding that at runtime.
+#
+# Our strategy is to rely on the more configurable downstream servers
+# (Nginx and arvados-controller) to reject oversized requests before
+# they hit this server at all.
+ENV["RACK_QUERY_PARSER_BYTESIZE_LIMIT"] = (4 << 30).to_s
+
 require "bundler/setup" # Set up gems listed in the Gemfile.
