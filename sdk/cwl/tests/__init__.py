3c900c4b9bced99560decb7fc2a0a0ae1eeaa9fb
Brett Smith <brett.smith@curii.com>
2025-07-19 11:39:16 -0400

    23006: Port CWL conformance+integration tests to pytest
    This removes arvados_tests.sh, simplifies test_with_arvbox.sh, and sets
    the stage to remove it as well.
    
    Arvados-DCO-1.1-Signed-off-by: Brett Smith <brett.smith@curii.com>

--- sdk/cwl/tests/__init__.py
+++ sdk/cwl/tests/__init__.py
@@ -2,4 +2,36 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import subprocess
 
+from pathlib import Path
+
+TESTS_DIR = Path(__file__).parent
+
+def run_cwltest(
+        cwl_test,
+        cwl_tool,
+        badges_dir=None,
+        *,
+        test_args=(),
+        tool_args=(),
+):
+    cmd = [
+        'cwltest',
+        '--test', str(cwl_test),
+        '--tool', str(cwl_tool),
+        '-j', '3',
+    ]
+    cmd.extend(test_args)
+    # FIXME?: cwltest badge generation seems buggy as of 2.5.20241122133319
+    # if badges_dir:
+    #     cmd.append('--badgedir')
+    #     cmd.append(str(badges_dir))
+    cmd.extend([
+        '--',
+        '--compute-checksum',
+        '--disable-reuse',
+        '--enable-dev',
+    ])
+    cmd.extend(tool_args)
+    return subprocess.run(cmd)
