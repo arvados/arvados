6f333274f0ca0606dc4764a36ad4b3989003968c
Brett Smith <brett.smith@curii.com>
2025-07-29 10:04:15 -0400

    Remove KeepClient API
    Arvados-DCO-1.1-Signed-off-by: Krzysztof Majewski <majewski.kielce@gmail.com>

--- contrib/java-sdk-v2/src/main/java/org/arvados/client/api/client/KeepWebApiClient.java
+++ contrib/java-sdk-v2/src/main/java/org/arvados/client/api/client/KeepWebApiClient.java
@@ -87,8 +87,9 @@ public class KeepWebApiClient extends BaseApiClient {
     private HttpUrl.Builder getUrlBuilder(String collectionUuid, String filePathName) {
         return new HttpUrl.Builder()
                 .scheme(config.getApiProtocol())
-                .host(collectionUuid + "." + config.getKeepWebHost())
+                .host(config.getKeepWebHost())
                 .port(config.getKeepWebPort())
+                .addPathSegment("c=" + collectionUuid)
                 .addPathSegment(filePathName);
     }
 }
