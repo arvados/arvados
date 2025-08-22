55cb52d4b921f2dead887a4e2ddc656a456d52ff
Stephen Smith <stephen@curii.com>
2025-08-22 15:19:41 -0400

    22787: Correct data-cy typo
    Arvados-DCO-1.1-Signed-off-by: Stephen Smith <stephen@curii.com>

--- services/workbench2/cypress/e2e/user-preferences.cy.js
+++ services/workbench2/cypress/e2e/user-preferences.cy.js
@@ -24,24 +24,24 @@ describe('User profile tests', function() {
         cy.loginAs(activeUser);
 
         // Verify default tab on load
-        cy.get('[data-cy=process-data]').should('exist');
-        cy.get('[data-cy=process-run]').should('not.exist');
+        cy.get('[data-cy=project-data]').should('exist');
+        cy.get('[data-cy=project-run]').should('not.exist');
 
         // Navigate to project and switch to runs tab
         cy.assertDataExplorerContains(testProjectName, true).click();
         cy.get('[data-cy=mpv-tabs]').contains("Workflow Runs").click();
 
         // Verify tab state
-        cy.get('[data-cy=process-data]').should('not.exist');
-        cy.get('[data-cy=process-run]').should('exist');
+        cy.get('[data-cy=project-data]').should('not.exist');
+        cy.get('[data-cy=project-run]').should('exist');
 
         // Navigate back to home
         cy.doBreadcrumbsNavigation("Home Projects");
 
         // Verify tabs switched back to data and project visible
         cy.assertDataExplorerContains(testProjectName, true);
-        cy.get('[data-cy=process-data]').should('exist');
-        cy.get('[data-cy=process-run]').should('not.exist');
+        cy.get('[data-cy=project-data]').should('exist');
+        cy.get('[data-cy=project-run]').should('not.exist');
 
         // Change default tab preferecne
         cy.doAccountMenuAction("Preferences");
@@ -50,19 +50,19 @@ describe('User profile tests', function() {
 
         // Verify new default tab
         cy.doSidePanelNavigation("Home Projects");
-        cy.get('[data-cy=process-data]').should('not.exist');
-        cy.get('[data-cy=process-run]').should('exist');
+        cy.get('[data-cy=project-data]').should('not.exist');
+        cy.get('[data-cy=project-run]').should('exist');
         cy.assertDataExplorerContains(testProjectName, false);
 
         // Switch to data tab and navigate to project
         cy.get('[data-cy=mpv-tabs]').contains("Data").click();
-        cy.get('[data-cy=process-data]').should('exist');
-        cy.get('[data-cy=process-run]').should('not.exist');
+        cy.get('[data-cy=project-data]').should('exist');
+        cy.get('[data-cy=project-run]').should('not.exist');
         cy.assertDataExplorerContains(testProjectName, true).click();
 
         // Verify switched back to runs and project absent
-        cy.get('[data-cy=process-data]').should('not.exist');
-        cy.get('[data-cy=process-run]').should('exist');
+        cy.get('[data-cy=project-data]').should('not.exist');
+        cy.get('[data-cy=project-run]').should('exist');
         cy.assertDataExplorerContains(testProjectName, false);
 
         // Change default tab preferecne back
