bcbbd49665510c48c3689c84785820d04b732b41
Brett Smith <brett.smith@curii.com>
2025-09-20 11:58:20 -0400

    23152: Copyedit comments
    Fix name typo. Format for easier readability.
    
    Arvados-DCO-1.1-Signed-off-by: Brett Smith <brett.smith@curii.com>

--- services/api/lib/validate_serialized.rb
+++ services/api/lib/validate_serialized.rb
@@ -2,8 +2,8 @@
 #
 # SPDX-License-Identifier: AGPL-3.0
 
-# HashAttrValidator is invoked via "validates :attr_name,
-# array_of_strings: {allow_empty_strings: true}".
+# ArrayOfStringsValidator is invoked via:
+#     validates :attr_name, array_of_strings: {allow_empty_strings: true}
 class ArrayOfStringsValidator < ActiveModel::EachValidator
   def validate_each(record, attr, val)
     errtxt = "must be an array of #{"non-empty " unless options[:allow_empty_strings]}strings"
@@ -20,9 +20,10 @@ class ArrayOfStringsValidator < ActiveModel::EachValidator
   end
 end
 
-# HashAttrValidator is invoked via "validates :attr_name, hash_attr:
-# true".  We don't call it just HashValidator because that conflicts
-# with the hash_validator gem.
+# HashAttrValidator is invoked via:
+#     validates :attr_name, hash_attr: true
+# We don't call it just HashValidator because that conflicts with the
+# hash_validator gem.
 class HashAttrValidator < ActiveModel::EachValidator
   def validate_each(record, attr, val)
     if !val.is_a?(Hash)
