e5d56d8faafdc506b3433fac27bddd9153dd9d60
Stephen Smith <stephen@curii.com>
2025-09-26 14:50:21 -0400

    23026: Move injectTokenParam to common/url along with tests
    Arvados-DCO-1.1-Signed-off-by: Stephen Smith <stephen@curii.com>

--- services/workbench2/src/views-components/details-card/service-menu.cy.js
+++ services/workbench2/src/views-components/details-card/service-menu.cy.js
@@ -4,7 +4,7 @@
 
 import { Provider } from "react-redux";
 import { combineReducers, createStore } from "redux";
-import { ServiceMenu, injectTokenParam } from './service-menu';
+import { ServiceMenu } from './service-menu';
 
 const apiToken = "v2/xxxxx-gj3su-000000000000000/00000000000000000000000000000000000000000000000000";
 
@@ -90,72 +90,5 @@ describe('ServiceMenu', () => {
                     .click();
                 cy.get('@open').should("have.been.calledWith", services[i].expected_url);
             });
-    });
-});
-
-describe('injectTokenParam', () => {
-    it('injects tokens into valid URLs', () => {
-        const testCases = [{
-            // Test normal case
-            url: "http://example.com/",
-            token: apiToken,
-            result: `http://example.com/?arvados_api_token=${apiToken}`,
-        },{
-            // Test no trailing slash - URL constructor will add trailing slash
-            url: "https://example.com",
-            token: "foobar",
-            result: "https://example.com/?arvados_api_token=foobar",
-        },{
-            // Test with basic auth
-            url: "https://user:pass@example.com/",
-            token: "baz",
-            result: "https://user:pass@example.com/?arvados_api_token=baz",
-        },{
-            // Test with existing params
-            url: "https://example.com/?foo=bar",
-            token: "foo123",
-            result: "https://example.com/?arvados_api_token=foo123&foo=bar",
-        },{
-            // Test with existing params and no slash - URL constructor will add slash
-            url: "https://example.com?foo=bar",
-            token: "foo123",
-            result: "https://example.com/?arvados_api_token=foo123&foo=bar",
-        },{
-            // Test with no params but with question mark
-            url: "http://example.com/?",
-            token: "foobar",
-            result: "http://example.com/?arvados_api_token=foobar",
-        }];
-
-        return Promise.all(testCases.map(async testCase => {
-            const result = await injectTokenParam(testCase.url, testCase.token);
-            expect(result).to.equal(testCase.result);
-        }));
-    });
-
-    it('raises exceptions for invalid situations', () => {
-        const invalidCases = [{
-            url: "http://example.com",
-            token: "",
-            msg: "User token required",
-        },{
-            url: "",
-            token: "foo",
-            msg: "URL cannot be empty",
-        }];
-
-        return Promise.all(invalidCases.map(testCase => {
-            const promise = injectTokenParam(testCase.url, testCase.token);
-
-            return promise.then(() => {
-                    throw new Error('Expected injectTokenParam() to return error but it did not. '
-                        + `Expected error: "${testCase.msg}" given url "${testCase.url}" and token "${testCase.token}"`);
-                }, (err) => {
-                    // Verify the promise rejection reason
-                    expect(err).to.equal(testCase.msg);
-                }
-            );
-        }));
-
     });
 });
