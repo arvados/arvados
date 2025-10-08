a106a00181bac107e669207445ceaac868c8a640
Lisa Knox <lisa.knox@curii.com>
2025-10-08 15:26:45 -0400

    23181: refined credential unit test
    Arvados-DCO-1.1-Signed-off-by: Lisa Knox <lisa.knox@curii.com>

--- services/api/test/unit/credential_test.rb
+++ services/api/test/unit/credential_test.rb
@@ -16,41 +16,53 @@ class CredentialTest < ActiveSupport::TestCase
     }
   end
 
-  test "valid credential with all required fields" do
-    credential = Credential.new(@valid_attrs)
-    assert credential.valid?
-  end
-
-  test "invalid without required fields" do
+  test "credential requires required fields" do
     [:name, :credential_class, :external_id, :secret, :expires_at].each do |field|
       attrs = @valid_attrs.dup
+      good_credential = Credential.new(attrs)
+      assert good_credential.valid?
       attrs[field] = nil
-      credential = Credential.new(attrs)
-      refute credential.valid?, "#{field} should be required"
-      assert_includes credential.errors.keys, field
+      bad_credential = Credential.new(attrs)
+      assert_not bad_credential.valid?
     end
   end
 
-  test "scopes defaults to empty array" do
+  test "credential scopes defaults to empty array" do
     credential = Credential.new(@valid_attrs)
     assert_equal [], credential.scopes
   end
 
-  test "logged_attributes excludes secret" do
-    credential = Credential.create!(@valid_attrs.merge(owner_uuid: SecureRandom.uuid))
+  test "credential logged_attributes excludes secret" do
+    credential = nil
+    act_as_system_user do
+      credential = Credential.create!(@valid_attrs.merge(owner_uuid: system_user_uuid))
+    end
     logged_attrs = credential.logged_attributes
+    # required fields should always be logged
+    assert_includes logged_attrs, "name"
+    assert_includes logged_attrs, "credential_class"
+    assert_includes logged_attrs, "external_id"
+    assert_includes logged_attrs, "expires_at"
+
+    # secret should never be logged
     refute logged_attrs.key?("secret")
   end
 
-  test "ensure_owner_uuid_is_permitted sets owner to system_user for new record" do
+  test "credential ensure_owner_uuid_is_permitted sets owner to system_user for new record" do
     credential = Credential.new(@valid_attrs.merge(owner_uuid: SecureRandom.uuid))
     system_uuid = Credential.system_user_uuid
     assert credential.ensure_owner_uuid_is_permitted
     assert_equal system_uuid, credential.owner_uuid
   end
 
-  test "ensure_owner_uuid_is_permitted raises if owner_uuid is not system user" do
-    credential = Credential.create!(@valid_attrs.merge(owner_uuid: SecureRandom.uuid))
+  test "credential ensure_owner_uuid_is_permitted raises if owner_uuid is not system user" do
+    credential = nil
+    act_as_system_user do
+      credential = Credential.create!(@valid_attrs.merge(owner_uuid: system_user_uuid))
+    end
+    assert_nothing_raised do
+      credential.ensure_owner_uuid_is_permitted
+    end
     credential.owner_uuid = "some_other_uuid"
     assert_raises RuntimeError do
       credential.ensure_owner_uuid_is_permitted
