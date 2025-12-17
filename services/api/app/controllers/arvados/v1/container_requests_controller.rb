367af1bd07bcc89973bfd4e6a3a3d99a1c17e418
Tom Clegg <tom@curii.com>
2025-12-17 14:48:10 -0500

    23382: Skip priority updates/locks for finished reused containers.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@curii.com>

--- services/api/app/controllers/arvados/v1/container_requests_controller.rb
+++ services/api/app/controllers/arvados/v1/container_requests_controller.rb
@@ -57,6 +57,12 @@ class Arvados::V1::ContainerRequestsController < ApplicationController
       # cascading changes to other rows/tables, the only lock will be
       # the single row lock on SQL UPDATE.
       super
+    elsif @object.container.state.in?([Container::Complete, Container::Cancelled])
+      # If the assigned container is already finalized,
+      # Container#update_priority will skip the cascading priority
+      # update, so there's no need to preemptively acquire row locks
+      # here.
+      super
     else
       # Get locks ahead of time to avoid deadlock in cascading priority
       # update
