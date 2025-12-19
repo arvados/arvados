2a4cbdbaacbf243fc60d8676c082fff7c4850428
Zoë Ma <zoe.ma@curii.com>
2025-12-19 12:26:20 -0500

    23330: Nested (sub-)parsers for arv CLI resource commands and methods.
    Following https://dev.arvados.org/issues/23330#note-5, the CLI parser
    for API resource subcommands now uses nested sub-subcommands for the
    methods associated with each resource.
    
    Arvados-DCO-1.1-Signed-off-by: Zoë Ma <zoe.ma@curii.com>

--- sdk/python/arvados/commands/arvcli.py
+++ sdk/python/arvados/commands/arvcli.py
@@ -17,14 +17,13 @@ The `ArvCLIArgumentParser` class, specializing the standard Python
 
 
 import sys
-import os.path
 import argparse
 import functools
 import arvados
 
 
 def singularize_resource(plural: str) -> str:
-    """Return the singular form of a resource term."""
+    """Returns the singular form of a resource term in the original plural."""
     match plural:
         case "vocabularies":
             return "vocabulary"
@@ -34,16 +33,30 @@ def singularize_resource(plural: str) -> str:
             return plural.removesuffix("s")
 
 
-def parameter_key_to_argument_name(parameter_key):
-    """Convert a parameter key in the discover doc in the form of "foo_bar"
-    into the form suitable for use as a CLI argument ("--foo-bar").
+def parameter_key_to_argument_name(parameter_key: str) -> str:
+    """Convert a parameter key in the discovery document to CLI parameter form.
+
+    Arguments:
+        * `parameter_key`: Parameter key in the form as they appear in the
+          discovery document, typically like `foo_bar`.
+
+    Return value:
+        * Parameter in the conventional CLI form, for example, `--foo-bar`.
     """
     return "--" + parameter_key.replace("_", "-")
 
 
 def parameter_schema_to_argument(parameter_schema):
-    """Convert a parameter's schema as specified in the discover doc into a
-    dictionary suitable for passing as the kwargs to add_argument().
+    """Convert a parameter's schema as specified in the discover document into
+    kwargs to add_argument().
+
+    Arguments:
+        * `parameter_schema`: dict containing the schema that defines the
+          parameter's type and behavior, from the discovery document.
+
+    Return value:
+        * A dict that contains the keyword arguments ready to be passed to
+          `argparser.ArgumentParser.add_argument()`
     """
     parameter_kwargs = {"required": parameter_schema.get("required")}
     parameter_kwargs["help"] = parameter_schema.get("description")
@@ -61,6 +74,9 @@ def parameter_schema_to_argument(parameter_schema):
             # Using the 'action="store_true" (or "store_false")' mechanism
             # results in flag-like action rather than an option that takes a
             # true or false value.
+            # NOTE: currently there are only boolean parameters with
+            # {"default": "false"}; the best way to generate command-line UI
+            # for generic case is yet to be determined.
             if "default" in parameter_schema:
                 match parameter_schema["default"]:
                     case "true":
@@ -82,31 +98,10 @@ def parameter_schema_to_argument(parameter_schema):
     return parameter_kwargs
 
 
-def make_method_parameters_parser(subcommand, method, method_schema):
-    """Create a plain `argparse.ArgumentParser` instance that implements the
-    options (i.e. command-line flags in the form of "--foo-bar") for a
-    particular resoure- (i.e. subcommand-)method combo, based on the
-    method's definition dict object `method_schema` passed as input.
-    """
-    prog=" ".join([os.path.basename(sys.argv[0]), subcommand, method])
-    parameters_parser = argparse.ArgumentParser(
-        prog=prog,
-        description=method_schema.get("description")
-    )
-    for parameter_key, parameter_schema in method_schema.get(
-        "parameters", {}
-    ).items():
-        parameters_parser.add_argument(
-            parameter_key_to_argument_name(parameter_key),
-            **parameter_schema_to_argument(parameter_schema)
-        )
-    return parameters_parser
-
-
 class ArvCLIArgumentParser(argparse.ArgumentParser):
     """Argument parser for `arv` commands.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, resource_dictionary, **kwargs):
         super().__init__(description="Arvados command line client", **kwargs)
         # Common flags to the main command.
         self.add_argument("-n", "--dry-run", action="store_true",
@@ -150,10 +145,17 @@ class ArvCLIArgumentParser(argparse.ArgumentParser):
 
         self.subparsers = subparsers
 
+        self.add_resource_subcommands(resource_dictionary)
+
     def add_resource_subcommands(self, resource_dictionary):
-        """Add resource subcommands based on the resource dictionary object
-        passed as argument. The resource dictionary can be obtained as the
-        `_resourceDesc["resources"]` attribute of an Arvados API client object.
+        """Add resources as subcommands, their associated methods as
+        sub-subcommands, and the parameters associated with each method.
+
+        Arguments:
+            * `resource dictionary`: Dict containing the resources defined in
+            the discovery document; can be obtained as the
+            `_resourceDesc["resources"]` attribute of an Arvados API client
+            object.
         """
         for resource, resource_schema in resource_dictionary.items():
             subcommand = singularize_resource(resource)
@@ -164,55 +166,39 @@ class ArvCLIArgumentParser(argparse.ArgumentParser):
             )
             methods_dict = resource_schema.get("methods")
             if methods_dict:
-                resource_subparser.add_argument(
-                    "method",
-                    choices=list(methods_dict.keys())
+                # Create a collection of "sub-subparsers" under the resource
+                # subparser for the methods.
+                method_subparsers = resource_subparser.add_subparsers(
+                    title="Methods",
+                    dest="method",
+                    parser_class=argparse.ArgumentParser,
+                    help="Methods for subcommand {}".format(subcommand),
+                    required=True
                 )
-            resource_subparser.set_defaults(resource_schema=resource_schema)
-
-
-# NOTE: The two-step parsing emulates the current behavior of the pass-through
-# commands, where the detailed parsing of CLI parameters happen in their own
-# implementation modules. But, should the functionality of generating a new
-# plain parser for parameters, inside the following function, be folded into
-# the class `ArvCLIArgumentParser` as a method?
-def resource_subcommand_handler_stub(arguments, parsed_options):
-    """Stub for handling a resource subcommand.
-
-    For a command-line invocation in the following manner,
-
-    `arvcli.py [--global-options ...] resource method --parameter=value [...]`
-
-    (e.g. arvcli.py user get --uuid=...),
-
-    an argument parser is constructed for the parameters (i.e. those flags
-    `--parameter=value`).
-
-    - `arguments`: list of unparsed command-line options that come after the
-      resource subcommand and the method positional argument.
-
-    - `parsed_options`: an `argparse.Namespace` instance containing the
-      argument parsing result for the global options, the resource
-      (subcommand), and the "method" positional argument.
-    """
-    method = parsed_options.method
-    method_schema = parsed_options.resource_schema["methods"][method]
-    parser = make_method_parameters_parser(
-        parsed_options.subcommand,
-        method,
-        method_schema
-    )
-    args = parser.parse_args(arguments)
-    return 0
+                for method, method_schema in methods_dict.items():
+                    # Add each specific method as a (sub-)subparser with its
+                    # associated parameters.
+                    # FIXME: the value of the "description" member doesn't get
+                    # displayed when help is requested from the cmdline, e.g.
+                    # "arvcli.py user list -h"
+                    method_parser = method_subparsers.add_parser(
+                        method,
+                        help=method_schema.get("description")
+                    )
+                    for parameter_key, parameter_schema in method_schema.get(
+                        "parameters", {}
+                    ).items():
+                        method_parser.add_argument(
+                            parameter_key_to_argument_name(parameter_key),
+                            **parameter_schema_to_argument(parameter_schema)
+                        )
 
 
 def dispatch(arguments=None):
-    cmd_parser = ArvCLIArgumentParser()
     api_client = arvados.api("v1")
-    cmd_parser.add_resource_subcommands(api_client._resourceDesc["resources"])
+    cmd_parser = ArvCLIArgumentParser(api_client._resourceDesc["resources"])
     args, remaining_args = cmd_parser.parse_known_args(arguments)
 
-    needs_global_args = False
     match args.subcommand:
         case "keep":
             match args.method:
@@ -229,18 +215,14 @@ def dispatch(arguments=None):
         case "copy":
             from arvados.commands.arv_copy import main
         case _:
-            main = resource_subcommand_handler_stub
-            needs_global_args = True
-    # NOTE: The idiosyncratic call signatures may be unified if we alter the
-    # main() functions to the pass-through commands to resemble
-    #     def main(arguments, ..., **kwargs):
-    # so that the additional 'parsed_options=...' parameter is absorbed into
-    # the kwargs, and may be even further utilized in the main function body
-    # there (currently it's not).
-    if needs_global_args:
-        status = main(remaining_args, parsed_options=args)
-    else:
-        status = main(remaining_args)
+            # FIXME
+            print("Called API resource {!r}, method {!r}".format(
+                args.subcommand, args.method
+            ))
+            for k, v in vars(args).items():
+                print("{!r}={!r}".format(k, v))
+            sys.exit(0)
+    status = main(remaining_args)
     sys.exit(status)
 
 
