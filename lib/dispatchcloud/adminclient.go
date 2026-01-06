c7456fd0f8252bc8ae965a567f1528de568a35b3
Tom Clegg <tom@curii.com>
2026-01-06 18:16:49 -0500

    20471: Add instance kill/drain/hold/run subcommands.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@curii.com>

--- lib/dispatchcloud/adminclient.go
+++ lib/dispatchcloud/adminclient.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 
 	"git.arvados.org/arvados.git/lib/cmd"
 	"git.arvados.org/arvados.git/lib/config"
@@ -18,7 +19,11 @@ import (
 )
 
 var InstanceCommand = cmd.Multi(map[string]cmd.Handler{
-	"list": instanceList{},
+	"list":  instanceList{},
+	"kill":  instanceAction{action: "kill", reason: true},
+	"drain": instanceAction{action: "drain"},
+	"hold":  instanceAction{action: "hold"},
+	"run":   instanceAction{action: "run"},
 })
 
 type instanceList struct{}
@@ -78,6 +83,75 @@ func (instanceList) RunCommand(prog string, args []string, stdin io.Reader, stdo
 			}
 			fmt.Fprintf(stdout, "%s\t%s\t%s\t%s\t%s\t%s\t%f\t%s\n", inst.Instance, inst.Address, inst.WorkerState, inst.IdleBehavior, inst.ArvadosInstanceType, inst.ProviderInstanceType, inst.Price, inst.LastContainerUUID)
 		}
+	}
+	return 0
+}
+
+type instanceAction struct {
+	action string
+	reason bool // accept "reason" flag
+}
+
+func (ia instanceAction) RunCommand(prog string, args []string, stdin io.Reader, stdout, stderr io.Writer) int {
+	logger := ctxlog.New(stderr, "text", "info")
+	loader := config.NewLoader(stdin, logger)
+	loader.SkipLegacy = true
+	flags := flag.NewFlagSet(prog, flag.ContinueOnError)
+	loader.SetupFlags(flags)
+	reason := new(string)
+	if ia.reason {
+		reason = flags.String("reason", "", "reason to write in dispatch system logs")
+	}
+	if ok, code := cmd.ParseFlags(flags, prog, args, "instance-id [...]", stderr); !ok {
+		return code
+	}
+	if len(flags.Args()) == 0 {
+		fmt.Fprintln(stderr, "usage error: no instance IDs provided")
+		return 2
+	}
+	todo := map[string]bool{}
+	for _, id := range flags.Args() {
+		todo[id] = true
+	}
+	cfg, err := loader.Load()
+	if err != nil {
+		return 1
+	}
+	cluster, err := cfg.GetCluster("")
+	if err != nil {
+		return 1
+	}
+	client := http.DefaultClient
+	if len(cluster.Services.DispatchCloud.InternalURLs) == 0 {
+		fmt.Fprintf(stderr, "no Services.DispatchCloud.InternalURLs configured")
+		return 1
+	}
+	for u := range cluster.Services.DispatchCloud.InternalURLs {
+		u.Path = "/arvados/v1/dispatch/instances/" + ia.action
+		for id := range todo {
+			u.RawQuery = url.Values{
+				"instance_id": []string{id},
+				"reason":      []string{*reason},
+			}.Encode()
+			req, err := http.NewRequest(http.MethodPost, u.String(), nil)
+			if err != nil {
+				fmt.Fprintf(stderr, "%s: error setting up API request: %s\n", id, err)
+				continue
+			}
+			req.Header.Set("Authorization", "Bearer "+cluster.ManagementToken)
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Fprintf(stderr, "%s: error doing API request: %s\n", id, err)
+				continue
+			}
+			fmt.Fprintf(stderr, "%s: %s (%s)\n", id, resp.Status, u.Host)
+			if resp.StatusCode == http.StatusOK {
+				delete(todo, id)
+			}
+		}
+	}
+	if len(todo) > 0 {
+		return 1
 	}
 	return 0
 }
