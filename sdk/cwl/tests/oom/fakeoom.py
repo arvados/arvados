49cc1f5b5982ddfb97b5565892fa7817f762b0ce
Brett Smith <brett.smith@curii.com>
2026-06-02 11:37:13 -0400

    405: Add debug log to fakeoom.py
    Arvados-DCO-1.1-Signed-off-by: Brett Smith <brett.smith@curii.com>

--- sdk/cwl/tests/oom/fakeoom.py
+++ sdk/cwl/tests/oom/fakeoom.py
@@ -3,8 +3,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import argparse
+import sys
 
 import arvados
+
+MB = 1_000_000
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument(
@@ -20,13 +23,15 @@ argparser.add_argument(
 )
 args = argparser.parse_args()
 
-arv = arvados.api()
+arv = arvados.api('v1')
 ctr = arv.containers().current().execute()
-
-if ctr['runtime_constraints']['ram'] >= (args.fail_under * 1_000_000):
-    exit()
+ctr_ram = ctr['runtime_constraints']['ram']
 try:
-    exit_status = int(args.fail_with)
+    exit_status = 0 if ctr_ram >= args.fail_under * MB else int(args.fail_with)
 except ValueError:
     exit_status = args.fail_with
+print("container {}: have {:.01f}MB RAM, want {:.01f}MB: exiting {!r}".format(
+    ctr['uuid'], ctr_ram / MB, args.fail_under, exit_status,
+), file=sys.stderr)
+
 exit(exit_status)
