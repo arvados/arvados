701c449b96419bcb9cbd9c066d66bd3d64089df8
Zoë Ma <zoe.ma@curii.com>
2026-08-31 11:13:39 +0800

    503: Add tests for users and projects.
    Arvados-DCO-1.1-Signed-off-by: Zoë Ma <zoe.ma@curii.com>

--- services/workbench2/cypress/e2e/api-details-dialog.cy.js
+++ services/workbench2/cypress/e2e/api-details-dialog.cy.js
@@ -2,7 +2,28 @@
 //
 // SPDX-License-Identifier: AGPL-3.0
 
-describe("'API Details' dialog tests", () => {
+function doTestWithResource(token, resourceType, testResource) {
+    cy.doToolbarAction("API Details");
+
+    // 'API Response' is default activated tab.
+    cy.get(".MuiDialog-container .MuiTabs-root .Mui-selected")
+        .should("contain", "API RESPONSE");
+
+    // Obtain API record of resource.
+    cy.getResource(token, resourceType, testResource.uuid)
+        .then((resource) => {
+            // In the content area, the <pre> element contains a valid JSON
+            // document that is a subset of the full API record.
+            cy.get(".MuiDialogContent-root pre")
+                .invoke("text")
+                .then(JSON.parse)
+                .should((displayedData) => {
+                    expect(resource).to.deep.include(displayedData);
+                });
+        });
+}
+
+describe("'API Details' dialog showing valid 'API Response'", () => {
     let adminUser;
 
     before(function () {
@@ -17,7 +38,7 @@ describe("'API Details' dialog tests", () => {
             });
     });
 
-    it("shows the expected 'API Response' for a collection", () => {
+    it("works for Collection", () => {
         cy.createCollection(adminUser.token, {
             name: `Test collection ${Math.floor(Math.random() * 999999)}`,
             owner_uuid: adminUser.user.uuid,
@@ -29,30 +50,31 @@ describe("'API Details' dialog tests", () => {
                     ham: null
                 }
             }
-        })
-            .then((testCollection) => {
-                cy.loginAs(adminUser);
-                cy.goToPath(`/collections/${testCollection.uuid}`);
+        }).then((testCollection) => {
+            cy.loginAs(adminUser);
+            cy.goToPath(`/collections/${testCollection.uuid}`);
 
-                cy.doToolbarAction("API Details");
+            doTestWithResource(adminUser.token, "collections", testCollection);
+        });
+    });
 
-                // 'API Response' is default activated tab.
-                cy.get(".MuiDialog-container .MuiTabs-root .Mui-selected").should("contain", "API RESPONSE");
+    it("works for User", () => {
+        cy.loginAs(adminUser);
+        // Go to home project.
+        cy.goToPath(`/projects/${adminUser.user.uuid}`);
 
-                // Obtain API record of collection.
-                cy.getCollection(adminUser.token, testCollection.uuid)
-                    .then((fullCollection) => {
-                        // In the content area, the <pre> element contains a
-                        // valid JSON document that is a subset of the API
-                        // record.
-                        cy.get(".MuiDialogContent-root pre")
-                            .invoke("text")
-                            .then(JSON.parse)
-                            .should((displayedData) => {
-                                expect(fullCollection)
-                                    .to.deep.include(displayedData);
-                            });
-                    });
-            });
+        doTestWithResource(adminUser.token, "users", adminUser.user);
+    });
+
+    it("works for Project", () => {
+        cy.createProject({
+            owningUser: adminUser,
+            projectName: "api-details-test"
+        }).then((testProject) => {
+            cy.loginAs(adminUser);
+            cy.goToPath(`/projects/${testProject.uuid}`);
+
+            doTestWithResource(adminUser.token, "groups", testProject);
+        });
     });
 });
