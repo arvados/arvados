3f373c7edc3dafd0d61c47ab9c61980eddab9b46
Zoë Ma <zoe.ma@curii.com>
2026-09-08 22:53:37 +0800

    526: Update webpack-dev-server and config files for WB.
    * Update to `webpack-dev-server@^5.0.0` in the lock file.
    * Fix Webpack Dev Server config for https server.
    * Update `start.js` script to use `devServer.stop()`.
    * Move `webpack-dev-server` from dependencies to devDependencies.
    
    Arvados-DCO-1.1-Signed-off-by: Zoë Ma <zoe.ma@curii.com>

--- services/workbench2/config/webpackDevServer.config.js
+++ services/workbench2/config/webpackDevServer.config.js
@@ -95,7 +95,7 @@ module.exports = function (proxy, allowedHost) {
       publicPath: paths.publicUrlOrPath.slice(0, -1),
     },
 
-    https: getHttpsConfig(),
+    server: getHttpsConfig(),
     host,
     historyApiFallback: {
       // Paths with dots should still use the history fallback.
@@ -105,27 +105,41 @@ module.exports = function (proxy, allowedHost) {
     },
     // `proxy` is run between `before` and `after` `webpack-dev-server` hooks
     proxy,
-    onBeforeSetupMiddleware(devServer) {
+    setupMiddlewares: (middlewares, devServer) => {
+      if (!devServer) {
+        throw new Error('webpack-dev-server is not defined');
+      }
+
       // Keep `evalSourceMapMiddleware`
       // middlewares before `redirectServedPath` otherwise will not have any effect
       // This lets us fetch source contents from webpack for the error overlay
-      devServer.app.use(evalSourceMapMiddleware(devServer));
+      middlewares.unshift({
+        name: 'eval-source-map',
+        middleware: evalSourceMapMiddleware(devServer),
+      });
 
       if (fs.existsSync(paths.proxySetup)) {
         // This registers user provided middleware for proxy reasons
         require(paths.proxySetup)(devServer.app);
       }
-    },
-    onAfterSetupMiddleware(devServer) {
+
       // Redirect to `PUBLIC_URL` or `homepage` from `package.json` if url not match
-      devServer.app.use(redirectServedPath(paths.publicUrlOrPath));
+      middlewares.push({
+        name: 'redirect-served-path',
+        middleware: redirectServedPath(paths.publicUrlOrPath),
+      });
 
       // This service worker file is effectively a 'no-op' that will reset any
       // previous service worker registered for the same host:port combination.
       // We do this in development to avoid hitting the production cache if
       // it used the same host and port.
       // https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
-      devServer.app.use(noopServiceWorkerMiddleware(paths.publicUrlOrPath));
+      middlewares.push({
+        name: 'noop-service-worker',
+        middleware: noopServiceWorkerMiddleware(paths.publicUrlOrPath),
+      });
+
+      return middlewares;
     },
   };
 };
